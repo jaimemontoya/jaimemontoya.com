8c4398b622af019e44413b33df876db34d6ace24
Jaime Montoya <webmaster@jaimemontoya.com>
2021-10-25 10:16:40 -0600

    Provide keyword metatags.

--- page.php
+++ page.php
@@ -4,5 +4,7 @@ class Page
   // class Page's attributes
   public $content;
   public $title = "Jaime Montoya";
+  public $keywords = "Jaime Montoya's website featuring books I have read, courses I have taken,
+                     my self-discipline agenda, r&eacute;&eacute; and achievements.";
 }
 ?>
