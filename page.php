b49c5f9796f099dc08cdcd9e3463080823c8f69e
Jaime Montoya <webmaster@jaimemontoya.com>
2021-10-25 10:21:28 -0600

    Use an array and store both the text for the button and the URL it should point to.

--- page.php
+++ page.php
@@ -6,5 +6,10 @@ class Page
   public $title = "Jaime Montoya";
   public $keywords = "Jaime Montoya's website featuring books I have read, courses I have taken,
                      my self-discipline agenda, r&eacute;&eacute; and achievements.";
+  public $buttons = array("Home"   => "home.php",
+                        "Self-discipline"  => "self-discipline.php",
+                        "R&eacute;sum&eacute;" => "resume.php",
+                        "Achievements" => "achievements.php"
+                    );
 }
 ?>
