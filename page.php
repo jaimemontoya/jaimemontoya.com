82c56e447ccbdee35afc9b4c0bdad0b84747967c
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 07:46:35 -0600

    Have elements of array on a single line.

--- page.php
+++ page.php
@@ -5,11 +5,7 @@ class Page
   public $content;
   public $title = "Jaime Montoya";
   public $keywords = "Jaime Montoya's website featuring books I have read, courses I have taken, my self-discipline agenda, r&eacute;&eacute; and achievements.";
-  public $buttons = array("Home"   => "home.php",
-                        "Self-discipline"  => "self-discipline.php",
-                        "R&eacute;sum&eacute;" => "resume.php",
-                        "Achievements" => "achievements.php"
-                    );
+  public $buttons = array("Home" => "home.php", "Self-discipline" => "self-discipline.php", "R&eacute;sum&eacute;" => "resume.php", "Achievements" => "achievements.php");
 					
   // class Page's operations
   public function __set($name, $value)
