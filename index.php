a964e3ea42e5a91fbb5d625cb4a1870998891177
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 13:52:30 -0600

    Add book to content in home page.

--- index.php
+++ index.php
@@ -3,7 +3,17 @@
   $index = new Page();
   $index->content ="\t\t<!-- page content -->\n\t\t
   <div>
-    Jaime Montoya
+    <a href=\"scala-programming-projects/\">
+	  <div style=\"background-image: url(&quot;scala-programming-projects/img/scala-programming-projects-thumb.jpg&quot;); background-position: left top;\"></div>
+    </a>
+    <div>
+	  <a href=\"scala-programming-projects/\">
+		<h5>Scala Programming Projects</h5>
+	  </a>
+	  <a href=\"scala-programming-projects/\">
+		<p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
+	  </a>
+    </div>
   </div>
   \n";
   $index->Display();
