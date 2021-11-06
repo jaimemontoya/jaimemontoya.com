d955753801fbf86319e921dc341b192bb1f7aadf
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 11:58:05 -0600

    Improve indentation.

--- page.php
+++ page.php
@@ -57,7 +57,7 @@ class Page
   }
   public function DisplayMenu($buttons)
   {
-    echo "\t\t\t<!-- menu --><nav><ul>";
+    echo "\t\t\t<!-- menu -->\n\t\t\t<nav>\n<ul>";
     while (list($name, $url) = each($buttons)) {
       $this->DisplayButton($name, $url, 
                !$this->IsURLCurrentPage($url));
