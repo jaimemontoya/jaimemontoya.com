79fd16c6191dcf6f2ddcf3a73401cbe9fa765410
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 07:54:39 -0600

    Remove empty spaces.

--- page.php
+++ page.php
@@ -6,13 +6,11 @@ class Page
   public $title = "Jaime Montoya";
   public $keywords = "Jaime Montoya's website featuring books I have read, courses I have taken, my self-discipline agenda, r&eacute;&eacute; and achievements.";
   public $buttons = array("Home" => "home.php", "Self-discipline" => "self-discipline.php", "R&eacute;sum&eacute;" => "resume.php", "Achievements" => "achievements.php");
-					
   // class Page's operations
   public function __set($name, $value)
   {
     $this->$name = $value;
-  }
-  
+  }  
   public function Display()
   {
     echo "<html lang=\"en-CA\">\n\t<head>\n";
@@ -23,17 +21,14 @@ class Page
     echo $this->content;
     echo "\t</body>\n</html>\n";
   }
-  
   public function DisplayTitle()
   {
     echo "\t\t<title>".$this->title."</title>\n";
   }
-  
   public function DisplayMetaKeywords()
   {
     echo "\t\t<meta name='keywords' content='".$this->keywords."' />\n";
   }
-  
   public function DisplayHeader()
   {
     echo "\t\t<!-- page header -->\n\t\t<header>\n\t\t\t<h1>Jaime Montoya</h1>\n\t\t</header>\n";
