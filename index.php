f679ff46c5fce3436239d419e4db4e090b02d68e
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 17:42:11 -0600

    Add book success-habits-dummies-zeller.

--- index.php
+++ index.php
@@ -19,15 +19,15 @@
         </div>
 		<div class=\"container\">
             <div class=\"book\">
-                <a href=\"scala-programming-projects/\">
-	                <div class=\"book-cover\" id=\"scala-programming-projects-img\"></div>
+                <a href=\"success-habits-dummies-zeller/\">
+	                <div class=\"book-cover\" id=\"success-habits-dummies-zeller-img\"></div>
                 </a>
                 <div class=\"book-title-authors-read-by\">
-	                <a href=\"scala-programming-projects/\">
-		                <span>Scala Programming Projects</span>
+	                <a href=\"success-habits-dummies-zeller/\">
+		                <span>Success Habits For Dummies</span>
 	                </a>
-	                <a href=\"scala-programming-projects/\">
-		                <p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
+	                <a href=\"success-habits-dummies-zeller/\">
+		                <p>By Dirk Zeller. Read by Jaime Montoya from 3:20 PM July 14, 2021 GMT to [in progress...]</p>
 	                </a>
                 </div>
             </div>
