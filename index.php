6448f9b49e723866e128d99c3c1e3e8cae2fe32f
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 16:52:43 -0600

    Fix indentation.

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
   $index = new Page();
   $index->content ="\t\t<!-- page content -->
         <div class=\"container\">
-          <div class=\"book\">
-            <a href=\"scala-programming-projects/\">
-	          <div class=\"book-cover\" id=\"scala-programming-projects-img\"></div>
-            </a>
-            <div class=\"book-title-authors-read-by\">
-	          <a href=\"scala-programming-projects/\">
-		        <span>Scala Programming Projects</span>
-	          </a>
-	          <a href=\"scala-programming-projects/\">
-		        <p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
-	          </a>
+            <div class=\"book\">
+              <a href=\"scala-programming-projects/\">
+	            <div class=\"book-cover\" id=\"scala-programming-projects-img\"></div>
+              </a>
+              <div class=\"book-title-authors-read-by\">
+	            <a href=\"scala-programming-projects/\">
+		          <span>Scala Programming Projects</span>
+	            </a>
+	            <a href=\"scala-programming-projects/\">
+		          <p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
+	            </a>
+              </div>
             </div>
-          </div>
         </div>\n";
   $index->Display();
 ?>
