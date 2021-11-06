67349b3d30d557e75a5f345ec0cd61065e038bf7
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 06:59:23 -0600

    Add Home Page.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+  require("page.php");
+
+  $index = new Page();
+
+  $index->content ="<!-- page content -->
+                       <p>Jaime Montoya</p>
+                       ";
+  $index->Display();
+?>
+
