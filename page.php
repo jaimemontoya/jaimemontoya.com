09049ca889752d36885b5c2d51855529fd6a8c15
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 07:23:30 -0600

    Have keywords in a single line.

--- page.php
+++ page.php
@@ -4,8 +4,7 @@ class Page
   // class Page's attributes
   public $content;
   public $title = "Jaime Montoya";
-  public $keywords = "Jaime Montoya's website featuring books I have read, courses I have taken,
-                     my self-discipline agenda, r&eacute;&eacute; and achievements.";
+  public $keywords = "Jaime Montoya's website featuring books I have read, courses I have taken, my self-discipline agenda, r&eacute;&eacute; and achievements.";
   public $buttons = array("Home"   => "home.php",
                         "Self-discipline"  => "self-discipline.php",
                         "R&eacute;sum&eacute;" => "resume.php",
