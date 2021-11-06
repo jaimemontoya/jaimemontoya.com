706db41b0fddbd48cb689914dd207fa8c75e8751
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 09:04:34 -0600

    Add div for logo.

--- page.php
+++ page.php
@@ -43,7 +43,7 @@ class Page
   }
   public function DisplayHeader()
   {
-    echo "\t\t<!-- page header -->\n\t\t<header>\n\t\t\t<div class=\"container\">\n\t\t\t\t<a href=\"/\" title=\"Jaime Montoya\">Jaime Montoya</a>\n\t\t\t</div>\n\t\t</header>\n";
+    echo "\t\t<!-- page header -->\n\t\t<header>\n\t\t\t<div class=\"container\">\n\t\t\t<div class=\"logo\">\n\t\t\t\t\t<a href=\"/\" title=\"Jaime Montoya\">Jaime Montoya</a>\n\t\t\t</div>\t\t\t\t</div>\n\t\t</header>\n";
   }
 }
 ?>
