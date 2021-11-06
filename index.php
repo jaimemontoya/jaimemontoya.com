d468700c59c27c7e4963b42f3c4e164415bb35b3
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 16:27:08 -0600

    Add container <div>.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   require("page.php");
   $index = new Page();
   $index->content ="\t\t<!-- page content -->\n\t\t
+  <div class=\"container\">
   <div class=\"book\">
     <a href=\"scala-programming-projects/\">
 	  <div class=\"book-cover\" id=\"scala-programming-projects-img\"></div>
@@ -14,6 +15,7 @@
 		<p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
 	  </a>
     </div>
+  </div>
   </div>
   \n";
   $index->Display();
