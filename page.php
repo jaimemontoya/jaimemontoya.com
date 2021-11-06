07ff9ff6d84cf7df712817696da7ff984ca1a7d3
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 17:11:52 -0600

    Add language to HTML document.

--- page.php
+++ page.php
@@ -15,7 +15,7 @@ class Page
   }  
   public function Display()
   {
-    echo "<!DOCTYPE html>\n\t<head>\n";
+    echo "<!DOCTYPE html lang='en'>\n\t<head>\n";
     $this -> DisplayTitle();
     $this -> DisplayFavicon();
     $this -> DisplayMetaKeywords();
