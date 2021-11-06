a37e72009bd15b0ea7e13710c872e5a1f3913d94
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 10:13:00 -0600

    Remove content below header.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
   $index = new Page();
 
-  $index->content ="\t\t<!-- page content -->\n\t\t<p>Jaime Montoya</p>\n";
+  $index->content ="\t\t<!-- page content -->\n\t\t<p></p>\n";
   $index->Display();
 ?>
 
