1080d9d73850b735eacb1eb68701aec671d2a588
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 12:52:55 -0600

    Improve indentation.

--- page.php
+++ page.php
@@ -52,8 +52,8 @@ class Page
   public function DisplayHeader()
   {
     echo "\t\t<!-- page header -->\n\t\t<header>\n\t\t\t<div class=\"logo\">\n\t\t\t\t<a href=\"/\" title=\"Jaime Montoya\">Jaime Montoya</a>\n\t\t\t</div>\n";
-	$this -> DisplayMenu($this->buttons);
-	echo "\t\t</header>\n";
+    $this -> DisplayMenu($this->buttons);
+    echo "\t\t</header>\n";
   }
   public function DisplayMenu($buttons)
   {
