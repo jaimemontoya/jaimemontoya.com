4bea34e7d1e1cf2f7d6972e295d6284f9735d6b2
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 16:04:40 -0600

    Use <span> instead of <div> for titles of books in home page.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     </a>
     <div class=\"book-title-authors-read-by\">
 	  <a href=\"scala-programming-projects/\">
-		<div>Scala Programming Projects</div>
+		<span>Scala Programming Projects</span>
 	  </a>
 	  <a href=\"scala-programming-projects/\">
 		<p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
