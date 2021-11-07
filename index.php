ba8c462870a90aff57cf90171d37d45851a7ecd6
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 19:12:12 -0600

    Add book wordpress-5-complete.

--- index.php
+++ index.php
@@ -136,6 +136,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"wordpress-5-complete/\">
+	                <div class=\"book-cover\" id=\"wordpress-5-complete-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"wordpress-5-complete/\">
+		                <span>WordPress 5 Complete: Build beautiful and feature-rich websites from scratch, 7th Edition</span>
+	                </a>
+	                <a href=\"wordpress-5-complete/\">
+		                <p>By Karol Krol. Read by Jaime Montoya from 1:27 AM Apr 1, 2019 to 00:58 AM Sep 10, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
