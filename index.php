4eaf8c89704c27dd3afcd983a5c182f7f4a0b6d7
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 18:57:26 -0600

    Add book outliers-malcolm-gladwell.

--- index.php
+++ index.php
@@ -91,6 +91,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"outliers-malcolm-gladwell/\">
+	                <div class=\"book-cover\" id=\"outliers-malcolm-gladwell-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"outliers-malcolm-gladwell/\">
+		                <span>Outliers: The Story of Success</span>
+	                </a>
+	                <a href=\"outliers-malcolm-gladwell/\">
+		                <p>By Malcolm Gladwell. Read by Jaime Montoya from 6:40 PM Feb 23, 2021 to 5:58 AM Mar 31, 2021 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
