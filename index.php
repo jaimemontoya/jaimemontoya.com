6e0bab9941166e7fe659a0d8be3c2fc363d877b5
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 22:09:00 -0600

    Revert previous commit.

--- index.php
+++ index.php
@@ -16,6 +16,351 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"success-habits-dummies-zeller/\">
+	                <div class=\"book-cover\" id=\"success-habits-dummies-zeller-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"success-habits-dummies-zeller/\">
+		                <span>Success Habits For Dummies</span>
+	                </a>
+	                <a href=\"success-habits-dummies-zeller/\">
+		                <p>By Dirk Zeller. Read by Jaime Montoya from 3:20 PM July 14, 2021 GMT to [in progress...]</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"lightbend-scala-language-professional/\">
+	                <div class=\"book-cover\" id=\"lightbend-scala-language-professional-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"lightbend-scala-language-professional/\">
+		                <span>Lightbend Scala Language - Professional</span>
+	                </a>
+	                <a href=\"lightbend-scala-language-professional/\">
+		                <p>By Lightbend, Course Staff Petro Verkhogliad. Completed by Jaime Montoya from 0:00 AM Jun 12, 2021 GMT to [in progress...]</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"10-000-hours-become-practice/\">
+	                <div class=\"book-cover\" id=\"book-10-000-hours-become-practice-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"10-000-hours-become-practice/\">
+		                <span>10,000 Hours: You Become What You Practice</span>
+	                </a>
+	                <a href=\"10-000-hours-become-practice/\">
+		                <p>By Phyllis Lane. Read by Jaime Montoya from 2:38 PM July 4, 2021 to 6:19 AM Jul 14, 2021 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"pragmatic-scala/\">
+	                <div class=\"book-cover\" id=\"pragmatic-scala-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"pragmatic-scala/\">
+		                <span>Pragmatic Scala: Create Expressive, Concise, and Scalable Applications. 1st. Edition.</span>
+	                </a>
+	                <a href=\"pragmatic-scala/\">
+		                <p>By Venkat Subramaniam. Read by Jaime Montoya from 2:33 AM May 31, 2021 to 5:48 AM Jul 6, 2021 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"scala-for-the-impatient-second-edition/\">
+	                <div class=\"book-cover\" id=\"scala-for-the-impatient-second-edition-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"scala-for-the-impatient-second-edition/\">
+		                <span>Scala for the Impatient Second Edition</span>
+	                </a>
+	                <a href=\"scala-for-the-impatient-second-edition/\">
+		                <p>By Cay Horstmann. Read by Jaime Montoya from 1:20 AM Apr 2, 2021 to 2:26 AM May 31, 2021 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"outliers-malcolm-gladwell/\">
+	                <div class=\"book-cover\" id=\"outliers-malcolm-gladwell-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"outliers-malcolm-gladwell/\">
+		                <span>Outliers: The Story of Success</span>
+	                </a>
+	                <a href=\"outliers-malcolm-gladwell/\">
+		                <p>By Malcolm Gladwell. Read by Jaime Montoya from 6:40 PM Feb 23, 2021 to 5:58 AM Mar 31, 2021 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"learning-spark-2nd/\">
+	                <div class=\"book-cover\" id=\"learning-spark-2nd-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"learning-spark-2nd/\">
+		                <span>Learning Spark, 2nd Edition</span>
+	                </a>
+	                <a href=\"learning-spark-2nd/\">
+		                <p>By Jules S. Damji, Brooke Wenig, Tathagata Das, Denny Lee. Read by Jaime Montoya from 2:50 AM Oct 14, 2020 to 5:54 AM Dec 6, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"apache-spark-quick/\">
+	                <div class=\"book-cover\" id=\"apache-spark-quick-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"apache-spark-quick/\">
+		                <span>Apache Spark Quick Start Guide: Quickly learn the art of writing efficient big data applications with Apache Spark</span>
+	                </a>
+	                <a href=\"apache-spark-quick/\">
+		                <p>By Shrey Mehrotra, Akash Grade. Read by Jaime Montoya from 2:21 AM Sep 24, 2020 to 12:31 PM Oct 5, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"wordpress-5-complete/\">
+	                <div class=\"book-cover\" id=\"wordpress-5-complete-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"wordpress-5-complete/\">
+		                <span>WordPress 5 Complete: Build beautiful and feature-rich websites from scratch, 7th Edition</span>
+	                </a>
+	                <a href=\"wordpress-5-complete/\">
+		                <p>By Karol Krol. Read by Jaime Montoya from 1:27 AM Apr 1, 2019 to 00:58 AM Sep 10, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"the-power-of-habit/\">
+	                <div class=\"book-cover\" id=\"the-power-of-habit-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"the-power-of-habit/\">
+		                <span>The Power of Habit: Why We Do What We Do in Life and Business</span>
+	                </a>
+	                <a href=\"the-power-of-habit/\">
+		                <p>By Charles Duhigg. Read by Jaime Montoya from 2:24 PM Apr 15, 2020 to 1:04 AM Jul 19, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"niv-gospel-john-devotional-notes/\">
+	                <div class=\"book-cover\" id=\"niv-gospel-john-devotional-notes-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"niv-gospel-john-devotional-notes/\">
+		                <span>NIV, Gospel of John, eBook: With Devotional Notes</span>
+	                </a>
+	                <a href=\"niv-gospel-john-devotional-notes/\">
+		                <p>By Zondervan. Read by Jaime Montoya from 10:11 PM Jul 12, 2020 to 0:20 PM Jul 13, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"place-participant-guide/\">
+	                <div class=\"book-cover\" id=\"place-participant-guide-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"place-participant-guide/\">
+		                <span>PLACE Participant Guide and Assessment Tool</span>
+	                </a>
+	                <a href=\"place-participant-guide/\">
+		                <p>By Jay McSwain. Read by Jaime Montoya from 1:42 PM Feb 25, 2020 to 7:52 PM Jul 11, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"the-lambs-supper-the-mass-as-heaven-on-earth/\">
+	                <div class=\"book-cover\" id=\"the-lambs-supper-the-mass-as-heaven-on-earth-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"the-lambs-supper-the-mass-as-heaven-on-earth/\">
+		                <span>The Lamb's Supper: The Mass as Heaven on Earth</span>
+	                </a>
+	                <a href=\"the-lambs-supper-the-mass-as-heaven-on-earth/\">
+		                <p>By Scott Hahn. Read by Jaime Montoya from 4:27 AM Jan 3, 2020 to 02:16 AM Apr 15, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"the-logic-of-god/\">
+	                <div class=\"book-cover\" id=\"the-logic-of-god-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"the-logic-of-god/\">
+		                <span>The Logic of God: 52 Christian Essentials for the Heart and Mind</span>
+	                </a>
+	                <a href=\"the-logic-of-god/\">
+		                <p>By Ravi Zacharias. Read by Jaime Montoya from 4:06 AM Jan 6, 2020 to 6:38 AM Apr 12, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"ab-testing-the/\">
+	                <div class=\"book-cover\" id=\"ab-testing-the-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"ab-testing-the/\">
+		                <span>A/B Testing: The Most Powerful Way to Turn Clicks Into Customers</span>
+	                </a>
+	                <a href=\"ab-testing-the/\">
+		                <p>By Dan Siroker and Pete Koomen. Read by Jaime Montoya from 4:37 AM Nov 8, 2019 to 1:02 PM Nov 15, 2019 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"personal-branding-for/\">
+	                <div class=\"book-cover\" id=\"personal-branding-for-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"personal-branding-for/\">
+		                <span>Personal Branding For Dummies, 2nd Edition</span>
+	                </a>
+	                <a href=\"personal-branding-for/\">
+		                <p>By Susan Chritton. Read by Jaime Montoya from 11:57 AM May 12, 2019 to 4:13 AM Aug 12, 2019 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"income-investing-an/\">
+	                <div class=\"book-cover\" id=\"income-investing-an-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"income-investing-an/\">
+		                <span>Income Investing: An Intelligent Approach to Profiting from Bonds, Stocks, and Money Markets</span>
+	                </a>
+	                <a href=\"income-investing-an/\">
+		                <p>By Jason Brady. Read by Jaime Montoya from 6:30 PM Jul 21, 2019 to 7:41 AM Jul 22, 2019 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"wharton-marketing/\">
+	                <div class=\"book-cover\" id=\"wharton-marketing-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"wharton-marketing/\">
+		                <span>Introduction to Marketing</span>
+	                </a>
+	                <a href=\"wharton-marketing/\">
+		                <p>By University of Pennsylvania. Successfully completed by Jaime Montoya from 4:30 PM Apr 22, 2019 to Apr 30, 2019 GMT. Verify at https://coursera.org/verify/UN9PAZ74UDAB.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"scaling-up-companies-rockefeller-habits/\">
+	                <div class=\"book-cover\" id=\"scaling-up-companies-rockefeller-habits-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"scaling-up-companies-rockefeller-habits/\">
+		                <span>Scaling Up: How a Few Companies Make It...and Why the Rest Don't (Mastering the Rockefeller Habits 2.0)</span>
+	                </a>
+	                <a href=\"scaling-up-companies-rockefeller-habits/\">
+		                <p>By Verne Harnish. Read by Jaime Montoya from 5:42 PM Feb 28, 2019 to 02:57 AM Mar 23, 2019 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"google-speaks-secrets/\">
+	                <div class=\"book-cover\" id=\"google-speaks-secrets-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"google-speaks-secrets/\">
+		                <span>Google Speaks: Secrets of the World's Greatest Billionaire Entrepreneurs, Sergey Brin and Larry Page</span>
+	                </a>
+	                <a href=\"google-speaks-secrets/\">
+		                <p>By Janet Lowe. Read by Jaime Montoya from 12:10 PM Sep 19, 2018 to 1:06 AM Sep 25, 2018 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"twitter-101-everything/\">
+	                <div class=\"book-cover\" id=\"twitter-101-everything-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"twitter-101-everything/\">
+		                <span>Twitter 101: Everything You Need to Know about Twitter Marketing</span>
+	                </a>
+	                <a href=\"twitter-101-everything/\">
+		                <p>By Lauren Dugan. Read by Jaime Montoya from 9:14 AM Sep 15, 2018 to 10:24 PM Sep 16, 2018 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"born-to-blog/\">
+	                <div class=\"book-cover\" id=\"born-to-blog-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"born-to-blog/\">
+		                <span>Born to Blog: Building Your Blog for Personal and Business Success One Post at a Time</span>
+	                </a>
+	                <a href=\"born-to-blog/\">
+		                <p>By Stanford Smith, Mark Schaefer. Read by Jaime Montoya from 7:40 AM Sep 14, 2018 to 8:45 AM Sep 15, 2018 GMT.</p>
+	                </a>
+                </div>
+            </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"success-in-programming/\">
+	                <div class=\"book-cover\" id=\"success-in-programming-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"success-in-programming/\">
+		                <span>Success in Programming: How to Gain Recognition, Power, and Influence through Personal Branding</span>
+	                </a>
+	                <a href=\"success-in-programming/\">
+		                <p>By Fr&eacute;d&eacute;ric Harper. Read by Jaime Montoya from 1:59 AM Aug 31, 2018 to 11:52 PM Sep 6, 2018 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
