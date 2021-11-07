4a0e430f87754c8031ca2be55b91cee4841b91e7
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 18:53:42 -0600

    Add book scala-for-the-impatient-second-edition.

--- index.php
+++ index.php
@@ -76,6 +76,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"scala-for-the-impatient-second-edition/\">
+	                <div class=\"book-cover\" id=\"scala-for-the-impatient-second-edition-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"scala-for-the-impatient-second-edition/\">
+		                <span>Scala for the Impatient Second Edition</span>
+	                </a>
+	                <a href=\"scala-for-the-impatient-second-edition/\">
+		                <p>By Cay Horstmann. Read by Jaime Montoya from 1:20 AM Apr 2, 2021 to 2:26 AM May 31, 2021 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
