985f3e4a2cbb2c3d0b025338e2c1abe22dcc8d55
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 20:05:22 -0600

    Add book twitter-101-everything.

--- index.php
+++ index.php
@@ -316,6 +316,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"twitter-101-everything/\">
+	                <div class=\"book-cover\" id=\"twitter-101-everything-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"twitter-101-everything/\">
+		                <span>Twitter 101: Everything You Need to Know about Twitter Marketing</span>
+	                </a>
+	                <a href=\"twitter-101-everything/\">
+		                <p>By Lauren Dugan. Read by Jaime Montoya from 9:14 AM Sep 15, 2018 to 10:24 PM Sep 16, 2018 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
