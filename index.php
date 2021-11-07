fea2135ac6921d67e5360047af6ed45c58f80154
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 19:29:55 -0600

    Add book the-lambs-supper-the-mass-as-heaven-on-earth.

--- index.php
+++ index.php
@@ -196,6 +196,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"the-lambs-supper-the-mass-as-heaven-on-earth/\">
+	                <div class=\"book-cover\" id=\"the-lambs-supper-the-mass-as-heaven-on-earth-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"the-lambs-supper-the-mass-as-heaven-on-earth/\">
+		                <span>The Lamb's Supper: The Mass as Heaven on Earth</span>
+	                </a>
+	                <a href=\"the-lambs-supper-the-mass-as-heaven-on-earth/\">
+		                <p>By Scott Hahn. Read by Jaime Montoya from 4:27 AM Jan 3, 2020 to 02:16 AM Apr 15, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
