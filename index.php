1d8af35799907d0b34dff4a8d6192bd98ab50043
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 19:32:57 -0600

    Add book the-logic-of-god.

--- index.php
+++ index.php
@@ -211,6 +211,20 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		    <div class=\"book\">
+                <a href=\"the-logic-of-god/\">
+	                <div class=\"book-cover\" id=\"the-logic-of-god-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"the-logic-of-god/\">
+		                <span>The Logic of God: 52 Christian Essentials for the Heart and Mind</span>
+	                </a>
+	                <a href=\"the-logic-of-god/\">
+		                <p>By Ravi Zacharias. Read by Jaime Montoya from 4:06 AM Jan 6, 2020 to 6:38 AM Apr 12, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
