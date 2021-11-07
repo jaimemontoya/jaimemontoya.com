5d77973b23355656fdfab60f65289bba2416c8d8
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 19:00:30 -0600

    Add book learning-spark-2nd.

--- index.php
+++ index.php
@@ -106,6 +106,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"learning-spark-2nd/\">
+	                <div class=\"book-cover\" id=\"learning-spark-2nd-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"learning-spark-2nd/\">
+		                <span>Learning Spark, 2nd Edition</span>
+	                </a>
+	                <a href=\"learning-spark-2nd/\">
+		                <p>By Jules S. Damji, Brooke Wenig, Tathagata Das, Denny Lee. Read by Jaime Montoya from 2:50 AM Oct 14, 2020 to 5:54 AM Dec 6, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
