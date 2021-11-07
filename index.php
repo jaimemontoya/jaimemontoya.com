5070e95cfcf4e0432b27daaaf29465bad790b2a7
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 18:16:45 -0600

    Add lightbend-scala-language-professional course to mhome page.

--- index.php
+++ index.php
@@ -31,6 +31,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"lightbend-scala-language-professional/\">
+	                <div class=\"book-cover\" id=\"lightbend-scala-language-professional-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"lightbend-scala-language-professional/\">
+		                <span>Lightbend Scala Language - Professional</span>
+	                </a>
+	                <a href=\"lightbend-scala-language-professional/\">
+		                <p>By Lightbend, Course Staff Petro Verkhogliad. Completed by Jaime Montoya from 0:00 AM Jun 12, 2021 GMT to [in progress...]</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
