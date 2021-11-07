190f7349397c655e80d7d1af8d2ef6172ffd34b1
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 19:05:54 -0600

    Add book apache-spark-quick.

--- index.php
+++ index.php
@@ -121,6 +121,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"apache-spark-quick/\">
+	                <div class=\"book-cover\" id=\"apache-spark-quick-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"apache-spark-quick/\">
+		                <span>Apache Spark Quick Start Guide: Quickly learn the art of writing efficient big data applications with Apache Spark</span>
+	                </a>
+	                <a href=\"apache-spark-quick/\">
+		                <p>By Shrey Mehrotra, Akash Grade. Read by Jaime Montoya from 2:21 AM Sep 24, 2020 to 12:31 PM Oct 5, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
