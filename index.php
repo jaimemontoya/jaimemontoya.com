b0877528e69e0fc581369790a51259c85fe2341c
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 19:24:05 -0600

    Add book place-participant-guide.

--- index.php
+++ index.php
@@ -181,6 +181,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"place-participant-guide/\">
+	                <div class=\"book-cover\" id=\"place-participant-guide-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"place-participant-guide/\">
+		                <span>PLACE Participant Guide and Assessment Tool</span>
+	                </a>
+	                <a href=\"place-participant-guide/\">
+		                <p>By Jay McSwain. Read by Jaime Montoya from 1:42 PM Feb 25, 2020 to 7:52 PM Jul 11, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
