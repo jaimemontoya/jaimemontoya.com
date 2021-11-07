9c7318bc0b8df493314544a43a02eba622d70bb4
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 19:19:58 -0600

    Add book niv-gospel-john-devotional-notes.

--- index.php
+++ index.php
@@ -166,6 +166,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"niv-gospel-john-devotional-notes/\">
+	                <div class=\"book-cover\" id=\"niv-gospel-john-devotional-notes-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"niv-gospel-john-devotional-notes/\">
+		                <span>NIV, Gospel of John, eBook: With Devotional Notes</span>
+	                </a>
+	                <a href=\"niv-gospel-john-devotional-notes/\">
+		                <p>By Zondervan. Read by Jaime Montoya from 10:11 PM Jul 12, 2020 to 0:20 PM Jul 13, 2020 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
