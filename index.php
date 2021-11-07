c0d0dc7ca8ce2e099a9d0d165152acd1022cebc4
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 18:28:11 -0600

    Add book 10-000-hours-become-practice.

--- index.php
+++ index.php
@@ -46,6 +46,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		<div class=\"container\">
+            <div class=\"book\">
+                <a href=\"10-000-hours-become-practice/\">
+	                <div class=\"book-cover\" id=\"10-000-hours-become-practice-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"10-000-hours-become-practice/\">
+		                <span>10,000 Hours: You Become What You Practice</span>
+	                </a>
+	                <a href=\"10-000-hours-become-practice/\">
+		                <p>By Phyllis Lane. Read by Jaime Montoya from 2:38 PM July 4, 2021 to 6:19 AM Jul 14, 2021 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
