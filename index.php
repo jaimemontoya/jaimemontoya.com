066647ad026a0cd2aaa8014c528016915cbd1451
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 19:42:50 -0600

    Add book personal-branding-for.

--- index.php
+++ index.php
@@ -241,6 +241,21 @@
 	                </a>
                 </div>
             </div>
+        </div>
+		div class=\"container\">
+		    <div class=\"book\">
+                <a href=\"personal-branding-for/\">
+	                <div class=\"book-cover\" id=\"personal-branding-for-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"personal-branding-for/\">
+		                <span>Personal Branding For Dummies, 2nd Edition</span>
+	                </a>
+	                <a href=\"personal-branding-for/\">
+		                <p>By Susan Chritton. Read by Jaime Montoya from 11:57 AM May 12, 2019 to 4:13 AM Aug 12, 2019 GMT.</p>
+	                </a>
+                </div>
+            </div>
         </div>\n";
   $index->Display();
 ?>
