a53e674a7ec3dddcbbd38cd26227446ee39f31e7
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 22:48:21 -0600

    Remove unnecessary placeholder text.

--- scala-programming-projects/index.php
+++ scala-programming-projects/index.php
@@ -9,12 +9,7 @@
 	                <div class=\"book-cover\" id=\"scala-programming-projects-img\"></div>
                 </a>
                 <div class=\"book-title-authors-read-by\">
-	                <a href=\"scala-programming-projects/\">
-		                <span>Scala Programming Projects</span>
-	                </a>
-	                <a href=\"scala-programming-projects/\">
-		                <p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
-	                </a>
+	                <p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
                 </div>
             </div>
         </div>\n";
