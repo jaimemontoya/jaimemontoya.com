b1dde2b37757113e6edd79eee7f7866066f15bcf
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-06 22:51:28 -0600

    Use <div> instead of <p>.

--- scala-programming-projects/index.php
+++ scala-programming-projects/index.php
@@ -9,7 +9,8 @@
 	                <div class=\"book-cover\" id=\"scala-programming-projects-img\"></div>
                 </a>
                 <div class=\"book-title-authors-read-by\">
-	                <p>By Mikael Valot, Nicolas Jorand. Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</p>
+	                <div>By Mikael Valot, Nicolas Jorand.</div>
+					<div>Read by Jaime Montoya from 1:23 PM July 6, 2021 to 8:21 AM September 10, 2021 GMT.</div>
                 </div>
             </div>
         </div>\n";
