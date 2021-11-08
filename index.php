c332d759def7421b8ef92d2306ccc3a215d34e65
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-08 08:56:37 -0600

    Add GitHub address where source code is hosted.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
   require("page.php");
   $index = new Page();
+  $index->content .= "https://github.com/jaimemontoya/jaimemontoya.com";
   require("scala-programming-projects/scala-programming-projects-book-info.php");
   require("success-habits-dummies-zeller/success-habits-dummies-zeller-book-info.php");
   require("lightbend-scala-language-professional/lightbend-scala-language-professional-book-info.php");
