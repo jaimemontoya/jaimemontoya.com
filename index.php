ab77e18bdd404a4e80f5ed31960002ca33460d20
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-08 00:05:04 -0600

    Place require() in the right place.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   require("page.php");
   $index = new Page();
   require("scala-programming-projects/scala-programming-projects-book-info.php");
+  require("success-habits-dummies-zeller-book-info.php");
   $index->content .= "
             <div class=\"book\">
                 <a href=\"lightbend-scala-language-professional/\">
--- scala-programming-projects/index.php
+++ scala-programming-projects/index.php
@@ -2,7 +2,6 @@
   require("../page.php");
   $index = new Page();
   require("scala-programming-projects-book-info.php");
-  require("success-habits-dummies-zeller-book-info.php");
   $index->Display();
 ?>
 
