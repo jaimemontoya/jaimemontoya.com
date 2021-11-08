192e08ca876d190741f2e31e36c15ce13a02f9bd
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-07 23:42:10 -0600

    Use placeholder text for testing purposes.

--- scala-programming-projects/index.php
+++ scala-programming-projects/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../page.php");
   $index = new Page();
-  $index->content = require("scala-programming-projects-book-info.php");
+  $index->content = "abc";//require("scala-programming-projects-book-info.php");
   $index->Display();
 ?>
 
