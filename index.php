fff905b9ed86217b4321dd7b5be44078756c1f71
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-07 23:45:46 -0600

    Include require().

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
   require("page.php");
   $index = new Page();
+  require("scala-programming-projects-book-info.php");
   $index->content = "
             <div class=\"book\">
                 <a href=\"scala-programming-projects/\">
