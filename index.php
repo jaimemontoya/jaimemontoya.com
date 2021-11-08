2e96955ebe3ac52f3d2dfa2020020f0e8c9b4007
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-08 00:03:41 -0600

    Add success-habits-dummies-zeller using require().

--- index.php
+++ index.php
@@ -4,19 +4,6 @@
   require("scala-programming-projects/scala-programming-projects-book-info.php");
   $index->content .= "
             <div class=\"book\">
-                <a href=\"success-habits-dummies-zeller/\">
-	                <div class=\"book-cover\" id=\"success-habits-dummies-zeller-img\"></div>
-                </a>
-                <div class=\"book-title-authors-read-by\">
-	                <a href=\"success-habits-dummies-zeller/\">
-		                <span>Success Habits For Dummies</span>
-	                </a>
-	                <a href=\"success-habits-dummies-zeller/\">
-		                <p>By Dirk Zeller. Read by Jaime Montoya from 3:20 PM July 14, 2021 GMT to [in progress...]</p>
-	                </a>
-                </div>
-            </div>
-            <div class=\"book\">
                 <a href=\"lightbend-scala-language-professional/\">
 	                <div class=\"book-cover\" id=\"lightbend-scala-language-professional-img\"></div>
                 </a>
--- scala-programming-projects/index.php
+++ scala-programming-projects/index.php
@@ -2,6 +2,7 @@
   require("../page.php");
   $index = new Page();
   require("scala-programming-projects-book-info.php");
+  require("success-habits-dummies-zeller-book-info.php");
   $index->Display();
 ?>
 
