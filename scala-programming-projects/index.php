48df10d621eb706990077bbd404ec83bb721c3f9
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-08 00:13:19 -0600

    Change page title.

--- scala-programming-projects/index.php
+++ scala-programming-projects/index.php
@@ -1,6 +1,7 @@
 <?php
   require("../page.php");
   $index = new Page();
+  $index->title = "Scala Programming Projects";
   require("scala-programming-projects-book-info.php");
   $index->Display();
 ?>
