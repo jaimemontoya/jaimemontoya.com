79fa61ff741c7848967e1bf87da6221330cd1ac6
Jaime Montoya <webmaster@jaimemontoya.com>
2021-11-08 01:53:25 -0600

    Fix path.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   require("wordpress-5-complete/wordpress-5-complete-book-info.php");
   require("the-power-of-habit/the-power-of-habit-book-info.php");
   require("niv-gospel-john-devotional-notes/niv-gospel-john-devotional-notes-book-info.php");
-  require("place-participant-guide-notes/place-participant-guide-notes-book-info.php");
+  require("place-participant-guide/place-participant-guide-book-info.php");
   /*require("the-lambs-supper-the-mass-as-heaven-on-earth/the-lambs-supper-the-mass-as-heaven-on-earth-book-info.php");
   require("the-logic-of-god/the-logic-of-god-book-info.php");
   require("ab-testing-the/ab-testing-the-book-info.php");
