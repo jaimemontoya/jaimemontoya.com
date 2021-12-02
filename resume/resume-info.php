8e897f66f01f567909964d882805780b8b4928fc
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-02 11:33:13 -0600

    Add double quote.

--- resume/resume-info.php
+++ resume/resume-info.php
@@ -1,5 +1,5 @@
 <?php
   $index->content .= 
            "\t\t\t<iframe src=\"https://docs.google.com/document/d/e/2PACX-1vSTmf-rdjwSpzwvEUZxCLaWXkLmRVUiRzUCIv8IRYCHxaCYlIY9ehBJ3lkcIN0A7IRwjo7lECSeMsBk/pub?embedded=true\" 
-id=\resume\"></iframe>\n";
+id=\"resume\"></iframe>\n";
 ?>
