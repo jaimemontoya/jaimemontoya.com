502071e10b38f810f441b679470d443bad9e4072
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-01 19:56:01 -0600

    Use Resume without accent marks.

--- page.php
+++ page.php
@@ -3,10 +3,10 @@ class Page
 {
   public $content;
   public $title = "Jaime Montoya";
-  public $metaKeywords = "Jaime Montoya's website featuring books I have read, courses I have taken, my self-discipline agenda, r&eacute;&eacute; and achievements.";
+  public $metaKeywords = "Jaime Montoya's website featuring books I have read, courses I have taken, my self-discipline agenda, resume and achievements.";
   public $metaDescription = "Scala programmer specialist in software development building ETL processes for big data applications.";
   public $metaViewport = "width=device-width, initial-scale=1.0";
-  public $buttons = array("Home" => "/", "Self-discipline" => "/self-discipline/", "R&eacute;sum&eacute;" => "/resume/", "Achievements" => "/achievements/");
+  public $buttons = array("Home" => "/", "Self-discipline" => "/self-discipline/", "Resume" => "/resume/", "Achievements" => "/achievements/");
   public function __set($name, $value)
   {
     $this->$name = $value;
