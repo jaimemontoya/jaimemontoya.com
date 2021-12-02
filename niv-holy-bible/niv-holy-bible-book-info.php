8706910dfe1b3cc07fc791746fe021ddb31d021c
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-02 17:39:48 -0600

    Add NIV Holy Bible.

---
+++ niv-holy-bible/niv-holy-bible-book-info.php
@@ -0,0 +1,16 @@
+<?php
+  $index->content .=
+           "\t\t\t<div class=\"book\">
+                <a href=\"/niv-holy-bible/\">
+	                <div class=\"book-cover\" id=\"niv-holy-bible-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"/niv-holy-bible/\">
+		                <span>Scala Programming Projects</span>
+	                </a>
+	                <a href=\"/niv-holy-bible/\">
+		                <p>By Zondervan. Read by Jaime Montoya from 0:00 AM September 30, 2019 to [in progress...]</p>
+	                </a>
+                </div>
+            </div>\n";
+?>
