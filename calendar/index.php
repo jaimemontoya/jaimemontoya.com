730fddb82cc24f71128052ab4dfd68efb7587a28
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-02 11:48:06 -0600

    Fix file name.

--- calendar/index.php
+++ calendar/index.php
@@ -3,6 +3,6 @@
   $index = new Page();
   $index->title = "Jaime Montoya's calendar";
   $index->metaDescription = "My calendar shows how I spend my time. It includes five components for my time: full-time-work, freelancing-and-volunteering-work, studying, reading and exercising.";
-  require("self-discipline-info.php");
+  require("calendar-info.php");
   $index->Display();
 ?>
--- resume/resume-info.php
+++ resume/resume-info.php
@@ -1,5 +1,5 @@
 <?php
   $index->content .= 
            "\t\t\t<iframe src=\"https://docs.google.com/document/d/e/2PACX-1vSTmf-rdjwSpzwvEUZxCLaWXkLmRVUiRzUCIv8IRYCHxaCYlIY9ehBJ3lkcIN0A7IRwjo7lECSeMsBk/pub?embedded=true\" 
-id=\"resume\"></iframe>\n";
+id=\"resume\" width=\"100%\" height=\"600\"></iframe>\n";
 ?>
