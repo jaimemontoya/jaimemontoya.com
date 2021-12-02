95fedcc342830b1c7bf97ea895624f1243d35ee9
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-02 11:26:18 -0600

    Add resume page.

---
+++ resume/resume-info.php
@@ -0,0 +1,4 @@
+<?php
+  $index->content .= 
+           "\t\t\t<iframe src=\"https://docs.google.com/document/d/e/2PACX-1vSTmf-rdjwSpzwvEUZxCLaWXkLmRVUiRzUCIv8IRYCHxaCYlIY9ehBJ3lkcIN0A7IRwjo7lECSeMsBk/pub?embedded=true\"></iframe>\n";
+?>
