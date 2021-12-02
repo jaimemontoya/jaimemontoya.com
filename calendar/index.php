4d83df22081911c2cc5f6113ce2cf10a7242462e
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-02 11:40:33 -0600

    Rename files and add proper metadata to calendar page.

--- calendar/index.php
+++ calendar/index.php
@@ -1,8 +1,8 @@
 <?php
   require("../page.php");
   $index = new Page();
-  $index->title = "Jaime Montoya's self-discipline";
-  $index->metaDescription = "Self-discipline and habits program to achieve goals. Calendar shows how I spend my time. It includes five components for my time: full-time-work, freelancing-and-volunteering-work, studying, reading and exercising.";
+  $index->title = "Jaime Montoya's calendar";
+  $index->metaDescription = "My calendar shows how I spend my time. It includes five components for my time: full-time-work, freelancing-and-volunteering-work, studying, reading and exercising.";
   require("self-discipline-info.php");
   $index->Display();
 ?>
