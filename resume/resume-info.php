7fc32cb71ba4722a43b9d1116c2a2150764728a2
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-02 11:31:18 -0600

    Add id to embed element in resume page.

--- resume/resume-info.php
+++ resume/resume-info.php
@@ -1,4 +1,5 @@
 <?php
   $index->content .= 
-           "\t\t\t<iframe src=\"https://docs.google.com/document/d/e/2PACX-1vSTmf-rdjwSpzwvEUZxCLaWXkLmRVUiRzUCIv8IRYCHxaCYlIY9ehBJ3lkcIN0A7IRwjo7lECSeMsBk/pub?embedded=true\"></iframe>\n";
+           "\t\t\t<iframe src=\"https://docs.google.com/document/d/e/2PACX-1vSTmf-rdjwSpzwvEUZxCLaWXkLmRVUiRzUCIv8IRYCHxaCYlIY9ehBJ3lkcIN0A7IRwjo7lECSeMsBk/pub?embedded=true\" 
+id=\resume\"></iframe>\n";
 ?>
