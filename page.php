3e443fd363f7181670ea1cc9fdd2f64648e74c03
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-03 07:46:04 -0600

    Add meta charset.

--- page.php
+++ page.php
@@ -17,6 +17,7 @@ class Page
 	$this -> DisplayGoogleAnalytics();
     $this -> DisplayTitle();
     $this -> DisplayFavicon();
+    $this -> DisplayMetaCharset();
     $this -> DisplayMetaKeywords();
     $this -> DisplayMetaDescription();
     $this -> DisplayMetaViewport();
