4306a0c79689230347036761705d23ece6ada7c5
Jaime Montoya <webmaster@jaimemontoya.com>
2021-12-31 16:10:14 -0600

    Add period at the end of sentence.

--- niv-holy-bible/niv-holy-bible-book-info.php
+++ niv-holy-bible/niv-holy-bible-book-info.php
@@ -9,7 +9,7 @@
 		                <span>NIV Holy Bible</span>
 	                </a>
 	                <a href=\"/niv-holy-bible/\">
-		                <p>By Zondervan. Read by Jaime Montoya from 0:00 AM September 30, 2019 to 9:56 PM December 31, 2021 GMT</p>
+		                <p>By Zondervan. Read by Jaime Montoya from 0:00 AM September 30, 2019 to 9:56 PM December 31, 2021 GMT.</p>
 	                </a>
                 </div>
             </div>\n";
