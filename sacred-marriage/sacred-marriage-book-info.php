11755c3e8e287c519c1677a78f6d061203db898c
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-02 17:44:55 -0600

    Add book Sacred Marriage.

---
+++ sacred-marriage/sacred-marriage-book-info.php
@@ -0,0 +1,16 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+                <a href=\"/sacred-marriage/\">
+	                <div class=\"book-cover\" id=\"sacred-marriage-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"/sacred-marriage/\">
+		                <span>Sacred Marriage: What If God Designed Marriage to Make Us Holy More Than to Make Us Happy?</span>
+	                </a>
+	                <a href=\"/sacred-marriage/\">
+		                <p>By Gary Thomas. Read by Jaime Montoya from 5:20 PM January 22, 2022 GMT to [in progress...]</p>
+	                </a>
+                </div>
+            </div>\n";
+?>
