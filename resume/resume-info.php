4f8f95a01fabeb96662b8ac08a2228c2f431cc4c
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-02 17:56:13 -0600

    Add PDF of resume into corresponding page.

--- resume/resume-info.php
+++ resume/resume-info.php
@@ -1,6 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div id=\"resume-container\">\n\t\t\t\t<iframe 
-src=\"https://docs.google.com/document/d/e/2PACX-1vSTmf-rdjwSpzwvEUZxCLaWXkLmRVUiRzUCIv8IRYCHxaCYlIY9ehBJ3lkcIN0A7IRwjo7lECSeMsBk/pub?embedded=true\" 
-id=\"resume\" width=\"100%\" height=\"600\"></iframe>\n\t\t\t</div>\n";
+           "\t\t\t<embed src=\"resume.pdf\" width=\"500\" height=\"375\" type=\"application/pdf\">\n";
 ?>
