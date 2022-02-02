ab1aadbc13e5a5d4594e131d8c955722627a0b8e
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-02 17:30:28 -0600

    Add details about when Modern Scala Projects book was started to read.

--- modern-scala-projects/modern-scala-projects-book-info.php
+++ modern-scala-projects/modern-scala-projects-book-info.php
@@ -1,15 +1,15 @@
 <?php
   $index->content .= 
            "\t\t\t<div class=\"book\">
-                <a href=\"/emotional-intelligence-dummies/\">
-	                <div class=\"book-cover\" id=\"emotional-intelligence-dummies-img\"></div>
+                <a href=\"/modern-scala-projects/\">
+	                <div class=\"book-cover\" id=\"modern-scala-projects-img\"></div>
                 </a>
                 <div class=\"book-title-authors-read-by\">
-	                <a href=\"/emotional-intelligence-dummies/\">
-		                <span>Emotional Intelligence For Dummies</span>
+	                <a href=\"/modern-scala-projects/\">
+		                <span>Modern Scala Projects: Leverage the power of Scala for building data-driven and high-performant projects</span>
 	                </a>
-	                <a href=\"/emotional-intelligence-dummies/\">
-		                <p>By Steven J. Stein. Read by Jaime Montoya from 5:05 PM January 3, 2022 GMT to [in progress...]</p>
+	                <a href=\"/modern-scala-projects/\">
+		                <p>By Ilango Gurusamy. Read by Jaime Montoya from 1:26 PM February 1, 2022 GMT to [in progress...]</p>
 	                </a>
                 </div>
             </div>\n";
