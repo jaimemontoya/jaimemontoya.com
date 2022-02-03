43644a820b2837368eefdedc3983af0500a990b2
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-02 18:04:10 -0600

    Align PDF with resume properly.

--- resume/resume-info.php
+++ resume/resume-info.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<embed src=\"resume.pdf\" width=\"500\" height=\"375\" type=\"application/pdf\">\n";
+           "\t\t\t<embed src=\"resume.pdf\" width=\"100%\" height=\"600\" type=\"application/pdf\">\n";
 ?>
