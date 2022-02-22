d96d977a3a274fec96d46e6f98fb1540bb6572a6
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-22 16:37:51 -0600

    Add GitHub link and refactor code to display Pablo de Sarasate quotation only on home page, not in every page.

---
+++ pablo-de-sarasate-14-hours-a-day/pablo-de-sarasate-14-hours-a-day.php
@@ -0,0 +1,4 @@
+<?php
+  $index->content .= 
+           "\t\t\t<p>Welcome to <a href=\"https://github.com/jaimemontoya/jaimemontoya.com\">https://github.com/jaimemontoya/jaimemontoya.com</a>! Let's start with inspiration: Spaniard Pablo de Sarasate, internationally acclaimed as the greatest violinist of the nineteenth century, was once called a genius by a famous critic. In reply to this Sarasate said, <mark>\"Genius! For 37 years I've practiced 14 hours a day, and now they call me a genius.\"</mark></p>\n";
+?>
