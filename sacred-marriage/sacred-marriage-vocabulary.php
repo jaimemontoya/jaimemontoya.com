e24dfd3699330e43465f111bc0ffd57a8a50d9d1
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-23 14:26:27 -0600

    Add outreach word to vocabulary.

--- sacred-marriage/sacred-marriage-vocabulary.php
+++ sacred-marriage/sacred-marriage-vocabulary.php
@@ -2,5 +2,6 @@
   $index->content .= 
            "\t\t\t<ul>
 				<li>Every couple who <mark>prizes</mark> their faith and their relationship needs to read this incredible book.
+				<li>Kevin G. Harney, author of Empowered by His Presence and the Organic <mark>Outreach</mark> series (KevinGHarney.com)
 			</ul>\n";
 ?>
