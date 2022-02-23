edb373d658c27e61fd1be193f97f0080c16fec27
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-23 16:58:07 -0600

    Add 'abhorrence' and 'dissect' to vocabulary.

--- sacred-marriage/sacred-marriage-vocabulary.php
+++ sacred-marriage/sacred-marriage-vocabulary.php
@@ -5,5 +5,6 @@
 				<li>Kevin G. Harney, author of Empowered by His Presence and the Organic <mark>Outreach</mark> series (KevinGHarney.com)
 				<li>I'm going to <mark>cut him open</mark>.
 				<li>The willingness to cut into a corpse, <mark>peel back</mark> the skin, pull a <mark>scalp</mark> off a <mark>skull</mark>, cut through the bone, and actually remove, examine, and <mark>chart</mark> the organs that <mark>lay</mark> within was a crucial first step in finding out how the human body really works.
+				<li>For thousands of years, physicians had speculated on what went on inside a human body, but there was a reluctance and even an <mark>abhorrence</mark> to actually <mark>dissect</mark> a cadaver.
 			</ul>\n";
 ?>
