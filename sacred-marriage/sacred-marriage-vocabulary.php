9e146ded70b4a4a1c313f6c703db7dd2220aa38a
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-23 14:24:55 -0600

    Fix indentation.

--- sacred-marriage/sacred-marriage-vocabulary.php
+++ sacred-marriage/sacred-marriage-vocabulary.php
@@ -1,4 +1,6 @@
 <?php
   $index->content .= 
-           "\t\t\t<ul><li>Every couple who <mark>prizes</mark> their faith and their relationship needs to read this incredible book.</ul>\n";
+           "\t\t\t<ul>
+				<li>Every couple who <mark>prizes</mark> their faith and their relationship needs to read this incredible book.
+			</ul>\n";
 ?>
