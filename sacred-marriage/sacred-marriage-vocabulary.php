c7a06470510fd65e9789804c2b0d8d3f3d901445
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-23 16:45:15 -0600

    Fix spelling issue.

--- sacred-marriage/sacred-marriage-vocabulary.php
+++ sacred-marriage/sacred-marriage-vocabulary.php
@@ -4,6 +4,6 @@
 				<li>Every couple who <mark>prizes</mark> their faith and their relationship needs to read this incredible book.
 				<li>Kevin G. Harney, author of Empowered by His Presence and the Organic <mark>Outreach</mark> series (KevinGHarney.com)
 				<li>I'm going to <mark>cut him open</mark>.
-				<li>The willingness to cut into a corpse, <mark>peel back</mark> the skin, pull a <mark>scalp</mark> off a <mark>skull</mark>, cut through the bone, and actually remove, exasmine, and <mark>chart</mark> the organs that <mark>lay</mark> within was a crucial first step in finding out how the human body really works.
+				<li>The willingness to cut into a corpse, <mark>peel back</mark> the skin, pull a <mark>scalp</mark> off a <mark>skull</mark>, cut through the bone, and actually remove, examine, and <mark>chart</mark> the organs that <mark>lay</mark> within was a crucial first step in finding out how the human body really works.
 			</ul>\n";
 ?>
