81df447a370cbe76c7e9f79963932359bdfd7129
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-23 14:49:42 -0600

    Add word/expression to vocabulary.

--- sacred-marriage/sacred-marriage-vocabulary.php
+++ sacred-marriage/sacred-marriage-vocabulary.php
@@ -3,5 +3,6 @@
            "\t\t\t<ul>
 				<li>Every couple who <mark>prizes</mark> their faith and their relationship needs to read this incredible book.
 				<li>Kevin G. Harney, author of Empowered by His Presence and the Organic <mark>Outreach</mark> series (KevinGHarney.com)
+				<li>I'm going to <mark>cut him open</mark>.
 			</ul>\n";
 ?>
