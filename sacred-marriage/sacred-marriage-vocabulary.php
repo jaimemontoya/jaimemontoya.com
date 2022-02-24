c7acc4946c322bc06a6b61a82f2ce71c2464053b
Jaime Montoya <webmaster@jaimemontoya.com>
2022-02-23 22:35:27 -0600

    Add new words to vocabulary.

--- sacred-marriage/sacred-marriage-vocabulary.php
+++ sacred-marriage/sacred-marriage-vocabulary.php
@@ -7,5 +7,7 @@
 				<li>The willingness to cut into a corpse, <mark>peel back</mark> the skin, pull a <mark>scalp</mark> off a <mark>skull</mark>, cut through the bone, and actually remove, examine, and <mark>chart</mark> the organs that <mark>lay</mark> within was a crucial first step in finding out how the human body really works.
 				<li>For thousands of years, physicians had speculated on what went on inside a human body, but there was a reluctance and even an <mark>abhorrence</mark> to actually <mark>dissect</mark> a cadaver. Some men refrained out of religious conviction; others just couldn't <mark>get over</mark> the <mark>eeriness</mark> of <mark>cutting away</mark> a human <mark>rib cage</mark>.
 				<li>One woman wrote in great distress, <mark>torn</mark> because she wanted to get married while a friend was encouraging her to remain single, insisting it would be \"more holy\" for her to care for her father and then devote herself as a celibate to God after her father died.
+				<li>De Sales put the troubled young woman <mark>at ease</mark>, telling her that, far from being a compromise, in one sense, marriage might be the toughest ministry she could ever undertake.
+				<li>\"It is a perpetual exercise of <mark>mortification...</mark> In spite of the bitter nature of its juice, you may be able to draw and make the honey of a holy life.\"
 			</ul>\n";
 ?>
