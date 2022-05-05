9f8dc23f21ef4e3d745ad97ed87fd48eb97769c6
Jaime Montoya <webmaster@jaimemontoya.com>
2022-05-05 11:23:57 -0600

    Add script for donation form.

--- index1.php
+++ index1.php
@@ -1,7 +1,7 @@
 <?php
   require("page.php");
   $index = new Page();
-  $index->content .= "Hi.";
+  $index->content .= "Form should appear next: ";
   $index->content .= "<script src=\"https://s3-us-west-2.amazonaws.com/bloomerang-public-cdn/lacasademipadre/.widget-js/6536192.js\" type=\"text/javascript\"></script>";
   $index->Display();
 ?>
