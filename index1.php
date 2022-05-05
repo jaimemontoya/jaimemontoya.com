3086c5e2af9b9bf92c9f37cf759e7f5df182c2ae
Jaime Montoya <webmaster@jaimemontoya.com>
2022-05-05 11:11:10 -0600

    Add file for testing purposes of a donation form.

---
+++ index1.php
@@ -0,0 +1,39 @@
+<?php
+  require("page.php");
+  $index = new Page();
+  //require("pablo-de-sarasate-14-hours-a-day/pablo-de-sarasate-14-hours-a-day.php");
+  require("modern-scala-projects/modern-scala-projects-book-info.php");
+  require("esv-holy-bible/esv-holy-bible-book-info.php");
+  require("lightbend-scala-language-professional/lightbend-scala-language-professional-book-info.php");
+  require("emotional-intelligence-dummies/emotional-intelligence-dummies-book-info.php");
+  require("sacred-marriage/sacred-marriage-book-info.php"); 
+  require("software-engineering-scratch/software-engineering-scratch-book-info.php");
+  require("success-habits-dummies-zeller/success-habits-dummies-zeller-book-info.php");
+  require("niv-holy-bible/niv-holy-bible-book-info.php");
+  require("get-programming-scala/get-programming-scala-book-info.php");  
+  require("programming-scala-3rd/programming-scala-3rd-book-info.php");
+  require("scala-programming-projects/scala-programming-projects-book-info.php");
+  require("10-000-hours-become-practice/10-000-hours-become-practice-book-info.php");
+  require("pragmatic-scala/pragmatic-scala-book-info.php");
+  require("scala-for-the-impatient-second-edition/scala-for-the-impatient-second-edition-book-info.php");
+  require("outliers-malcolm-gladwell/outliers-malcolm-gladwell-book-info.php");
+  require("learning-spark-2nd/learning-spark-2nd-book-info.php");
+  require("apache-spark-quick/apache-spark-quick-book-info.php");
+  require("wordpress-5-complete/wordpress-5-complete-book-info.php");
+  require("the-power-of-habit/the-power-of-habit-book-info.php");
+  require("niv-gospel-john-devotional-notes/niv-gospel-john-devotional-notes-book-info.php");
+  require("place-participant-guide/place-participant-guide-book-info.php");
+  require("the-lambs-supper-the-mass-as-heaven-on-earth/the-lambs-supper-the-mass-as-heaven-on-earth-book-info.php");
+  require("the-logic-of-god/the-logic-of-god-book-info.php");
+  require("ab-testing-the/ab-testing-the-book-info.php");
+  require("personal-branding-for/personal-branding-for-book-info.php");
+  require("income-investing-an/income-investing-an-book-info.php");
+  require("wharton-marketing/wharton-marketing-book-info.php");
+  require("scaling-up-companies-rockefeller-habits/scaling-up-companies-rockefeller-habits-book-info.php");
+  require("google-speaks-secrets/google-speaks-secrets-book-info.php");
+  require("twitter-101-everything/twitter-101-everything-book-info.php");
+  require("born-to-blog/born-to-blog-book-info.php");
+  require("success-in-programming/success-in-programming-book-info.php");
+  $index->Display();
+?>
+
