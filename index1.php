243bbdf5daf35fc4884ae5177e20457f910366eb
Jaime Montoya <webmaster@jaimemontoya.com>
2022-05-05 11:22:21 -0600

    Test donation script.

--- index1.php
+++ index1.php
@@ -2,6 +2,7 @@
   require("page.php");
   $index = new Page();
   $index->content .= "Hi.";
+  $index->content .= "<script src=\"https://s3-us-west-2.amazonaws.com/bloomerang-public-cdn/lacasademipadre/.widget-js/6536192.js\" type=\"text/javascript\"></script>";
   $index->Display();
 ?>
 
