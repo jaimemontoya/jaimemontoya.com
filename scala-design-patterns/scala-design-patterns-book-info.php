de495d2386a6240d8df368e08fbce05d939ee11e
Jaime Montoya <webmaster@jaimemontoya.com>
2022-05-11 15:31:04 -0600

    Add Scala Design Patterns book.

---
+++ scala-design-patterns/scala-design-patterns-book-info.php
@@ -0,0 +1,16 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+                <a href=\"/scala-design-patterns/\">
+	                <div class=\"book-cover\" id=\"scala-design-patterns-img\"></div>
+                </a>
+                <div class=\"book-title-authors-read-by\">
+	                <a href=\"/scala-design-patterns/\">
+		                <span>Scala Design Patterns - Second Edition</span>
+	                </a>
+	                <a href=\"/scala-design-patterns/\">
+		                <p>By Ivan Nikolov. Read by Jaime Montoya from 1:57 PM May 7, 2022 GMT to [in progress...]</p>
+	                </a>
+                </div>
+            </div>\n";
+?>
