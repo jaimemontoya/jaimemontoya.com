2c2279177d52c4c50efe4f3397aff6263cf00134
Jaime Montoya <webmaster@jaimemontoya.com>
2022-05-28 13:30:40 -0600

    Remove link from book.

--- emotional-intelligence-dummies/emotional-intelligence-dummies-book-info.php
+++ emotional-intelligence-dummies/emotional-intelligence-dummies-book-info.php
@@ -1,16 +1,12 @@
 <?php
   $index->content .= 
            "\t\t\t<div class=\"book\">
-                <a href=\"/emotional-intelligence-dummies/\">
+                <div class=\"book-thumbnail\">
 	                <div class=\"book-cover\" id=\"emotional-intelligence-dummies-img\"></div>
-                </a>
+                </div>
                 <div class=\"book-title-authors-read-by\">
-	                <a href=\"/emotional-intelligence-dummies/\">
-		                <span>Emotional Intelligence For Dummies</span>
-	                </a>
-	                <a href=\"/emotional-intelligence-dummies/\">
-		                <p>By Steven J. Stein. Read by Jaime Montoya from 5:05 PM January 3, 2022 to 4:23 AM April 20, 2022 GMT.</p>
-	                </a>
+	                <span>Emotional Intelligence For Dummies</span>
+	                <p>By Steven J. Stein. Read by Jaime Montoya from 5:05 PM January 3, 2022 to 4:23 AM April 20, 2022 GMT.</p>
                 </div>
             </div>\n";
 ?>
