0bbdbb21929cb93c32c71bc1cd34158d04d0b3d1
Jaime Montoya <webmaster@jaimemontoya.com>
2022-05-28 12:40:15 -0600

    Add link.

--- modern-scala-projects/modern-scala-projects-book-info.php
+++ modern-scala-projects/modern-scala-projects-book-info.php
@@ -1,10 +1,12 @@
 <?php
   $index->content .= 
            "\t\t\t<div class=\"book\">
-	            <div class=\"book-cover\" id=\"modern-scala-projects-img\"></div>
+				<a href=\"#\">
+					<div class=\"book-cover\" id=\"modern-scala-projects-img\"></div>
+				</a>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Modern Scala Projects: Leverage the power of Scala for building data-driven and high-performant projects</span>
-		            <p>By Ilango Gurusamy. Read by Jaime Montoya from 1:26 PM February 1, 2022 to 1:56 PM May 7, 2022 GMT.</p>
+					<p>By Ilango Gurusamy. Read by Jaime Montoya from 1:26 PM February 1, 2022 to 1:56 PM May 7, 2022 GMT.</p>
                 </div>
             </div>\n";
 ?>
