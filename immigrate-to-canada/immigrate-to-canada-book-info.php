12aaf72c2dc1d7b95bc07f18e947513dad2f668e
Jaime Montoya <webmaster@jaimemontoya.com>
2022-07-04 10:32:04 -0600

    Add 'Immigrate to Canada...' book.

---
+++ immigrate-to-canada/immigrate-to-canada-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"immigrate-to-canada-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Immigrate to Canada: How to Prepare Materials for Express Entry Like a Pro</span>
+					<p>By Ye Tian. Read by Jaime Montoya from 1:14 PM June 30, 2022 to 1:30 PM July 3, 2022 GMT.</p>
+                </div>
+            </div>\n";
+?>
