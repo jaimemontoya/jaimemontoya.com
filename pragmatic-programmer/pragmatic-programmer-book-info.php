396f590fbd0b2bdec1a9793450279f1988178441
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-07-29 06:38:08 -0600

    Add Pragmatic Programmer book.

---
+++ pragmatic-programmer/pragmatic-programmer-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"pragmatic-programmer-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>The Pragmatic Programmer: Your Journey To Mastery, 20th Anniversary Edition (2nd Edition)</span>
+					<p>By David Thomas, Andrew Hunt. Read by Jaime Montoya from 2:33 AM May 31, 2022 to 5:36 AM July 29, 2022 GMT.</p>
+                </div>
+            </div>\n";
+?>
