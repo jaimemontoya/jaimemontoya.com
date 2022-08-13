8d58befe981b7deb86dfc6e4d5507c5fb1cb5dcf
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-08-13 12:55:06 -0600

    Add book Own it, Love it, Make it work.

---
+++ own-it-love-it-make-it-work/own-it-love-it-make-it-work-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"own-it-love-it-make-it-work-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Own It. Love It. Make It Work.: How to Make Any Job Your Dream Job</span>
+					<p>By Carson Tate. Read by Jaime Montoya from 4:04 PM July 29, 2022 to 6:37 PM August 5, 2022 GMT.</p>
+                </div>
+            </div>\n";
+?>
