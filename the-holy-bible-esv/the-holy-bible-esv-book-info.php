1821b98f0e62efc338161284c2b7b96ef184ba9c
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-08-17 21:48:01 -0600

    Add ESV Bible.

---
+++ the-holy-bible-esv/the-holy-bible-esv-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"the-holy-bible-esv-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>The Holy Bible, English Standard Version</span>
+					<p>By Crossway. Read by Jaime Montoya from January 1, 2022 GMT to [in progress...]</p>
+                </div>
+            </div>\n";
+?>
