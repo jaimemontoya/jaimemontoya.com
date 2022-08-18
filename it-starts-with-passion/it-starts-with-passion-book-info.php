f0b0db6268b0ab1ac7c1a4e1e9666fa8c7ed6767
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-08-17 20:44:16 -0600

    Add book It starts With Passion.

---
+++ it-starts-with-passion/it-starts-with-passion-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"it-starts-with-passion-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>It Starts With Passion: Do What You Love and Love What You Do</span>
+					<p>By Keith Abraham. Read by Jaime Montoya from 6:37 PM August 5, 2022 to 4:08 AM August 11, 2022 GMT.</p>
+                </div>
+            </div>\n";
+?>
