ab152bcf9c42f650a0aebabc0031bfa881a2bba7
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-08-17 21:31:42 -0600

    Add book Man's Search for Meaning.

---
+++ man-s-search-for-meaning/man-s-search-for-meaning-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"man-s-search-for-meaning-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Man's Search for Meaning</span>
+					<p>By Viktor E. Frankl. Read by Jaime Montoya from 2:07 PM August 15, 2022 GMT to [in progress...]</p>
+                </div>
+            </div>\n";
+?>
