cfc791260c5c08bda9f87ab9be793694f07d35e3
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-08-24 17:52:03 -0600

    Remove apostrophe.

--- mind-gym/mind-gym-book-info.php
+++ mind-gym/mind-gym-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"mind-gym-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>Mind Gym: An Athlete&apos;s Guide to Inner Excellence</span>
+		            <span>Mind Gym: An Athletes Guide to Inner Excellence</span>
 					<p>By Gary Mack, David Casstevens. Read by Jaime Montoya from 7:20 PM August 24, 2022 GMT to [in progress...]</p>
                 </div>
             </div>\n";
