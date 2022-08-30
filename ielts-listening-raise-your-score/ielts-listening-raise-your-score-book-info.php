88e0134d9eacc15a6ed2222c99772b39586a098b
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-08-29 20:40:13 -0600

    Update picture for IELTS book.

--- ielts-listening-raise-your-score/ielts-listening-raise-your-score-book-info.php
+++ ielts-listening-raise-your-score/ielts-listening-raise-your-score-book-info.php
@@ -2,7 +2,7 @@
   $index->content .= 
            "\t\t\t<div class=\"book\">
 				<div class=\"book-thumbnail\">
-					<div class=\"book-cover\" id=\"mind-gym-img\"></div>
+					<div class=\"book-cover\" id=\"ielts-listening-raise-your-score-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>IELTS - Listening: Raise your Score & Work or Study Abroad (IELTS - Knowing How!)</span>
