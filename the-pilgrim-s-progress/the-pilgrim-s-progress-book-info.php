0aa4d9766aa848d4ba86cf6f03e0693c3274c378
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-09-08 17:07:51 -0600

    Add The Pilgrim's Progress book.

---
+++ the-pilgrim-s-progress/the-pilgrim-s-progress-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"the-pilgrim-s-progress-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>The Pilgrim's Progress: A Readable Modern-Day Version of John Bunyan’s Pilgrim’s Progress</span>
+					<p>By John Bunyan, Alan Vermilye. Read by Jaime Montoya from 11:00 PM September 8, 2022 GMT to [in progress...]</p>
+                </div>
+            </div>\n";
+?>
