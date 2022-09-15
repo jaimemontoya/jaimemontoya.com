a2658752724414802790879103420f722c708922
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-09-15 08:14:01 -0600

    Add Authentic Faith book.

---
+++ authentic-faith/authentic-faith-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"authentic-faith-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Authentic Faith: The Power of a Fire-Tested Life</span>
+					<p>By Gary Thomas. Read by Jaime Montoya from 6:47 PM September 14, 2022 GMT to [in progress...]</p>
+                </div>
+            </div>\n";
+?>
