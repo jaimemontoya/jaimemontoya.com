c565842e7fc64246ee2b685ea984a02a5ad7dcb4
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-12-15 15:07:32 -0600

    Add initial weight.

--- about/about-info.php
+++ about/about-info.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<span class=\"floatleft margintop25\"><span class=\"bold\">Mission: </span><span>To achieve a weight of 49.89 kilograms (109.98862 pounds). First written and last updated: December 15, 2022 2:49 PM*.</span></span><span class=\"floatleft margintop15 clearboth\"><span class=\"bold\">Diary of Mission Accomplishment: </span></span><span class=\"floatleft margintop15 clearboth\"><ul><li>Dec. 15, 2022:</ul></span><span class=\"floatleft clearboth\">* GMT-6 for all time zones in this page.</span>\n";
+           "\t\t\t<span class=\"floatleft margintop25\"><span class=\"bold\">Mission: </span><span>To achieve a weight of 49.89 kilograms (109.98862 pounds). First written and last updated: December 15, 2022 at 2:49 PM*.</span></span><span class=\"floatleft margintop15 clearboth\"><span class=\"bold\">Diary of Mission Accomplishment: Initial weight on December 15, 2022 at 2:49 PM: 60.9 kg.</span></span><span class=\"floatleft margintop15 clearboth\"><ul><li>Dec. 15, 2022:</ul></span><span class=\"floatleft clearboth\">* GMT-6 for all time zones in this page.</span>\n";
 ?>
