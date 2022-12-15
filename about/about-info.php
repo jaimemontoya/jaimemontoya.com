6724b0c8f897bef7091a9be3f37feaf7e4caac57
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-12-15 15:01:49 -0600

    All time zones in GMT-6.

--- about/about-info.php
+++ about/about-info.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<span class=\"floatleft margintop25\"><span class=\"bold\">Mission: </span><span>To achieve a weight of 49.89 kilograms (109.98862 pounds). First written and last updated: December 15, 2022 8:49 PM GMT.</span></span><span class=\"floatleft margintop15 clearboth\"><span class=\"bold\">Diary of Mission Accomplishment: </span></span><span class=\"floatleft margintop15 clearboth\"><ul><li>Dec 15, 2022:</ul></span>\n";
+           "\t\t\t<span class=\"floatleft margintop25\"><span class=\"bold\">Mission: </span><span>To achieve a weight of 49.89 kilograms (109.98862 pounds). First written and last updated: December 15, 2022 2:49 PM*.</span></span><span class=\"floatleft margintop15 clearboth\"><span class=\"bold\">Diary of Mission Accomplishment: </span></span><span class=\"floatleft margintop15 clearboth\"><ul><li>Dec. 15, 2022:</ul></span><span>* GMT-6 for all time zones in this page.</span>\n";
 ?>
