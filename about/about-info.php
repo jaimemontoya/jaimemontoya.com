ceb789bc91a130040b54d5cef0ac47e6af0dfbe5
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-12-25 11:24:16 -0600

    Add details.

--- about/about-info.php
+++ about/about-info.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<span class=\"floatleft margintop25\">Jaime Montoya is a Data Analyst who works for TEAM International Services, Inc. His current primary role is to build Microsoft SSAS ETL packages to migrate data after the Republic Services' acquisition of US Ecology.</span>\n";
+           "\t\t\t<span class=\"floatleft margintop25\">Jaime Montoya is a Data Analyst who works for TEAM International Services, Inc. His current primary role is to build Microsoft SSAS ETL packages to migrate data after the Republic Services' acquisition of US Ecology. Jaime believes that data analysis technologies provide an essential foundation for companies to make intelligent strategic decisions.</span>\n";
 ?>
