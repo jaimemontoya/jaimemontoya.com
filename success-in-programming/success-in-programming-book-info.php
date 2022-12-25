b35af7c890dbe90d660721d37a1a86f94738c557
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-12-25 17:28:10 -0600

    Add index page.

--- success-in-programming/success-in-programming-book-info.php
+++ success-in-programming/success-in-programming-book-info.php
@@ -1,6 +1,6 @@
 <?php
   $index->content .= 
-           "\t\t\t<div class=\"book\">
+           "\t\t\t<a href=\"index.php\"><div class=\"book\">
                 <div class=\"book-thumbnail\">
 	                <div class=\"book-cover\" id=\"success-in-programming-img\"></div>
                 </div>
@@ -8,5 +8,5 @@
 	                <span>Success in Programming: How to Gain Recognition, Power, and Influence through Personal Branding</span>
 	                <p>By Fr&eacute;d&eacute;ric Harper. Read by Jaime Montoya from 1:59 AM August 31, 2018 to 11:52 PM September 6, 2018 GMT.</p>
                 </div>
-            </div>\n";
+            </div></a>\n";
 ?>
