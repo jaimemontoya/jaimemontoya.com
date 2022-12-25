725d165eb60df0d376154c2a128ebe5141916136
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-12-25 11:15:07 -0600

    Fix grammar.

--- about/about-info.php
+++ about/about-info.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<span class=\"floatleft margintop25\">Jaime Montoya is a Data Analyst who works for TEAM International Services, Inc. His current primary role is to build Microsoft SSAS ETL packages to migrate data after the Republic Services' Acquisition Of Us Ecology.</span>\n";
+           "\t\t\t<span class=\"floatleft margintop25\">Jaime Montoya is a Data Analyst who works for TEAM International Services, Inc. His current primary role is to build Microsoft SSAS ETL packages to migrate data after the Republic Services' acquisition of US Ecology.</span>\n";
 ?>
