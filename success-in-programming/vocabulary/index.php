f446e601d4c8b1eeac654f061911938eb5d269b9
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-12-26 07:47:44 -0600

    Fix path of required file.

--- success-in-programming/vocabulary/index.php
+++ success-in-programming/vocabulary/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "About Jaime Montoya";
   $index->metaDescription = "Mission, vision and values statements for Jaime Montoya.";
-  //require("success-in-programming-book-info.php");
+  require("../success-in-programming-book-info.php");
   require("vocabulary.php");
   $index->Display();
 ?>
