614add620d5593b01916ac8bd765fe41f720e1f6
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-12-31 13:26:03 -0600

    Add contact information.

--- resume/resume-info.php
+++ resume/resume-info.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div class=\"logo center margintop25\"><a href=\"/\" title=\"Jaime Montoya\" class=\"black\">Jaime Montoya</a><div class=\"center\">Condominio Los Casta&ntilde;os, apartamento 36, Colonia Escal&oacute;n, San Salvador, El Salvador</div></div><embed src=\"resume.pdf\" width=\"100%\" height=\"600\" type=\"application/pdf\">\n";
+           "\t\t\t<div class=\"logo center margintop25\"><a href=\"/\" title=\"Jaime Montoya\" class=\"black\">Jaime Montoya</a><div class=\"center\">Condominio Los Casta&ntilde;os, apartamento 36, Colonia Escal&oacute;n, San Salvador, El Salvador</div><div></div>+503 7696-3839 | <a href=\"mailto:jmontoya@jaimemontoya.com\">jmontoya@jaimemontoya.com</a></div><embed src=\"resume.pdf\" width=\"100%\" height=\"600\" type=\"application/pdf\">\n";
 ?>
