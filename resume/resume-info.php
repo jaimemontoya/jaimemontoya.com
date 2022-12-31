9bca0d2ab908e84709a6886685738e89ce352c57
Jaime Montoya <jmontoya@jaimemontoya.com>
2022-12-31 13:21:29 -0600

    Use div instead of span to have a break.

--- resume/resume-info.php
+++ resume/resume-info.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div class=\"logo center margintop25\"><a href=\"/\" title=\"Jaime Montoya\" class=\"black\">Jaime Montoya</a><span class=\"center\">Condominio Los Casta&ntilde;os, apartamento 36, Colonia Escal&oacute;n, San Salvador, El Salvador</span></div><embed src=\"resume.pdf\" width=\"100%\" height=\"600\" type=\"application/pdf\">\n";
+           "\t\t\t<div class=\"logo center margintop25\"><a href=\"/\" title=\"Jaime Montoya\" class=\"black\">Jaime Montoya</a><div class=\"center\">Condominio Los Casta&ntilde;os, apartamento 36, Colonia Escal&oacute;n, San Salvador, El Salvador</div></div><embed src=\"resume.pdf\" width=\"100%\" height=\"600\" type=\"application/pdf\">\n";
 ?>
