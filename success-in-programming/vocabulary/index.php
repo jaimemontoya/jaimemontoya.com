8da0276e000d9c8bdf9ee8a2fd3108b2469cd0af
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-14 03:29:33 -0600

    Add title to page.

--- success-in-programming/vocabulary/index.php
+++ success-in-programming/vocabulary/index.php
@@ -2,7 +2,7 @@
   require("../../page.php");
   $index = new Page();
   $index->title = "About Jaime Montoya";
-  $index->metaDescription = "Mission, vision and values statements for Jaime Montoya.";
+  $index->metaDescription = "Success in Programming: How to Gain Recognition, Power, and Influence through Personal Branding";
   require("../success-in-programming-book-info.php");
   require("vocabulary.php");
   $index->Display();
