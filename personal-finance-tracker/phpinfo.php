184845db925d8a98688cae5d45f7c1012d00cb83
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-21 18:22:33 -0600

    Change HTML for testing database connection.

--- personal-finance-tracker/phpinfo.php
+++ personal-finance-tracker/phpinfo.php
@@ -1,21 +1,5 @@
 <?php
-//echo phpinfo();
-/*$user = "jmontoya";
-$password = "SuperEasy@1";
-$database = "finance";
-$table = "FactSales";
 
-try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  echo "<h2>TODO</h2><ol>"; 
-  foreach($db->query("SELECT content FROM $table") as $row) {
-    echo "<li>" . $row['content'] . "</li>";
-  }
-  echo "</ol>";
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}*/
 $servername = "localhost";
 $username = "jmontoya";
 $password = "SuperEasy@1";
@@ -29,3 +13,41 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>Personal Finance Tracker</head>
+	<body>
+
+	<?php
+	$servername = "localhost";
+	$username = "jmontoya";
+	$password = "SuperEasy@1";
+	$dbname = "finance";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "SELECT id, firstname, lastname FROM MyGuests";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		echo "<table><tr><th>ID</th><th>Name</th></tr>";
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<tr><td>" . $row["id"]. "</td><td>" . $row["firstname"]. " " . $row["lastname"]. "</td></tr>";
+		}
+		echo "</table>";
+	} else {
+		echo "0 results";
+	}
+
+	$conn->close();
+	?>
+
+	</body>
+</html>
