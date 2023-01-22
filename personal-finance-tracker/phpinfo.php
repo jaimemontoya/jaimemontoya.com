ca6bef2612683ee26a04a49a56a6dfa7a618e569
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-21 18:04:37 -0600

    Test MySQL connection.

--- personal-finance-tracker/phpinfo.php
+++ personal-finance-tracker/phpinfo.php
@@ -1,3 +1,18 @@
 <?php
-	phpinfo();
+$user = "jmontoya";
+$password = "SuperEasy@1";
+$database = "finance";
+$table = "FactSales";
+
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  echo "<h2>TODO</h2><ol>"; 
+  foreach($db->query("SELECT content FROM $table") as $row) {
+    echo "<li>" . $row['content'] . "</li>";
+  }
+  echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 ?>
