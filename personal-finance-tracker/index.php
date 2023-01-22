8310692d59f59f0543579cd8c04db284e8c9d32a
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-21 18:49:37 -0600

    Print entire array of results.

--- personal-finance-tracker/index.php
+++ personal-finance-tracker/index.php
@@ -18,9 +18,8 @@
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 			echo "<table><tr><th>Description</th><th>Date</th><th>City</th><th>Payment methodPayment method</th><th>Buyer</th><th>Category</th><th>Total</th></tr>";
-			// output data of each row
 			while($row = $result->fetch_assoc()) {
-				echo "<tr><td>" . $row["Description"]. "</td><td>" . $row["DateKey"]. " " . $row["CityName"]. "</td><td>" . $row["PaymentMethodName"]. " " . $row["BuyerName"]. "</td><td>" . $row["Category"]. " " . $row["TotalSales"]. "</td></tr>";
+				echo "<tr><td>" . $row["Description"]. "</td><td>" . $row["DateKey"]. " " . $row["CityName"]. "</td><td>" . $row["PaymentMethodName"]. " " . $row["BuyerName"]. "</td><td>" . $row["Category"]. " " . $row["TotalSales"]. "</td></tr>";print_r($result);
 			}
 			echo "</table>";
 		} else {
