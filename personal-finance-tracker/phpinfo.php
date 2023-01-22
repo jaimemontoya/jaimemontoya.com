e63a66cff5ea048e6d7b86bea44cd0a733697f8e
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-21 18:27:48 -0600

    Remove unnecessary code.

--- personal-finance-tracker/phpinfo.php
+++ personal-finance-tracker/phpinfo.php
@@ -1,40 +1,18 @@
-<?php
-
-$servername = "localhost";
-$username = "jmontoya";
-$password = "SuperEasy@1";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
-
 <!DOCTYPE html>
 <html>
 	<head>Personal Finance Tracker</head>
 	<body>
-
 	<?php
 	$servername = "localhost";
 	$username = "jmontoya";
 	$password = "SuperEasy@1";
 	$dbname = "finance";
-
-	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-
 	$sql = "SELECT TotalSales, Description, CityID FROM FactSales";
 	$result = $conn->query($sql);
-
 	if ($result->num_rows > 0) {
 		echo "<table><tr><th>ID</th><th>Name</th></tr>";
 		// output data of each row
