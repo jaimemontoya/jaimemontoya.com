9d621b0aa5e54227ac5fc43998d3f72abcd788f3
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-22 21:16:19 -0600

    Use 'Studied' instead of 'Read'.

--- knights-24-hour-trainer-sql-server-2008-integration-services/knights-24-hour-trainer-sql-server-2008-integration-services-book-info.php
+++ knights-24-hour-trainer-sql-server-2008-integration-services/knights-24-hour-trainer-sql-server-2008-integration-services-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Knight's 24-Hour Trainer: Microsoft® SQL Server® 2008 Integration Services</span>
-					<p>By Brian Knight, Devin Knight, Mike Davis. Read by Jaime Montoya from 6:30 PM October 27, 2022 GMT to [reading progress: 9% read]</p>
+					<p>By Brian Knight, Devin Knight, Mike Davis. Studied by Jaime Montoya from 6:30 PM October 27, 2022 GMT to [reading progress: 9% read]</p>
                 </div>
             </div>\n";
 ?>
