795faceb5697dd7015cd9fb7a2bcde9e026eb12a
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-22 21:44:54 -0600

    Add link to verify Coursera course completion.

--- wharton-marketing/wharton-marketing-book-info.php
+++ wharton-marketing/wharton-marketing-book-info.php
@@ -1,6 +1,6 @@
 <?php
   $index->content .= 
-           "\t\t\t<div class=\"book\">
+           "\t\t\t<a href=\"https://coursera.org/verify/UN9PAZ74UDAB\"><div class=\"book\">
                 <div class=\"book-thumbnail\">
 	                <div class=\"book-cover\" id=\"wharton-marketing-img\"></div>
                 </div>
@@ -8,5 +8,5 @@
 	                <span>Introduction to Marketing</span>
 	                <p>By University of Pennsylvania. Successfully completed by Jaime Montoya from 4:30 PM April 22, 2019 to April 30, 2019 GMT. Verify at https://coursera.org/verify/UN9PAZ74UDAB.</p>
                 </div>
-            </div>\n";
+            </div></a>\n";
 ?>
