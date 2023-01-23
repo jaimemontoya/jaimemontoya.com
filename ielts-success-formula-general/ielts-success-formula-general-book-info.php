2edd485ba39d1ab42aa530a10193008aaed68472
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-22 20:53:03 -0600

    Add book.

---
+++ ielts-success-formula-general/ielts-success-formula-general-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"ielts-success-formula-general-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>IELTS Success Formula General: The Complete Practical Guide to a Top IELTS Score</span>
+					<p>By Simone Braverman, Stephen Slater. Read by Jaime Montoya from 1:13 PM November 19, 2022 GMT to [reading progress: 86% read]</p>
+                </div>
+            </div>\n";
+?>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   require("ielts-listening-strategies/ielts-listening-strategies-book-info.php");
   require("ielts-listening-tips/ielts-listening-tips-book-info.php");  
   require("amplified-bible-classic-edition/amplified-bible-classic-edition-book-info.php");  
+  require("ielts-success-formula-general/ielts-success-formula-general-book-info.php");  
   require("the-everything-store/the-everything-store-book-info.php");  
   require("multiply/multiply-book-info.php");
   require("the-holy-bible-esv/the-holy-bible-esv-book-info.php");
