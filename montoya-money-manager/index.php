dc7003628ee9d0ff86eb2fe0b718dff36b1c1486
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-01-31 13:01:10 -0600

    Print totalExpenses.

--- montoya-money-manager/index.php
+++ montoya-money-manager/index.php
@@ -30,8 +30,7 @@
 		$sumSales = $conn->query($sqlSumSales);
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
-			echo "<table><tr><th>Description</th><th>Date</th><th>City</th><th>Payment method</th><th>Seller</th><th>Category</th><th>Total = ".$totalExpenses->fetch_assoc()["TotalExpenses"]."</th></tr>";			
-			//echo "<table><tr><th>Description</th><th>Date</th><th>City</th><th>Payment method</th><th>Seller</th><th>Category</th><th>Total = </th></tr>";		
+			print_r($totalExpenses);echo "<table><tr><th>Description</th><th>Date</th><th>City</th><th>Payment method</th><th>Seller</th><th>Category</th><th>Total = ".$totalExpenses->fetch_assoc()["TotalExpenses"]."</th></tr>";
 			while($row = $result->fetch_assoc()) {
 				echo "<tr><td>" . $row["Description"]. "</td><td>" . $row["Date"]. "</td><td>" . $row["City"]. "</td><td>" . preg_replace('/[0-9]/', '*', $row["Payment method"]). "</td><td>" . $row["Seller"]. "</td><td>" . $row["Category"]. "</td><td>" . $row["Total"]. "</td></tr>";
 			}
