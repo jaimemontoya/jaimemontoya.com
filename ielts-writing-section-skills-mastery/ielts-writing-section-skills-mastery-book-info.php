783fd03805ef4bd38d5a31108f96910a6820429a
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-02-20 17:01:46 -0600

    Add link to Coursera course completed.

--- ielts-writing-section-skills-mastery/ielts-writing-section-skills-mastery-book-info.php
+++ ielts-writing-section-skills-mastery/ielts-writing-section-skills-mastery-book-info.php
@@ -1,12 +1,12 @@
 <?php
   $index->content .= 
-           "\t\t\t<div class=\"book\">
+           "\t\t\t<a href=\"https://coursera.org/account/accomplishments/certificate/TXEKWGBBSBML\"><div class=\"book\">
                 <div class=\"book-thumbnail\">
 	                <div class=\"book-cover\" id=\"ielts-writing-section-skills-mastery-img\"></div>
                 </div>
                 <div class=\"book-title-authors-read-by\">
 	                <span>IELTS Writing Section Skills Mastery</span>
-	                <p>By University of California, Irvine. Successfully completed by Jaime Montoya from 5:18 PM January 19, 2019 to [completion progress: 20% completed] GMT.</p>
+	                <p>By University of California, Irvine. Successfully completed by Jaime Montoya from 5:18 PM January 19, 2023 to February 18, 2023 GMT. Verify at https://coursera.org/account/accomplishments/certificate/TXEKWGBBSBML.</p>
                 </div>
-            </div>\n";
+            </div></a>\n";
 ?>
