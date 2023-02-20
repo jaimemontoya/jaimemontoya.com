165ecde92c29f7e57260f84e5fdefc8504ae3002
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-02-20 17:07:56 -0600

    Use shorter link.

--- ielts-writing-section-skills-mastery/ielts-writing-section-skills-mastery-book-info.php
+++ ielts-writing-section-skills-mastery/ielts-writing-section-skills-mastery-book-info.php
@@ -1,12 +1,12 @@
 <?php
   $index->content .= 
-           "\t\t\t<a href=\"https://coursera.org/account/accomplishments/certificate/TXEKWGBBSBML\"><div class=\"book\">
+           "\t\t\t<a href=\"https://coursera.org/verify/TXEKWGBBSBML\"><div class=\"book\">
                 <div class=\"book-thumbnail\">
 	                <div class=\"book-cover\" id=\"ielts-writing-section-skills-mastery-img\"></div>
                 </div>
                 <div class=\"book-title-authors-read-by\">
 	                <span>IELTS Writing Section Skills Mastery</span>
-	                <p>By University of California, Irvine. Successfully completed by Jaime Montoya from 5:18 PM January 19, 2023 to 3:16 AM February 19, 2023 GMT. Verify at https://coursera.org/account/accomplishments/certificate/TXEKWGBBSBML.</p>
+	                <p>By University of California, Irvine. Successfully completed by Jaime Montoya from 5:18 PM January 19, 2023 to 3:16 AM February 19, 2023 GMT. Verify at https://coursera.org/verify/TXEKWGBBSBML.</p>
                 </div>
             </div></a>\n";
 ?>
