6e739108777111274d19e30dd1ef29ee1fdaf0fc
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-03-21 19:26:52 -0600

    Show login option only when needed.

--- montoya-money-manager/index.php
+++ montoya-money-manager/index.php
@@ -136,42 +136,7 @@
 		}
 		*/
 		?>
-		<h2>Montoya Money Manager</h2>
-		<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
-		<div id="id01" class="modal">
-		  <form class="modal-content animate" action="" method="post">
-			<div class="imgcontainer">
-			  <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-			  <div class="logo">
-				<a href="/" title="Montoya Money Manager">Montoya Money Manager</a>
-			</div>
-			</div>
-			<div class="container">
-			  <label for="uname"><b>Username</b></label>
-			  <input type="text" placeholder="Enter Username" name="uname" required>
-			  <label for="psw"><b>Password</b></label>
-			  <input type="password" placeholder="Enter Password" name="psw" required>				
-			  <button type="submit">Login</button>
-			  <label>
-				<input type="checkbox" checked="checked" name="remember"> Remember me
-			  </label>
-			</div>
-			<div class="container" style="background-color:#f1f1f1">
-			  <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-			  <span class="psw">Forgot <a href="#">password?</a></span>
-			</div>
-		  </form>
-		</div>
-		<script>
-		// Get the modal
-		var modal = document.getElementById('id01');
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
-		</script>
+		
 		<?php
 		/* These are our valid username and passwords */
 		$user = 'jmontoya';
@@ -284,6 +249,44 @@
 			}
 		} else {
 			echo 'You must supply a username and password.';
+		?>
+		<h2>Montoya Money Manager</h2>
+		<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+		<div id="id01" class="modal">
+		  <form class="modal-content animate" action="" method="post">
+			<div class="imgcontainer">
+			  <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+			  <div class="logo">
+				<a href="/" title="Montoya Money Manager">Montoya Money Manager</a>
+			</div>
+			</div>
+			<div class="container">
+			  <label for="uname"><b>Username</b></label>
+			  <input type="text" placeholder="Enter Username" name="uname" required>
+			  <label for="psw"><b>Password</b></label>
+			  <input type="password" placeholder="Enter Password" name="psw" required>				
+			  <button type="submit">Login</button>
+			  <label>
+				<input type="checkbox" checked="checked" name="remember"> Remember me
+			  </label>
+			</div>
+			<div class="container" style="background-color:#f1f1f1">
+			  <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+			  <span class="psw">Forgot <a href="#">password?</a></span>
+			</div>
+		  </form>
+		</div>
+		<script>
+		// Get the modal
+		var modal = document.getElementById('id01');
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
+		</script>
+		<?php
 		}
 		?>
 	</body>
