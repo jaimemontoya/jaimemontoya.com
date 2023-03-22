9140bacac4068c1b71f6d6ab3c3ca9158778fc9a
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-03-21 19:16:37 -0600

    Fix syntax.

--- montoya-money-manager/index.php
+++ montoya-money-manager/index.php
@@ -176,8 +176,7 @@
 		/* These are our valid username and passwords */
 		$user = 'jmontoya';
 		$pass = 'MontoyaMM$$$1';
-		//if (isset($_POST['username']) && isset($_POST['password')) {echo "Hi";/*
-		if (isset($_POST['username']) && isset($_POST['username')) {echo "Hi";/*
+		if (isset($_POST['username']) && isset($_POST['password'])) {echo "Hi";/*
 			if (($_POST['username'] == $user) && ($_POST['password'] == $pass)) {
 				if (isset($_POST['rememberme'])) {
 					/* Set cookie to last 1 year 
