c534f36cf92321faa27803989016a3dca2426388
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-06-11 07:11:01 -0600

    Fix numbers.

--- pragmatic-scala/pragmatic-scala-book-info.php
+++ pragmatic-scala/pragmatic-scala-book-info.php
@@ -6,7 +6,7 @@
                 </div>
                 <div class=\"book-title-authors-read-by\">
 	                <span>Pragmatic Scala: Create Expressive, Concise, and Scalable Applications. 1st. Edition.</span>
-	                <p>By Venkat Subramaniam. <mark>Book 18 read by Jaime Montoya</mark> from 2:33 AM May 31, 2021 to 5:48 AM July 6, 2021 GMT.</p>
+	                <p>By Venkat Subramaniam. <mark>Book 19 read by Jaime Montoya</mark> from 2:33 AM May 31, 2021 to 5:48 AM July 6, 2021 GMT.</p>
                 </div>
             </div>\n";
 ?>
--- programming-scala-3rd/programming-scala-3rd-book-info.php
+++ programming-scala-3rd/programming-scala-3rd-book-info.php
@@ -6,7 +6,7 @@
                 </div>
                 <div class=\"book-title-authors-read-by\">
 	                <span>Programming Scala, 3rd Edition</span>
-	                <p>By Dean Wampler. Read by Jaime Montoya from 1:23 PM February 5, 2021 to 1:37 AM October 17, 2021 GMT.</p>
+	                <p>By Dean Wampler. <mark>Book 22 read by Jaime Montoya</mark> from 1:23 PM February 5, 2021 to 1:37 AM October 17, 2021 GMT.</p>
                 </div>
             </div>\n";
 ?>
