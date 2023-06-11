d8ac7e4745fe9d746abddea247ffbfddd45bc1d8
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-06-10 22:03:38 -0600

    Add Endure book.

---
+++ endure/endure-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"endure-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Endure: Mind, Body, and the Curiously Elastic Limits of Human Performance</span>
+					<p>By Alex Hutchinson. Read by Jaime Montoya from 1:03 PM April 18, 2023 to 12:02 AM May 15, 2023 GMT.</p>
+                </div>
+            </div>\n";
+?>
