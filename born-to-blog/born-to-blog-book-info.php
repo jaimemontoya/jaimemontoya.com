b64d32ed3792133318e9d2f5239f5c2d5f583174
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-06-11 06:55:30 -0600

    Highlight more text.

--- born-to-blog/born-to-blog-book-info.php
+++ born-to-blog/born-to-blog-book-info.php
@@ -6,7 +6,7 @@
                 </div>
                 <div class=\"book-title-authors-read-by\">
 	                <span>Born to Blog: Building Your Blog for Personal and Business Success One Post at a Time</span>
-	                <p>By Stanford Smith, Mark Schaefer. <mark>Book 2</mark> read by Jaime Montoya from 7:40 AM September 14, 2018 to 8:45 AM September 15, 2018 GMT.</p>
+	                <p>By Stanford Smith, Mark Schaefer. <mark>Book 2 read by Jaime Montoya</mark> from 7:40 AM September 14, 2018 to 8:45 AM September 15, 2018 GMT.</p>
                 </div>
             </div>\n";
 ?>
