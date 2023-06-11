76362e220ff2ba74c2d95aafc3a56fdb303592d8
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-06-11 07:33:58 -0600

    Add numbers.

--- the-mental-training-guide-for-elite-athletes/the-mental-training-guide-for-elite-athletes-book-info.php
+++ the-mental-training-guide-for-elite-athletes/the-mental-training-guide-for-elite-athletes-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>THE MENTAL TRAINING GUIDE FOR ELITE ATHLETES: How the Mental Master Method Helps Players, Parents, and Coaches Create a Championship Mindset (PLAYER ADVANCEMENT SERIES Book 3)</span>
-					<p>By David Angeron. Read by Jaime Montoya from 2:36 PM October 8, 2022 to 9:53 PM October 13, 2022 GMT.</p>
+					<p>By David Angeron. <mark>Book 43 read by Jaime Montoya</mark> from 2:36 PM October 8, 2022 to 9:53 PM October 13, 2022 GMT.</p>
                 </div>
             </div>\n";
 ?>
