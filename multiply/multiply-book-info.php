7958a6d114da4da8a71205e77fcc9e6074bc18a3
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-06-10 19:41:04 -0600

    Change date of done reading Multiply book.

--- multiply/multiply-book-info.php
+++ multiply/multiply-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Multiply: Disciples Making Disciples</span>
-					<p>By Francis Chan. Read by Jaime Montoya from 2:15 PM October 8, 2022 to 1:25 AM June 11, 2023 GMT.</p>
+					<p>By Francis Chan. Read by Jaime Montoya from 2:15 PM October 8, 2022 to 7:37 PM March 8, 2023 GMT.</p>
                 </div>
             </div>\n";
 ?>
