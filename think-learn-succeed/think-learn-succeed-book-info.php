0cdfbf9f1a2f22daf6929d4d958cf4eb59fa37f7
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-06-28 17:37:28 -0600

    Use quote only on home page.

--- think-learn-succeed/think-learn-succeed-book-info.php
+++ think-learn-succeed/think-learn-succeed-book-info.php
@@ -1,5 +1,6 @@
 <?php
   $index->content .= 
+			"\t\t\t'Spaniard Pablo de Sarasate, internationally acclaimed as the greatest violinist of the nineteenth century, was once called a genius by a famous critic. In reply to this Sarasate said, \"Genius! For 37 years I've practiced 14 hours a day, and now they call me a genius.\"' Pellegrini, R. J. (2014). <i>Education For The Joy Of It: How To Thrive Not Just Survive In High School, College and Beyond.</i> AuthorHouse. https://bit.ly/202306282328";
            "\t\t\t<div class=\"book\">
 				<div class=\"book-thumbnail\">
 					<div class=\"book-cover\" id=\"think-learn-succeed-img\"></div>
