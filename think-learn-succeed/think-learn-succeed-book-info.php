02c598f7f2fc6546658aeef7c868d572ee92574b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-06-28 05:25:24 -0600

    Add book Think, Learn, Succeed.

---
+++ think-learn-succeed/think-learn-succeed-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"think-learn-succeed-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Think, Learn, Succeed: Understanding and Using Your Mind to Thrive at School, the Workplace, and Life</span>
+					<p>By Dr. Caroline Leaf. <mark>Book 57 read by Jaime Montoya</mark> from 12:03 PM June 1, 2023 to 3:46 PM June 23, 2023 GMT.</p>
+                </div>
+            </div>\n";
+?>
