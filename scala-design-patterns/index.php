a640b5a89ef9c8e80a81579d84ea22479bdbc517
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-06 11:02:10 -0600

    Remove files that are not linked to from anywhere in the website.

--- scala-design-patterns/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require("../page.php");
-  $index = new Page();
-  $index->title = "Scala Design Patterns - Second Edition";
-  $index->metaDescription = "If you want to increase your understanding of Scala and apply design patterns to real-life application development, then this book is for you.";
-  require("scala-design-patterns-book-info.php");
-  $index->Display();
-?>
--- success-in-programming/vocabulary/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require("../../page.php");
-  $index = new Page();
-  $index->title = "Success in Programming: How to Gain Recognition, Power, and Influence through Personal Branding";
-  $index->metaDescription = "Success in Programming: How to Gain Recognition, Power, and Influence through Personal Branding";
-  require("../success-in-programming-book-info.php");
-  require("vocabulary.php");
-  $index->Display();
-?>
