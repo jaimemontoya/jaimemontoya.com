398a6f249a935997be5464d65072edd0e460d423
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-06 11:08:07 -0600

    Remove unnecessary files.

--- the-everything-store/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require("../page.php");
-  $index = new Page();
-  $index->title = "The Everything Store: Jeff Bezos and the Age of Amazon";
-  $index->metaDescription = "The Everything Store is the revealing, definitive biography of the company that placed one of the first and largest bets on the Internet and forever changed the way we shop and read.";
-  require("the-everything-store-book-info.php");
-  $index->Display();
-?>
