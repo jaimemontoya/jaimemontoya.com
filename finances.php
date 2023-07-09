7d7da0fb950236f05436094cb6c19028ce0637c0
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-09 17:56:49 -0600

    Create Display() function in FinancesPage class.

--- finances.php
+++ finances.php
@@ -1,7 +1,26 @@
 <?php
-  require ("page.php");
-  class FinancesPage extends Page
+require ("page.php");
+class FinancesPage extends Page
+{
+  public function Display()
   {
-    
+    echo "<!doctype html>\n<html lang=\"en\">\n\t<head>\n";
+    $this -> DisplayGoogleAnalytics();
+    $this -> DisplayFontAwesome();
+    $this -> DisplayTitle();
+    $this -> DisplayFavicon();
+    $this -> DisplayMetaCharset();
+    $this -> DisplayMetaKeywords();
+    $this -> DisplayMetaDescription();
+    $this -> DisplayMetaViewport();
+    $this -> DisplayStyles();
+    echo "\t</head>\n\t<body>\n";
+    $this -> DisplayHeader();
+    echo "\t\t<div class=\"container\">\n";
+    echo $this->content;
+    echo "\t\t</div>\n";
+	$this -> DisplayFooter();
+    echo "\t</body>\n</html>\n";
   }
+}
 ?>
