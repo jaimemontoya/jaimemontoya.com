fc80ec1a91298089066a85d1b81b2c1642f8785b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-09 19:11:47 -0600

    Remove code used for testing purposes.

--- finances/finances.php
+++ finances/finances.php
@@ -1,19 +1,6 @@
 <?php
-  include '../../db.inc';
-  $sqlGetCategories = "SELECT * FROM DimCategory ORDER BY CategoryName ASC;";
-  $resultCategories = $conn->query($sqlGetCategories);
-  if ($resultCategories->num_rows > 0) {
-	while($row = $resultCategories->fetch_assoc()) {
-	$finances ->content .=
-	   "\t\t\t<div>".$row['CategoryName']."
-		</div>\n";
-	}
-  } else {
+  include '../../db.inc';  
   $finances ->content .=
-	   "\t\t\t<div>bye
-		</div>\n";	
-  }
-  /*$finances ->content .=
            "\t\t\t<div>a
-            </div>\n";*/
+            </div>\n";
 ?>
