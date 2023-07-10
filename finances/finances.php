32d04d4577e8c534d57c1960d52596e460d3de62
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-10 01:21:02 -0600

    Add tabs to h1 element.

--- finances/finances.php
+++ finances/finances.php
@@ -3,7 +3,7 @@
   if (!isset($_POST['uname']) && !isset($_POST['psw']) && !isset($_SESSION["user"])) {
     $finances ->content .=
     "\t\t\t<div>
-	<h1>Finances</h1>
+	 \t\t\t\t<h1>Finances</h1>
 	 </div>\n";
   }
 ?>
