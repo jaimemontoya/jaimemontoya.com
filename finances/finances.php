b5ebfa72d54ad0dcce38a8b1a3f1cf200f7d693a
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-09 19:07:29 -0600

    Test MySQL connection.

--- finances/finances.php
+++ finances/finances.php
@@ -1,6 +1,17 @@
 <?php
   include '../../db.inc';
+  $sqlGetCategories = "SELECT * FROM DimCategory ORDER BY CategoryName ASC;";
+  $resultCategories = $conn->query($sqlGetCategories);
+  if ($resultCategories->num_rows > 0) {
   $finances ->content .=
+	   "\t\t\t<div>hi
+		</div>\n";	
+  } else {
+  $finances ->content .=
+	   "\t\t\t<div>bye
+		</div>\n";	
+  }
+  /*$finances ->content .=
            "\t\t\t<div>a
-            </div>\n";		   
+            </div>\n";*/
 ?>
