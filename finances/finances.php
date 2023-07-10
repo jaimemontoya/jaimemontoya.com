8df8f99a7c1758d4c0a3e00d27549ba154c836a7
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-10 01:19:15 -0600

    Add h1 element to Finances page.

--- finances/finances.php
+++ finances/finances.php
@@ -2,7 +2,8 @@
   include '../../db.inc';
   if (!isset($_POST['uname']) && !isset($_POST['psw']) && !isset($_SESSION["user"])) {
     $finances ->content .=
-    "\t\t\t<div>Finances
+    "\t\t\t<div>
+	<h1>Finances</h1>
 	 </div>\n";
   }
 ?>
