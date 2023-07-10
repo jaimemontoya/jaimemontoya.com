e573a902dc09c8e90d50e5b87ba21709a76917ef
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-10 01:58:40 -0600

    Add styles for login pop-up.

--- finances/finances.php
+++ finances/finances.php
@@ -5,6 +5,8 @@
     "\t\t\t<div>
 	 \t\t\t<h1>Finances</h1>
 	 \t\t\t<button onclick=\"document.getElementById('loginform').style.display='block'\" style=\"width:auto;\">Login</button>
+	 \t\t\t<div id=\"loginform\" class=\"modal\">
+	 \t\t\t</div>
 	 \t\t</div>\n";
   }
 ?>
