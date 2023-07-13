195f289da982ff9b57be72f95dd1817682725c36
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-12 22:29:46 -0600

    Move in-line CSS to external CSS file.

--- finances/finances.php
+++ finances/finances.php
@@ -4,7 +4,7 @@
     $finances ->content .=
     "\t\t\t<div>
 	 \t\t\t<h1>Finances</h1>
-	 \t\t\t<button onclick=\"document.getElementById('loginform').style.display='block'\" style=\"width:auto;\">Login</button>
+	 \t\t\t<button onclick=\"document.getElementById('loginform').style.display='block'\" class=\"widthauto\">Login</button>
 	 \t\t\t<div id=\"loginform\" class=\"modal\">
 	 \t\t\t</div>
 	 \t\t</div>\n";
