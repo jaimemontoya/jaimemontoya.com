c68bfeaf2f2b3828c2f6adf785fa0fedc8e7e756
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-14 00:22:45 -0600

    Fix indentation.

--- finances/finances.php
+++ finances/finances.php
@@ -13,16 +13,16 @@
      \t\t\t\t\t\t<div class=\"logo\">
      \t\t\t\t\t\t\t<a href=\"/\" title=\"Finances\">Finances</a>
      \t\t\t\t\t\t</div>
-	 \t\t\t\t\t\t<div class=\"container\">
-	 \t\t\t\t\t\t\t<label for=\"uname\"><b>Username</b></label>
-	 \t\t\t\t\t\t\t<input type=\"text\" placeholder=\"Enter Username\" name=\"uname\" required>
-	 \t\t\t\t\t\t\t<label for=\"psw\"><b>Password</b></label>
-	 \t\t\t\t\t\t\t<input type=\"password\" placeholder=\"Enter Password\" name=\"psw\" required>
-	 \t\t\t\t\t\t\t<button type=\"submit\">Login</button>
-	 \t\t\t\t\t\t\t<label>
-	 \t\t\t\t\t\t\t\t<input type=\"checkbox\" checked=\"checked\" name=\"remember\"> Remember me
-	 \t\t\t\t\t\t\t</label>
-	 \t\t\t\t\t\t</div>
+     \t\t\t\t\t\t<div class=\"container\">
+     \t\t\t\t\t\t\t<label for=\"uname\"><b>Username</b></label>
+     \t\t\t\t\t\t\t<input type=\"text\" placeholder=\"Enter Username\" name=\"uname\" required>
+     \t\t\t\t\t\t\t<label for=\"psw\"><b>Password</b></label>
+     \t\t\t\t\t\t\t<input type=\"password\" placeholder=\"Enter Password\" name=\"psw\" required>
+     \t\t\t\t\t\t\t<button type=\"submit\">Login</button>
+     \t\t\t\t\t\t\t<label>
+     \t\t\t\t\t\t\t\t<input type=\"checkbox\" checked=\"checked\" name=\"remember\"> Remember me
+     \t\t\t\t\t\t\t</label>
+     \t\t\t\t\t\t</div>
      \t\t\t\t\t</div>
      \t\t\t\t</form>
      \t\t\t</div>
