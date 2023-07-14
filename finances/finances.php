d9a56a4fab94d2a80e04350dd9bfc44f764b4f6c
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-14 00:10:07 -0600

    Fix indentation.

--- finances/finances.php
+++ finances/finances.php
@@ -2,7 +2,8 @@
   include '../../db.inc';
   if (!isset($_POST['uname']) && !isset($_POST['psw']) && !isset($_SESSION["user"])) {
     $finances ->content .=
-    "\t\t\t<div>\t\t\t<h1>Finances</h1>
+    "\t\t\t<div>
+     \t\t\t<h1>Finances</h1>
      \t\t\t<button onclick=\"document.getElementById('loginform').style.display='block'\" class=\"widthauto\">Login</button>
      \t\t\t<div id=\"loginform\" class=\"modal\">
      \t\t\t<div id=\"loginform\" class=\"modal\">
