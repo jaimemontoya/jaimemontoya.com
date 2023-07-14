5a12b1e82bdc7e1c237dc53e7648290dc59622c0
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-14 00:06:05 -0600

    Add indentation.

--- finances/finances.php
+++ finances/finances.php
@@ -3,7 +3,7 @@
   if (!isset($_POST['uname']) && !isset($_POST['psw']) && !isset($_SESSION["user"])) {
     $finances ->content .=
     "\t\t\t<div>
-     \t\t\t<h1>Finances</h1>
+	 \t\t\t<h1>Finances</h1>
      \t\t\t<button onclick=\"document.getElementById('loginform').style.display='block'\" class=\"widthauto\">Login</button>
      \t\t\t<div id=\"loginform\" class=\"modal\">
      \t\t\t<div id=\"loginform\" class=\"modal\">
