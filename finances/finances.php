939cee09de6fad34e6338fe741083feb9f94d44b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-23 01:48:03 -0600

    Add form.

--- finances/finances.php
+++ finances/finances.php
@@ -52,7 +52,9 @@
       }
       $_SESSION["user"] = "jaimemontoya";
       $finances->content .=
-      "\t\t\t<h1>Finances</h1>\n";
+      "\t\t\t<h1>Finances</h1>\n
+      \t\t\t<form action=\""."index.php"."\" method=\"GET\">\n
+      \t\t\t</form>";
     } else {
       $finances->content .=
 	  "\t\t\t<a href=\"/finances\"><button class=\"widthauto\">Try again</button></a>
