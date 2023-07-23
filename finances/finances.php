77f4c213c9843c1c9cffc9390d50b25cf9dfd969
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-23 01:23:55 -0600

    Reduce width of button.

--- finances/finances.php
+++ finances/finances.php
@@ -55,7 +55,7 @@
       "\t\t\t<h1>Finances</h1>\n";
     } else {
       $finances->content .=
-	  "Username/Password Invalid. <a href=\"/finances\"><button>Try again</button></a>.";
+	  "Username/Password Invalid. <a href=\"/finances\"><button class=\"widthauto\">Try again</button></a>.";
     }
   } else {
     $finances->content .=
