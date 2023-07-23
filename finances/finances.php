c8d2f6f05c4d46a14b60dfbae18ba4a1f471161b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-23 02:48:14 -0600

    Use input instead of button.

--- finances/finances.php
+++ finances/finances.php
@@ -80,7 +80,7 @@
       }
 	  $finances->content .=
 	  "\t\t\t\t</div>
-      \t\t\t<div><button type=\"submit\" value=\"Submit\"></div>
+      \t\t\t<div><input type=\"submit\" value=\"Submit\"></div>
       \t\t</form>\n";
     } else {
       $finances->content .=
