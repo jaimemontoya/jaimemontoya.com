064265ed03651d45e08c6a5445c59afda985c769
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-24 13:28:04 -0600

    Have selection of report type in separate block.

--- finances/finances.php
+++ finances/finances.php
@@ -54,11 +54,13 @@
       $finances->content .=
       "\t\t\t<h1>Finances</h1>
       \t\t<form action=\""."index.php"."\" method=\"GET\">
+	  \t\t\t<div>
 	  \t\t\t<label for=\"reporttype\">Choose a report type:</label>
 	  \t\t\t<select id=\"reporttype\" name=\"reporttype\">
 	  \t\t\t\t<option value=\"None\">[Choose report type]</option>
 	  \t\t\t\t<option value=\"Expenses\">Expenses</option>
 	  \t\t\t\t<option value=\"Income\">Income</option>
+	  \t\t\t</div>
       \t\t\t<div>Date format 'Y-m-d', e.g. 2023-01-18</div>
       \t\t\t<div>Start date:</div>
 	  \t\t\t<div><input type=\"text\" name=\"startDateKey\" class=\"widthauto\"value=\"".$_GET['startDateKey']."\"></div>
