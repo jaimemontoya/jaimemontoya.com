f5b1f5782e03718801af55b771e1f8738f5d7d6c
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-24 14:37:50 -0600

    Debug code.

--- finances/finances.php
+++ finances/finances.php
@@ -162,10 +162,10 @@
 		$sumSales = $conn->query($sqlSumSales);		
 		$resultSales = $conn->query($sqlSales);
 		if ($resultSales->num_rows > 0) {
+		  $finances->content .="";
+		  //"\t\t\t<table><tr><th>Description</th><th>Date</th><th>City</th><th>Payment method</th><th>Buyer</th><th>Category</th><th>Total = ".$sumSales->fetch_assoc()[sumSales]."</th></tr>";
 		  $finances->content .=
-		  "<table><tr><th>Description</th><th>Date</th><th>City</th><th>Payment method</th><th>Buyer</th><th>Category</th><th>Total = ".$sumSales->fetch_assoc()[sumSales]."</th></tr>";
-		  $finances->content .=
-		  "</table>";
+		  "\t\t\t</table>";
 		} else {echo "Bye";
 		  //$finances->content .=
 		  //"0 results";
