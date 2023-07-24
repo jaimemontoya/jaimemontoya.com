11b0ee779fa128a0606f5d53fa610b11f470f35f
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-07-24 14:39:48 -0600

    Remove sum.

--- finances/finances.php
+++ finances/finances.php
@@ -162,7 +162,8 @@
 		$sumSales = $conn->query($sqlSumSales);		
 		$resultSales = $conn->query($sqlSales);
 		if ($resultSales->num_rows > 0) {
-		  $finances->content .="";
+		  $finances->content .=
+		  "\t\t\t<table><tr><th>Description</th><th>Date</th><th>City</th><th>Payment method</th><th>Buyer</th><th>Category</th><th>Total = </th></tr>";
 		  //"\t\t\t<table><tr><th>Description</th><th>Date</th><th>City</th><th>Payment method</th><th>Buyer</th><th>Category</th><th>Total = ".$sumSales->fetch_assoc()[sumSales]."</th></tr>";
 		  $finances->content .=
 		  "\t\t\t</table>";
