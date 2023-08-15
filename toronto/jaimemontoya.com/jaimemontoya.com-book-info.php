6521a57c6013835e54b055e6ce26e8aaa021dc50
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-08-15 10:51:00 -0600

    Link to jaimemontoya.com

--- toronto/jaimemontoya.com/jaimemontoya.com-book-info.php
+++ toronto/jaimemontoya.com/jaimemontoya.com-book-info.php
@@ -1,6 +1,6 @@
 <?php
   $index->content .= 
-           "\t\t\t<div class=\"book\">
+           "\t\t\t<a href=\"https://jaimemontoya.com\"><div class=\"book\">
 				<div class=\"book-thumbnail\">
 					<div class=\"book-cover\" id=\"jaimemontoya.com-img\"></div>
 				</div>
@@ -8,5 +8,5 @@
 		            <span>jaimemontoya.com</span>
 					<p>By Jaime Montoya. <mark>Website 1 hosted by Jaime Montoya (138.197.165.222, Toronto)</mark> from 1:05 PM January 5, 2018 GMT.</p>
                 </div>
-            </div>\n";
+            </div></a>\n";
 ?>
