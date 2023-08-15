a364eae4468483d296d3786d7f86d76fc31176db
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-08-15 10:54:25 -0600

    Remove periods from titles in home page.

--- fp-magazine-summer-2023/fp-magazine-summer-2023-book-info.php
+++ fp-magazine-summer-2023/fp-magazine-summer-2023-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"fp-magazine-summer-2023-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>Foreign Policy (magazine). Summer 2023.</span>
+		            <span>Foreign Policy (magazine). Summer 2023</span>
 					<p>By Graham Holdings Company. <mark>Magazine 2 read by Jaime Montoya</mark> from 8:00 PM July 11, 2023 to 2:53 PM August 3, 2023 GMT.</p>
                 </div>
             </div>\n";
--- time-magazine-2023-07-03/time-magazine-2023-07-03-book-info.php
+++ time-magazine-2023-07-03/time-magazine-2023-07-03-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"time-magazine-2023-07-03-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>Time (magazine). July 3, 2023.</span>
+		            <span>Time (magazine). July 3, 2023</span>
 					<p>By TIME USA, LLC. <mark>Magazine 1 read by Jaime Montoya</mark> from 12:45 PM June 24, 2023 to 7:34 PM July 11, 2023 GMT.</p>
                 </div>
             </div>\n";
