39dd60f6f0b59582b60dbe2b6bb86daf4ca14402
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-21 13:54:13 -0600

    Add server historical details.

--- blog/2023/09/21/18/59/book-info-20230921-1859.php
+++ blog/2023/09/21/18/59/book-info-20230921-1859.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>6:59 PM GMT · Sep 21, 2023</div><p>The source code of jaimemontoya.com is available at <a href='https://github.com/jaimemontoya/jaimemontoya.com/'>https://github.com/jaimemontoya/jaimemontoya.com/</a>. I am hosting the website on a DigitalOcean droplet located in Toronto, Canada. I created this server on Friday, January 5, 2018 at 7:05 AM CST.</p><hr>\n";
+           "\t\t\t<div>6:59 PM GMT · Sep 21, 2023</div><p>The source code of jaimemontoya.com is available at <a href='https://github.com/jaimemontoya/jaimemontoya.com/'>https://github.com/jaimemontoya/jaimemontoya.com/</a>. I am hosting the website on a DigitalOcean droplet located in Toronto, Canada. I created this server on Friday, January 5, 2018 at 7:05 AM CST, upgrading it to Ubuntu 22.04.3 LTS on August 17, 2023.</p><hr>\n";
 ?>
