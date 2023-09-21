801aca432293b5933d343d36fd1ad2075263aedf
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-21 13:35:42 -0600

    Reorganize files.

--- blog/index.php
+++ blog/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "Jaime Montoya's blog";
   $index->metaDescription = "Welcome to my blog, where you will find information about jaimemontoya.com and updates related to the content of this website.";
-  require("2023/09/21/18/59/blog-info-20230921-1859.php");
-  require("2023/09/21/18/09/blog-info-20230921-1809.php");
+  require("2023/09/21/18/59/book-info-20230921-1859.php");
+  require("2023/09/21/18/09/book-info-20230921-1809.php");
   $index->Display();
 ?>
