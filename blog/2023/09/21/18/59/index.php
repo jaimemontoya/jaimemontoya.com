17bbfd1806878cf8adaa6bef3c260bbc4184aaba
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-21 13:41:11 -0600

    Fix file structure for second blog post.

---
+++ blog/2023/09/21/18/59/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "6:59 PM GMT · Sep 21, 2023";
+  $index->metaDescription = "The source code of jaimemontoya.com is available at https://github.com/jaimemontoya/jaimemontoya.com/";
+  require("book-info-20230921-1859.php");
+  $index->Display();
+?>
