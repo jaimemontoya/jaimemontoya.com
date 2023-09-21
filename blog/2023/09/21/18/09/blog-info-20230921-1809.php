5674eefd663be3d13a6f3ef36d9b9782c3ffce05
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-21 12:13:30 -0600

    Move horizontal rule to the bottom.

--- blog/2023/09/21/18/09/blog-info-20230921-1809.php
+++ blog/2023/09/21/18/09/blog-info-20230921-1809.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div><hr>6:09 PM GMT · Sep 21, 2023</div><p>Hello, World!</p>\n";
+           "\t\t\t<div>6:09 PM GMT · Sep 21, 2023</div><p>Hello, World!</p><hr>\n";
 ?>
