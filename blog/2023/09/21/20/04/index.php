d4500e1d8e4a677c570b3375504ac9133656268d
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-21 14:03:34 -0600

    Fix date.

--- blog/2023/09/21/20/04/index.php
+++ blog/2023/09/21/20/04/index.php
@@ -1,8 +1,8 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "6:59 PM GMT · Sep 21, 2023";
+  $index->title = "8:04 PM GMT · Sep 21, 2023";
   $index->metaDescription = "The source code of jaimemontoya.com is available at https://github.com/jaimemontoya/jaimemontoya.com/";
-  require("book-info-20230921-1859.php");
+  require("book-info-20230921-2004.php");
   $index->Display();
 ?>
