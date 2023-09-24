73c349303b1b70e531e7fcaf14a25eb2c1b2a6c4
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-23 22:59:32 -0600

    Adding picture with Ruben de la Barrera.

--- blog/2023/09/24/05/00/book-info-20230924-0500.php
+++ blog/2023/09/24/05/00/book-info-20230924-0500.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>5:00 AM GMT · Sep 24, 2023</div><p>Head coach for the El Salvador national football team, Rub&eacute;n de la Barrera. Finding him by coincidence on September 23, 2023 GMT at Crowne Plaza San Salvador.</p><hr>\n";
+           "\t\t\t<div>5:00 AM GMT · Sep 24, 2023</div><p>Head coach for the El Salvador national football team, Rub&eacute;n de la Barrera. Finding him by coincidence on September 23, 2023 GMT at Crowne Plaza San Salvador.</p><p><img src=\"img/rubenDeLaBarrera.jpg\" alt=\"Rub&eacute;n de la Barrera\" width=\"512\" height=\"683\"></p><hr>\n";
 ?>
