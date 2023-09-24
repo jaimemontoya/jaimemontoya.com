02f1dc42d7b084244d81d88acc40b3fabf4ce99c
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-23 22:53:49 -0600

    Add information about encounter with head coach for El Salvador soccer team.

--- blog/2023/09/24/05/00/book-info-20230924-0500.php
+++ blog/2023/09/24/05/00/book-info-20230924-0500.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>6:09 PM GMT · Sep 21, 2023</div><p>Hello, World!</p><hr>\n";
+           "\t\t\t<div>5:00 AM GMT · Sep 24, 2023</div><p>Head coach for the El Salvador national football team, Rub&eacute;n de la Barrera. Finding him by coincidence on September 23, 2023 GMT at Crowne Plaza San Salvador.</p><hr>\n";
 ?>
