542b048e84dddfa00ef922837475835c9975e56d
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-23 22:46:11 -0600

    Add encounter with Ruben de la Barrera at Crowne Plaza San Salvador.

---
+++ blog/2023/09/24/05/00/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "5:00 AM GMT · Sep 24, 2023";
+  $index->metaDescription = "Head coach for the El Salvador national football team, Rub&eacute;n de la Barrera. Finding him by coincidence on September 23, 2023 GMT at Crowne Plaza San Salvador.";
+  require("book-info-20230924-0500.php");
+  $index->Display();
+?>
