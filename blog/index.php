36fd8afc86b89bdce415dffc48539998ec03c1ea
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-09-24 16:55:34 -0600

    Add Google AdSense entry to blog.

--- blog/index.php
+++ blog/index.php
@@ -3,6 +3,7 @@
   $index = new Page();
   $index->title = "Jaime Montoya's blog";
   $index->metaDescription = "Welcome to my blog, where you will find information about jaimemontoya.com and updates related to the content of this website.";
+  require("2023/09/24/22/57/book-info-20230924-2257.php");
   require("2023/09/24/05/00/book-info-20230924-0500.php");
   require("2023/09/21/20/04/book-info-20230921-2004.php");
   require("2023/09/21/18/09/book-info-20230921-1809.php");
