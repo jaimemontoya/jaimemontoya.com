8eefd11728b08a91dffbdd7924c89d661f0d78e6
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-04 00:14:41 -0600

    Add numbers to organize text.

--- american-prometheus/american-prometheus-summary-critique-review.php
+++ american-prometheus/american-prometheus-summary-critique-review.php
@@ -1,21 +1,21 @@
 <?php
   $index->content .= 
            "\t\t\t<div>
-                <div class=\"margintop15 underline bold\">INTRODUCING THE BOOK</div>
-                <div class=\"margintop15 bold\">Defining the general topic of the book</div>
+                <div class=\"margintop15 underline bold\">1. INTRODUCING THE BOOK</div>
+                <div class=\"margintop15 bold\">1.1. Defining the general topic of the book</div>
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 				<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <div class=\"margintop15 bold\">Informing about potential readership</div>
-                <div class=\"margintop15 bold\">Informing about the author</div>
-                <div class=\"margintop15 bold\">Making topic generalizations</div>
-                <div class=\"margintop15 bold\">Inserting book in the field</div>
-				<div class=\"margintop15 underline bold\">OUTLINING THE BOOK</div>
-				<div class=\"margintop15 bold\">Providing general view of the organization of the book</div>
-				<div class=\"margintop15 bold\">Stating the topic of each chapter</div>
-				<div class=\"margintop15 bold\">Citing extra-text material</div>
-				<div class=\"margintop15 underline bold\">HIGHLIGHTING PARTS OF THE BOOK</div>
-				<div class=\"margintop15 bold\">Providing focused evaluation</div>
-				<div class=\"margintop15 underline bold\">PROVIDING CLOSING EVALUATION OF THE BOOK</div>
-				<div class=\"margintop15 bold\">Definitely recommending/disqualifying the book or recommending the book despite indicated shortcomings</div>
+                <div class=\"margintop15 bold\">1.2. Informing about potential readership</div>
+                <div class=\"margintop15 bold\">1.3. Informing about the author</div>
+                <div class=\"margintop15 bold\">1.4. Making topic generalizations</div>
+                <div class=\"margintop15 bold\">1.5. Inserting book in the field</div>
+				<div class=\"margintop15 underline bold\">2. OUTLINING THE BOOK</div>
+				<div class=\"margintop15 bold\">2.1. Providing general view of the organization of the book</div>
+				<div class=\"margintop15 bold\">2.2. Stating the topic of each chapter</div>
+				<div class=\"margintop15 bold\">2.3. Citing extra-text material</div>
+				<div class=\"margintop15 underline bold\">3. HIGHLIGHTING PARTS OF THE BOOK</div>
+				<div class=\"margintop15 bold\">3.1. Providing focused evaluation</div>
+				<div class=\"margintop15 underline bold\">4. PROVIDING CLOSING EVALUATION OF THE BOOK</div>
+				<div class=\"margintop15 bold\">4.1. Definitely recommending/disqualifying the book or recommending the book despite indicated shortcomings</div>
             </div>\n";
 ?>
