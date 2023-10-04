bcf73845b90ef4895d35926eb2e1accd2b1c8e0c
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-03 23:39:10 -0600

    Fix spacing.

--- american-prometheus/american-prometheus-summary-critique-review.php
+++ american-prometheus/american-prometheus-summary-critique-review.php
@@ -2,18 +2,18 @@
   $index->content .= 
            "\t\t\t<div>
                 <div class=\"margintop15 underline bold\">INTRODUCING THE BOOK</div>
-                <div class=\"bold\">Defining the general topic of the book</div>
-                <div class=\"bold\">Informing about potential readership</div>
-                <div class=\"bold\">Informing about the author</div>
-                <div class=\"bold\">Making topic generalizations</div>
-                <div class=\"bold\">Inserting book in the field</div>
+                <div class=\"margintop15 bold\">Defining the general topic of the book</div>
+                <div class=\"margintop15 bold\">Informing about potential readership</div>
+                <div class=\"margintop15 bold\">Informing about the author</div>
+                <div class=\"margintop15 bold\">Making topic generalizations</div>
+                <div class=\"margintop15 bold\">Inserting book in the field</div>
 				<div class=\"margintop15 underline bold\">OUTLINING THE BOOK</div>
-				<div class=\"bold\">Providing general view of the organization of the book</div>
-				<div class=\"bold\">Stating the topic of each chapter</div>
-				<div class=\"bold\">Citing extra-text material</div>
+				<div class=\"margintop15 bold\">Providing general view of the organization of the book</div>
+				<div class=\"margintop15 bold\">Stating the topic of each chapter</div>
+				<div class=\"margintop15 bold\">Citing extra-text material</div>
 				<div class=\"margintop15 underline bold\">HIGHLIGHTING PARTS OF THE BOOK</div>
-				<div class=\"bold\">Providing focused evaluation</div>
+				<div class=\"margintop15 bold\">Providing focused evaluation</div>
 				<div class=\"margintop15 underline bold\">PROVIDING CLOSING EVALUATION OF THE BOOK</div>
-				<div class=\"bold\">Definitely recommending/disqualifying the book or recommending the book despite indicated shortcomings</div>
+				<div class=\"margintop15 bold\">Definitely recommending/disqualifying the book or recommending the book despite indicated shortcomings</div>
             </div>\n";
 ?>
