12b5b13e2f8f8e204f4f79399aba015696581b52
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-04 01:02:23 -0600

    Use bold.

--- page.php
+++ page.php
@@ -95,7 +95,7 @@ class Page
   }
   public function BookReviewsBibliography()
   {
-    return "\t\t<div class=\"margintop15\">* Book review template bibliography: \"<span>Motta-Roth, D. (1998), Discourse analysis and academic book reviews: A study of text and disciplinary cultures. In 1. Fartanet, S. Posteguillo, J.C. Palmer,</span> and J. F. Coli (Eds.), <span class=\"italic\">Genre studies in English for academic purposes</span> (pp. 29-58). Castello, Spain: Universitat Jaume I.\"</div>";
+    return "\t\t<div class=\"margintop15\">* Book review template bibliography: \"<span class=\"bold\">Motta-Roth, D. (1998), Discourse analysis and academic book reviews: A study of text and disciplinary cultures. In 1. Fartanet, S. Posteguillo, J.C. Palmer,</span> and J. F. Coli (Eds.), <span class=\"italic\">Genre studies in English for academic purposes</span> (pp. 29-58). Castello, Spain: Universitat Jaume I.\"</div>";
   }
   public function DisplayFooter()
   {
