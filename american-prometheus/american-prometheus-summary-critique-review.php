bf490863e69bb334c3ef8ccdfc877d843968efd5
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-04 01:42:55 -0600

    Add book topics.

--- american-prometheus/american-prometheus-summary-critique-review.php
+++ american-prometheus/american-prometheus-summary-critique-review.php
@@ -3,9 +3,14 @@
            "\t\t\t<div>
                 <div class=\"margintop15 underline bold\">1. INTRODUCING THE BOOK</div>
                 <div class=\"margintop15 bold\">1.1. Defining the general topic of the book</div>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-				<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<ul>
+				    <li>World War I, World War II and the Cold War history.
+					<li>The United States government supporting global anti-communism in the context of World War I, World War II and the Cold War.
+					<li>Biography of J. Robert Oppenheimer.
+					<li>The atomic bomb.
+				</ul>
                 <div class=\"margintop15 bold\">1.2. Informing about potential readership</div>
+				<p><span class=\"\">American Prometheus: The Inspiration for the Major Motion Picture OPPENHEIMER</span> by  Kai Bird and Martin J. Sherwin is a book for readers interested in World War II history and the political anti-communism battles in the United States. This is also an excellent biography of J. Robert Oppenheimer for people who want to know about his leadership to build the atomic bomb.</p>
                 <div class=\"margintop15 bold\">1.3. Informing about the author</div>
                 <div class=\"margintop15 bold\">1.4. Making topic generalizations</div>
                 <div class=\"margintop15 bold\">1.5. Inserting book in the field</div>
