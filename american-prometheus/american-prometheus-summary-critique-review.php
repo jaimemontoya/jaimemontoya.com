5c9f174e75f4ec1a5f809e767133657ba07d8e09
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-04 01:45:16 -0600

    Have book title in quotes.

--- american-prometheus/american-prometheus-summary-critique-review.php
+++ american-prometheus/american-prometheus-summary-critique-review.php
@@ -10,7 +10,7 @@
 					<li>The atomic bomb.
 				</ul>
                 <div class=\"margintop15 bold\">1.2. Informing about potential readership</div>
-				<p><span class=\"\">American Prometheus: The Inspiration for the Major Motion Picture OPPENHEIMER</span> by  Kai Bird and Martin J. Sherwin is a book for readers interested in World War II history and the political anti-communism battles in the United States. This is also an excellent biography of J. Robert Oppenheimer for people who want to know about his leadership to build the atomic bomb.</p>
+				<p><span class=\"\">\"American Prometheus: The Inspiration for the Major Motion Picture OPPENHEIMER</span>\" by  Kai Bird and Martin J. Sherwin is a book for readers interested in World War II history and the political anti-communism battles in the United States. This is also an excellent biography of J. Robert Oppenheimer for people who want to know about his leadership to build the atomic bomb.</p>
                 <div class=\"margintop15 bold\">1.3. Informing about the author</div>
                 <div class=\"margintop15 bold\">1.4. Making topic generalizations</div>
                 <div class=\"margintop15 bold\">1.5. Inserting book in the field</div>
