5bd4f72d2c75d48886b05c42efcdfc404d1a9b5a
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-04 00:39:22 -0600

    Concatenate content properly.

--- american-prometheus/american-prometheus-summary-critique-review.php
+++ american-prometheus/american-prometheus-summary-critique-review.php
@@ -17,5 +17,7 @@
 				<div class=\"margintop15 bold\">3.1. Providing focused evaluation</div>
 				<div class=\"margintop15 underline bold\">4. PROVIDING CLOSING EVALUATION OF THE BOOK</div>
 				<div class=\"margintop15 bold\">4.1. Definitely recommending/disqualifying the book or recommending the book despite indicated shortcomings</div>
-            </div>\n".$index->BookReviewsBibliography().$index->DisplayFooter();
+            </div>\n";
+	$index->content .= $index->BookReviewsBibliography();
+	$index->content .= $index->DisplayFooter();
 ?>
