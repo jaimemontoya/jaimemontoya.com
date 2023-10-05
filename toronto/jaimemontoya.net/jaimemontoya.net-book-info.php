8a7379fb4ae6366a3edbb60d4321236041ca1d5d
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-05 14:56:18 -0600

    Add website to portfolio.

---
+++ toronto/jaimemontoya.net/jaimemontoya.net-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"https://jaimemontoya.net\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"jaimemontoya.net-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>jaimemontoya.net</span>
+					<p>By Jaime Montoya. <mark>Website 2; designed, developed and hosted by Jaime Montoya</mark> (167.99.190.165, Toronto, since 4:19 PM October 2, 2023 GMT).</p>
+                </div>
+            </div></a>\n";
+?>
