64f13ebbc3f2041b13952f73b6a50261540c34c0
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-06 06:42:02 -0600

    Use Book review as part of the title of the page.

--- american-prometheus/index.php
+++ american-prometheus/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../page.php");
   $index = new Page();
-  $index->title = "American Prometheus: The Inspiration for the Major Motion Picture OPPENHEIMER";
+  $index->title = "American Prometheus: The Inspiration for the Major Motion Picture OPPENHEIMER - Book review";
   $index->metaDescription = "In this magisterial, acclaimed biography twenty-five years in the making, Kai Bird and Martin Sherwin capture Oppenheimer’s life and times, from his early career to his central role in the Cold War.";
   require("american-prometheus-book-info.php");
   require("american-prometheus-summary-critique-review.php");
