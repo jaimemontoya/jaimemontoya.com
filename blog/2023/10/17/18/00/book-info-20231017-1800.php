fb221f63e99aebb99696a92942d4026e2e61e988
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 10:59:08 -0600

    Add draft for new blog entry.

---
+++ blog/2023/10/17/18/00/book-info-20231017-1800.php
@@ -0,0 +1,4 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\"</p> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 on 2022-06-09 15:15:30:<hr>\n";
+?>
---
+++ blog/2023/10/17/18/00/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "6:00 PM GMT · Oct 17, 2023";
+  $index->metaDescription = "Hosting for unionchurchss.org, comparing InMotion Hosting versus DigitalOcean web hosting options.";
+  require("book-info-20231017-1800.php");
+  $index->Display();
+?>
