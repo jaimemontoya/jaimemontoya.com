8c0a1706154a115b58ed23904d4c0543f599f1ed
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 11:06:29 -0600

    Change date format.

--- blog/2023/10/17/18/00/book-info-20231017-1800.php
+++ blog/2023/10/17/18/00/book-info-20231017-1800.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 on 2022-06-09 15:15:30:</p><hr>\n";
+           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 on Jun 9, 2022:</p><hr>\n";
 ?>
