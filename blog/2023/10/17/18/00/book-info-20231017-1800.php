b974b9daa0532d18f63c0ddba8c48c3a0a8685e7
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 11:47:30 -0600

    Add question.

--- blog/2023/10/17/18/00/book-info-20231017-1800.php
+++ blog/2023/10/17/18/00/book-info-20231017-1800.php
@@ -1,5 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a> on InMotion Hosting has been a good experience, even though on Jun 9, 2023, they increased the price from $119.88 to $155.88 a year:</p>
-		   <img src=\"/blog/2023/10/17/18/00/img/inmotion-hosting-pricing.jpg\"><hr>\n";
+           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a> on InMotion Hosting has been a good experience, even though on Jun 9, 2023, they increased the price from $119.88 to $155.88 a year:</p><img src=\"/blog/2023/10/17/18/00/img/inmotion-hosting-pricing.jpg\"><p>Why not renting a virtual machine from a company such as DigitalOcean that specializes in cloud hosting for developers?</p><hr>\n";
 ?>
