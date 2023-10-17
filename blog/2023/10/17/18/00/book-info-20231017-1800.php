8c0e8973254e3c3eb38c7dbc85ed03b63e50f634
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 11:32:12 -0600

    Add pricing table.

--- blog/2023/10/17/18/00/book-info-20231017-1800.php
+++ blog/2023/10/17/18/00/book-info-20231017-1800.php
@@ -1,4 +1,5 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 a year on Jun 9, 2023:</p><hr>\n";
+           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 a year on Jun 9, 2023:</p>
+		   <img src=\"/blog/2023/10/17/18/00/img/inmotion-hosting-pricing.jpg\"><hr>\n";
 ?>
