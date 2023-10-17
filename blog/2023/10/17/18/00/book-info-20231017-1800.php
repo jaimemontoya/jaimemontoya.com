13199ac00c466f20bb6e0e46b49bb4055cb05c46
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 11:03:34 -0600

    Fixing link declaration.

--- blog/2023/10/17/18/00/book-info-20231017-1800.php
+++ blog/2023/10/17/18/00/book-info-20231017-1800.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\"</p> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 on 2022-06-09 15:15:30:<hr>\n";
+           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a></p> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 on 2022-06-09 15:15:30:<hr>\n";
 ?>
