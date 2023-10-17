bc4ef37a9fcccddb2e977ccab5def9534b0622e7
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 17:31:35 -0600

    Change date.

--- blog/2023/10/17/23/21/index.php
+++ blog/2023/10/17/23/21/index.php
@@ -1,8 +1,8 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "6:00 PM GMT · Oct 17, 2023";
+  $index->title = "11:21 PM GMT · Oct 17, 2023";
   $index->metaDescription = "Hosting for unionchurchss.org, comparing InMotion Hosting versus DigitalOcean web hosting options.";
-  require("book-info-20231017-1800.php");
+  require("book-info-20231017-2321.php");
   $index->Display();
 ?>
