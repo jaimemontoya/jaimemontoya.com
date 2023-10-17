9fc5c56df9be0d91f9757f37943356e90a29777c
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 11:08:48 -0600

    Use timeframe for price.

--- blog/2023/10/17/18/00/book-info-20231017-1800.php
+++ blog/2023/10/17/18/00/book-info-20231017-1800.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 on Jun 9, 2022:</p><hr>\n";
+           "\t\t\t<div>6:00 PM GMT · Oct 21, 2023</div><p>Hosting <a href=\"https://unionchurchss.org\">https://unionchurchss.org</a> on InMotion Hosting has been a good experience, even though they increased the price from $119.88 to $155.88 a year on Jun 9, 2022:</p><hr>\n";
 ?>
