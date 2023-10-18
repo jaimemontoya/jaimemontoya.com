417313edae2184eb63ea19ab814a68eb40901808
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 18:36:03 -0600

    Add comma.

--- portfolio/jaimemontoya.com/jaimemontoya.com-book-info.php
+++ portfolio/jaimemontoya.com/jaimemontoya.com-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>jaimemontoya.com</span>
-					<p>By Jaime Montoya. <mark>Website 1; created, designed, developed upgraded, maintained and hosted by Jaime Montoya</mark> (138.197.165.222, Toronto, since 1:05 PM January 5, 2018 GMT).</p>
+					<p>By Jaime Montoya. <mark>Website 1; created, designed, developed, upgraded, maintained and hosted by Jaime Montoya</mark> (138.197.165.222, Toronto, since 1:05 PM January 5, 2018 GMT).</p>
                 </div>
             </div></a>\n";
 ?>
