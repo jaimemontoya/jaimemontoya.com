b8e1122ff7b7a4ddd7acb4f16eab319fd8a9a255
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 18:40:43 -0600

    Change website numbers.

--- portfolio/index.php
+++ portfolio/index.php
@@ -3,8 +3,8 @@
   $index = new Page();
   $index->title = "Jaime Montoya's portfolio";
   $index->metaDescription = "Websites designed, developed and/or hosted by Jaime Montoya";
-  require("unionchurchss.org/unionchurchss.org-book-info.php");
   require("jaimemontoya.net/jaimemontoya.net-book-info.php");
+  require("unionchurchss.org/unionchurchss.org-book-info.php");
   require("jaimemontoya.com/jaimemontoya.com-book-info.php");
   $index->Display();
 ?>
