7a3247b9e3d5577bc0913319286b7424116d068b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-17 19:21:20 -0600

    Use word IP.

--- portfolio/jaimemontoya.net/jaimemontoya.net-book-info.php
+++ portfolio/jaimemontoya.net/jaimemontoya.net-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>jaimemontoya.net</span>
-					<p>By Jaime Montoya. <mark>Website 3; hosted by Jaime Montoya</mark> (DigitalOcean 143.198.36.10, Toronto, since 4:03 PM October 11, 2023 GMT).</p>
+					<p>By Jaime Montoya. <mark>Website 3; hosted by Jaime Montoya</mark> (DigitalOcean IP 143.198.36.10, Toronto, since 4:03 PM October 11, 2023 GMT).</p>
                 </div>
             </div></a>\n";
 ?>
