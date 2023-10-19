0d875b605ecdfbc312e961e9b5694847ca14c9f5
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-19 12:26:06 -0600

    Change page name from resume to about.

---
+++ about/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "About Jaime Montoya";
+  $index->metaDescription = "The latest version of my resume is always available from this page. It includes my skills, experience, education, standardized tests and certifications in a one-page size.";
+  require("about-info.php");
+  $index->Display();
+?>
