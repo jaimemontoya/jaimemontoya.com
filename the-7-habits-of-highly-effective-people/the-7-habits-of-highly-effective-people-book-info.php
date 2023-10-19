ee81aad6b42d94088f5977a19b0ad1910de89fa7
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-19 11:57:11 -0600

    Add book The 7 Habits of Highly Effective People.

---
+++ the-7-habits-of-highly-effective-people/the-7-habits-of-highly-effective-people-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"/the-7-habits-of-highly-effective-people/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"the-7-habits-of-highly-effective-people-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>The 7 Habits of Highly Effective People: Infographics Edition: Powerful Lessons in Personal Change</span>
+					<p>By Stephen R. Covey. <mark>Book 60 read by Jaime Montoya</mark> from 9:08 PM October 3, 2023 to 5:23 PM October 19, 2023 GMT.</p>
+                </div>
+            </div></a>\n";
+?>
