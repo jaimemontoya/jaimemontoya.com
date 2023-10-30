0ee464d0985d88b564c7b0e9311cf3ca80b7035a
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-30 15:56:50 -0600

    Specify how to enable module for PHP 8.2.

--- blog/2023/10/30/22/15/book-info-20231030-2215.php
+++ blog/2023/10/30/22/15/book-info-20231030-2215.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>10:15 PM GMT · Oct 30, 2023</div><p>Upgrading PHP from 8.0 to 8.2 on Apache and Ubuntu was easier than I thought, achieved with just five commands:</p><ol><li>apt-get update<li>apt-get install php8.2 php8.2-cli php8.2-common php8.2-mysql php8.2-soap php8.2-bcmath php8.2-xml php8.2-mbstring php8.2-gd php8.2-curl<li>a2dismod php8.2<li>php8.2<li>systemctl restart apache2</ol><hr>\n";
+           "\t\t\t<div>10:15 PM GMT · Oct 30, 2023</div><p>Upgrading PHP from 8.0 to 8.2 on Apache and Ubuntu was easier than I thought, achieved with just five commands:</p><ol><li>apt-get update<li>apt-get install php8.2 php8.2-cli php8.2-common php8.2-mysql php8.2-soap php8.2-bcmath php8.2-xml php8.2-mbstring php8.2-gd php8.2-curl<li>a2dismod php8.2<li>a2enmod php8.2<li>systemctl restart apache2</ol><hr>\n";
 ?>
