37dad56c2c46068dbf222132dabe76a4fb3cbe79
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-10-30 16:04:06 -0600

    Update time of when latest blog post was published.

--- blog/2023/10/30/21/58/index.php
+++ blog/2023/10/30/21/58/index.php
@@ -1,8 +1,8 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "10:15 PM GMT · Oct 30, 2023";
+  $index->title = "9:58 PM GMT · Oct 30, 2023";
   $index->metaDescription = "Upgrading PHP from 8.0 to 8.2 on Apache and Ubuntu.";
-  require("book-info-20231030-2215.php");
+  require("book-info-20231030-2158.php");
   $index->Display();
 ?>
