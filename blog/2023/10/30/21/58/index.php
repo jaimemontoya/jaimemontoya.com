fbec99f293c3571b8cc938e49734495be7eab7b3
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-01 16:40:46 -0600

    Add parent page to blog post.

--- blog/2023/10/30/21/58/index.php
+++ blog/2023/10/30/21/58/index.php
@@ -2,6 +2,7 @@
   require("../../../../../../page.php");
   $index = new Page();
   $index->title = "9:58 PM GMT · Oct 30, 2023";
+  $index->parentPage = "Portfolio";
   $index->metaDescription = "Upgrading PHP from 8.0 to 8.2 on Apache and Ubuntu.";
   require("book-info-20231030-2158.php");
   $index->Display();
