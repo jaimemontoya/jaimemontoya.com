8d57a928e557939b0df97a6935b00912cdb57fef
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-01 22:34:00 -0600

    Add breadcrumb element to Blog entries.

--- blog/2023/09/21/20/04/index.php
+++ blog/2023/09/21/20/04/index.php
@@ -2,6 +2,7 @@
   require("../../../../../../page.php");
   $index = new Page();
   $index->title = "8:04 PM GMT · Sep 21, 2023";
+  $index->parentPage = "Blog";
   $index->metaDescription = "The source code of jaimemontoya.com is available at https://github.com/jaimemontoya/jaimemontoya.com/";
   require("book-info-20230921-2004.php");
   $index->Display();
--- blog/2023/09/24/05/00/index.php
+++ blog/2023/09/24/05/00/index.php
@@ -2,6 +2,7 @@
   require("../../../../../../page.php");
   $index = new Page();
   $index->title = "5:00 AM GMT · Sep 24, 2023";
+  $index->parentPage = "Blog";
   $index->metaDescription = "Head coach for the El Salvador national football team, Rub&eacute;n de la Barrera.";
   require("book-info-20230924-0500.php");
   $index->Display();
