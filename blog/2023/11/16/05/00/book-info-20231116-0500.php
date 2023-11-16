ffdb8bca1574697196217f1110f03cba2ecba4ca
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-16 14:19:28 -0600

    Add images to blog post.

--- blog/2023/11/16/05/00/book-info-20231116-0500.php
+++ blog/2023/11/16/05/00/book-info-20231116-0500.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>5:00 AM GMT · Nov 16, 2023</div><p>I needed to have two websites on the same server: one at jaimemontoya.net and the other at sponsorship.jaimemontoya.net. My first step was to create A and CNAME records for sponsorship.jaimemontoya.net and www.sponsorship.jaimemontoya.net, respectively.</p><hr>\n";
+           "\t\t\t<div>5:00 AM GMT · Nov 16, 2023</div><p>I needed to have two websites on the same server: one at jaimemontoya.net and the other at sponsorship.jaimemontoya.net. My first step was to create A and CNAME records for sponsorship.jaimemontoya.net and www.sponsorship.jaimemontoya.net, respectively.</p><div><img src=\"/blog/2023/11/16/05/00/img/createARecord.png\" alt=\"Create A Record\" width=\"1189\" height=\"776\"></div><div><img src=\"/blog/2023/11/16/05/00/img/createCNAMERecord.png\" alt=\"Create CNAME Record\" width=\"1087\" height=\"317\"></div><div><img src=\"/blog/2023/11/16/05/00/img/AAndCNAMERecordsCreatedSuccessfully.png\" alt=\"A and CNAME Records created successfully\" width=\"1186\" height=\"531\"></div><hr>\n";
 ?>
