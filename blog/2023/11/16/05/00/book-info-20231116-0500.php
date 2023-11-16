275627b585cb8dd90dec7f78efd2170702acf9d8
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-16 10:18:13 -0600

    Remove Stack Overflow, LinkedIn and Strava from main navigation menu.

--- blog/2023/11/16/05/00/book-info-20231116-0500.php
+++ blog/2023/11/16/05/00/book-info-20231116-0500.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>5:00 AM GMT · Nov 16, 2023</div><p>I needed to have two websites on the same server. One at jaimemontoya.net and the other one at sponsorship.jaimemontoya.net.</p><hr>\n";
+           "\t\t\t<div>5:00 AM GMT · Nov 16, 2023</div><p>I needed to have two websites on the same server: one at jaimemontoya.net and the other at sponsorship.jaimemontoya.net. My first step was to create A and CNAME records for sponsorship.jaimemontoya.net and www.sponsorship.jaimemontoya.net, respectively.</p><hr>\n";
 ?>
--- stack-overflow/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require("../page.php");
-  $index = new Page();
-  $index->title = "Stack Overflow";
-  $index->metaDescription = "Stack Overflow profile for Jaime Montoya.";
-  require("stack-overflow-info.php");
-  $index->Display();
-?>
