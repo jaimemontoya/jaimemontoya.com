a27010ce0dab6d82452d35e8c2e80f18161fb996
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-15 19:00:16 -0600

    Add new blog entry.

---
+++ blog/2023/11/16/05/00/book-info-20231116-0500.php
@@ -0,0 +1,4 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div>5:00 AM GMT · Nov 16, 2023</div><p>I needed to have two websites on the same server. One at jaimemontoya.net and the other one at sponsorship.jaimemontoya.net.</p><hr>\n";
+?>
