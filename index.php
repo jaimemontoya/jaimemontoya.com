d65ad51b5728cfed6e4af04de1e0171772694681
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-17 09:11:57 -0600

    Remove book.

--- index.php
+++ index.php
@@ -54,7 +54,6 @@
   require("wordpress-5-complete/wordpress-5-complete-book-info.php");
   require("the-power-of-habit/the-power-of-habit-book-info.php");
   require("niv-gospel-john-devotional-notes/niv-gospel-john-devotional-notes-book-info.php");
-  require("the-lambs-supper-the-mass-as-heaven-on-earth/the-lambs-supper-the-mass-as-heaven-on-earth-book-info.php");
   require("the-logic-of-god/the-logic-of-god-book-info.php");
   require("ab-testing-the/ab-testing-the-book-info.php");
   require("personal-branding-for/personal-branding-for-book-info.php");
