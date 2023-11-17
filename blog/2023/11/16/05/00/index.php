300445b0a17b88f2be699b7586166ee5d753148d
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-17 07:16:30 -0600

    Change date for blog post.

--- blog/2023/11/16/05/00/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require("../../../../../../page.php");
-  $index = new Page();
-  $index->title = "5:00 AM GMT · Nov 16, 2023";
-  $index->parentPage = "Blog";
-  $index->metaDescription = "Hosting a site on a subdomain by using VirtualHost from Apache.";
-  require("book-info-20231116-0500.php");
-  $index->Display();
-?>
