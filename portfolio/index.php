8cfc5b04689dd6014e702f76665fc3ad6d8f5c0e
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-22 04:07:22 -0600

    Add mfh-elsalvador.org to Portfolio and remove jaimemontoya.net from it.

--- portfolio/index.php
+++ portfolio/index.php
@@ -2,8 +2,8 @@
   require("../page.php");
   $index = new Page();
   $index->title = "Portfolio";
-  $index->metaDescription = "Websites designed, developed and/or hosted by Jaime Montoya";
-  require("jaimemontoya.net/jaimemontoya.net-book-info.php");
+  $index->metaDescription = "Websites created, designed, developed, upgraded, maintained and/or hosted by Jaime Montoya";
+  require("mfh-elsalvador.org/mfh-elsalvador.org-book-info.php");
   require("unionchurchss.org/unionchurchss.org-book-info.php");
   require("jaimemontoya.com/jaimemontoya.com-book-info.php");
   $index->Display();
--- portfolio/mfh-elsalvador.org/mfh-elsalvador.org-book-info.php
+++ portfolio/mfh-elsalvador.org/mfh-elsalvador.org-book-info.php
@@ -1,11 +1,11 @@
 <?php
   $index->content .= 
-           "\t\t\t<a href=\"https://jaimemontoya.net\"><div class=\"book\">
+           "\t\t\t<a href=\"https://mfh-elsalvador.org\"><div class=\"book\">
 				<div class=\"book-thumbnail\">
-					<div class=\"book-cover\" id=\"jaimemontoya.net-img\"></div>
+					<div class=\"book-cover\" id=\"mfh-elsalvador.org-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>jaimemontoya.net</span>
+		            <span>mfh-elsalvador.org</span>
 					<p>By Jaime Montoya. <mark>Website 3; hosted by Jaime Montoya</mark> (DigitalOcean IP 143.198.36.10; dedicated server; Toronto, Ontario, Canada; since 4:03 PM October 11, 2023 GMT).</p>
                 </div>
             </div></a>\n";
