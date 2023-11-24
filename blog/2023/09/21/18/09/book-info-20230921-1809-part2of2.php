a04d457678c52af1cb4ffb316f50c7a7f87d568b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 10:18:03 -0600

    Fix files required.

--- blog/2023/09/21/18/09/book-info-20230921-1809-part2of2.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $index->content .= 
-           "\t\t\t<p>Hello, World!</p><div>Published: 6:09 PM GMT · Sep 21, 2023</div><hr>\n";
-?>
--- blog/2023/09/21/18/09/index.php
+++ blog/2023/09/21/18/09/index.php
@@ -4,11 +4,6 @@
   $index->title = "My first blog entry";
   $index->parentPage = "Blog";
   $index->metaDescription = "Hello, World!";
-  require("book-info-20230921-1809-part1of2.php");
-  $index->content .= $index->title;
-  require("book-info-20230921-1809-part2of2.php");
-
-  
-  
+  require("book-info-20230921-1809.php");
   $index->Display();
 ?>
