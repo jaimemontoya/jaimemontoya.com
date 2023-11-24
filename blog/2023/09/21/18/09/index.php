17a181ea7791156d3bbf935736383ec06ef994a0
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 10:26:32 -0600

    Use h1.

--- blog/2023/09/21/18/09/index.php
+++ blog/2023/09/21/18/09/index.php
@@ -4,7 +4,7 @@
   $index->title = "My first blog entry";
   $index->parentPage = "Blog";
   $index->metaDescription = "Hello, World!";
-  $index->content .= "\t\t\t".$index->title;
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20230921-1809.php");
   $index->Display();
 ?>
