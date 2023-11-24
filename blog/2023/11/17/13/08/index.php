35a1b55394e379f56adf3734fef51ba997c044dc
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 13:55:41 -0600

    Reconfigured blog.

--- blog/2023/11/17/13/08/index.php
+++ blog/2023/11/17/13/08/index.php
@@ -1,9 +1,10 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "1:08 PM GMT · Nov 17, 2023";
+  $index->title = "Apache Virtual Hosts to have two websites on the same server. Configuring subdomain.";
   $index->parentPage = "Blog";
-  $index->metaDescription = "Hosting a site on a subdomain using Apache VirtualHost.";
+  $index->metaDescription = "Upgrading PHP from 8.0 to 8.2 on Apache and Ubuntu with five commands.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20231117-1308.php");
   $index->Display();
 ?>
