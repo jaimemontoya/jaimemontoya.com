94b0cffc5b067d9726b88baed4feeaeb3d19f681
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 13:25:12 -0600

    List post differently.

--- blog/index.php
+++ blog/index.php
@@ -6,7 +6,7 @@
   require("2023/11/21/02/20/book-info-20231121-0220.php");
   require("2023/11/17/13/08/book-info-20231117-1308.php");
   require("2023/10/30/21/58/book-info-20231030-2158.php");
-  require("2023/10/17/23/21/book-info-20231017-2321.php");
+  require("2023/10/17/23/21/20231017-2321-book-info.php");
   require("2023/09/24/22/57/20230924-2257-book-info.php");
   require("2023/09/21/20/04/20230921-2004-book-info.php");
   require("2023/09/21/18/09/20230921-1809-book-info.php");
