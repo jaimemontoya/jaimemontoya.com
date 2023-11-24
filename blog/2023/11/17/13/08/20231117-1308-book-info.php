eb9246a191cf6bc7ffb65f20326a3e024ea5132c
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 14:01:51 -0600

    Change title.

--- blog/2023/11/17/13/08/20231117-1308-book-info.php
+++ blog/2023/11/17/13/08/20231117-1308-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"blog-20231117-1308-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>Apache Virtual Hosts to have two websites on the same server. Configuring subdomain.</span>
+		            <span>Configure Apache Virtual Hosts for multiple websites on the same server. Configuring subdomain.</span>
 					<p>Hosting multiple websites on the same server using Apache Virtual Hosts. Configuring subdomains to host different websites stored on separate folders.<mark>Published: 1:08 PM GMT · Nov 17, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
--- blog/2023/11/17/13/08/index.php
+++ blog/2023/11/17/13/08/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Apache Virtual Hosts to have two websites on the same server. Configuring subdomain.";
+  $index->title = "Configure Apache Virtual Hosts for multiple websites on the same server. Configuring subdomain.";
   $index->parentPage = "Blog";
   $index->metaDescription = "Upgrading PHP from 8.0 to 8.2 on Apache and Ubuntu with five commands.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
