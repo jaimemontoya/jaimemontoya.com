019c6f0301e71bcde4da2dad887c98b70267af54
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 10:57:28 -0600

    Describe blog entry.

--- blog/2023/09/21/18/09/20230921-1809-book-info.php
+++ blog/2023/09/21/18/09/20230921-1809-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>My first blog entry</span>
-					<p>By Jaime Montoya. <mark>Website 1; created, designed, developed, upgraded, maintained and hosted by Jaime Montoya</mark> (DigitalOcean IP 138.197.165.222; 1 other site hosted on this server; Toronto, Ontario, Canada; since 1:05 PM January 5, 2018 GMT).</p>
+					<p>Instead of social media, I decided to create a blog from scratch, not only in terms of content, but also in how I built it from a technical software development standpoint. I started with a \"Hello, World!\" program program.</p>
                 </div>
             </div></a>\n";
 ?>
--- blog/2023/09/21/18/09/index.php
+++ blog/2023/09/21/18/09/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "My first blog entry";
   $index->parentPage = "Blog";
-  $index->metaDescription = "Hello, World!";
+  $index->metaDescription = "Instead of social media, I decided to create a blog from scratch, not only in terms of content, but also in how I built it from a technical software development standpoint. I started with a \"Hello, World!\" program program.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20230921-1809.php");
   $index->Display();
