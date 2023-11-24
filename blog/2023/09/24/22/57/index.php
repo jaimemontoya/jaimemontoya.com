72dd2b8cb611cacc5de4f1b65bd60a01a0340fd4
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 13:04:36 -0600

    Add h1.

--- blog/2023/09/24/22/57/index.php
+++ blog/2023/09/24/22/57/index.php
@@ -4,6 +4,7 @@
   $index->title = "Using Google AdSense again";
   $index->parentPage = "Blog";
   $index->metaDescription = "After a few years without ads on my website, I am running Google AdSense again.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20230924-2257.php");
   $index->Display();
 ?>
