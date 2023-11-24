c40acbb44ebb94d64209e7765f35dcc7d928176f
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 09:46:43 -0600

    Fix how variable is invoked.

--- blog/2023/09/21/18/09/index.php
+++ blog/2023/09/21/18/09/index.php
@@ -17,7 +17,7 @@
 		            <span>jaimemontoya.com</span>
 					<p>By Jaime Montoya. <mark>Website 1; created, designed, developed, upgraded, maintained and hosted by Jaime Montoya</mark> (DigitalOcean IP 138.197.165.222; 1 other site hosted on this server; Toronto, Ontario, Canada; since 1:05 PM January 5, 2018 GMT).</p>
                 </div>
-            </div></a>\n"."Start".$this->title."End";
+            </div></a>\n"."Start".$index->title."End";
   
   
   $index->Display();
