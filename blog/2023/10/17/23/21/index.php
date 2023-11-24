2e8ea426624e4efa6f0296aa70210f9b974bca8b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 13:26:28 -0600

    Add title.

--- blog/2023/10/17/23/21/index.php
+++ blog/2023/10/17/23/21/index.php
@@ -4,6 +4,7 @@
   $index->title = "DigitalOcean vs. InMotion Hosting. IaaS vs. PaaS. Web hosting pricing comparison.";
   $index->parentPage = "Blog";
   $index->metaDescription = "Pros and cons of hosting a website on DigitalOcean vs. InMotion Hosting. Compare advantages and disadvantages of IaaS vs. PaaS when hosting websites. Evaluate pricing structures.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20231017-2321.php");
   $index->Display();
 ?>
