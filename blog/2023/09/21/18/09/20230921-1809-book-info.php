aaddcd12eee352b2061506453fbcc518da59667e
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 10:42:11 -0600

    Use title for blog.

--- blog/2023/09/21/18/09/20230921-1809-book-info.php
+++ blog/2023/09/21/18/09/20230921-1809-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"blog-20230921-1809-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>".$index->title."jaimemontoya.com</span>
+		            <span>My first blog entry</span>
 					<p>By Jaime Montoya. <mark>Website 1; created, designed, developed, upgraded, maintained and hosted by Jaime Montoya</mark> (DigitalOcean IP 138.197.165.222; 1 other site hosted on this server; Toronto, Ontario, Canada; since 1:05 PM January 5, 2018 GMT).</p>
                 </div>
             </div></a>\n";
