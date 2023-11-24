8d672b4b023ffc22e7e6beee415c9a2708d1898b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 12:50:19 -0600

    Remove link and use GitHub text instead.

--- blog/2023/09/21/20/04/20230921-2004-book-info.php
+++ blog/2023/09/21/20/04/20230921-2004-book-info.php
@@ -5,8 +5,8 @@
 					<div class=\"book-cover\" id=\"no-photo-available-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>jaimemontoya.com full source code at https://github.com/jaimemontoya/jaimemontoya.com</span>
-					<p>The full source code of jaimemontoya.com is available at https://github.com/jaimemontoya/jaimemontoya.com/. <mark>Published: 8:04 PM GMT · Sep 21, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+		            <span>jaimemontoya.com full source code available on GitHub</span>
+					<p>Feel free to find, download and/or use the source code of jaimemontoya.com, available on GitHub. <mark>Published: 8:04 PM GMT · Sep 21, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
             </div></a>\n";
--- blog/2023/09/21/20/04/index.php
+++ blog/2023/09/21/20/04/index.php
@@ -1,9 +1,9 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "jaimemontoya.com full source code at https://github.com/jaimemontoya/jaimemontoya.com";
+  $index->title = "jaimemontoya.com full source code available on GitHub";
   $index->parentPage = "Blog";
-  $index->metaDescription = "The full source code of jaimemontoya.com is available at https://github.com/jaimemontoya/jaimemontoya.com/.";
+  $index->metaDescription = "Feel free to find, download and/or use the source code of jaimemontoya.com, available on GitHub.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20230921-2004.php");
   $index->Display();
