041db37a869cf78cba739758a6653b924eba549d
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 11:58:49 -0600

    Use icon for link.

--- blog/2023/09/21/18/09/20230921-1809-book-info.php
+++ blog/2023/09/21/18/09/20230921-1809-book-info.php
@@ -6,7 +6,8 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>My first blog entry</span>
-					<p>Instead of using social media, I decided to create a blog from scratch, not only in terms of content, but also in how I built it from a software development standpoint. I started with a \"Hello, World!\" program. <mark>Published: 6:09 PM GMT · Sep 21, 2023</mark> <span>Read more</span> <span class=\"read-more-icon\"></span></p>
+					<p>Instead of using social media, I decided to create a blog from scratch, not only in terms of content, but also in how I built it from a software development standpoint. I started with a \"Hello, World!\" program. <mark>Published: 6:09 PM GMT · Sep 21, 2023</mark> <span>Read more</span> <i class=\"fa fa-link\" aria-hidden=\"true\"></i>
+</p>
                 </div>
             </div></a>\n";
 ?>
