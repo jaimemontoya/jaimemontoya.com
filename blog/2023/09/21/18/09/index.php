cd3038e48fe7e84d1639b37554e4c1d6c43c14c5
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 09:54:20 -0600

    Try passing parameters to require().

--- blog/2023/09/21/18/09/index.php
+++ blog/2023/09/21/18/09/index.php
@@ -4,7 +4,7 @@
   $index->title = "First entry of my blog";
   $index->parentPage = "Blog";
   $index->metaDescription = "Hello, World!";
-  require("book-info-20230921-1809.php");
+  require("book-info-20230921-1809.php")(['some', 'data']);
   
   
   
