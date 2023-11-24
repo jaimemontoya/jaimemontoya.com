7670ce73403f9f495f6521140330e180569de64b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-24 10:25:40 -0600

    Concatenate value.

--- blog/2023/09/21/18/09/book-info-20230921-1809.php
+++ blog/2023/09/21/18/09/book-info-20230921-1809.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<p>Hello, World!</p><div>Published: 6:09 PM GMT · Sep 21, 2023</div><hr>\n";
+           "<p>Hello, World!</p><div>Published: 6:09 PM GMT · Sep 21, 2023</div><hr>\n";
 ?>
