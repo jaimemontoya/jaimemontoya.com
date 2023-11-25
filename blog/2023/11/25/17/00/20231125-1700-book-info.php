fb8dffa6b27637c50161b0f1ea9c4ee2178c1e87
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-25 11:14:30 -0600

    Add blog entry.

--- blog/2023/11/25/17/00/20231125-1700-book-info.php
+++ blog/2023/11/25/17/00/20231125-1700-book-info.php
@@ -1,12 +1,12 @@
 <?php
   $index->content .= 
-           "\t\t\t<a href=\"2023/11/25/03/19/\"><div class=\"book\">
+           "\t\t\t<a href=\"2023/11/25/17/00/\"><div class=\"book\">
 				<div class=\"book-thumbnail\">
-					<div class=\"book-cover\" id=\"no-photo-available-img\"></div>
+					<div class=\"book-cover\" id=\"blog-20231125-1700-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>Happy launch of https://mfh-elsalvador.org!</span>
-					<p>Happy launch of the redesigned version of https://mfh-elsalvador.org! <mark>Published: 3:19 AM GMT · Nov 25, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+		            <span>Migrating email from InMotion Hosting to Namecheap</span>
+					<p>A Black Friday Sale was the trigger for me to migrate email from InMotion Hosting to Namecheap. <mark>Published: 5:00 PM GMT · Nov 25, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
             </div></a>\n";
--- blog/2023/11/25/17/00/index.php
+++ blog/2023/11/25/17/00/index.php
@@ -1,9 +1,9 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Happy launch of <a href=\"https://mfh-elsalvador.org\">https://mfh-elsalvador.org</a>!";
+  $index->title = "Migrating email from InMotion Hosting to Namecheap";
   $index->parentPage = "Blog";
-  $index->metaDescription = "Happy launch of the redesigned version of https://mfh-elsalvador.org!";
+  $index->metaDescription = "A Black Friday Sale was the trigger for me to migrate email from InMotion Hosting to Namecheap.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20231125-1700.php");
   $index->Display();
