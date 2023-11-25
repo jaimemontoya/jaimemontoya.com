cc5f4a11ab37d1b9be282b27e03a43a5d0bb6370
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-25 08:23:22 -0600

    Fix file invokation.

--- blog/2023/11/25/17/00/index.php
+++ blog/2023/11/25/17/00/index.php
@@ -5,6 +5,6 @@
   $index->parentPage = "Blog";
   $index->metaDescription = "Happy launch of the redesigned version of https://mfh-elsalvador.org!";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20231125-0319.php");
+  require("book-info-20231125-1700.php");
   $index->Display();
 ?>
