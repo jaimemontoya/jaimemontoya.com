d5c6e7434bec028d2beec558e495cfd1aaece852
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-25 11:44:08 -0600

    Change title of post.

--- blog/2023/11/25/17/00/index.php
+++ blog/2023/11/25/17/00/index.php
@@ -1,9 +1,9 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Migrating email from InMotion Hosting to Namecheap";
+  $index->title = "Changing business email service provider from InMotion Hosting to Namecheap";
   $index->parentPage = "Blog";
-  $index->metaDescription = "A Black Friday Sale was the trigger for me to migrate email from InMotion Hosting to Namecheap.";
+  $index->metaDescription = "A Black Friday Sale was the trigger for me to change business email service provider from InMotion Hosting to Namecheap.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20231125-1700.php");
   $index->Display();
