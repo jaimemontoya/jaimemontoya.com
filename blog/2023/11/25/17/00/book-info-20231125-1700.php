e1ef097c4251cf9e1ed45aa709f110f6a9d19608
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-25 08:25:48 -0600

    Fix image URL.

--- blog/2023/11/25/17/00/book-info-20231125-1700.php
+++ blog/2023/11/25/17/00/book-info-20231125-1700.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "<p>Today I received an email from <span class=\"cod\">Namecheap &lt;hello@namecheap.com&gt;</span> with the following subject line:\"Black Friday Sale — Get up to 99% off\". From their list of deals, I was curious about this one:</p><div><img src=\"/blog/2023/11/17/13/08/img/nameCheapProfessionalEmailBlackFridayDeal.jpg\" alt=\"Namecheap Professional Email Black Friday deal\" width=\"217\" height=\"279\"></div><div>Published: 5:00 PM GMT · Nov 25, 2023</div>\n";
+           "<p>Today I received an email from <span class=\"cod\">Namecheap &lt;hello@namecheap.com&gt;</span> with the following subject line:\"Black Friday Sale — Get up to 99% off\". From their list of deals, I was curious about this one:</p><div><img src=\"/blog/2023/11/25/17/00/img/nameCheapProfessionalEmailBlackFridayDeal.jpg\" alt=\"Namecheap Professional Email Black Friday deal\" width=\"217\" height=\"279\"></div><div>Published: 5:00 PM GMT · Nov 25, 2023</div>\n";
 ?>
