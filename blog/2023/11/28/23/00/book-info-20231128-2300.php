7ae493584b24c3e8a0d10032c2a0f40f0d9fabf0
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-28 10:48:02 -0600

    Mention that I decided to change domain name registrar.

--- blog/2023/11/28/23/00/book-info-20231128-2300.php
+++ blog/2023/11/28/23/00/book-info-20231128-2300.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "<p>According to <a href=\"https://getreadyletsgrow.com/turbify-url-changes/\">https://getreadyletsgrow.com/turbify-url-changes/</a>, \"On June 10, 2022, Verizon's Small Business Essentials (formerly Yahoo Small Business) was acquired by Infinite Computer Solutions. It is being rebranded as Turbify.\"</p> <div>Published: 11:00 PM GMT · Nov 28, 2023</div>\n";
+           "<p>According to <a href=\"https://getreadyletsgrow.com/turbify-url-changes/\">https://getreadyletsgrow.com/turbify-url-changes/</a>, \"On June 10, 2022, Verizon's Small Business Essentials (formerly Yahoo Small Business) was acquired by Infinite Computer Solutions. It is being rebranded as Turbify.\" I decided to change domain name registrar from Turbify to Namecheap.</p> <div>Published: 11:00 PM GMT · Nov 28, 2023</div>\n";
 ?>
