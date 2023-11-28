1db2a9bd1826315135a0f033aba3aabb90f62d4f
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-28 10:44:34 -0600

    Add blog entry.

---
+++ blog/2023/11/28/23/00/book-info-20231128-2300.php
@@ -0,0 +1,4 @@
+<?php
+  $index->content .= 
+           "<p>According to <a href=\"https://getreadyletsgrow.com/turbify-url-changes/\">https://getreadyletsgrow.com/turbify-url-changes/</a>, \"On June 10, 2022, Verizon's Small Business Essentials (formerly Yahoo Small Business) was acquired by Infinite Computer Solutions. It is being rebranded as Turbify.\"</p> <div>Published: 11:00 PM GMT · Nov 28, 2023</div>\n";
+?>
