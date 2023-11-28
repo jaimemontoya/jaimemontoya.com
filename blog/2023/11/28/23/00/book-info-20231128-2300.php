b8ef1167bb45d57b1dcad2383ae0e5c24cff89b2
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-28 10:52:43 -0600

    Show current DNS Records from Turbify.

--- blog/2023/11/28/23/00/book-info-20231128-2300.php
+++ blog/2023/11/28/23/00/book-info-20231128-2300.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "<p>According to <a href=\"https://getreadyletsgrow.com/turbify-url-changes/\">https://getreadyletsgrow.com/turbify-url-changes/</a>, \"On June 10, 2022, Verizon's Small Business Essentials (formerly Yahoo Small Business) was acquired by Infinite Computer Solutions. It is being rebranded as Turbify.\" I decided to change domain name registrar from Turbify to Namecheap.</p> <div>Published: 11:00 PM GMT · Nov 28, 2023</div>\n";
+           "<p>According to <a href=\"https://getreadyletsgrow.com/turbify-url-changes/\">https://getreadyletsgrow.com/turbify-url-changes/</a>, \"On June 10, 2022, Verizon's Small Business Essentials (formerly Yahoo Small Business) was acquired by Infinite Computer Solutions. It is being rebranded as Turbify.\" I decided to change domain name registrar from Turbify to Namecheap.</p><p>I am sharing below my DNS Records configured from the Turbify interface.</p><p>A and CNAME Records:</p><p>TXT Records:</p><p>MX Records:</p><p>Name Servers:</p> <div>Published: 11:00 PM GMT · Nov 28, 2023</div>\n";
 ?>
