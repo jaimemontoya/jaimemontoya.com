ff63a4a3f1eecb47d9b6800fb7d6d1e0696e9f54
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-11-28 21:00:45 -0600

    Change date.

--- blog/2023/11/29/02/48/index.php
+++ blog/2023/11/29/02/48/index.php
@@ -5,6 +5,6 @@
   $index->parentPage = "Blog";
   $index->metaDescription = "Yahoo Small Business became Verizon's Small Business Essentials. Finally, it was rebranded as Turbify. I decided to change domain name registrar from Turbify to Namecheap.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20231128-2300.php");
+  require("book-info-20231129-0248.php");
   $index->Display();
 ?>
