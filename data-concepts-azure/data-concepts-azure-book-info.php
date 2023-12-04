4ca375aa1ca6e18b99467b2cd53b0b9f7daa34c0
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-04 17:08:27 -0600

    Display link to Certificate.

--- data-concepts-azure/data-concepts-azure-book-info.php
+++ data-concepts-azure/data-concepts-azure-book-info.php
@@ -7,6 +7,7 @@
                 <div class=\"book-title-authors-read-by\">
 	                <span>Explore Core Data Concepts in Microsoft Azure</span>
 	                <p>By Microsoft. <mark>Course 3 completed by Jaime Montoya</mark> from 10:47 PM June 1, 2023 to 2:51 PM August 2, 2023 GMT. Verify at https://coursera.org/verify/W3Z9DJ7BZG97.</p>
+					<span class=\"linkstyle\">View Certificate</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
                 </div>
             </div></a>\n";
 ?>
