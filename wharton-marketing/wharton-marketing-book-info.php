7b98cbc26f504bd969b4d07a2f15cab381c5a501
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-04 17:11:39 -0600

    Link to Coursera Certificates.

--- wharton-marketing/wharton-marketing-book-info.php
+++ wharton-marketing/wharton-marketing-book-info.php
@@ -6,7 +6,7 @@
                 </div>
                 <div class=\"book-title-authors-read-by\">
 	                <span>Introduction to Marketing</span>
-	                <p>By University of Pennsylvania. <mark>Course 1 completed by Jaime Montoya</mark> from 4:30 PM April 22, 2019 to April 30, 2019 GMT. Verify at https://coursera.org/verify/UN9PAZ74UDAB.</p>
+	                <p>By University of Pennsylvania. <mark>Course 1 completed by Jaime Montoya</mark> from 4:30 PM April 22, 2019 to April 30, 2019 GMT. Verify at https://coursera.org/verify/UN9PAZ74UDAB. <span class=\"linkstyle\">View Certificate</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
