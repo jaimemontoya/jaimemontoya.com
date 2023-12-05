decf1017ab7cc7511fcde362d245e5006704d0ce
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-05 01:09:56 -0600

    Add link.

--- the-pilgrim-s-progress/the-pilgrim-s-progress-book-info.php
+++ the-pilgrim-s-progress/the-pilgrim-s-progress-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>The Pilgrim's Progress: A Readable Modern-Day Version of John Bunyan’s Pilgrim’s Progress</span>
-					<p>By John Bunyan, Alan Vermilye. <mark>Book 37 read by Jaime Montoya</mark> from 11:00 PM September 8, 2022 to 6:31 PM September 14, 2022 GMT.</p>
+					<p>By John Bunyan, Alan Vermilye. <mark>Book 37 read by Jaime Montoya</mark> from 11:00 PM September 8, 2022 to 6:31 PM September 14, 2022 GMT. <span class=\"linkstyle\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div>\n";
 ?>
