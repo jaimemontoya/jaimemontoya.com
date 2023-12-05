591b379448977feb9c3fb46c516d5cbf934349f4
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-05 01:33:47 -0600

    Explain how Faithful and Christian were different.

--- the-pilgrim-s-progress/the-pilgrim-s-progress-summary-critique-review.php
+++ the-pilgrim-s-progress/the-pilgrim-s-progress-summary-critique-review.php
@@ -18,6 +18,7 @@
 				<div class=\"margintop15 bold\">2.2.6. Chapter 6</div>
 				<div class=\"margintop15 bold\">2.2.7. Chapter 7</div>
 				<p>As Christian and Faithful were walking to the Celestial City, they arrived at a town called Vanity, a place of never ending festivities. Lusts, pleasures and all kinds of delights could be purchased in that town. It was impossible to reach the Celestial City without going through Vanity in the journey for those who were alive. Even the King of Kings had to pass through Vanity, when he was invited by Beelzebub to buy vanities.</p>
+				<p>Christian and Faithful entered the fair in Vanity, disturbing the town because they were different than those at the fair in three ways: their attire, speech, and and the value they placed on the merchandise being sold.</p>
 				<div class=\"margintop15 bold\">2.3. Citing extra-text material</div>
 				<div class=\"margintop15 underline bold\">3. HIGHLIGHTING PARTS OF THE BOOK</div>
 				<div class=\"margintop15 bold\">3.1. Providing focused evaluation</div>
