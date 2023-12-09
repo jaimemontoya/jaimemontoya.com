f2dd55c742266f284d9a85349a00fa4ca1aff8ac
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-09 17:58:59 -0600

    Modify description.

--- blog/2023/12/10/05/59/20231210-0559-book-info.php
+++ blog/2023/12/10/05/59/20231210-0559-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Migrating WordPress website from InMotion Hosting to DigitalOcean</span>
-					<p>Step-by-step explanation of process followed to migrate unionchurchss.org website from InMotion Hosting to DigitalOcean, including modifications of DNS Records for business email and website in new server. <mark>Published: 5:59 AM GMT · Dec 10, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+					<p>Step-by-step explanation of process followed to migrate unionchurchss.org website from InMotion Hosting to DigitalOcean, including DNS Records modifications for business email and website in new server. <mark>Published: 5:59 AM GMT · Dec 10, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
             </div></a>\n";
