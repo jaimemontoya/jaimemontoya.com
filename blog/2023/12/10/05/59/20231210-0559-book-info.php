79077c6720a07f902aa51a343b146427abee3931
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-09 07:33:22 -0600

    Add new blog post.

---
+++ blog/2023/12/10/05/59/20231210-0559-book-info.php
@@ -0,0 +1,13 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2023/12/10/05/59/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"blog-20231210-0559-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Migrating WordPress website from InMotion Hosting to DigitalOcean</span>
+					<p>Step-by-step explanation of process followed to migrate unionchurchss.org website from InMotion Hosting to DigitalOcean, including modifications of DNS Records for business email and website in new server. <mark>Published: 5:59 AM GMT · Dec 10, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+</p>
+                </div>
+            </div></a>\n";
+?>
---
+++ blog/2023/12/10/05/59/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Migrating WordPress website from InMotion Hosting to DigitalOcean";
+  $index->parentPage = "Blog";
+  $index->metaDescription = "Step-by-step explanation of process followed to migrate unionchurchss.org website from InMotion Hosting to DigitalOcean, including modifications of DNS Records for business email and website in new server.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20231210-0559.php");
+  $index->Display();
+?>
