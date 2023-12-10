df7d106a76cb4e1cca6ba67ca704d1e17534432f
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-09 19:09:17 -0600

    Specify that website is WordPress.

--- blog/2023/12/10/05/59/index.php
+++ blog/2023/12/10/05/59/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "Migrating WordPress website from InMotion Hosting to DigitalOcean";
   $index->parentPage = "Blog";
-  $index->metaDescription = "Step-by-step explanation of process followed to migrate unionchurchss.org website from InMotion Hosting to DigitalOcean, including DNS Records modifications for business email and website in new server.";
+  $index->metaDescription = "Step-by-step explanation of process followed to migrate unionchurchss.org  WordPress website from InMotion Hosting to DigitalOcean, including DNS Records modifications for business email and website in new server.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20231210-0559.php");
   $index->Display();
