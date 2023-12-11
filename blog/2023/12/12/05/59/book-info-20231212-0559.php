f68e5a102e3c066c3b88e3caf539923dc319e798
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-11 08:40:09 -0600

    Provide name and location for new project.

--- blog/2023/12/12/05/59/book-info-20231212-0559.php
+++ blog/2023/12/12/05/59/book-info-20231212-0559.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "<p>I used <a href=\"includes/FuzzyGroupingExample.txt\">FuzzyGroupingExample.txt</a> to create my reference table. I opened Visual Studio and created a new project:</p><div><img src=\"/blog/2023/12/12/05/59/img/createNewProject.jpg\" alt=\"Create new project\" width=\"619\" height=\"153\"></div><div>Published: 5:59 AM GMT · Dec 12, 2023</div>\n";
+           "<p>I used <a href=\"includes/FuzzyGroupingExample.txt\">FuzzyGroupingExample.txt</a> to create my reference table. I opened Visual Studio and created a new project:</p><div><img src=\"/blog/2023/12/12/05/59/img/createNewProject.jpg\" alt=\"Create new project\" width=\"619\" height=\"153\"></div><p>I provided a name and location for my new project:</p><div><img src=\"/blog/2023/12/12/05/59/img/provideNameAndLocationForNewProject.jpg\" alt=\"Provide name and location for new project\" width=\"939\" height=\"649\"></div><div>Published: 5:59 AM GMT · Dec 12, 2023</div>\n";
 ?>
