313ff545bb09dd5983a1f25838eeb87928cdda66
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-11 08:09:39 -0600

    Fix file name.

--- blog/2023/12/12/05/59/index.php
+++ blog/2023/12/12/05/59/index.php
@@ -5,6 +5,6 @@
   $index->parentPage = "Blog";
   $index->metaDescription = "Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates from a TXT file of 22 rows, plus the title.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20231210-0559.php");
+  require("book-info-20231212-0559.php");
   $index->Display();
 ?>
