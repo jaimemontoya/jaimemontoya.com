471e230857f6725f8b6e75ac8ac649a3accedc61
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-12 08:30:07 -0600

    Change blog description.

--- blog/2023/12/12/05/59/20231212-0559-book-info.php
+++ blog/2023/12/12/05/59/20231212-0559-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Removing duplicates using Fuzzy Grouping in SQL Server Integration Services</span>
-					<p>Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates from a TXT file of 22 rows, plus the title. <mark>Published: 5:59 AM GMT · Dec 12, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+					<p>Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates from a TXT file of 22 rows that resulted in an output of 6 rows without duplicates. <mark>Published: 5:59 AM GMT · Dec 12, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
             </div></a>\n";
