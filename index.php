fc8d612f3d3692d03f93e495ee27e40e6babe9ef
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-12 15:28:25 -0600

    Include call to action.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require("page.php");
   $index = new Page();
-  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in three main categories:</p><ol><li><a href=\"portfolio/\">Portfolio</a>: Websites created, designed, developed, upgraded, maintained and/or hosted by Jaime Montoya<li><a href=\"blog/\">Blog</a>: Software technical documentation blog for myself and the world<li><a href=\"about/\">About</a>: Jaime Montoya's one-page resume</ol>";
+  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in three main categories:</p><ol><li><a href=\"portfolio/\">Portfolio</a>: Websites created, designed, developed, upgraded, maintained and/or hosted by Jaime Montoya<li><a href=\"blog/\">Blog</a>: Software technical documentation blog for myself and the world<li><a href=\"about/\">About</a>: Jaime Montoya's one-page resume</ol><p>Call to action: Get get a $200, 60-day credit!</p>";
   $index->Display();
 ?>
 
