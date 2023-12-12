4ba9a8ad164c874df97ef24a8f911089a90070e1
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-12 13:28:56 -0600

    Describe website.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require("page.php");
   $index = new Page();
-  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1>";
+  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in three main categories:</p>";
   $index->Display();
 ?>
 
