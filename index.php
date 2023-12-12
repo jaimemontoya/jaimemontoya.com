d4662cf027de810540f32a1140c0a5102510f01e
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-12 13:35:27 -0600

    Specify organization of the website.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require("page.php");
   $index = new Page();
-  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in three main categories:</p>";
+  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in three main categories:</p><ol><li>Portfolio: Websites created, designed, developed, upgraded, maintained and/or hosted by Jaime Montoya<li>Blog: Software technical documentation blog for myself and the world<li>About: Jaime Montoya's one-page resume</ol>";
   $index->Display();
 ?>
 
