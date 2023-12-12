40cd4293fc3ec610fdd7a55c4343c94036749df8
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-12 11:54:38 -0600

    Add h1.

--- american-prometheus/american-prometheus-summary-critique-review.php
+++ american-prometheus/american-prometheus-summary-critique-review.php
@@ -1,6 +1,6 @@
 <?php
   $index->content .= 
-           "\t\t\t<div>
+           "\t\t\t<h1>Book review for American Prometheus: The Inspiration for the Major Motion Picture OPPENHEIMER</h1><div>
                 <div class=\"margintop15 underline bold\">1. INTRODUCING THE BOOK</div>
                 <div class=\"margintop15 bold\">1.1. Defining the general topic of the book</div>
 				<ul class=\"margintop15\">
