b924cc6999295762bfa4c10f1bdd1d2a60b5e097
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-12 21:30:44 -0600

    Use h2.

--- blog/2023/09/21/18/09/20230921-1809-book-info.php
+++ blog/2023/09/21/18/09/20230921-1809-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"no-photo-available-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>My first blog entry</span>
+		            <h2>My first blog entry</h2>
 					<p>Instead of using social media, I decided to create a blog from scratch, not only in terms of content, but also in how I built it from a software development standpoint. I started with a \"Hello, World!\" program. <mark>Published: 6:09 PM GMT · Sep 21, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
