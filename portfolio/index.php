3c014016484af4a1b6cf3100da4f23f0942ace82
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-12 21:22:27 -0600

    Add description.

--- portfolio/index.php
+++ portfolio/index.php
@@ -2,7 +2,7 @@
   require("../page.php");
   $index = new Page();
   $index->title = "Websites created, designed, developed, upgraded, maintained and/or hosted by Jaime Montoya";
-  $index->metaDescription = "Websites created, designed, developed, upgraded, maintained and/or hosted by Jaime Montoya";
+  $index->metaDescription = "List of websites created, designed, developed, upgraded, maintained and/or hosted by Jaime Montoya";
   $index->content .= "<h1>Websites created, designed, developed, upgraded, maintained and/or hosted by Jaime Montoya</h1>";
   require("mfh-elsalvador.org/mfh-elsalvador.org-book-info.php");
   require("unionchurchss.org/unionchurchss.org-book-info.php");
