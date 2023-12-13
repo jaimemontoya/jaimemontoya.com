2e399e30fe31856d8208b6d125250ac19cf56e9d
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-12 22:47:22 -0600

    Remove website counter.

--- portfolio/unionchurchss.org/unionchurchss.org-book-info.php
+++ portfolio/unionchurchss.org/unionchurchss.org-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>unionchurchss.org</h2>
-					<p><mark>Website 2; created, developed, upgraded and maintained by Jaime Montoya</mark> (Server location: Ashburn, Virginia, United States; since June 9, 2019 GMT). <span class=\"linkstyle\">Visit website</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p><mark>Created, developed, upgraded and maintained by Jaime Montoya</mark> (Server location: Ashburn, Virginia, United States; since June 9, 2019 GMT). <span class=\"linkstyle\">Visit website</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
