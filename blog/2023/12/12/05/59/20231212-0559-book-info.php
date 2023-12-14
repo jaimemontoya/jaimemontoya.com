99c9c8c3e4ba3e5b2d8141b281d2f4b57b75b668
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-14 09:36:20 -0600

    Change blog title.

--- blog/2023/12/12/05/59/20231212-0559-book-info.php
+++ blog/2023/12/12/05/59/20231212-0559-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"blog-20231212-0559-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <h2>Removing duplicates using Fuzzy Grouping in SQL Server Integration Services plus fixing spelling errors and converting abbreviations to full job title names</h2>
+		            <h2>Using Fuzzy Grouping in SQL Server Integration Services for removing duplicates, fixing spelling errors, and converting abbreviations to full job title names</h2>
 					<p>Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates. This implementation also fixes spelling errors and performs a conversion of the job tables that were abbreviated into non-abbreviated job title names. <mark>Published: 5:59 AM GMT · Dec 12, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
--- blog/2023/12/12/05/59/index.php
+++ blog/2023/12/12/05/59/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Removing duplicates using Fuzzy Grouping in SQL Server Integration Services plus fixing spelling errors and converting abbreviations to full job title names";
+  $index->title = "Using Fuzzy Grouping in SQL Server Integration Services for removing duplicates, fixing spelling errors, and converting abbreviations to full job title names";
   $index->parentPage = "Blog";
   $index->metaDescription = "Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates. This implementation also fixes spelling errors and performs a conversion of the job tables that were abbreviated into non-abbreviated job title names.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
