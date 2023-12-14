0f4899ee60bc7bc35bc152cc6e103b83ba04e37b
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-14 11:21:37 -0600

    Change description.

--- blog/2023/12/12/05/59/20231212-0559-book-info.php
+++ blog/2023/12/12/05/59/20231212-0559-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Using Fuzzy Grouping in SQL Server Integration Services for removing duplicates, fixing spelling errors, and converting abbreviations to full job titles</h2>
-					<p>Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates. This implementation also fixes spelling errors and performs a conversion of the job tables that were abbreviated into non-abbreviated job title names. <mark>Published: 5:59 AM GMT · Dec 12, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+					<p>Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates. This implementation also fixes spelling errors and performs a conversion of the job tables that were abbreviated into non-abbreviated job titles. <mark>Published: 5:59 AM GMT · Dec 12, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
             </div></a>\n";
