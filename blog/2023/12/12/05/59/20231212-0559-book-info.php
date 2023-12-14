3efb90bfc3f504f4853127731edd59071f1110e4
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-14 09:32:44 -0600

    Change blog post title.

--- blog/2023/12/12/05/59/20231212-0559-book-info.php
+++ blog/2023/12/12/05/59/20231212-0559-book-info.php
@@ -5,8 +5,8 @@
 					<div class=\"book-cover\" id=\"blog-20231212-0559-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <h2>Removing duplicates using Fuzzy Grouping in SQL Server Integration Services</h2>
-					<p>Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates from a TXT file of 22 rows that resulted in an output of 6 rows without duplicates. <mark>Published: 5:59 AM GMT · Dec 12, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+		            <h2>Removing duplicates using Fuzzy Grouping in SQL Server Integration Services plus fixing spelling errors and converting abbreviations to full job title names</h2>
+					<p>Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates. This implementation also fixes spelling errors and performs a conversion of the job tables that were abbreviated into non-abbreviated job title names. <mark>Published: 5:59 AM GMT · Dec 12, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
             </div></a>\n";
--- blog/2023/12/12/05/59/index.php
+++ blog/2023/12/12/05/59/index.php
@@ -1,9 +1,9 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Removing duplicates using Fuzzy Grouping in SQL Server Integration Services";
+  $index->title = "Removing duplicates using Fuzzy Grouping in SQL Server Integration Services plus fixing spelling errors and converting abbreviations to full job title names";
   $index->parentPage = "Blog";
-  $index->metaDescription = "Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates from a TXT file of 22 rows that resulted in an output of 6 rows without duplicates.";
+  $index->metaDescription = "Practical example of the use of Fuzzy Grouping in SQL Server Integration Services to remove duplicates. This implementation also fixes spelling errors and performs a conversion of the job tables that were abbreviated into non-abbreviated job title names.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20231212-0559.php");
   $index->Display();
