919fbca097e37230fc1cc6e313a2429f9420ccb9
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-17 06:24:34 -0600

    Create a resource.

---
+++ blog/2023/12/17/11/41/20231217-1141-book-info.php
@@ -0,0 +1,13 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2023/12/17/11/41/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"<!--blog-20231217-1141-->no-photo-available-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Provisioning Azure relational database services to create an Azure SQL Database resource</h2>
+					<p>Example of steps to provision from Azure subscription, an Azure SQL Database resource that is queried using SQL. <mark>Published: 11:41 AM GMT · Dec 17, 2023</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+</p>
+                </div>
+            </div></a>\n";
+?>
---
+++ blog/2023/12/17/11/41/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Provisioning Azure relational database services to create an Azure SQL Database resource";
+  $index->parentPage = "Blog";
+  $index->metaDescription = "Example of steps to provision from Azure subscription, an Azure SQL Database resource that is queried using SQL.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20231217-1141.php");
+  $index->Display();
+?>
