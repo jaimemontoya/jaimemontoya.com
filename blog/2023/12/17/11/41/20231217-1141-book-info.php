f01d593381fa113d2ce78818f1bc0dc6cd9345a2
Jaime Montoya <jmontoya@jaimemontoya.com>
2023-12-17 06:28:45 -0600

    Fix image path.

--- blog/2023/12/17/11/41/20231217-1141-book-info.php
+++ blog/2023/12/17/11/41/20231217-1141-book-info.php
@@ -2,7 +2,7 @@
   $index->content .= 
            "\t\t\t<a href=\"2023/12/17/11/41/\"><div class=\"book\">
 				<div class=\"book-thumbnail\">
-					<div class=\"book-cover\" id=\"<!--blog-20231217-1141-->no-photo-available-img\"></div>
+					<div class=\"book-cover\" id=\"no-photo-available-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Provisioning Azure relational database services to create an Azure SQL Database resource</h2>
