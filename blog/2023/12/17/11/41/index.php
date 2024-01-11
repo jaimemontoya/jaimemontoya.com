6331d27ed704b337f6427259ff93f5d18c949cab
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-01-10 22:20:11 -0600

    Change blog title and description.

--- blog/2023/12/17/11/41/index.php
+++ blog/2023/12/17/11/41/index.php
@@ -1,9 +1,9 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Provisioning Azure relational database services to create an Azure SQL Database resource";
+  $index->title = "Provisioning Azure relational database services to create Azure SQL, MySQL, and PostgreSQL database resources";
   $index->parentPage = "Blog";
-  $index->metaDescription = "Example of steps to provision from an Azure subscription, an Azure SQL Database resource that is queried using SQL.";
+  $index->metaDescription = "Steps to provision from an Azure subscription, the following resources: Azure SQL, MySQL, and PostgreSQL databases.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20231217-1141.php");
   $index->Display();
