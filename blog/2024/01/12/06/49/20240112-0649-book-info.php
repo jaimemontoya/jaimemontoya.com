300ccca9f5b4ce5f3c92a609165159d93deeb189
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-01-12 01:05:38 -0600

    Add new blog entry.

---
+++ blog/2024/01/12/06/49/20240112-0649-book-info.php
@@ -0,0 +1,13 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2024/01/12/06/49/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"blog-20240112-0649-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Connecting to an Azure Database for MySQL server with Azure Client Shell</h2>
+					<p>This quickstart shows how to connect to a server that runs an Azure Database for MySQL, using Azure Client Shell (an interactive, authenticated, browser-accessible terminal for managing Azure resources). <mark>Published: 6:49 AM GMT · Jan 12, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+</p>
+                </div>
+            </div></a>\n";
+?>
