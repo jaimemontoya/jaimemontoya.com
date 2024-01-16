1fc484fad04d09e32b12b5af8c1f965497bc5944
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-01-15 20:06:08 -0600

    Fix typo.

--- blog/2024/01/16/01/50/index.php
+++ blog/2024/01/16/01/50/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "Checking versions of PHP installed on Ubuntu server";
   $index->parentPage = "Blog";
-  $index->metaDescription = "This post provides a way to check if there are multiple versions of PHP installed on Ubunto, and to display the directory correspoding to each version.";
+  $index->metaDescription = "This post provides a way to check if there are multiple versions of PHP installed on Ubunto, and to display the directory corresponding to each version.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20240116-0150.php");
   $index->Display();
