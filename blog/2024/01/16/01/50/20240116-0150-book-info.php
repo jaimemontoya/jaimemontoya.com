f72e6d4cb6fa419fe00bb99dbaa679e959e5c167
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-01-15 19:58:41 -0600

    Show multiple versions of PHP.

---
+++ blog/2024/01/16/01/50/20240116-0150-book-info.php
@@ -0,0 +1,13 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2024/01/16/01/50/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"no-photo-available-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Checking versions of PHP installed on Ubuntu server</h2>
+					<p>This post provides a way to check if there are multiple versions of PHP installed on Ubunto, and to display the directory correspoding to each version. <mark>Published: 1:50 AM GMT · Jan 16, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+</p>
+                </div>
+            </div></a>\n";
+?>
---
+++ blog/2024/01/16/01/50/book-info-20240116-0150.php
@@ -0,0 +1,17 @@
+<?php
+  $index->content .= 
+           "After connecting to my server, I executed the following commands:
+<pre>
+root@jaimemontoya:/etc/php# pwd
+/etc/php
+root@jaimemontoya:/etc/php# ls -al
+total 32
+drwxr-xr-x   6 root root  4096 Aug 17 08:56 .
+drwxr-xr-x 160 root root 12288 Jan 11 06:20 ..
+drwxr-xr-x   6 root root  4096 Aug  7  2019 7.0
+drwxr-xr-x   5 root root  4096 Aug  7  2019 7.3
+drwxr-xr-x   5 root root  4096 Nov  6  2021 8.0
+drwxr-xr-x   5 root root  4096 Aug 17 08:56 8.1
+</pre>
+		   <div>Published: 1:50 AM GMT · Jan 16, 2024</div>\n";
+?>
