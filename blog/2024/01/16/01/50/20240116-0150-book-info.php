09776136e4354498c381dffb52539d8c20092085
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-01-15 21:13:35 -0600

    Add blog post.

--- blog/2024/01/16/01/50/20240116-0150-book-info.php
+++ blog/2024/01/16/01/50/20240116-0150-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Checking versions of PHP installed on Ubuntu server</h2>
-					<p>This post provides a way to check if there are multiple versions of PHP installed on Ubunto, and to display the directory corresponding to each version. <mark>Published: 1:50 AM GMT · Jan 16, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
+					<p>This post provides a way to check if there are multiple versions of PHP installed on Ubuntu, and to display the directory corresponding to each version. <mark>Published: 1:50 AM GMT · Jan 16, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i>
 </p>
                 </div>
             </div></a>\n";
--- blog/2024/01/16/02/10/index.php
+++ blog/2024/01/16/02/10/index.php
@@ -1,10 +1,10 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Checking versions of PHP installed on Ubuntu server";
+  $index->title = "Upgrading PHP from 8.1 to 8.3 on Apache and Ubuntu";
   $index->parentPage = "Blog";
-  $index->metaDescription = "This post provides a way to check if there are multiple versions of PHP installed on Ubunto, and to display the directory corresponding to each version.";
+  $index->metaDescription = "Upgrading PHP from 8.1 to 8.3 on Apache and Ubuntu with a few commands.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20240116-0150.php");
+  require("book-info-20240116-0210.php");
   $index->Display();
 ?>
