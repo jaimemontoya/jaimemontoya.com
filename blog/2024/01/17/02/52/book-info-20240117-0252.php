9649c8468e33f1105c72315afe39eabc47f1c022
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-01-16 21:26:21 -0600

    Fix grammar.

--- blog/2024/01/17/02/52/book-info-20240117-0252.php
+++ blog/2024/01/17/02/52/book-info-20240117-0252.php
@@ -1,5 +1,5 @@
 <?php
   $index->content .= 
-           "<p>I completed the first 8 steps of the <span class=\"cod\">Facebook Login for Android - Quickstart</span> available at <a href=\"https://developers.facebook.com/docs/facebook-login/android/\">https://developers.facebook.com/docs/facebook-login/android/</a>. After that I contiued with the remaining steps shared below.</p><p></p>
+           "<p>I completed the first 8 steps of the <span class=\"cod\">Facebook Login for Android - Quickstart</span> available at <a href=\"https://developers.facebook.com/docs/facebook-login/android/\">https://developers.facebook.com/docs/facebook-login/android/</a>. After that, I continued with the remaining steps shared below.</p><p></p>
 		   <div>Published: 2:52 AM GMT · Jan 17, 2024</div>\n";
 ?>
