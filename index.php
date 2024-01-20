236193845e33c88fdc8eff2140d3ac7b9c0c4066
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-01-19 18:09:49 -0600

    Add Mission statement.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require("page.php");
   $index = new Page();
-  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in three categories:</p><ol><li><a href=\"mission-vision-values\">Mission, Vision, and Values:</a> Definition of my personal Mission and Vision statements, including a list of Values that distinghish my character.<li><a href=\"blog/\">Blog</a>: Software technical documentation blog for myself and the world<li><a href=\"about/\">About</a>: Jaime Montoya's one-page resume</ol>";
+  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in two categories:</p><h2>Mission</h2><p>To document repeatable and scalable software technical solutions based on my real-world experience working as a programmer for my full-time employers, clients of my freelancing services, individuals/organizations beneficiaries of my donated work/software/services, and myself (research/personal projects); providing software integrations that contribute to increase profits and reduce costs.</p><ol><li><a href=\"blog/\">Blog</a>: Software technical documentation blog for myself and the world<li><a href=\"about/\">About</a>: Jaime Montoya's one-page resume</ol>";
   $index->Display();
 ?>
 
