792c959d19f4b976bf99aad69b635f05f0b1aaa4
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-03-16 06:29:59 -0600

    Fix placement of code.

--- blog/2024/01/17/02/52/index.php
+++ blog/2024/01/17/02/52/index.php
@@ -1,9 +1,9 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Setting up the dark theme in SQL Server Management Studio";
+  $index->title = "Facebook Login for Android";
   $index->parentPage = "Blog";
-  $index->metaDescription = "Enable the SSMS Dark theme in SQL Server Management Studio by switching between the settings configurable from Tools | Options | Environment | General | Color theme.";
+  $index->metaDescription = "Implementation of Facebook Login using the Facebook SDK for Android.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20240117-0252.php");
   $index->Display();
