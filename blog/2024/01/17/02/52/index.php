0e88a29d6e42cf6db01de932c08c61a52916ca2a
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-03-16 06:18:44 -0600

    Add new blog entry for setting up the dark theme in SQL Server Management Studio.

--- blog/2024/01/17/02/52/index.php
+++ blog/2024/01/17/02/52/index.php
@@ -1,10 +1,10 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Facebook Login for Android";
+  $index->title = "Setting up the dark theme in SQL Server Management Studio";
   $index->parentPage = "Blog";
-  $index->metaDescription = "Implementation of Facebook Login using the Facebook SDK for Android.";
+  $index->metaDescription = "Enable the SSMS Dark theme in SQL Server Management Studio by switching between the settings configurable from Tools | Options | Environment | General | Color theme.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20240117-0252.php");
+  require("book-info-20240315-1153.php");
   $index->Display();
 ?>
---
+++ blog/2024/03/15/11/53/book-info-20240315-1153.php
@@ -0,0 +1,5 @@
+<?php
+  $index->content .= 
+           "<p>I wanted to enable and use the Dark theme in SQL Server Management Studio. To my surprise, that option was not available by default. The only options available for me from <span class=\"cod\">Tools | Options | Environment | General | Color theme</span> were <span class=\"cod\">Blue</span>, <span class=\"cod\">Blue (Extra Contrast)</span>, and <span class=\"cod\">Light</span>:</p>
+		   <div>Published: 11:53 AM GMT · Mar 15, 2024</div>\n";
+?>
