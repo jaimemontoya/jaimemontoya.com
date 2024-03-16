ee6d4a9b9b1a6e4eb185917275439c8ac0f7fcc2
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-03-16 06:24:02 -0600

    Comment out code for debugging purposes.

--- blog/2024/01/17/02/52/index.php
+++ blog/2024/01/17/02/52/index.php
@@ -5,6 +5,6 @@
   $index->parentPage = "Blog";
   $index->metaDescription = "Enable the SSMS Dark theme in SQL Server Management Studio by switching between the settings configurable from Tools | Options | Environment | General | Color theme.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20240315-1153.php");
+  //require("book-info-20240315-1153.php");
   $index->Display();
 ?>
