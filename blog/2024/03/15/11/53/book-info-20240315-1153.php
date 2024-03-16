ac65892e101b7654dadf9bd718d7ae176dda70db
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-03-16 06:43:19 -0600

    Add picture.

--- blog/2024/03/15/11/53/book-info-20240315-1153.php
+++ blog/2024/03/15/11/53/book-info-20240315-1153.php
@@ -1,5 +1,5 @@
 <?php
   $index->content .= 
-           "<p>I wanted to enable and use the Dark theme in SQL Server Management Studio. To my surprise, that option was not available by default. The only options available for me from <span class=\"cod\">Tools | Options | Environment | General | Color theme</span> were <span class=\"cod\">Blue</span>, <span class=\"cod\">Blue (Extra Contrast)</span>, and <span class=\"cod\">Light</span>:</p>
+           "<p>I wanted to enable and use the Dark theme in SQL Server Management Studio. To my surprise, that option was not available by default. The only options available for me from <span class=\"cod\">Tools | Options | Environment | General | Color theme</span> were <span class=\"cod\">Blue</span>, <span class=\"cod\">Blue (Extra Contrast)</span>, and <span class=\"cod\">Light</span>:</p><div><img src=\"/blog/2024/03/15/11/53/img/darkThemeNotAvailable.jpg\" alt=\"Dark Theme not available\" width=\"962\" height=\"727\"></div>
 		   <div>Published: 11:53 AM GMT · Mar 15, 2024</div>\n";
 ?>
