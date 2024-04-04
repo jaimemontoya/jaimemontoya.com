07093aab0c5209da63fcdfb44c14870a38ea6aa6
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-04 17:43:03 -0600

    Add bibliography of template.

--- book/american-prometheus/index.php
+++ book/american-prometheus/index.php
@@ -6,5 +6,6 @@
   $index->metaDescription = "In this magisterial, acclaimed biography twenty-five years in the making, Kai Bird and Martin Sherwin capture Oppenheimer’s life and times, from his early career to his central role in the Cold War.";
   require("american-prometheus-book-info.php");
   require("american-prometheus-summary-critique-review.php");
+  require("book-reviews-bibliography.php");
   $index->Display();
 ?>
