07babee860f8fe0a9419d839f0522f6abddb5f91
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 16:29:34 -0600

    Improve grammar.

--- run/2023/chart.php
+++ run/2023/chart.php
@@ -1,4 +1,4 @@
 <?php
   $running->content .= 
-           "\t\t\t<h1>2023 Jaime Montoya's running history by month</h1><div><div id=\"running-2023\"></div></div>\n";
+           "\t\t\t<h1>Jaime Montoya's 2023 running history by month</h1><div><div id=\"running-2023\"></div></div>\n";
 ?>
