f8ffdd2809a9f1b8b8fe651c47960afb30e27d6c
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 10:30:50 -0600

    Change variable name.

--- run/all/index.php
+++ run/all/index.php
@@ -1,10 +1,10 @@
 <?php
   require("../../running.php");
-  $index = new Page();
-  $index->title = "Jaime Montoya's running history by year";
-  $index->parentPage = "Running";
-  $index->metaDescription = "Jaime Montoya's running history by year since since August 2, 2020 to present.";
+  $running = new Page();
+  $running->title = "Jaime Montoya's running history by year";
+  $running->parentPage = "Running";
+  $running->metaDescription = "Jaime Montoya's running history by year since since August 2, 2020 to present.";
   require("all-book-info.php");
   require("chart.php");
-  $index->Display();
+  $running->Display();
 ?>
