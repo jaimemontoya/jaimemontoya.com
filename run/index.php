84198804e819cd6b28e865f674c05cdd86cba356
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 09:47:28 -0600

    Create instance of class.

--- run/index.php
+++ run/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../running.php");
-  /*$running = new FinancesPage();
-  $running->title = "Running";
+  $running = new FinancesPage();
+  /*$running->title = "Running";
   $running->metaDescription = "Jaime Montoya's running history since August 2, 2020.";
   $running->content .= "<h1>Running</h1>";
   require("all/all-book-info.php");
