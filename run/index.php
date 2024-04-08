6dbecc5e7e7d2513560c0eaed87e01fe22ccdbbb
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 09:42:30 -0600

    Create instance of running page.

--- run/index.php
+++ run/index.php
@@ -1,9 +1,9 @@
 <?php
   require("../running.php");
-  $index = new Page();
-  $index->title = "Running";
-  $index->metaDescription = "Jaime Montoya's running history since August 2, 2020.";
-  $index->content .= "<h1>Running</h1>";
+  $running = new FinancesPage();
+  $running->title = "Running";
+  $running->metaDescription = "Jaime Montoya's running history since August 2, 2020.";
+  $running->content .= "<h1>Running</h1>";
   require("all/all-book-info.php");
-  $index->Display();
+  $running->Display();
 ?>
