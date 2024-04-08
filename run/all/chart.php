5b06ab30602879e087b1b01916597ca0a5b5e4ba
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 15:35:49 -0600

    Draw graph.

--- run/all/chart.php
+++ run/all/chart.php
@@ -1,5 +1,4 @@
 <?php
   $running->content .= 
-           "\t\t\t<h1>Jaime Montoya's running history by year</h1><div>
-            </div>\n";
+           "\t\t\t<h1>Jaime Montoya's running history by year</h1><div><div id=\"mouseoverdiv\"></div></div>\n";
 ?>
