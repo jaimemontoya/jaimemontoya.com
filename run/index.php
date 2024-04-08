e434bca4377afe4d0f2126da40e3cf7e57d61f90
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 09:50:06 -0600

    Show code from class.

--- run/index.php
+++ run/index.php
@@ -1,9 +1,9 @@
 <?php
   require("../running.php");
   $running = new RunningPage();
-  /*$running->title = "Running";
+  $running->title = "Running";
   $running->metaDescription = "Jaime Montoya's running history since August 2, 2020.";
   $running->content .= "<h1>Running</h1>";
   require("all/all-book-info.php");
-  $running->Display();*/
+  $running->Display();
 ?>
