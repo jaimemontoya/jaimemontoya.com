713c5cb02686e42e9c7eeec49c9d4a84df84a7e6
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 10:27:38 -0600

    Add breadcrumb to Running page.

--- running.php
+++ running.php
@@ -19,6 +19,7 @@ class RunningPage extends Page
     echo "\t</head>\n\t<body>\n";
     $this -> DisplayHeader();
     echo "\t\t<div class=\"container-running\">\n";
+	echo $this -> DisplayBreadcrumb($this->title, $this->parentPage, $this->parentPages);
     echo $this->content;
     echo "\t\t</div>\n";
 	$this -> DisplayFooter();
