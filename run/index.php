2df8015c4d75a0cb10bd9faa7e771bb7c952d8e4
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 16:14:11 -0600

    Add 2024 running history to page.

--- run/index.php
+++ run/index.php
@@ -5,5 +5,6 @@
   $running->metaDescription = "Jaime Montoya's running history since August 2, 2020.";
   $running->content .= "<h1>Running</h1>";
   require("all/all-book-info.php");
+  require("2024/2024-book-info.php");
   $running->Display();
 ?>
