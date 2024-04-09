73aedc0eb40dc6635fd4078aa76a35d7fce7e35d
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-09 05:12:46 -0600

    Use weight instead of running for CSS class.

--- weight/2021/chart.php
+++ weight/2021/chart.php
@@ -1,4 +1,4 @@
 <?php
   $weight->content .= 
-           "\t\t\t<h1>Jaime Montoya's 2021 running history by month</h1><div><div id=\"running-2021\"></div></div>\n";
+           "\t\t\t<h1>Jaime Montoya's 2021 weight history by month</h1><div><div id=\"weight-2021\"></div></div>\n";
 ?>
