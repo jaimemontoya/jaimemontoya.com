5c075a7ff2ecbd7d8e8b02a8977079bbec92b38f
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-08 18:57:46 -0600

    Add Weight page.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require("page.php");
   $index = new Page();
-  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in four categories:</p><ol><li><a href=\"book/\">Book reviews</a>: Reviews for all books I have read from cover to cover.<li><a href=\"blog/\">Blog</a>: Software technical documentation blog for myself and the world.<li><a href=\"run/\">Running</a>: Jaime Montoya's running history since August 2, 2020.<li><a href=\"about/\">About</a>: Jaime Montoya's one-page resume.</ol></p>";
+  $index->content .= "<h1>jaimemontoya.com: Jaime Montoya's Personal Home Page</h1><p>Welcome to jaimemontoya.com, Jaime Montoya's Personal Home Page, a website organized in five categories:</p><ol><li><a href=\"book/\">Book reviews</a>: Reviews for all books I have read from cover to cover.<li><a href=\"blog/\">Blog</a>: Software technical documentation blog for myself and the world.<li><a href=\"run/\">Running</a>: Jaime Montoya's running history since August 2, 2020.<li><a href=\"run/\">Weight</a>: Jaime Montoya's weight history since October 19, 2019.<li><a href=\"about/\">About</a>: Jaime Montoya's one-page resume.</ol></p>";
   $index->Display();
 ?>
 
