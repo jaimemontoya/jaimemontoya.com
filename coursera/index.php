d9709ac7b1fc642ebafc5f5d9251e1c1af2dd2e8
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-11 16:40:34 -0600

    Add Coursera to home page.

--- coursera/index.php
+++ coursera/index.php
@@ -2,7 +2,7 @@
   require("../page.php");
   $index = new Page();
   $index->title = "Coursera";
-  $index->metaDescription = "List of Coursera courses successfully completed by Jaime Montoya.";
+  $index->metaDescription = "Coursera courses successfully completed by Jaime Montoya.";
   $index->content .= "<h1>Coursera</h1>";
   require("introduction-to-marketing/introduction-to-marketing-book-info.php");
   $index->Display();
