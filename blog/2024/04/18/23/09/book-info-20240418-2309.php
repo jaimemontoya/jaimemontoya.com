ef382b1a45ee59ca035b0f81bdeb3cf79a415bd4
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-20 06:47:09 -0600

    Add image.

--- blog/2024/04/18/23/09/book-info-20240418-2309.php
+++ blog/2024/04/18/23/09/book-info-20240418-2309.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "<p>This is the error I was getting when trying to access my database that was in Recovery Pending state:</p><div><img src=\"/blog/2024/04/18/23/09/img/recoveryPendingError.jpg\" alt=\"Database in Recovery Pending state\" width=\"875\" height=\"207\"></div><div>Published: 11:09 PM GMT · Apr 18, 2024</div>\n";
+           "<p>This is the error I was getting when trying to access my database that was in Recovery Pending state:</p><div><img src=\"/blog/2024/04/18/23/09/img/recoveryPendingError.jpg\" alt=\"Database in Recovery Pending state\" width=\"875\" height=\"207\"></div><p>I ran <span class=\"cod\">SELECT name, state_desc from sys.databases</span> and realized my database was in Recovery Pending state:</p><div><img src=\"/blog/2024/04/18/23/09/img/databaseInRecoveryPendingState.jpg\" alt=\"Show database status\" width=\"384\" height=\"365\"></div><div>Published: 11:09 PM GMT · Apr 18, 2024</div>\n";
 ?>
