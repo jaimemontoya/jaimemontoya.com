f0416d121e19911ece16cf493f750c5bc67cdb7e
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-22 11:28:10 -0600

    Add Introduction to Marketing Coursera course.

--- coursera/index.php
+++ coursera/index.php
@@ -4,6 +4,7 @@
   $index->title = "Coursera";
   $index->metaDescription = "Coursera courses successfully completed by Jaime Montoya.";
   $index->content .= "<h1>Coursera</h1>";
+  require("ielts-writing/ielts-writing-book-info.php");
   require("introduction-to-marketing/introduction-to-marketing-book-info.php");
   $index->Display();
 ?>
--- coursera/introduction-to-marketing/introduction-to-marketing-book-info.php
+++ coursera/introduction-to-marketing/introduction-to-marketing-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Introduction to Marketing</span>
-					<p>By University of Pennsylvania. Instructors: Barbara E. Kahn, Peter Fader and Jagmohan Raju. <mark>Course 1 successfully completed by Jaime Montoya</mark>  from 4:30 PM April 22, 2019 to April 30, 2019 GMT. <span class=\"linkstyle\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>By University of Pennsylvania. Instructors: Barbara E. Kahn, Peter Fader and Jagmohan Raju. <mark>Course 1 successfully completed by Jaime Montoya</mark> from 4:30 PM April 22, 2019 to April 30, 2019 GMT. <span class=\"linkstyle\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
