89bf60f849a1f7f5031ab984d73eff1e88838193
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-22 11:30:28 -0600

    Remove text.

--- coursera/ielts-writing/ielts-writing-book-info.php
+++ coursera/ielts-writing/ielts-writing-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>IELTS Writing Section Skills Mastery</span>
-					<p>By University of California, Irvine. Instructors: Jay Daniyarova and Helen Nam and. <mark>Course 2 successfully completed by Jaime Montoya</mark> from 5:18 PM January 19, 2023 to 12:28 AM February 19, 2023 GMT. <span class=\"linkstyle\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>By University of California, Irvine. Instructors: Jay Daniyarova and Helen Nam. <mark>Course 2 successfully completed by Jaime Montoya</mark> from 5:18 PM January 19, 2023 to 12:28 AM February 19, 2023 GMT. <span class=\"linkstyle\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
