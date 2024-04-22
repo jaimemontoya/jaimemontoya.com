adc5bf51550229bba243fbda18184cf58c3f6083
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-04-22 13:14:05 -0600

    Add Microsoft Coursera course.

---
+++ coursera/explore-core-data-concepts-microsoft-azure/explore-core-data-concepts-microsoft-azure.php
@@ -0,0 +1,8 @@
+<?php
+  $index->content .= 
+           "\t\t\t<h1>Explore Core Data Concepts in Microsoft Azure</h1>
+		    <div>
+			    <p>Verify at <a href=\"https://coursera.org/verify/W3Z9DJ7BZG97\">https://coursera.org/verify/W3Z9DJ7BZG97</a>.</p>
+                <img src=\"/coursera/explore-core-data-concepts-microsoft-azure/img/explore-core-data-concepts-microsoft-azure.jpg\" alt=\"Explore Core Data Concepts in Microsoft Azure - Coursera - Jaime Montoya\" width=\"1050\" height=\"813\">
+            </div>\n";
+?>
