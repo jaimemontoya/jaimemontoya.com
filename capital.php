871065846de05e99bf6cc93ed776ae1d4b87195c
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-05 08:39:21 -0600

    Add USD value.

--- capital.php
+++ capital.php
@@ -62,7 +62,7 @@ class CapitalPage extends Page
 	echo "\t\t\t\t// Create the data table.\n";
 	echo "\t\t\t\tvar data = new google.visualization.DataTable();\n";
 	echo "\t\t\t\tdata.addColumn('string', 'Month');\n";
-	echo "\t\t\t\tdata.addColumn('number', 'Kg');\n";
+	echo "\t\t\t\tdata.addColumn('number', 'USD');\n";
 	echo "\t\t\t\tdata.addRows([\n";
 	echo "\t\t\t\t\t['May',4023.84]\n";
 	echo "\t\t\t\t]);\n";
