127127e497184d0dd47170a2784ea51af7797e1f
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-06 12:27:57 -0600

    Add script to connect to Azure SQL database.

---
+++ sqltest.sql
@@ -0,0 +1,19 @@
+<?php
+    $serverName = "jaimemontoya.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "Finances", // update me
+        "Uid" => "jaimemontoya", // update me
+        "PWD" => "SuperEasy@1" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT DayID, FullDateAlternateKey FROM DimDay ORDER BY DayID DESC";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['DayID'] . " " . $row['FullDateAlternateKey'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
