eb7dba7613680b4f07157b31bdb58bbeb5140233
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-06 09:54:37 -0600

    Add firewall rule.

--- blog/2024/05/03/16/18/book-info-20240503-1618.php
+++ blog/2024/05/03/16/18/book-info-20240503-1618.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "<p>From my SQL database, I clicked <span class=\"cod\">Set server firewall</span>:</p><div><img src=\"/blog/2024/05/03/16/18/img/setServerFirewall.jpg\" alt=\"Set server firewall\" width=\"846\" height=\"391\"></div><p>I clicked <span class=\"cod\">Add a firewall rule</span>:</p><div><img src=\"/blog/2024/05/03/16/18/img/setServerFirewall.jpg\" alt=\"Add a firewall rule\" width=\"1604\" height=\"722\"></div><div>Published: 4:18 PM GMT · May 3, 2024</div>\n";
+           "<p>From my SQL database, I clicked <span class=\"cod\">Set server firewall</span>:</p><div><img src=\"/blog/2024/05/03/16/18/img/setServerFirewall.jpg\" alt=\"Set server firewall\" width=\"846\" height=\"391\"></div><p>I clicked <span class=\"cod\">Add a firewall rule</span>:</p><div><img src=\"/blog/2024/05/03/16/18/img/addAFirewallRule.jpg\" alt=\"Add a firewall rule\" width=\"1604\" height=\"722\"></div><div>Published: 4:18 PM GMT · May 3, 2024</div>\n";
 ?>
