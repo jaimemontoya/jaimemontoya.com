349e1785f30da1b2139daa523be63d3c2335e4a2
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-06 22:43:07 -0600

    Show results retrieved from database.

--- blog/2024/05/03/16/18/book-info-20240503-1618.php
+++ blog/2024/05/03/16/18/book-info-20240503-1618.php
@@ -1372,6 +1372,5 @@ root@jaimemontoya:/var/www/jaimemontoya.com# service apache2 restart
     sqlsrv_free_stmt($getResults);
 &quest;>
 </pre>
-		   <p>The connection was successful, retrieving data from a table in the database:</p>
-		   <div>Published: 4:18 PM GMT · May 3, 2024</div>\n";
+		   <p>The connection was successful, retrieving data from a table in the database:</p><div><img src=\"/blog/2024/05/03/16/18/img/resultsRetrievedFromDatabase.jpg\" alt=\"Results retrieved from database\" width=\"461\" height=\"987\"></div><div>Published: 4:18 PM GMT · May 3, 2024</div>\n";
 ?>
