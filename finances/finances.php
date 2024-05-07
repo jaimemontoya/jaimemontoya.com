dbfb1dcf53c6c0a30c371f177f66e8522c37a407
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-07 16:51:06 -0600

    Add label to specify report type.

--- finances/finances.php
+++ finances/finances.php
@@ -4,6 +4,7 @@
   $finances->content .=
   "\t\t\t<h1>Finances</h1>
   \t\t<form action=\""."index.php"."\" method=\"GET\">
+  \t\t\t<label for=\"reporttype\">Choose a report type:</label>
   \t\t</form>
   ";
   
