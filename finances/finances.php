589334dd8493ec6488c149d9eb343784cc538254
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-07 17:25:14 -0600

    Add validation.

--- finances/finances.php
+++ finances/finances.php
@@ -38,9 +38,9 @@
 	  $sqlGetCategories = "SELECT * FROM DimCategory WHERE CategoryID IN (SELECT DISTINCT CategoryID FROM FactSalesXDimCategory) ORDER BY CategoryName ASC;";
 	}
 	$resultCategories = sqlsrv_query($conn, $sqlGetCategories);
-	/*if ($resultCategories == FALSE)
+	if ($resultCategories == FALSE)
       echo (sqlsrv_errors());
-    print_r($resultCategories);*/
+    /*print_r($resultCategories);*/
 	/*if ($resultCategories->num_rows > 0) {
       echo "Greater than 0";
     } else {
