3e5b507692b33c3d694fb423696db570d2c5fde8
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-06 22:50:37 -0600

    Add code.

--- blog/2024/05/03/16/18/book-info-20240503-1618.php
+++ blog/2024/05/03/16/18/book-info-20240503-1618.php
@@ -1344,7 +1344,14 @@ root@jaimemontoya:/var/www/jaimemontoya.com# service apache2 restart
 <pre>
 <&quest;php
 	ini_set('display_errors', 1);
-	
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	$serverName = \"jaimemontoya.database.windows.net\"; // update me
+	$connectionOptions = array(
+        \"Database\" => \"Finances\", // update me
+        \"Uid\" => \"fakeuservalue\", // update me
+        \"PWD\" => \"MyPassword!9\" // update me
+    );
 &quest;>
 </pre>
 		   <p>The connection was successful, retrieving data from a table in the database:</p><div><img src=\"/blog/2024/05/03/16/18/img/resultsRetrievedFromDatabase.jpg\" alt=\"Results retrieved from database\" width=\"461\" height=\"987\"></div><div>Published: 4:18 PM GMT · May 3, 2024</div>\n";
