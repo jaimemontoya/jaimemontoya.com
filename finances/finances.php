d79df998d5f346ba91310296e0d870afd3971cd9
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-07 16:56:16 -0600

    Add controls to the form.

--- finances/finances.php
+++ finances/finances.php
@@ -5,7 +5,30 @@
   "\t\t\t<h1>Finances</h1>
   \t\t<form action=\""."index.php"."\" method=\"GET\">
   \t\t\t<label for=\"reporttype\">Choose a report type:</label>
+  \t\t\t<select id=\"reporttype\" name=\"reporttype\">
+  \t\t\t\t<option value=\"None\">[Choose report type]</option>
+  \t\t\t\t<option value=\"Expenses\"";
+  if($_GET["reporttype"]=="Expenses"){
+    $finances->content .=
+    " selected";
+  }
+  $finances->content .=
+  ">Expenses</option>
+  \t\t\t\t<option value=\"Income\"";
+  if($_GET["reporttype"]=="Income"){
+    $finances->content .=
+    " selected";
+  }
+  $finances->content .=
+  ">Income</option>
+  \t\t\t</select>
+  \t\t\t<div>Date format 'Y-m-d', e.g. 2023-01-18</div>
+  \t\t\t<div>Start date:</div>
+  \t\t\t<div><input type=\"text\" name=\"startDateKey\" class=\"widthauto\" value=\"".$_GET['startDateKey']."\"></div>
+  \t\t\t<div>End date:</div>
+  \t\t\t<div><input type=\"text\" name=\"endDateKey\" class=\"widthauto\" value=\"".$_GET['endDateKey']."\"></div>\n";
+  $finances->content .=
+  "\t\t\t<div><input name=\"submit\" type=\"submit\" value=\"Submit\" id=\"submit\"></div>
   \t\t</form>
   ";
-  
 ?>
