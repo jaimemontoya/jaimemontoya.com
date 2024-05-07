65e35cc49e7d73af2c4eaf9246bfdb2eb1f0bef0
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-06 19:48:06 -0600

    Write SQL query.

--- sqltest.php
+++ sqltest.php
@@ -13,12 +13,12 @@
 	
 	
     $tsql= "SELECT DayID, FullDateAlternateKey FROM DimDay ORDER BY DayID DESC";
-    $getResults= sqlsrv_query($conn, $tsql);
+    /*$getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
     if ($getResults == FALSE)
         echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
      echo ($row['DayID'] . " " . $row['FullDateAlternateKey'] . PHP_EOL);
     }
-    sqlsrv_free_stmt($getResults);
+    sqlsrv_free_stmt($getResults);*/
 ?>
