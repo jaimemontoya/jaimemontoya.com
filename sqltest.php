30c08a9e49944c2ab413b633577ca984b42c7f42
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-06 20:28:55 -0600

    Declare global variable.

--- sqltest.php
+++ sqltest.php
@@ -2,6 +2,7 @@
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
+	global $conn;
 	$serverName = "jaimemontoya.database.windows.net"; // update me
 	$connectionOptions = array(
         "Database" => "Finances", // update me
