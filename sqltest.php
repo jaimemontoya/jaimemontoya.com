41b8ab3e02abf2d46945221fafb4aa3238daadfc
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-06 22:12:39 -0600

    Display PHP info.

--- sqltest.php
+++ sqltest.php
@@ -1,6 +1,6 @@
 <?php
 	//phpinfo();
-	ini_set('display_errors', 1);
+	/*ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 	$serverName = "jaimemontoya.database.windows.net"; // update me
@@ -26,5 +26,5 @@
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
      echo ($row['DayID'] . "<br />");
     }
-    sqlsrv_free_stmt($getResults);
+    sqlsrv_free_stmt($getResults);*/
 ?>
