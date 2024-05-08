0ad745b2f775af0bfc031ba2ae37bfa762d9b822
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-07 19:35:00 -0600

    Print GET[category].

--- finances/finances.php
+++ finances/finances.php
@@ -55,7 +55,8 @@
 		if (in_array('12.4', $a, true)) {
           echo "'12.4' found with strict check\n";
         }
-		if (in_array($row['CategoryID'], $_GET['category'], true)) {
+		echo $_GET['category'];
+		if (in_array($row['CategoryID'], $a, true)) {
           echo "1.13 found with strict check\n";
         }
 	  }
