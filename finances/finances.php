f3857026d52d6fef34d68cc64d2f2156573e650d
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-07 19:21:58 -0600

    Comment out code.

--- finances/finances.php
+++ finances/finances.php
@@ -45,12 +45,12 @@
       while ($row = sqlsrv_fetch_array($resultCategories, SQLSRV_FETCH_ASSOC)) {
         $finances->content .=
         "\t\t\t\t\t<input type=\"checkbox\" name=\"category[]\" value=\"".$row['CategoryID']."\" ";
-        if (in_array($row['CategoryID'], $_GET['category'])) {
+        /*if (in_array($row['CategoryID'], $_GET['category'])) {
           $finances->content .=
           "checked";
         }
         $finances->content .=
-        " />".$row['CategoryName']." \n";
+        " />".$row['CategoryName']." \n";*/
 	  }
     } else {
       $finances->content .=
