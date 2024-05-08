00dd3b075358c5b81ab0d1e8d2d6c00fbb9b791d
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-07 20:03:16 -0600

    Add logging.

--- finances/finances.php
+++ finances/finances.php
@@ -1,5 +1,7 @@
 <?php
-  session_start();
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
   include '../../db.inc';
   $finances->content .=
   "\t\t\t<h1>Finances</h1>
