221f42fe6fdd44bab46adc85a0aabfafeb463f57
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-07 19:33:00 -0600

    Test in_array().

--- finances/finances.php
+++ finances/finances.php
@@ -55,7 +55,7 @@
 		if (in_array('12.4', $a, true)) {
           echo "'12.4' found with strict check\n";
         }
-		if (in_array(1.13, $a, true)) {
+		if (in_array($row['CategoryID'], $a, true)) {
           echo "1.13 found with strict check\n";
         }
 	  }
