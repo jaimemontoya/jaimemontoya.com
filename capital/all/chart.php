11d0582545b523c80216358cd93022591381c39e
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-08 20:26:06 -0600

    Remove dollar sign.

--- capital/all/chart.php
+++ capital/all/chart.php
@@ -1,4 +1,4 @@
 <?php
   $weight->content .= 
-           "\t\t\t<h1>Jaime Montoya's all-time capital history by year</h1><div><div id=\"capital-all-years\"></div></div><table></table><table><tr><th>Date</th><th>Capital</th><th>Performance</th></tr><tr><td>2024-05-08</td><td>$5,033.90</td><td>24.9578%</td></tr><tr><td>2024-05-07</td><td>$4,028.48</td><td>0.1123%</td></tr><tr><td>$2024-05-06</td><td>$4,023.96</td><td>0.0030%</td></tr><tr><td>2024-05-05</td><td>$4,023.84</td><td>0.0000%</td></tr></table>\n";
+           "\t\t\t<h1>Jaime Montoya's all-time capital history by year</h1><div><div id=\"capital-all-years\"></div></div><table></table><table><tr><th>Date</th><th>Capital</th><th>Performance</th></tr><tr><td>2024-05-08</td><td>$5,033.90</td><td>24.9578%</td></tr><tr><td>2024-05-07</td><td>$4,028.48</td><td>0.1123%</td></tr><tr><td>2024-05-06</td><td>$4,023.96</td><td>0.0030%</td></tr><tr><td>2024-05-05</td><td>$4,023.84</td><td>0.0000%</td></tr></table>\n";
 ?>
