9a1351a316212b288c8b7673f07746569362d312
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-22 11:32:20 -0600

    Show table of contents.

--- teaching/genesis-36-lesson/genesis-36-lesson.php
+++ teaching/genesis-36-lesson/genesis-36-lesson.php
@@ -19,5 +19,20 @@
 				</ul>
 				<h2>Scripture quotations</h2>
 				<p>All Scripture quotations outside of references from bibliography, unless otherwise indicated, are taken from the <span class=\"italic\">Holy Bible: New International Version</span>&reg;. NIV&reg;. Copyright &copy; 1973, 1978, 1984 by International Bible Society. Used by permission of Zondervan. All rights reserved.</p>
+				<h2>Table of contents</h2>
+				<ul>
+					<li>Purpose of biblical genealogies
+					<li>Why an entire chapter given to Esau’s descendants? (36:1-43)
+					<li>How could Moses have written Genesis 36? (36:1-43)
+					<li>Why was Esau called Edom? (36:1)
+					<li>Why did Esau marry Canaanite women? (36:2)
+					<li>What is the historic and prophetic relationship between Israel and Edom (36:6-8)
+					<li>Why did God bless Esau if He knew that the Edomites would not \"return the favor\" and bless Israel? What does this teach readers about Who God is and How He thinks and acts? (36:6-8)
+					<li>What is the connection between Esau (that is, Edom), and Seir? (36:8-9; 36:20-21; 36:25; 36:30)
+					<li>What is the connection between Esau (that is, Edom), and the Amalekites? (36:12, 16)
+					<li>Why two lists of chiefs? (36:15-19; 36:40-43)
+					<li>How to interpret the Greek word βέβηλος bebēlos used in Hebrews 12:16 to describe Esau?
+					<li>How to understand Romans 9:13, where God “hated” Esau?
+				</ul>
             </div>\n";
 ?>
