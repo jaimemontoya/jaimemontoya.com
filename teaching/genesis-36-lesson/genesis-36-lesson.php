df67dd157d08d8a5d461f6857539c9d34fc5404f
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-22 11:19:24 -0600

    Show where Scripture is taken from.

--- teaching/genesis-36-lesson/genesis-36-lesson.php
+++ teaching/genesis-36-lesson/genesis-36-lesson.php
@@ -5,5 +5,19 @@
 				<ul>
 					<li>Union Church of San Salvador
 				</ul>
+				<h2>Date</h2>
+				<ul>
+					<li>May 23, 2024
+				</ul>
+				<h2>Lead Class Instructor</h2>
+				<ul>
+					<li>Theodore Jedlicka
+				</ul>
+				<h2>Facilitator</h2>
+				<ul>
+					<li>Jaime Montoya
+				</ul>
+				<h2>Scripture quotations</h2>
+				<p>All Scripture quotations outside of references from bibliography, unless otherwise indicated, are taken from the Holy Bible: New International Version®. NIV®. Copyright &copy; 1973, 1978, 1984 by International Bible Society. Used by permission of Zondervan. All rights reserved.</p>
             </div>\n";
 ?>
