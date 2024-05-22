a604ad171f735d21a9a52e7cb880ee36b6c708f5
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-22 11:14:33 -0600

    Add li element.

--- teaching/genesis-36-lesson/genesis-36-lesson.php
+++ teaching/genesis-36-lesson/genesis-36-lesson.php
@@ -2,5 +2,8 @@
   $index->content .= 
            "\t\t\t<h1>Genesis 36 lesson</h1><div>
 				<h2>Location</h2>
+				<ul>
+					<li>Union Church of San Salvador
+				</ul>
             </div>\n";
 ?>
