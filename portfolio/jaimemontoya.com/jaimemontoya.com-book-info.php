ffea36a223c9153ee3f99b0a5157b3f8f5708884
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-28 11:18:41 -0600

    Use Visit website link.

--- portfolio/jaimemontoya.com/jaimemontoya.com-book-info.php
+++ portfolio/jaimemontoya.com/jaimemontoya.com-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>jaimemontoya.com</span>
-					<p>By Jaime Montoya. <mark>Website 1; created, designed, developed, upgraded, maintained and hosted by Jaime Montoya</mark> (DigitalOcean IP 138.197.165.222; 1 other site hosted on this server; Toronto, Ontario, Canada; since 1:05 PM January 5, 2018 GMT).</p>
+					<p>By Jaime Montoya. <mark>Website 1; created, designed, developed, upgraded, maintained and hosted by Jaime Montoya</mark> (DigitalOcean IP 138.197.165.222; 1 other site hosted on this server; Toronto, Ontario, Canada; since 1:05 PM January 5, 2018 GMT). <span class=\"linkstyle\">Visit website</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></p>
                 </div>
             </div></a>\n";
 ?>
--- portfolio/unionchurchss.org/unionchurchss.org-book-info.php
+++ portfolio/unionchurchss.org/unionchurchss.org-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>unionchurchss.org</span>
-					<p>By Jaime Montoya. <mark>Website 2; created, developed, upgraded and maintained by Jaime Montoya</mark> (InMotion Hosting IP 173.231.209.31; 343 other sites hosted on this server; Ashburn, Virginia, United States; since June 9, 2019 GMT).</p>
+					<p>By Jaime Montoya. <mark>Website 2; created, developed, upgraded and maintained by Jaime Montoya</mark> (InMotion Hosting IP 173.231.209.31; 343 other sites hosted on this server; Ashburn, Virginia, United States; since June 9, 2019 GMT). <span class=\"linkstyle\">Visit website</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></p>
                 </div>
             </div></a>\n";
 ?>
