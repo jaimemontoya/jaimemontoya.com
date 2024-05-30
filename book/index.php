84bf084d846ebcbd82ca4fdd1cb740193620bef5
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-05-29 18:27:40 -0600

    Add elements.

--- book/index.php
+++ book/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "Book reviews";
   $index->metaDescription = "Reviews for books read by Jaime Montoya from cover to cover.";
-  $index->content .= "<h1>Book reviews</h1>";
+  $index->content .= "<h1>Book reviews</h1><p>Reviews for all books I have read from cover to cover.</p>";
   require("american-prometheus/american-prometheus-book-info.php");
   $index->Display();
 ?>
