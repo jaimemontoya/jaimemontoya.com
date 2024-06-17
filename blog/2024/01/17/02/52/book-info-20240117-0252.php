509e020f7dc53e110935ed29bd2fb0fbbb4d97e0
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-17 15:35:09 -0600

    Format code properly.

--- blog/2024/01/17/02/52/book-info-20240117-0252.php
+++ blog/2024/01/17/02/52/book-info-20240117-0252.php
@@ -1,7 +1,7 @@
 <?php
   $index->content .= 
            "<p>I completed the first 8 steps of the <span class=\"cod\">Facebook Login for Android - Quickstart</span> available at <a href=\"https://developers.facebook.com/docs/facebook-login/android/\">https://developers.facebook.com/docs/facebook-login/android/</a>. After that, I continued with the remaining steps shared below.</p><p>In my <span class=\"cod\">SignIn.java</span> file, I added the following code:</p>
-<code>
+<pre>
 ...
 import com.facebook.CallbackManager;
 public class SignIn ...{
@@ -13,6 +13,6 @@ public class SignIn ...{
     ...
   }
 }
-</code>
+</pre>
 		   <div>Published: 2:52 AM GMT · Jan 17, 2024</div>\n";
 ?>
