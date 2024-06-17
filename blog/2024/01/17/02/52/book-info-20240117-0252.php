32eda02af53e2556392db2d541a52e99b7bca9a4
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-17 15:34:12 -0600

    Add code.

--- blog/2024/01/17/02/52/book-info-20240117-0252.php
+++ blog/2024/01/17/02/52/book-info-20240117-0252.php
@@ -1,5 +1,18 @@
 <?php
   $index->content .= 
-           "<p>I completed the first 8 steps of the <span class=\"cod\">Facebook Login for Android - Quickstart</span> available at <a href=\"https://developers.facebook.com/docs/facebook-login/android/\">https://developers.facebook.com/docs/facebook-login/android/</a>. After that, I continued with the remaining steps shared below.</p><p>In my <span class=\"cod\">SignIn.java</span> file, I added <span class=\"cod\">import com.facebook.CallbackManager;</span>. In my <span class=\"cod\">public class SignIn</span>, I defined <span class=\"cod\">private CallbackManager callbackManager;</span>.</p>
+           "<p>I completed the first 8 steps of the <span class=\"cod\">Facebook Login for Android - Quickstart</span> available at <a href=\"https://developers.facebook.com/docs/facebook-login/android/\">https://developers.facebook.com/docs/facebook-login/android/</a>. After that, I continued with the remaining steps shared below.</p><p>In my <span class=\"cod\">SignIn.java</span> file, I added the following code:</p>
+<code>
+...
+import com.facebook.CallbackManager;
+public class SignIn ...{
+  private CallbackManager callbackManager;
+  ...
+  protected void onCreate(...){
+    ...
+	callbackManager = CallbackManager.Factory.create();
+    ...
+  }
+}
+</code>
 		   <div>Published: 2:52 AM GMT · Jan 17, 2024</div>\n";
 ?>
