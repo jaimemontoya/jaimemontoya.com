1ed69a31318c508636e02a6d22dfa56b70daf520
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-17 16:50:10 -0600

    Add code.

--- blog/2024/01/17/02/52/book-info-20240117-0252.php
+++ blog/2024/01/17/02/52/book-info-20240117-0252.php
@@ -2,6 +2,8 @@
   $index->content .= 
            "<p>I completed the first 8 steps of the <span class=\"cod\">Facebook Login for Android - Quickstart</span> available at <a href=\"https://developers.facebook.com/docs/facebook-login/android/\">https://developers.facebook.com/docs/facebook-login/android/</a>. After that, I continued with the remaining steps shared below.</p><p>In my <span class=\"cod\">SignIn.java</span> file, I added the following code:</p>
 <pre>
+package ...;
+import static android.content.ContentValues.TAG;
 ...
 import com.facebook.CallbackManager;
 public class SignIn ...{
