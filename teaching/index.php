3457a3fe79933d4313c2ed7ef47dce076c268a1f
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 14:49:35 -0600

    Add Genesis 38 lesson to catalog list.

--- teaching/index.php
+++ teaching/index.php
@@ -4,6 +4,7 @@
   $index->title = "Teaching";
   $index->metaDescription = "In-person classes taught by Jaime Montoya.";
   $index->content .= "<h1>Teaching</h1><p>In-person classes I have taught.</p>";
+  require("genesis-38-lesson/genesis-38-lesson-book-info.php");
   require("genesis-36-lesson/genesis-36-lesson-book-info.php");
   $index->Display();
 ?>
