471fb11a1aaece949ad49a8b46270ad4725c3cf8
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 16:49:32 -0600

    Add AB Testing book.

---
+++ reading/ab-testing-the/ab-testing-the-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+                <div class=\"book-thumbnail\">
+	                <div class=\"book-cover\" id=\"ab-testing-the-img\"></div>
+                </div>
+                <div class=\"book-title-authors-read-by\">
+	                <span>A/B Testing: The Most Powerful Way to Turn Clicks Into Customers</span>
+	                <p>By Dan Siroker and Pete Koomen. <mark>Book 8 read by Jaime Montoya</mark> from 4:37 AM November 8, 2019 to 1:02 PM November 15, 2019 GMT.</p>
+                </div>
+            </div>\n";
+?>
