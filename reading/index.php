3f4548bfa1c1729c57534f5a1a36aed8dd0e8da9
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 16:32:42 -0600

    Add Twitter 101 book.

--- reading/index.php
+++ reading/index.php
@@ -5,6 +5,7 @@
   $index->metaDescription = "Listing all of the books read by Jaime Montoya from cover to cover.";
   $index->content .= "<h1>Reading</h1><p>Listing all of the books I have read from cover to cover.</p>";
   require("american-prometheus/american-prometheus-book-info.php");
+  require("twitter-101-everything/twitter-101-everything-book-info.php");
   require("born-to-blog/born-to-blog-book-info.php");
   require("success-in-programming/success-in-programming-book-info.php");
   $index->Display();
---
+++ reading/twitter-101-everything/twitter-101-everything-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+                <div class=\"book-thumbnail\">
+	                <div class=\"book-cover\" id=\"twitter-101-everything-img\"></div>
+                </div>
+                <div class=\"book-title-authors-read-by\">
+	                <span>Twitter 101: Everything You Need to Know about Twitter Marketing</span>
+	                <p>By Lauren Dugan. <mark>Book 3 read by Jaime Montoya</mark> from 9:14 AM September 15, 2018 to 10:24 PM September 16, 2018 GMT.</p>
+                </div>
+            </div>\n";
+?>
