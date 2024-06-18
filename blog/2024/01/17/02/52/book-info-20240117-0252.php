c0c83aa0067f55d1e14b4207da4087f505d265d9
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-17 18:38:31 -0600

    Add code to .java file.

--- blog/2024/01/17/02/52/book-info-20240117-0252.php
+++ blog/2024/01/17/02/52/book-info-20240117-0252.php
@@ -1,6 +1,6 @@
 <?php
   $index->content .= 
-           "<p>I completed the first 8 steps of the <span class=\"cod\">Facebook Login for Android - Quickstart</span> available at <a href=\"https://developers.facebook.com/docs/facebook-login/android/\">https://developers.facebook.com/docs/facebook-login/android/</a>. After that, I continued with the remaining steps shared below.</p><p>In my <span class=\"cod\">SignIn.java</span> file, I added the following code:</p>
+           "<p>I completed the first 8 steps of the <span class=\"cod\">Facebook Login for Android - Quickstart</span> available at <a href=\"https://developers.facebook.com/docs/facebook-login/android/\">https://developers.facebook.com/docs/facebook-login/android/</a>. After that, I continued with the remaining steps shared below.</p><p>I added the following code to my <span class=\"cod\">SignIn.java</span> file:</p>
 <pre>
 package ...;
 import static android.content.ContentValues.TAG;
@@ -9,10 +9,12 @@ import com.facebook.CallbackManager;
 public class SignIn ...{
   private CallbackManager callbackManager;
   private static final String EMAIL = \"email\";
+  private LoginButton loginButton;
   ...
   protected void onCreate(...){
     ...
 	callbackManager = CallbackManager.Factory.create();
+	loginButton = (LoginButton) findViewById(R.id.fbLoginButton);
     ...
   }
 }
