a7cc6c0dc55a651c64d4af8ee734b4497ee4efce
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 16:37:19 -0600

    Add Scaling Up book.

---
+++ reading/scaling-up-companies-rockefeller-habits/scaling-up-companies-rockefeller-habits-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+                <div class=\"book-thumbnail\">
+	                <div class=\"book-cover\" id=\"scaling-up-companies-rockefeller-habits-img\"></div>
+                </div>
+                <div class=\"book-title-authors-read-by\">
+	                <span>Scaling Up: How a Few Companies Make It...and Why the Rest Don't (Mastering the Rockefeller Habits 2.0)</span>
+	                <p>By Verne Harnish. <mark>Book 5 read by Jaime Montoya</mark> from 5:42 PM February 28, 2019 to 02:57 AM March 23, 2019 GMT.</p>
+                </div>
+            </div>\n";
+?>
