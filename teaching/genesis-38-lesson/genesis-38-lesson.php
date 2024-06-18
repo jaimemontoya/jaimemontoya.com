0251c8d5dff85404db1dfaca0484d4ecb21327cc
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 11:46:30 -0600

    Add Genesis 38 lesson.

---
+++ teaching/genesis-38-lesson/genesis-38-lesson.php
@@ -0,0 +1,69 @@
+<?php
+  $index->content .= 
+           "\t\t\t<h1>Genesis 38 lesson</h1><div>
+				<h2>Location</h2>
+				<ul>
+					<li>Union Church of San Salvador
+				</ul>
+				<h2>Date</h2>
+				<ul>
+					<li>June 13, 2024
+				</ul>
+				<h2>Lead Class Instructor</h2>
+				<ul>
+					<li>Theodore Jedlicka
+				</ul>
+				<h2>Facilitator</h2>
+				<ul>
+					<li>Jaime Montoya
+				</ul>
+				<h2>Scripture quotations</h2>
+				<p>All Scripture quotations outside of references from bibliography, unless otherwise indicated, are taken from the <span class=\"italic\">Holy Bible: New International Version</span>&reg;. NIV&reg;. Copyright &copy; 1973, 1978, 1984 by International Bible Society. Used by permission of Zondervan. All rights reserved.</p>
+				<h2>Table of contents</h2>
+				<ul>
+					<li><a href=\"#levirate-marriage-in-ancient-israelite-society\">Levirate marriage in ancient Israelite society</a>
+					<li><a href=\"#even-if-god-did-not-institute-levirate-marriage-why-does-he-condone-it\">Even if God did not institute levirate marriage, why does he condone it?</a>
+					<li><a href=\"#honorability-and-righteousness-versus-economic-self-interest\">Honorability and righteousness versus economic self-interest</a>
+					<li><a href=\"#where-did-judah-find-his-wife\">Where did Judah find his wife? (38:1-2)</a>
+					<li><a href=\"#wicked-associations-resulting-in-spiritual-contamination\">Wicked associations resulting in spiritual contamination (38:1-2, 12, 20-22)</a>
+					<li><a href=\"#giving-names-to-each-of-their-children\">Giving names to each of their children (38:3-5)</a>
+					<li><a href=\"#why-did-judah-fear-that-shelah-could-experience-the-same-fate-as-his-brothers\">Why did Judah fear that Shelah could experience the same fate as his brothers? (38:11)</a>
+					<li><a href=\"#reputation-with-men-versus-good-standing-with-god\">Reputation with men versus good standing with God (38:12-23)</a>
+					<li><a href=\"#what-kind-of-prostitute-did-tamar-pretend-to-be\">What kind of prostitute did Tamar pretend to be? (38:21)</a>
+					<li><a href=\"#did-god-also-kill-shelah\">Did God also kill Shelah? (38:26)</a>
+					<li><a href=\"#pattern-of-second-son-being-greater-than-the-firstborn\">Pattern of second son being greater than the firstborn (38:27-30)</a>
+				</ul>
+				<h2 id=\"levirate-marriage-in-ancient-israelite-society\">Levirate marriage in ancient Israelite society</h2>
+				<p>This custom or law was used in ancient Israelite society “to perpetuate the line of a man who died without offspring.” (Encyclopædia Britannica, n.d., levirate).</p>
+				<p>Weisberg (2009) explains that the Israelites did not invent levirate, but they adapted it to their needs. She provides reasons that reveal why it is not surprising that Israelite society practiced levirate:</p>
+				<ul class=\"quote\">
+					<li>Continuity of the family and preservation of familial land were important.
+					<li>Inheritance was patrilineal.
+					<li>Wives were not recognized as their husband’s heirs. (Chapter 1)
+				</ul>
+				<p>Levirate marriage “was designed to prevent extinction of the family name and property” (Nelson, 2017, p. 351). According to Deuteronomy 25:5-10, its primary goal was “to provide the deceased with an heir.” (Weisberg, 2009, Chapter 2). The text acknowledges that it is possible that “the man does not want to marry his brother’s [widowed] wife” (Deuteronomy 25:7, Amplified Bible). Weisberg (2009) says that based on this Deuteronomy passage, the man is not obliged to offer an explanation, but he could simply say, “I do not want to marry her”, even when that would be “a refusal to honor his obligations to his brother”. Weisberg provides an interpretation that rationalizes why a man might refuse to marry his brother’s widow:</p>
+				<p class=\"quote\">One could argue that levirate marriage is a completely altruistic act on the part of a brother. A man has little or nothing to gain and a great deal to lose by providing his brother with a posthumous heir. The death of a childless man would result in larger portions of the paternal estate going to the surviving brothers. Levirate marriage, if “successful,” results in the original distribution of property being maintained. Furthermore, the “loss” incurred by the levir comes through a child he himself has fathered; the levir is disinherited through his own actions! The only incentive Deuteronomy offers the levir is couched in negative terms; by agreeing to perform levirate marriage, he spares himself -and his descendants- public humiliation. (Chapter 2)</p>
+				<p>Three passages in the Old Testament mention levirate marriage: Genesis 38, Deuteronomy 25, and Ruth. In the New Testament, the Sadducees quoted levirate marriage when they came with a question to Jesus, as described in Matthew 22:23-32, but they were quoting the passage in Deuteronomy 25:5-10.</p>
+				
+				
+				
+				<h2 class=\"center\">References</h2>
+				<div id=\"references\">
+					<p>Crossway Bibles. (2005). <em>The Holy Bible. Life Discovery Bible. ESV.</em></p>
+					<p>Dean, R., & Ballinger, J. (n.d.). <em>The Levirate Marriage Custom.</em> Retrieved June 11, 2024, from <a href=\"https://kukis.org/Doctrines/leviratemarriage.htm\">https://kukis.org/Doctrines/leviratemarriage.htm</a></p>
+					<p>Encyclopædia Britannica. (n.d.). <em>levirate.</em> Retrieved June 7, 2024, from <a href=\"https://www.britannica.com/topic/levirate\">https://www.britannica.com/topic/levirate</a></p>
+					<p>Evans, T., (2019). <em>CSB Tony Evans Study Bible.</em> <a href=\"https://www.amazon.com/CSB-Tony-Evans-Study-Bible-ebook/dp/B07ZPKY6TX/\">https://www.amazon.com/CSB-Tony-Evans-Study-Bible-ebook/dp/B07ZPKY6TX/</a></p>
+					<p>Frymer-Kensky, T. (n.d.). <em>Tamar: Bible.</em> Retrieved June 7, 2024, from <a href=\"https://jwa.org/encyclopedia/article/tamar-bible\">https://jwa.org/encyclopedia/article/tamar-bible</a></p>
+					<p>Henry, M. (1811). <em>An Exposition of the Old and New Testament.</em> <a href=\"https://www.google.com.sv/books/edition/An_Exposition_of_the_Old_and_New_Testame/fg8aY9ZqSMYC?hl=es-419&gbpv=1\">https://www.google.com.sv/books/edition/An_Exposition_of_the_Old_and_New_Testame/fg8aY9ZqSMYC?hl=es-419&gbpv=1</a></p>
+					<p>Kadari, T. (n.d.). <em>Shua's daughter: Midrash and Aggadah.</em> Retrieved June 11, 2024, from <a href=\"https://jwa.org/encyclopedia/article/shuas-daughter-midrash-and-aggadah\">https://jwa.org/encyclopedia/article/shuas-daughter-midrash-and-aggadah</a></p>
+					<p>Walton, J. (2001). <em>The NIV Application Commentary: Genesis.</em> <a href=\"https://www.amazon.com/Genesis-Application-Commentary-John-Walton-ebook/dp/B004FPZ29A/\">https://www.amazon.com/Genesis-Application-Commentary-John-Walton-ebook/dp/B004FPZ29A/</a></p>
+					<p>Tan, B. (2017). <em>Tamar is Righteous, but Judah is Not: A Narrative Analysis of Genesis 38. A Thesis Submitted to the Faculty in Candidacy for the Degree of Master of Theology. Singapore Bible College.</em> Retrieved June 13, 2024, from <a href=\"https://www.academia.edu/33060247/Tamar_Is_Righteous_but_Judah_Is_Not_A_Narrative_Analysis_of_Genesis_38\">https://www.academia.edu/33060247/Tamar_Is_Righteous_but_Judah_Is_Not_A_Narrative_Analysis_of_Genesis_38</a></p>
+					<p>Thomas Nelson. (2002). <em>NKJV New Spirit-Filled Life Bible</em></p>
+					<p>Thomas Nelson. (2017). <em>The King James Study Bible.</em></p>
+					<p>Waldrip, J. (n.d.). <em>Judah’s friend Hirah.</em> Retrieved June 11, 2024, from <a href=\"https://calvaryroadbaptist.church/sermon.php?sermonID=5\">https://calvaryroadbaptist.church/sermon.php?sermonID=5</a></p>
+					<p>Weisberg, D. (2009). <em>Levirate Marriage and the Family in Ancient Judaism.</em> <a href=\"https://www.amazon.com/Levirate-Marriage-Family-Ancient-Judaism-ebook/dp/B07PQ1ZK57/\">https://www.amazon.com/Levirate-Marriage-Family-Ancient-Judaism-ebook/dp/B07PQ1ZK57/</a></p>
+					<p>Zondervan. (2015). <em>NIV Exhaustive Concordance Dictionary.</em> <a href=\"https://www.biblegateway.com/plus/\">https://www.biblegateway.com/plus/</a></p>
+					<p>Zondervan. (2014). <em>NIV First-Century Study Bible.</em> <a href=\"https://www.amazon.com/NIV-First-Century-Study-Bible-eBook-ebook/dp/B00MTB1YSU/\">https://www.amazon.com/NIV-First-Century-Study-Bible-eBook-ebook/dp/B00MTB1YSU/</a></p>
+				</div>
+            </div>\n";
+?>
