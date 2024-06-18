a459d1e1a06c03e93eb102299cef37c85d838829
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 16:07:51 -0600

    Add books read section.

---
+++ reading/index.php
@@ -0,0 +1,9 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "Reading";
+  $index->metaDescription = "List of books read by Jaime Montoya from cover to cover.";
+  $index->content .= "<h1>Reading</h1><p>Listing all of the books I have read from cover to cover.</p>";
+  require("american-prometheus/american-prometheus-book-info.php");
+  $index->Display();
+?>
