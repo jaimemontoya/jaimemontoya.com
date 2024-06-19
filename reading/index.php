d8863e556f54e8309ac31aedc898c205a3bb5659
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 19:25:52 -0600

    Add book.

--- reading/index.php
+++ reading/index.php
@@ -5,6 +5,7 @@
   $index->metaDescription = "Listing all of the books read by Jaime Montoya from cover to cover.";
   $index->content .= "<h1>Reading</h1><p>Listing all of the books I have read from cover to cover.</p>";
   require("american-prometheus/american-prometheus-book-info.php");
+  require("own-it-love-it-make-it-work/own-it-love-it-make-it-work-book-info.php");
   require("pragmatic-programmer/pragmatic-programmer-book-info.php");
   require("moving-to-canada/moving-to-canada-book-info.php");
   require("immigrating-to-canada/immigrating-to-canada-book-info.php");
