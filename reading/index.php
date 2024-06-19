90e8f383ab64d8b4349d21c7fbb8283abd4a8eb6
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 19:10:54 -0600

    Add book Sacred Marriage.

--- reading/index.php
+++ reading/index.php
@@ -5,6 +5,7 @@
   $index->metaDescription = "Listing all of the books read by Jaime Montoya from cover to cover.";
   $index->content .= "<h1>Reading</h1><p>Listing all of the books I have read from cover to cover.</p>";
   require("american-prometheus/american-prometheus-book-info.php");
+  require("sacred-marriage/sacred-marriage-book-info.php");
   require("software-engineering-scratch/software-engineering-scratch-book-info.php");
   require("success-habits-dummies-zeller/success-habits-dummies-zeller-book-info.php");
   require("niv-holy-bible/niv-holy-bible-book-info.php");
---
+++ reading/sacred-marriage/sacred-marriage-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+                <div class=\"book-thumbnail\">
+	                <div class=\"book-cover\" id=\"sacred-marriage-img\"></div>
+                </div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Sacred Marriage: What If God Designed Marriage to Make Us Holy More Than to Make Us Happy?</span>
+	                <p>By Gary Thomas. <mark>Book 25 read by Jaime Montoya</mark> from 5:20 PM January 22, 2022 to 12:56 AM March 15, 2022 GMT.</p>
+                </div>
+            </div>\n";
+?>
