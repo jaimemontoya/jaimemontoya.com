6ff88337dc6cde8c17872d3d8b04cfd8e4893157
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 19:45:58 -0600

    Add book for Review.

--- book/index.php
+++ book/index.php
@@ -5,5 +5,6 @@
   $index->metaDescription = "Reviews for books read by Jaime Montoya from cover to cover.";
   $index->content .= "<h1>Book reviews</h1><p>My reviews for some of the books I have read from cover to cover.</p>";
   require("american-prometheus/american-prometheus-book-info.php");
+  require("the-pilgrim-s-progress/the-pilgrim-s-progress-book-info.php");
   $index->Display();
 ?>
