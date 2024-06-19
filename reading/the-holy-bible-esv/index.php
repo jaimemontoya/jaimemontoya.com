7587e7e6e52c384a250bc0274d96277b97454e98
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 20:24:57 -0600

    The Holy Bible ESV, add book.

---
+++ reading/the-holy-bible-esv/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "The Holy Bible, English Standard Version";
+  $index->metaDescription = "The English Standard Version (ESV) is an English translation of the Bible. The ESV was published in 2001 by Crossway, having been \"created by a team of more than 100 leading evangelical scholars and pastors.\"";
+  require("the-holy-bible-esv-book-info.php");
+  $index->Display();
+?>
---
+++ reading/the-holy-bible-esv/the-holy-bible-esv-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"the-holy-bible-esv-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>The Holy Bible, English Standard Version</span>
+					<p>By Crossway. <mark>Book 53 read by Jaime Montoya</mark> from January 1, 2022 to 1:16 AM June 11, 2023 GMT.</p>
+                </div>
+            </div>\n";
+?>
