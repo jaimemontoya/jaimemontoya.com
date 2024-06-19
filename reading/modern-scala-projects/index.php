af20d9306a8e50376eae885b1a329c5eacbb6ab7
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 19:14:48 -0600

    Add book Modern Scala Projects.

---
+++ reading/modern-scala-projects/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "Modern Scala Projects: Leverage the power of Scala for building data-driven and high-performant projects";
+  $index->metaDescription = "Modern Scala Projects focuses on the application of supervisory learning ML techniques that classify data and make predictions.";
+  require("modern-scala-projects-book-info.php");
+  $index->Display();
+?>
