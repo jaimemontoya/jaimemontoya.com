c5f3767a47086da7aae479b29b222a2098f22eaf
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 18:43:55 -0600

    Add Pragmatic Scala book.

---
+++ reading/pragmatic-scala/pragmatic-scala-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+                <div class=\"book-thumbnail\">
+	                <div class=\"book-cover\" id=\"pragmatic-scala-img\"></div>
+                </div>
+                <div class=\"book-title-authors-read-by\">
+	                <span>Pragmatic Scala: Create Expressive, Concise, and Scalable Applications. 1st. Edition.</span>
+	                <p>By Venkat Subramaniam. <mark>Book 17 read by Jaime Montoya</mark> from 2:33 AM May 31, 2021 to 5:48 AM July 6, 2021 GMT.</p>
+                </div>
+            </div>\n";
+?>
