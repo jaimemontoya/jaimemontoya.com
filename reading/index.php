f94143ad80f094334c44dbaf47a898ff16ef9cce
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 19:30:50 -0600

    Specify since when I started reading the books I am listing.

--- reading/index.php
+++ reading/index.php
@@ -2,8 +2,8 @@
   require("../page.php");
   $index = new Page();
   $index->title = "Reading";
-  $index->metaDescription = "Listing all of the books read by Jaime Montoya from cover to cover.";
-  $index->content .= "<h1>Reading</h1><p>Listing all of the books I have read from cover to cover.</p>";
+  $index->metaDescription = "Listing all of the books read by Jaime Montoya from cover to cover since August 31, 2018.";
+  $index->content .= "<h1>Reading</h1><p>Listing all of the books I have read from cover to cover since August 31, 2018.</p>";
   require("american-prometheus/american-prometheus-book-info.php");
   require("it-starts-with-passion/it-starts-with-passion-book-info.php");
   require("own-it-love-it-make-it-work/own-it-love-it-make-it-work-book-info.php");
