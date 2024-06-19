65fb4056ea6bef136c07a4c324ba0e3a5935bcc0
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-19 09:57:41 -0600

    Refactor code.

--- reading/blogging-for-dummies/index.php
+++ reading/blogging-for-dummies/index.php
@@ -4,5 +4,7 @@
   $index->title = "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones - Book review";
   $index->metaDescription = "No matter your goals, Atomic Habits offers a proven framework for improving--every day.";
   require("atomic-habits-book-info.php");
+  require("atomic-habits-summary-critique-review.php");
+  require("book-reviews-bibliography.php");
   $index->Display();
 ?>
