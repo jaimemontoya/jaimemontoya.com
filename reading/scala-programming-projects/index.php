cca44bee8a97950ef39d06d82e89cb07d529ed35
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 18:54:09 -0600

    Add book.

---
+++ reading/scala-programming-projects/index.php
@@ -0,0 +1,9 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "Scala Programming Projects";
+  $index->metaDescription = "Discover unique features and powerful capabilities of Scala Programming as you build projects in a wide range of domains.";
+  require("scala-programming-projects-book-info.php");
+  $index->Display();
+?>
+
