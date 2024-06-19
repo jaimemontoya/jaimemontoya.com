1b72d3c635bd0e64bb7c981528929e1e2239101e
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 19:09:19 -0600

    Add book Software Engineering from Scratch.

---
+++ reading/software-engineering-scratch/software-engineering-scratch-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+                <div class=\"book-thumbnail\">
+	                <div class=\"book-cover\" id=\"software-engineering-scratch-img\"></div>
+                </div>
+                <div class=\"book-title-authors-read-by\">
+	                <span>Software Engineering from Scratch: A Comprehensive Introduction Using Scala</span>
+		            <p>By Jason Lee Hodges. <mark>Book 24 read by Jaime Montoya</mark> from 4:47 PM January 1, 2022 GMT to 1:01 PM January 28, 2022 GMT.</p>
+                </div>
+            </div>\n";
+?>
