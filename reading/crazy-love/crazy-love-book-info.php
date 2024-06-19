3f92adcb563c9d445e56df3a922a1560696e342b
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 19:55:14 -0600

    Add book Crazy Love.

---
+++ reading/crazy-love/crazy-love-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"crazy-love-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Crazy Love: Overwhelmed by a Relentless God</span>
+					<p>By Francis Chan. <mark>Book 39 read by Jaime Montoya</mark> from 12:04 AM October 4, 2022 to 2:36 PM October 8, 2022 GMT.</p>
+                </div>
+            </div>\n";
+?>
