b5351cc7cad5fdeb69cadbe512e55447804cb908
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 19:50:06 -0600

    Add parent page to generate breadcrumb properly.

--- book/the-pilgrim-s-progress/index.php
+++ book/the-pilgrim-s-progress/index.php
@@ -2,6 +2,7 @@
   require("../../page.php");
   $index = new Page();
   $index->title = "The Pilgrim's Progress: A Readable Modern-Day Version of John Bunyan’s Pilgrim’s Progress";
+  $index->parentPage = "Book reviews";
   $index->metaDescription = "The story chronicles the epic adventure of a man named Christian who leaves his home in the City of Destruction and begins a life-long quest to the Celestial City.";
   require("the-pilgrim-s-progress-book-info.php");
   require("the-pilgrim-s-progress-summary-critique-review.php");
