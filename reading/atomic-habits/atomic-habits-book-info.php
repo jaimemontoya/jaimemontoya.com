833b48b6401058d55f772545831e3ae23c81312b
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-18 20:38:36 -0600

    Add book Atomic Habits.

---
+++ reading/atomic-habits/atomic-habits-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<!--<a href=\"/atomic-habits/\">--><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"atomic-habits-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</span>
+					<p>By James Clear. <mark>Book 60 read by Jaime Montoya</mark> from 5:23 PM October 19, 2023 to 1:37 PM October 31, 2023 GMT.</p>
+                </div>
+            </div><!--</a>-->\n";
+?>
---
+++ reading/atomic-habits/book-reviews-bibliography.php
@@ -0,0 +1,4 @@
+<?php
+  $index->content .= 
+           $index->BookReviewsBibliography();
+?>
