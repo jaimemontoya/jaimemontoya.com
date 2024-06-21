6194399242f1c2e980bee0f666b73d670083287a
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 00:34:35 -0600

    Update book number.

--- reading/rich-dad-poor-dad/rich-dad-poor-dad-book-info.php
+++ reading/rich-dad-poor-dad/rich-dad-poor-dad-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!</span>
-					<p>By Robert T. Kiyosaki. <mark>Book 65 read by Jaime Montoya</mark> from 6:02 PM January 30, 2024 to 5:46 PM February 24, 2024 GMT.</p>
+					<p>By Robert T. Kiyosaki. <mark>Book 66 read by Jaime Montoya</mark> from 6:02 PM January 30, 2024 to 5:46 PM February 24, 2024 GMT.</p>
                 </div>
             </div>\n";
 ?>
