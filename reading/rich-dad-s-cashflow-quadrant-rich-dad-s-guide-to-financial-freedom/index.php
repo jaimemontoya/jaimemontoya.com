f104dcbc75e3024a94ff345669dd681bdf7c0790
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 02:13:43 -0600

    Add book Cashblow Quadrant.

---
+++ reading/rich-dad-s-cashflow-quadrant-rich-dad-s-guide-to-financial-freedom/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "Rich Dad's CASHFLOW Quadrant: Rich Dad's Guide to Financial Freedom";
+  $index->metaDescription = "Rich Dad’s CASHFLOW Quadrant is a guide to financial freedom. It’s the second book in the Rich Dad Series and reveals how some people work less, earn more, pay less in taxes, and learn to become financially free.";
+  require("rich-dad-s-cashflow-quadrant-rich-dad-s-guide-to-financial-freedom-book-info.php");
+  $index->Display();
+?>
---
+++ reading/rich-dad-s-cashflow-quadrant-rich-dad-s-guide-to-financial-freedom/rich-dad-s-cashflow-quadrant-rich-dad-s-guide-to-financial-freedom-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"rich-dad-s-cashflow-quadrant-rich-dad-s-guide-to-financial-freedom-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Rich Dad's CASHFLOW Quadrant: Rich Dad's Guide to Financial Freedom</span>
+					<p>By Robert T. Kiyosaki. <mark>Book 72 read by Jaime Montoya</mark> from 11:57 AM April 2, 2024 to 1:09 PM May 5, 2024 GMT.</p>
+                </div>
+            </div>\n";
+?>
