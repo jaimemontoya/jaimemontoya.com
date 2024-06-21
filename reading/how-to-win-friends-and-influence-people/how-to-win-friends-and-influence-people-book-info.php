4bf6cd03f4fd709b7f31eea5a8aeba8133bb59cc
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 00:45:43 -0600

    Update book number.

--- reading/how-to-win-friends-and-influence-people/how-to-win-friends-and-influence-people-book-info.php
+++ reading/how-to-win-friends-and-influence-people/how-to-win-friends-and-influence-people-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>How to Win Friends and Influence People: Updated For the Next Generation of Leaders (Dale Carnegie Books)</span>
-					<p>By Dale Carnegie. <mark>Book 69 read by Jaime Montoya</mark> from 12:15 PM March 14, 2024 to 11:57 AM April 2, 2024 GMT.</p>
+					<p>By Dale Carnegie. <mark>Book 70 read by Jaime Montoya</mark> from 12:15 PM March 14, 2024 to 11:57 AM April 2, 2024 GMT.</p>
                 </div>
             </div>\n";
 ?>
