89aa2ef165353e7848f1e3461ced0c555fc90ff7
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 02:55:38 -0600

    Add Bible for Dummies book.

---
+++ reading/the-bible-for-dummies-for-dummies-lifestyle/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "The Bible For Dummies (For Dummies (Lifestyle))";
+  $index->metaDescription = "The Bible For Dummies appeals to people of all faiths, as well as those who don't practice any particular religion, by providing interfaith coverage of the entire Bible and the often fascinating background information that makes the Bible come alive.";
+  require("the-bible-for-dummies-for-dummies-lifestyle-book-info.php");
+  $index->Display();
+?>
---
+++ reading/the-bible-for-dummies-for-dummies-lifestyle/the-bible-for-dummies-for-dummies-lifestyle-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"the-bible-for-dummies-for-dummies-lifestyle-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>The Bible For Dummies (For Dummies (Lifestyle))</span>
+					<p>By Jeffrey Geoghegan & Michael Homan. <mark>Book 73 read by Jaime Montoya</mark> from 12:47 PM May 3, 2024 to 5:39 AM June 3, 2024 GMT.</p>
+                </div>
+            </div>\n";
+?>
