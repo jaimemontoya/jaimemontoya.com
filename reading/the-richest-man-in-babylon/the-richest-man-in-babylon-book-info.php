b87f3aa827976580dd8e4e9f870552f52913f4fc
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-20 23:02:35 -0600

    Change date.

--- reading/the-richest-man-in-babylon/the-richest-man-in-babylon-book-info.php
+++ reading/the-richest-man-in-babylon/the-richest-man-in-babylon-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>The Richest Man in Babylon</span>
-					<p>By George S. Clason. <mark>Book 66 read by Jaime Montoya</mark> from 6:31 PM February 20, 2024 to 7:21 PM March 5, 2024 GMT.</p>
+					<p>By George S. Clason. <mark>Book 66 read by Jaime Montoya</mark> from 6:31 PM February 20, 2024 to 5:03 AM March 17, 2024 GMT.</p>
                 </div>
             </div>\n";
 ?>
