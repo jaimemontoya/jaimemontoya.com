bb01be4089a681eb9f3324851208fb98c0fff276
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-20 22:55:09 -0600

    Add book How to Win Friends and Influence People.

---
+++ reading/how-to-win-friends-and-influence-people/how-to-win-friends-and-influence-people-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"how-to-win-friends-and-influence-people-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>How to Win Friends and Influence People: Updated For the Next Generation of Leaders (Dale Carnegie Books)</span>
+					<p>By Dale Carnegie. <mark>Book 69 read by Jaime Montoya</mark> from 12:15 PM March 14, 2024 to 11:57 AM April 2, 2024 GMT.</p>
+                </div>
+            </div>\n";
+?>
---
+++ reading/how-to-win-friends-and-influence-people/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "How to Win Friends and Influence People: Updated For the Next Generation of Leaders (Dale Carnegie Books)";
+  $index->metaDescription = "Updated for the first time in more than forty years, Dale Carnegie’s timeless bestseller How to Win Friends and Influence People—a classic that has improved and transformed the personal and professional lives of millions.";
+  require("how-to-win-friends-and-influence-people-book-info.php");
+  $index->Display();
+?>
