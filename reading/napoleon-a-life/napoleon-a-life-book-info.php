7d88208748dfd0e61ce746b40c5c38e0aa654739
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 00:27:27 -0600

    Update book number.

--- reading/napoleon-a-life/napoleon-a-life-book-info.php
+++ reading/napoleon-a-life/napoleon-a-life-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Napoleon: A Life</span>
-					<p>By Andrew Roberts. <mark>Book 62 read by Jaime Montoya</mark> from 6:15 PM December 16, 2023 to 7:44 PM January 23, 2024 GMT.</p>
+					<p>By Andrew Roberts. <mark>Book 63 read by Jaime Montoya</mark> from 6:15 PM December 16, 2023 to 7:44 PM January 23, 2024 GMT.</p>
                 </div>
             </div>\n";
 ?>
