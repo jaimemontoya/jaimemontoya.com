99cf6934b5d4e098bd261202169a856a1ccdd309
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 00:29:30 -0600

    Update book number.

--- reading/personal-finance-101/personal-finance-101-book-info.php
+++ reading/personal-finance-101/personal-finance-101-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Personal Finance 101: From Saving and Investing to Taxes and Loans, an Essential Primer on Personal Finance (Adams 101 Series)</span>
-					<p>By Alfred Mill. <mark>Book 63 read by Jaime Montoya</mark> from 7:44 PM January 23, 2024 to 6:02PM PM January 30, 2024 GMT.</p>
+					<p>By Alfred Mill. <mark>Book 64 read by Jaime Montoya</mark> from 7:44 PM January 23, 2024 to 6:02PM PM January 30, 2024 GMT.</p>
                 </div>
             </div>\n";
 ?>
