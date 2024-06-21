07d8fefdb20a93c5cb3f436a1e94b20778091925
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 00:03:55 -0600

    Update date.

--- reading/blogging-for-dummies/blogging-for-dummies-book-info.php
+++ reading/blogging-for-dummies/blogging-for-dummies-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Blogging For Dummiess. 7th Edition</span>
-					<p>By Amy Lupold Bair. <mark>Book 61 read by Jaime Montoya</mark> from 1:37 PM October 31, 2023 to 6:15 PM December 16, 2023 GMT.</p>
+					<p>By Amy Lupold Bair. <mark>Book 62 read by Jaime Montoya</mark> from 9:29 PM November 15, 2023 to 6:15 PM December 16, 2023 GMT.</p>
                 </div>
             </div>\n";
 ?>
