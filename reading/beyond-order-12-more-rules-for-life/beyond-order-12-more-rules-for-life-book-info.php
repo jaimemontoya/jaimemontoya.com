1749724a55f4cc2171b1efe2dcb88dc227faee3f
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 01:28:10 -0600

    Update book.

--- reading/beyond-order-12-more-rules-for-life/beyond-order-12-more-rules-for-life-book-info.php
+++ reading/beyond-order-12-more-rules-for-life/beyond-order-12-more-rules-for-life-book-info.php
@@ -2,11 +2,11 @@
   $index->content .= 
            "\t\t\t<div class=\"book\">
 				<div class=\"book-thumbnail\">
-					<div class=\"book-cover\" id=\"how-to-win-friends-and-influence-people-img\"></div>
+					<div class=\"book-cover\" id=\"beyond-order-12-more-rules-for-life-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <span>How to Win Friends and Influence People: Updated For the Next Generation of Leaders (Dale Carnegie Books)</span>
-					<p>By Dale Carnegie. <mark>Book 70 read by Jaime Montoya</mark> from 12:15 PM March 14, 2024 to 11:57 AM April 2, 2024 GMT.</p>
+		            <span>Beyond Order: 12 More Rules for Life</span>
+					<p>By Jordan B. Peterson. <mark>Book 71 read by Jaime Montoya</mark> from 5:03 AM March 17, 2024 to 12:47 PM May 3, 2024 GMT.</p>
                 </div>
             </div>\n";
 ?>
