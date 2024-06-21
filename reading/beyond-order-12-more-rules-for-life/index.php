9586eba05fd863e59f4bbd0e08935414d02be105
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-06-21 01:15:29 -0600

    Add book Beond Order.

---
+++ reading/beyond-order-12-more-rules-for-life/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "Beyond Order: 12 More Rules for Life";
+  $index->metaDescription = "In 12 Rules for Life, clinical psychologist and celebrated professor at Harvard and the University of Toronto Dr. Jordan B. Peterson helped millions of readers impose order on the chaos of their lives.";
+  require("beyond-order-12-more-rules-for-life-book-info.php");
+  $index->Display();
+?>
