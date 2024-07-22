3b5e66fe04d99aa2ac950ea6af0f885f1129693a
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-07-22 09:12:37 -0600

    Add book.

---
+++ reading/rich-dads-guide-investing/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "Rich Dad's Guide to Investing: What the Rich Invest in, That the Poor and the Middle Class Do Not!";
+  $index->metaDescription = " Rich Dad’s Guide to Investing, one of the three core titles in the Rich Dad Series, covers the basic rules of investing, how to reduce your investment risk, how to convert your earned income into passive income- plus Rich Dad’s 10 Investor Controls.";
+  require("rich-dads-guide-investing-book-info.php");
+  $index->Display();
+?>
---
+++ reading/rich-dads-guide-investing/rich-dads-guide-investing-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"rich-dads-guide-investing-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Rich Dad's Guide to Investing: What the Rich Invest in, That the Poor and the Middle Class Do Not!</span>
+					<p>By Robert T. Kiyosaki. <mark>Book 74 read by Jaime Montoya</mark> from 1:09 PM May 5, 2024 to 1:26 PM July 22, 2024 GMT.</p>
+                </div>
+            </div>\n";
+?>
