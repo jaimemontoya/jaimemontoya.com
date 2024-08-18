cf89144d477a945f299279cd4ec5cc53d33279b0
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-08-18 00:33:29 -0600

    Add running records.

--- running.php
+++ running.php
@@ -56,7 +56,7 @@ class RunningPage extends Page
 	echo "\t\t\t\t\t['2021',2101.0],\n";
 	echo "\t\t\t\t\t['2022',1575.0],\n";
 	echo "\t\t\t\t\t['2023',1698.8],\n";
-	echo "\t\t\t\t\t['2024',482.6] // 133.4+74.7+81.0+46.6+77.1+69.8\n";
+	echo "\t\t\t\t\t['2024',595.6] // 133.4+74.7+81.0+46.6+77.1+82.3+35.7+64.8\n";
 	echo "\t\t\t\t]);\n";
 	echo "\t\t\t\t// Set chart options\n";
 	echo "\t\t\t\tvar options = {'title':'Jaime Montoya\'s running history by year'};\n";
@@ -77,7 +77,9 @@ class RunningPage extends Page
 	echo "\t\t\t\t\t['Mar',81.0],\n";
 	echo "\t\t\t\t\t['Apr',46.6],\n";
 	echo "\t\t\t\t\t['May',77.1],\n";
-	echo "\t\t\t\t\t['Jun',69.8]\n";
+	echo "\t\t\t\t\t['Jun',82.3],\n";
+	echo "\t\t\t\t\t['Jul',35.7],\n";
+	echo "\t\t\t\t\t['Aug',64.8]\n";
 	echo "\t\t\t\t]);\n";
 	echo "\t\t\t\t// Set chart options\n";
 	echo "\t\t\t\tvar options = {'title':'Jaime Montoya\'s 2024 running history by month'};\n";
