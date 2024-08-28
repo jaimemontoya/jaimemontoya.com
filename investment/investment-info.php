d63dcdc1339f7e55837dd70b44209def3b466752
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-08-28 01:36:39 -0600

    Add table for investment 2.

--- investment/investment-info.php
+++ investment/investment-info.php
@@ -9,12 +9,48 @@
     <th>Date</th>
   </tr>
   <tr>
-    <td>-$250.00</td>
-    <td>12-Apr-24</td>
+    <td>-$2083.76</td>
+    <td>28-Jun-2024</td>
   </tr>
   <tr>
-    <td>-$1,750.00</td>
-    <td>16-Apr-24</td>
+    <td>-$1500.00</td>
+    <td>30-Jul-2024</td>
+  </tr>
+  <tr>
+    <td>-$-3647.79</td>
+    <td>27-Aug-24</td>
+  </tr>
+  <tr>
+    <td>XIRR()</td>
+    <td>0.148605436</td>
+  </tr>
+  <tr>
+    <td>Investment 2</td>
+    <td>$3583.76</td>
+  </tr>
+  <tr>
+    <td>Return 2</td>
+    <td>$3647.79</td>
+  </tr>
+  <tr>
+    <td>Profit</td>
+    <td>$64.03</td>
+  </tr>
+  <tr>
+    <td>ROI 2</td>
+    <td>1.79%</td>
+  </tr>
+  <tr>
+    <td>Yearly profit projection</td>
+    <td>$389.52</td>
+  </tr>
+  <tr>
+    <td>Yearly performance projection</td>
+    <td>10.87%</td>
+  </tr>
+  <tr>
+    <td>Days</td>
+    <td>60</td>
   </tr>
 </table>
 		   
