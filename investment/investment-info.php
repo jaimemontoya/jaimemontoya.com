4f4191564e9c24b190224b8f13b5417c3469bbfe
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-08-28 01:22:36 -0600

    Add table.

--- investment/investment-info.php
+++ investment/investment-info.php
@@ -1,4 +1,22 @@
 <?php
   $index->content .= 
-           "\t\t\t<ul><li>Yearly performance average: 11.13%<li>Total profit: $143.82<li>Total profit per day: $1.081353383</ul><h2>Investment 2</h2>\n";
+           "\t\t\t<ul><li>Yearly performance average: 11.13%<li>Total profit: $143.82<li>Total profit per day: $1.081353383</ul><h2>Investment 2</h2>
+		   
+		   
+<table>
+  <tr>
+    <th>Cash flow</th>
+    <th>Date</th>
+  </tr>
+  <tr>
+    <td>-$250.00</td>
+    <td>12-Apr-24</td>
+  </tr>
+  <tr>
+    <td>-$1,750.00</td>
+    <td>16-Apr-24</td>
+  </tr>
+</table>
+		   
+		   \n";
 ?>
