a34505c534d450762e0a13224aa87413a85ad3c4
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-08-28 01:15:53 -0600

    Display heading for Investment 2.

--- investment/investment-info.php
+++ investment/investment-info.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "\t\t\t<ul><li>Yearly performance average: 11.13%<li>Total profit: $143.82<li>Total profit per day: $1.081353383</ul>\n";
+           "\t\t\t<ul><li>Yearly performance average: 11.13%<li>Total profit: $143.82<li>Total profit per day: $1.081353383</ul><h2>Investment 2</h2>\n";
 ?>
