2ea15202b42a7a6b38ba92bdd701f1d2e948cf2d
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-08-29 16:37:13 -0600

    Fix time.

--- alpha-and-omega/index.php
+++ alpha-and-omega/index.php
@@ -2,7 +2,7 @@
   require("../page.php");
   $index = new Page();
   $index->title = "Alpha and Omega";
-  $index->metaDescription = "My official statement of faith since 10:40 PM August 29, 2024 GMT.";
+  $index->metaDescription = "My official statement of faith since 10:36 PM August 29, 2024 GMT.";
   $index->content .= "<h1>Alpha and Omega</h1><p>My official statement of faith since 10:40 PM August 29, 2024 GMT.</p>";
   require("alpha-and-omega-info.php");
   $index->Display();
