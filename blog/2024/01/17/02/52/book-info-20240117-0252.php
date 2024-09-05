a64d272c475c448794a34778e6d08129d1a4663c
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-09-05 16:53:53 -0600

    Test change.

--- blog/2024/01/17/02/52/book-info-20240117-0252.php
+++ blog/2024/01/17/02/52/book-info-20240117-0252.php
@@ -17,7 +17,7 @@ public class SignIn ...{
     callbackManager = CallbackManager.Factory.create();
     fbLoginButton = (LoginButton) findViewById(R.id.fbLoginButton);
     fbLoginButton.setReadPermissions(Arrays.asList(EMAIL));
-    fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+    fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {a
       @Override
       public void onSuccess(LoginResult loginResult) {
         // App code
