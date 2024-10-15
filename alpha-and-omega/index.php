6915a0cdc0a0815215d911e839e5e5e82657f175
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-10-15 14:14:47 -0600

    Remove Alpha and Omega section.

--- alpha-and-omega/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require("../page.php");
-  $index = new Page();
-  $index->title = "Alpha and Omega";
-  $index->metaDescription = "My official statement of faith since 10:36 PM August 29, 2024 GMT.";
-  $index->content .= "<h1>Alpha and Omega</h1><p>My official statement of faith since 10:36 PM August 29, 2024 GMT.</p>";
-  require("alpha-and-omega-info.php");
-  $index->Display();
-?>
