0991b7d69ec399d744c4f024affd11980d1f9f98
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-10-15 14:22:05 -0600

    Change description for one-page resume.

--- about/index.php
+++ about/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "About";
   $index->metaDescription = "Latest version of my resume, including skills, experience, education, standardized tests and certifications in a one-page size.";
-  $index->content .= "<h1>About</h1><p>My one-page resume available on this website and LinkedIn.</p>";
+  $index->content .= "<h1>About</h1><p>My one-page resume.</p>";
   require("about-info.php");
   $index->Display();
 ?>
