d76cdca1fca22015e52ea5d8fc488b5a6264beba
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-07 10:45:00 -0600

    Add Systematic Theology book.

---
+++ reading/systematic-theology-2nd-introduction-biblical-doctrine/index.php
@@ -0,0 +1,8 @@
+<?php
+  require("../page.php");
+  $index = new Page();
+  $index->title = "Systematic Theology, Second Edition: An Introduction to Biblical Doctrine";
+  $index->metaDescription = "This new edition of Systematic Theology by Wayne Grudem may be the most important resource you can own for helping you understand Scripture and grow as a Christian.";
+  require("systematic-theology-2nd-introduction-biblical-doctrine-book-info.php");
+  $index->Display();
+?>
---
+++ reading/systematic-theology-2nd-introduction-biblical-doctrine/systematic-theology-2nd-introduction-biblical-doctrine-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"systematic-theology-2nd-introduction-biblical-doctrine-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Systematic Theology, Second Edition: An Introduction to Biblical Doctrine</span>
+					<p>By Wayne A. Grudem. <mark>Book [in progress...] read by Jaime Montoya</mark> from 5:39 AM June 3, 2024 to [in progress...] GMT.</p>
+                </div>
+            </div>\n";
+?>
