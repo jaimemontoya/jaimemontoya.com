5fde361cebb0ab57f93b019f76396b3e8ef78f0f
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-06 18:25:21 -0600

    Change page name to News.

--- news/index.php
+++ news/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../page.php");
   $index = new Page();
-  $index->title = "Blog";
+  $index->title = "News";
   $index->metaDescription = "Personal news since November 7, 2024, 00:00 AM GMT.";
   $index->content .= "<h1>News</h1><p>Personal news since November 7, 2024, 00:00 AM GMT.</p>";
   require("2024/11/07/00/00/20241107-0000-book-info.php");
