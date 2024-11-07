127edd54a1de0c92940289fe558ae90bffb3e1b6
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-06 18:23:09 -0600

    Fix image.

--- news/2024/11/07/00/00/book-info-20241107-0000.php
+++ news/2024/11/07/00/00/book-info-20241107-0000.php
@@ -1,4 +1,4 @@
 <?php
   $index->content .= 
-           "<p>On Nov 5, 2024, 1:45 AM GMT-6, I received an email notification confirming that my Interactive Brokers account had been approved:</p><div><img src=\"/blog/2024/11/07/00/00/img/interactiveBrokersAccountApproved.jpg\" alt=\"Interactive Brokers account approved\" width=\"713\" height=\"877\"></div><div>Published: 00:00 AM GMT · Nov 7, 2024</div>\n";
+           "<p>On Nov 5, 2024, 1:45 AM GMT-6, I received an email notification confirming that my Interactive Brokers account had been approved:</p><div><img src=\"/news/2024/11/07/00/00/img/interactiveBrokersAccountApproved.jpg\" alt=\"Interactive Brokers account approved\" width=\"713\" height=\"877\"></div><div>Published: 00:00 AM GMT · Nov 7, 2024</div>\n";
 ?>
