33632358894edb895938239a5f79f986fcba2af1
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-08 16:36:03 -0600

    Add Amplified Bible.

---
+++ reading/amplified-bible/amplified-bible-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"amplified-bible-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Amplified Bible</span>
+					<p>By Zondervan. <mark>Book [in progress...] read by Jaime Montoya</mark> from 1:16 AM June 11, 2023 to [in progress...] GMT.</p>
+                </div>
+            </div>\n";
+?>
--- reading/stock-investing-for-dummies/stock-investing-for-dummies-book-info.php
+++ reading/stock-investing-for-dummies/stock-investing-for-dummies-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Stock Investing For Dummies</span>
-					<p>By Paul J. Mladjenovic. <mark>Book 78 read by Jaime Montoya</mark> from 11:01 AM November 7, 2024 to [in progress...] GMT.</p>
+					<p>By Paul J. Mladjenovic. <mark>Book [in progress...] read by Jaime Montoya</mark> from 11:01 AM November 7, 2024 to [in progress...] GMT.</p>
                 </div>
             </div>\n";
 ?>
