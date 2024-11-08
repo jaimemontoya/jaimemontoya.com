bc3ed2b9ca885c2a967c78663e35eef137e18083
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-08 16:37:22 -0600

    Add Amplified Bible.

--- reading/index.php
+++ reading/index.php
@@ -7,6 +7,7 @@
   require("stock-investing-for-dummies/stock-investing-for-dummies-book-info.php");
   require("academic-writing-for-graduate-students/academic-writing-for-graduate-students-book-info.php");
   require("systematic-theology-2nd-introduction-biblical-doctrine/systematic-theology-2nd-introduction-biblical-doctrine-book-info.php");
+  require("amplified-bible/amplified-bible-book-info.php");
   require("sticky-teams/sticky-teams-book-info.php");
   require("the-psychology-of-money/the-psychology-of-money-book-info.php");
   require("investing-for-dummies-9th-edition/investing-for-dummies-9th-edition-book-info.php");
