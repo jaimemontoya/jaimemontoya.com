a13461032584d815d7dfb65986fd28ca51e0a23d
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-11 17:09:42 -0600

    Remove unnecessary space.

--- news/2024/11/11/22/13/20241111-2213-book-info.php
+++ news/2024/11/11/22/13/20241111-2213-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>International transfer to Interactive Brokers LLC, sent</h2>
-					<p>Another milestone completed in the process to enter stock market investments . My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. <mark>Published: 10:13 PM GMT · Nov 1, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>Another milestone completed in the process to enter stock market investments. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. <mark>Published: 10:13 PM GMT · Nov 1, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
