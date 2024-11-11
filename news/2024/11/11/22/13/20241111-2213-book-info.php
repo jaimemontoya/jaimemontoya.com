cec0716d567db05e10bda839c81636ce3c2a3d20
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-11 17:05:03 -0600

    Add news about my wire transfer progress.

---
+++ news/2024/11/11/22/13/20241111-2213-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2024/11/11/22/13/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"news-20241111-2213-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>International transfer to Interactive Brokers LLC, sent</h2>
+					<p>Another milestone completed in the process to enter investments stock market. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. <mark>Published: 10:13 PM GMT · Nov 1, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ news/2024/11/11/22/13/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "International transfer to Interactive Brokers LLC, sent";
+  $index->parentPage = "News";
+  $index->metaDescription = "Another milestone completed in the process to enter investments stock market. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20241111-2213.php");
+  $index->Display();
+?>
