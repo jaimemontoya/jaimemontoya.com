e0e11c0a521f7c260ffb1eddf3c59826c212f23c
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-12 09:09:56 -0600

    Use preregistered instead of sent.

--- news/2024/11/11/22/13/20241111-2213-book-info.php
+++ news/2024/11/11/22/13/20241111-2213-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"news-20241111-2213-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <h2>International transfer to Interactive Brokers LLC, sent</h2>
+		            <h2>International transfer to Interactive Brokers LLC, preregistered</h2>
 					<p>Another milestone completed in the process to enter stock market investments. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. <mark>Published: 10:13 PM GMT · Nov 11, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
--- news/2024/11/11/22/13/index.php
+++ news/2024/11/11/22/13/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "International transfer to Interactive Brokers LLC, sent";
+  $index->title = "International transfer to Interactive Brokers LLC, preregistered";
   $index->parentPage = "News";
   $index->metaDescription = "Another milestone completed in the process to enter investments stock market. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
