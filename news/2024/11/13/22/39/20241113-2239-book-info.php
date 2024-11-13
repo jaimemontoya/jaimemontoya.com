9e066936cb6f48ba77bfce59ea8e3175be0fd4b7
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-11-13 17:13:00 -0600

    Add news about my first purchase of NVIDIA stock.

---
+++ news/2024/11/13/22/39/20241113-2239-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2024/11/13/22/39/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"news-20241113-2239-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>NVIDIA Corporation (NVDA), my first stock purchase</h2>
+					<p>After reading a few books related to investing and the stock market, using a couple of simulators, and completing my research, I made my first official, real stock purchase from NVIDIA Corporation (NVDA). <mark>Published: 10:39 PM GMT · Nov 13, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
