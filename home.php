6cbab85d8c54ec1a4a8736510a888204ec5b2d99
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-24 04:18:34 -0600

    Use true for bold style in title.

--- home.php
+++ home.php
@@ -107,6 +107,9 @@ class HomePage extends Page
     echo "\t\t\t\t]);\n";
     echo "\t\t\t\tvar options = {\n";
     echo "\t\t\t\t\t'title':'Jaime Montoya\'s capital',\n";
+    echo "\t\t\t\t\ttitleTextStyle:{\n";
+    echo "\t\t\t\t\t\tbold: true\n";
+    echo "\t\t\t\t\t}\n";
     echo "\t\t\t\t\tcolors: [\"#4184F3\"],\n";
     echo "\t\t\t\t\tlineWidth: 3,\n";
     echo "\t\t\t\t\tlegend: {\n";
