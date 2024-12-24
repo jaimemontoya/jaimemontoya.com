e4dd3deea46c5217f3181c3e21794b8fca20e5a1
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-24 07:42:58 -0600

    Add values to capital.

--- capital.php
+++ capital.php
@@ -48,7 +48,7 @@ class CapitalPage extends Page
 	echo "\t\t\t\tdata.addColumn('string', 'Year');\n";
 	echo "\t\t\t\tdata.addColumn('number', 'USD');\n";
 	echo "\t\t\t\tdata.addRows([\n";
-	echo "\t\t\t\t\t['2024',5240.611144]\n";
+	echo "\t\t\t\t\t['2024',5836.906496]\n";
 	echo "\t\t\t\t]);\n";
 	echo "\t\t\t\t// Set chart options\n";
 	echo "\t\t\t\tvar options = {'title':'Jaime Montoya\'s capital history by year'};\n";
@@ -70,7 +70,8 @@ class CapitalPage extends Page
 	echo "\t\t\t\t\t['Aug',3652.596774],\n";
 	echo "\t\t\t\t\t['Sep',5684.295],\n";
 	echo "\t\t\t\t\t['Oct',6532.467419],\n";
-	echo "\t\t\t\t\t['Nov',7919.943333]\n";
+	echo "\t\t\t\t\t['Nov',8079.072333],\n";
+	echo "\t\t\t\t\t['Dec',9850.83]\n";
 	echo "\t\t\t\t]);\n";
 	echo "\t\t\t\t// Set chart options\n";
 	echo "\t\t\t\tvar options = {'title':'Jaime Montoya\'s 2024 capital history by month'};\n";
