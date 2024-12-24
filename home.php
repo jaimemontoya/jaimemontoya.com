ac91240886133d512ae461cdffe78d50e7af78e8
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-24 03:53:47 -0600

    Add Git line package.

--- home.php
+++ home.php
@@ -37,7 +37,7 @@ class HomePage extends Page
 	echo "\t\t<script type=\"text/javascript\">\n";
 	echo "\t\t\t// Load the Visualization API and the corechart package.\n";
 	echo "\t\t\tgoogle.charts.load('current', {'packages':['corechart']});\n";
-	//echo "\t\t\tgoogle.charts.load('current', {'packages':['line']});\n";
+	echo "\t\t\tgoogle.charts.load('current', {'packages':['line']});\n";
 	echo "\t\t\t// Set a callback to run when the Google Visualization API is loaded.\n";
 	echo "\t\t\tgoogle.charts.setOnLoadCallback(drawRichestCompaniesChart);\n";
 	echo "\t\t\tgoogle.charts.setOnLoadCallback(drawRichestPeopleChart);\n";
