efa5aef397dddb0b2950bf7175c44102023c7353
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-27 17:26:34 -0600

    Fix border.

--- home.php
+++ home.php
@@ -338,7 +338,7 @@ class HomePage extends Page
     echo "\t\t\t\t\t'title':'Jaime Montoya\'s capital',\n";
     echo "\t\t\t\t\tbackgroundColor: {\n";
     echo "\t\t\t\t\t\tfill: '#000',\n";
-    echo "\t\t\t\t\t\tfillOpacity: 1\n";
+    echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#0f0'\n";
     echo "\t\t\t\t\t},\n";
 	echo "\t\t\t\t\tchartArea: {\n";
     echo "\t\t\t\t\t\tbackgroundColor: '#000'\n";
