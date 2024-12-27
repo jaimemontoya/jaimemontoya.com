d7c61611f31489771b15703ccf1e19e5da200292
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-27 17:35:36 -0600

    Fix lines.

--- home.php
+++ home.php
@@ -354,7 +354,9 @@ class HomePage extends Page
     echo "\t\t\t\t\t\tposition: \"none\"\n";
     echo "\t\t\t\t\t},\n";
     echo "\t\t\t\t\thAxis: {\n";
-	echo "\t\t\t\t\t\tbaseLineColor: '#0f0',\n";
+	echo "\t\t\t\t\t\tminorGridlines: {
+			color: '#e6f7ff'  
+		  },\n";
     echo "\t\t\t\t\t\tpointSize: 2,\n";
     echo "\t\t\t\t\t\tformat: 'd MMM yyyy',\n";
     echo "\t\t\t\t\t\ttitle: '',\n";
