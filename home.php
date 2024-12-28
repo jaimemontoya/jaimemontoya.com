f29c6e25ac900c3f89502aa3cfd1cb30fb2c8c84
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-27 23:12:37 -0600

    Fix syntax.

--- home.php
+++ home.php
@@ -78,7 +78,7 @@ class HomePage extends Page
 	
     echo "\t\t\t\t\tvAxis: {\n";
     echo "\t\t\t\t\t\ttextStyle: {color: '#0f0'}\n";
-    echo "\t\t\t\t\t}\n";
+    echo "\t\t\t\t\t},\n";
 	
 	
 	echo "\t\t\t\t\tchartArea: {\n";
