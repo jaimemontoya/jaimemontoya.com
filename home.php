710b0598c6267ca7a86f9bcec09469070c036aa5
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-27 23:40:37 -0600

    Change color.

--- home.php
+++ home.php
@@ -79,7 +79,14 @@ class HomePage extends Page
 	
     echo "\t\t\t\t\tvAxis: {\n";
     echo "\t\t\t\t\t\ttitle: 'Trillions (USD)',\n";
-    echo "\t\t\t\t\t\ttextStyle: {color: '#0f0'}\n";
+    echo "\t\t\t\t\t\ttextStyle: {color: '#0f0'},\n";
+	
+	
+	    echo "\t\t\t\t\tbackgroundColor: {\n";
+    echo "\t\t\t\t\t\tfill: '#000',\n";
+    echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#0f0'\n";
+    echo "\t\t\t\t\t}\n";
+	
     echo "\t\t\t\t\t},\n";
 	
     echo "\t\t\t\t\tcolors: ['#0f0'],\n";
