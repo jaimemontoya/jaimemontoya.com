be31ed729c1fc598517b721c1c7a64625e5f6f4f
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-27 22:40:35 -0600

    Change line color.

--- home.php
+++ home.php
@@ -336,12 +336,9 @@ class HomePage extends Page
     echo "\t\t\t\t]);\n";
     echo "\t\t\t\tvar options = {\n";
     echo "\t\t\t\t\t'title':'Jaime Montoya\'s capital',\n";
-	/*echo "\t\t\t\t\t\ttitleTextStyle: {\n";
-    echo "\t\t\t\t\t\t\tcolor: '#0f0'\n";
-    echo "\t\t\t\t\t\t},\n";*/
     echo "\t\t\t\t\tbackgroundColor: {\n";
     echo "\t\t\t\t\t\tfill: '#000',\n";
-    echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#0f0'\n";
+    echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#9E9E9E'\n";
     echo "\t\t\t\t\t},\n";
 	echo "\t\t\t\t\tchartArea: {\n";
     echo "\t\t\t\t\t\tbackgroundColor: '#000'\n";
