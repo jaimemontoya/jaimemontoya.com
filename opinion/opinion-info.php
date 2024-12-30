1ab94327526c6813bee9043086e19479718ce278
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-30 16:59:27 -0600

    Add styles.

---
+++ opinion/opinion-info.php
@@ -0,0 +1,4 @@
+<?php
+  $index->content .= 
+           "\t\t\t<iframe src=\"https://docs.google.com/document/d/e/2PACX-1vSI9um9QUifKrVuSKbRJexc1qWy1aaFrSOc3Rjzx8WrGU9_INb5x8vR3EmTPkcdom2mgN5kwW9J6RTq/pub?embedded=true\" id=\"opinion-spreadsheet></iframe>\n";
+?>
