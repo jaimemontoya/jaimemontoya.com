c525c51054361ab1bc2352de55d715fdc8a2359b
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-30 23:59:59 -0600

    Add Opinion category and first post.

---
+++ opinion/2024/12/30/21/47/20241230-2147-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2024/12/30/21/47/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"blog-20241230-2147-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Another bad day in the stock market</h2>
+					<p>Another bad day in the stock market. Weak performance to finish the year. <mark>Published: 9:47 PM GMT · Dec 30, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
--- opinion/index.php
+++ opinion/index.php
@@ -2,8 +2,8 @@
   require("../page.php");
   $index = new Page();
   $index->title = "Opinion";
-  $index->metaDescription = "Stock Market, Business & Finance Opinion since 2024-Dec-30 9:47 PM GMT";
-  $index->content .= "<h1>Opinion</h1><p>Stock Market, Business & Finance Opinion since 2024-Dec-30 9:47 PM GMT</p>";
-  require("opinion-info.php");
+  $index->metaDescription = "Stock Market, Business & Finance Opinion since 2024-Dec-30 9:47 PM GMT.";
+  $index->content .= "<h1>Opinion</h1><p>Stock Market, Business & Finance Opinion since 2024-Dec-30 9:47 PM GMT.</p>";
+  require("2024/12/30/21/47/20241230-2147-book-info.php");
   $index->Display();
 ?>
