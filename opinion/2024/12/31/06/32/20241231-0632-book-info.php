cc19fbc511d14c0483c25d98f779e9b8564fff76
Jaime Montoya <jmontoya@jaimemontoya.com>
2024-12-31 01:53:09 -0600

    Fix code.

--- opinion/2024/12/31/06/32/20241231-0632-book-info.php
+++ opinion/2024/12/31/06/32/20241231-0632-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>NVDA is trying to control the AI ecosystem. AVGO facing overvaluation challenges. Musk may have more competition for the richest crown in 2025. Huang is keeping AI celebrity status.</h2>
-					<p>NVDA's $700 million acquisition of Israeli AI startup Run:ai could be an attempt to control the AI ecosystem: hardware, software, and cloud-based artificial intelligence systems. Investors seemed to feel optimistic by NVDA's purchase, resulting in positive results when almost all of the top ten most valuable companies had negative numbers near the end of the year.  <mark>Published: 6:32 AM GMT · Dec 31, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>NVDA's $700 million acquisition of Israeli AI startup Run:ai could be an attempt to control the AI ecosystem: hardware, software, and cloud-based artificial intelligence systems. Investors seemed to feel optimistic by NVDA's purchase, resulting in positive results when almost all of the top ten most valuable companies had negative numbers near the end of the year. <mark>Published: 6:32 AM GMT · Dec 31, 2024</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
--- opinion/2024/12/31/06/32/index.php
+++ opinion/2024/12/31/06/32/index.php
@@ -1,10 +1,10 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Another bad day in the stock market";
+  $index->title = "NVDA is trying to control the AI ecosystem. AVGO facing overvaluation challenges. Musk may have more competition for the richest crown in 2025. Huang is keeping AI celebrity status.";
   $index->parentPage = "Opinion";
-  $index->metaDescription = "Another bad day in the stock market. Weak performance to finish the year.";
+  $index->metaDescription = "NVDA's $700 million acquisition of Israeli AI startup Run:ai could be an attempt to control the AI ecosystem: hardware, software, and cloud-based artificial intelligence systems. Investors seemed to feel optimistic by NVDA's purchase, resulting in positive results when almost all of the top ten most valuable companies had negative numbers near the end of the year.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20241230-2147.php");
+  require("book-info-20241231-0632.php");
   $index->Display();
 ?>
