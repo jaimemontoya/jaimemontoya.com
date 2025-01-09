1168b3f510979e2c12119cc990beaf7ef11fae5c
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-09 00:53:18 -0600

    Add Opinion entry for Jan 8th, 2025.

---
+++ opinion/2025/01/09/02/54/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Saudi Arabian Oil Co. solid and consistent. Meta in legal battles against the European Commission.Ellison does not give up. Arnault with an inconsistent performance.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Bernard Arnault continues with his roller coaster performance in 2025, and today was a day at the bottom for him, decreasing his wealth more than any other person from the elite list of billionaires.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250109-0254.php");
+  $index->Display();
+?>
