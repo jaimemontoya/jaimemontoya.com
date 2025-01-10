39854d601cb44cfcdd4edd4c4ad73d991c7f5631
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-09 23:36:41 -0600

    Post Opinion entry.

---
+++ opinion/2025/01/10/04/24/20250110-0424-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/10/04/24/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250110-0424-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Microsoft demonstrating impressive longevity. Identity crisis for Meta?</h2>
+					<p>Meta Platforms could be in a difficult position in terms of consistency in terms of its political alignments and policies for the platform. In a blog post on the company's website by Joel Kaplan, Chief Global Affairs Officer, published on January 7th, 2025, Meta recognizes in its effort to respond to societal and political pressure to moderate content, their systems have made too many mistakes with false positives, meaning penalizing harmless content in a way that became a form of censorship. <mark>Published: 4:24 AM GMT · Jan 10, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/01/10/04/24/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Microsoft demonstrating impressive longevity. Identity crisis for Meta?";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Meta Platforms could be in a difficult position in terms of consistency in terms of its political alignments and policies for the platform. In a blog post on the company's website by Joel Kaplan, Chief Global Affairs Officer, published on January 7th, 2025, Meta recognizes in its effort to respond to societal and political pressure to moderate content, their systems have made too many mistakes with false positives, meaning penalizing harmless content in a way that became a form of censorship.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250110-0424.php");
+  $index->Display();
+?>
