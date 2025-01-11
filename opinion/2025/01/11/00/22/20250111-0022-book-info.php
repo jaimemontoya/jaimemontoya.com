fc461a8ec4ab6b86e84a872ab34ac575061a05c8
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-10 21:06:14 -0600

    Add Opinion post for Jan 11, 2025.

---
+++ opinion/2025/01/11/00/22/20250111-0022-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/11/00/22/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250111-0022-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia and Ellison were negatively impacted by Biden exports restrictions. Meta avoiding hostility with the new USA administration. Arnault favored by not relying on the American stock market results.</h2>
+					<p>Bernard Arnault had a positive day in his wealth growth. It helped that his company is in the European, not American stock market, so he was not vulnerable to the Federal Reserve robust job’s report that apparently affected Dow Jones, NASDAQ and S&P 500 today. <mark>Published: 00:22 AM GMT · Jan 11, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/01/11/00/22/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Nvidia and Ellison were negatively impacted by Biden exports restrictions. Meta avoiding hostility with the new USA administration. Arnault favored by not relying on the American stock market results.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Bernard Arnault had a positive day in his wealth growth. It helped that his company is in the European, not American stock market, so he was not vulnerable to the Federal Reserve robust job’s report that apparently affected Dow Jones, NASDAQ and S&P 500 today.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250111-0022.php");
+  $index->Display();
+?>
