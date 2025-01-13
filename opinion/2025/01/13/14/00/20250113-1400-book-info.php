ddaf5124c5834c092418f7797555c1250994fa92
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-13 08:58:54 -0600

    Add Opinion entry.

---
+++ opinion/2025/01/13/14/00/20250113-1400-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/13/14/00/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250113-1400-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia’s shares in free fall. Meta finding it difficult to be apolitical.</h2>
+					<p>On the episode number 2255 of “The Joe Rogan Experience”, corresponding to January 10th, 2025, Meta CEO Mark Zuckerberg said: \"Basically, these people from the Biden administration would call up our team and, like, scream at them and curse.\" <mark>Published: 2:00 PM GMT · Jan 13, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/01/13/14/00/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Nvidia’s shares in free fall. Meta finding it difficult to be apolitical.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "On the episode number 2255 of “The Joe Rogan Experience”, corresponding to January 10th, 2025, Meta CEO Mark Zuckerberg said: \"Basically, these people from the Biden administration would call up our team and, like, scream at them and curse.\"";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250113-1400.php");
+  $index->Display();
+?>
