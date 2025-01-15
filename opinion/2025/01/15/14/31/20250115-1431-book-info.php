e06101e52039e5dedb5966018fd9c080558b793f
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-15 10:13:17 -0600

    Add Opinion entry for Jan 15, 2025.

---
+++ opinion/2025/01/15/14/31/20250115-1431-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/15/14/31/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250115-1431-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Steady growth for Berkshire Hathaway. Meta experimenting transitions. Ortega is richer than Huang. Arnault still not challenging the richest Americans.</h2>
+					<p>Excellent week thus far for Berkshire Hathaway Inc. with steady growth, unlike last week that ended with a fall in its stock price. 2025 is now taking a balance of growth for the company. <mark>Published: 2:31 PM GMT · Jan 15, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/01/15/14/31/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Steady growth for Berkshire Hathaway. Meta experimenting transitions. Ortega is richer than Huang. Arnault still not challenging the richest Americans.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Excellent week thus far for Berkshire Hathaway Inc. with steady growth, unlike last week that ended with a fall in its stock price. 2025 is now taking a balance of growth for the company.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250115-1431.php");
+  $index->Display();
+?>
