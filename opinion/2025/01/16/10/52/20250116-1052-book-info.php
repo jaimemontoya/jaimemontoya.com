2633942f0f815979034e07bab8885934cdb33a10
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-16 06:04:34 -0600

    Add Opinion entry for Jan 16, 2025.

---
+++ opinion/2025/01/16/10/52/20250116-1052-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/16/10/52/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250116-1052-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Bright future for Tesla. Aramco with unimpressive growth for its ranking. Arnault having a good 2025. Ellison needing to recover. Ortega lost competition with Huang easily.</h2>
+					<p>2025 is now taking a trend of outstanding growth for Bernard Arnault, after a poor performance in 2024. The year is looking promising to his net worth. <mark>Published: 10:52 AM GMT · Jan 16, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/01/16/10/52/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Bright future for Tesla. Aramco with unimpressive growth for its ranking. Arnault having a good 2025. Ellison needing to recover. Ortega lost competition with Huang easily.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "2025 is now taking a trend of outstanding growth for Bernard Arnault, after a poor performance in 2024. The year is looking promising to his net worth.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250116-1052.php");
+  $index->Display();
+?>
