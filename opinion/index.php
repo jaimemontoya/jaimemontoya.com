6050f20c6ccf0ed12c39feceb836cb023eda65e6
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-17 04:34:19 -0600

    Fix path.

--- opinion/index.php
+++ opinion/index.php
@@ -6,7 +6,7 @@
   $index->content .= "<h1>Opinion</h1><p>Stock Market, Business & Finance Opinion since 2024-Dec-30 9:47 PM GMT.</p>";
   $index->content .= 
            "\t\t\t<iframe src=\"https://docs.google.com/document/d/e/2PACX-1vSI9um9QUifKrVuSKbRJexc1qWy1aaFrSOc3Rjzx8WrGU9_INb5x8vR3EmTPkcdom2mgN5kwW9J6RTq/pub?embedded=true\" id=\"opinion-spreadsheet\"></iframe>\n";
-  require("2025/01/17/10/52/20250117-0509-book-info.php");
+  require("2025/01/17/05/09/20250117-0509-book-info.php");
   require("2025/01/16/10/52/20250116-1052-book-info.php");
   require("2025/01/15/14/31/20250115-1431-book-info.php");
   require("2025/01/14/10/26/20250114-1026-book-info.php");
