19d6b587811f80a8a351fda467da242c06d787b6
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-18 09:09:20 -0600

    Add Opinion entry for Jan 18, 2025.

---
+++ opinion/2025/01/18/14/14/20250118-1414-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/18/14/14/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250118-1414-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Broadcom keeps growing since 24 Nov 2024. Meta missed a growth opportunity.</h2>
+					<p>Meta Platforms, Inc. did not have a good Friday in terms of stock market performance because even though it experienced a little growth, it was almost flat when compared to all other elite companies in the top ten ranking of the wealthiest in the world. Meta did not grow proportionally to the growth experienced on Friday by the Dow Jones, NASDAQ and S&P 500, which makes the positive situation for the stock market on Friday, as a missed opportunity for Meta. <mark>Published: 2:14 PM GMT · Jan 18, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/01/18/14/14/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Broadcom keeps growing since 24 Nov 2024. Meta missed a growth opportunity.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Meta Platforms, Inc. did not have a good Friday in terms of stock market performance because even though it experienced a little growth, it was almost flat when compared to all other elite companies in the top ten ranking of the wealthiest in the world. Meta did not grow proportionally to the growth experienced on Friday by the Dow Jones, NASDAQ and S&P 500, which makes the positive situation for the stock market on Friday, as a missed opportunity for Meta.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250118-1414.php");
+  $index->Display();
+?>
