74a976d02257802f91d8f00778ea669486bfd56a
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-18 09:14:11 -0600

    Add word.

--- opinion/2025/01/18/14/14/20250118-1414-book-info.php
+++ opinion/2025/01/18/14/14/20250118-1414-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Broadcom keeps growing since 24 Nov 2024. Meta missed a growth opportunity.</h2>
-					<p>Meta Platforms, Inc. did not have a good Friday in terms of stock market performance because even though it experienced a little growth, it was almost flat when compared to all other elite companies in the top ten ranking of the wealthiest in the world. Meta did not grow proportionally to the growth experienced on Friday by the Dow Jones, NASDAQ and S&P 500, which makes the positive situation for the stock market on Friday, as a missed opportunity for Meta. <mark>Published: 2:14 PM GMT · Jan 18, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>Meta Platforms, Inc. did not have a good Friday in terms of stock market performance because even though it experienced a little growth, it was almost flat when compared to all other elite companies in the top ten ranking of the wealthiest in the world. Meta did not grow proportionally to the growth experienced on Friday by the Dow Jones, NASDAQ and S&P 500, which makes the positive situation for the stock market on Friday, look as a missed opportunity for Meta. <mark>Published: 2:14 PM GMT · Jan 18, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
