f57a1cf8625e3c78cff91ee43a3eda5e73d75d6b
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-20 10:05:40 -0600

    Add Opinion entry for Jan 20, 2025.

---
+++ opinion/2025/01/20/15/07/20250120-1507-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/20/15/07/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250120-1507-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Broadcom raising momentum. Meta in makeover season.</h2>
+					<p>The Artificial Intelligence wars have already started and Broadcom had a fantastic December 2024, raising momentum as a leader in application-specific integrated circuits used in artificial intelligence. <mark>Published: 3:07 PM GMT · Jan 20, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/01/20/15/07/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Broadcom raising momentum. Meta in makeover season.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "The Artificial Intelligence wars have already started and Broadcom had a fantastic December 2024, raising momentum as a leader in application-specific integrated circuits used in artificial intelligence.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250120-1507.php");
+  $index->Display();
+?>
