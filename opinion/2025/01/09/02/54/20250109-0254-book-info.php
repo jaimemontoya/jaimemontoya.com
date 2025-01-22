16de9f0e7fe6b91bf8825356055f9a9da41cbf96
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-21 21:40:59 -0600

    Add space.

--- opinion/2025/01/09/02/54/20250109-0254-book-info.php
+++ opinion/2025/01/09/02/54/20250109-0254-book-info.php
@@ -5,7 +5,7 @@
 					<div class=\"book-cover\" id=\"opinion-20250109-0254-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <h2>Saudi Arabian Oil Co. solid and consistent. Meta in legal battles against the European Commission.Ellison does not give up. Arnault with an inconsistent performance.</h2>
+		            <h2>Saudi Arabian Oil Co. solid and consistent. Meta in legal battles against the European Commission. Ellison does not give up. Arnault with an inconsistent performance.</h2>
 					<p>Bernard Arnault continues with his roller coaster performance in 2025, and today was a day at the bottom for him, decreasing his wealth more than any other person from the elite list of billionaires. <mark>Published: 2:54 AM GMT · Jan 9, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
