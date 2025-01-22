fa1cc34a0539391fe479a5717ee492cf4fce08ad
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-21 21:42:49 -0600

    Add space.

--- opinion/2025/01/09/02/54/index.php
+++ opinion/2025/01/09/02/54/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Saudi Arabian Oil Co. solid and consistent. Meta in legal battles against the European Commission.Ellison does not give up. Arnault with an inconsistent performance.";
+  $index->title = "Saudi Arabian Oil Co. solid and consistent. Meta in legal battles against the European Commission. Ellison does not give up. Arnault with an inconsistent performance.";
   $index->parentPage = "Opinion";
   $index->metaDescription = "Bernard Arnault continues with his roller coaster performance in 2025, and today was a day at the bottom for him, decreasing his wealth more than any other person from the elite list of billionaires.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
