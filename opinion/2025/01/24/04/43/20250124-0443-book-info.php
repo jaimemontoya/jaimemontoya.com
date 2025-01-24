8419ad4530e405b421869c25ca604b08d03642eb
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-24 00:06:16 -0600

    Add Opinion entry for 24 Jan 2025.

---
+++ opinion/2025/01/24/04/43/20250124-0443-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/24/04/43/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250124-0443-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>All-time high price for Meta stock price. Zuckerberg running for top positions in ranking of wealthiest people. Tesla with bad performance since Trump started his second term. Musk needing to has his company back on track to outstanding results.</h2>
+					<p>Since 21 Jan 2025, Tesla’s stock price has been in decline. Interestingly enough, Tesla has not had one positive day for its stock price, after Donald Trump officially started his new term as President of the United States, despite the excellent relationship between Elon Musk and Trump. Musk’s fortune is still gigantic, with Tesla having a couple of bad days in the stock market, Musk loses a couple of billions in his net worth. <mark>Published: 4:43 PM GMT · Jan 24, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
