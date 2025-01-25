5bca96d1488387479f302f0c5f6b0d2e342b4922
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-24 19:42:33 -0600

    Add Opinion entry for 25 Jan 2025.

---
+++ opinion/2025/01/25/00/54/20250125-0054-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/25/00/54/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250125-0054-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia and Huang with a bad day in the stock market. Broadcom is not stopping its pattern of growth. Arnault making 2025 a great year for his net worth.</h2>
+					<p>Since 15 Jan 2025, Broadcom’s stock price has been growing steadily and 2025 is looking great for the company. As a prominent player in the semiconductor industry, the Artificial Intelligence revolution is helping Broadcom to maintain its position as the 9th richest company in the world. <mark>Published: 12:54 AM GMT · Jan 25, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/01/25/00/54/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Nvidia and Huang with a bad day in the stock market. Broadcom is not stopping its pattern of growth. Arnault making 2025 a great year for his net worth.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Since 15 Jan 2025, Broadcom’s stock price has been growing steadily and 2025 is looking great for the company. As a prominent player in the semiconductor industry, the Artificial Intelligence revolution is helping Broadcom to maintain its position as the 9th richest company in the world.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250125-0054.php");
+  $index->Display();
+?>
