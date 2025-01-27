c1f4e54b96e14b4d6eb209a979b8fb6962614756
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-27 17:26:12 -0600

    Add entry for 26 Jan 2025.

---
+++ opinion/2025/01/27/14/04/20250127-1404-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/27/14/04/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250127-1404-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia highly volatile. Broadcom still growing consistently until 24 Jan 2025.</h2>
+					<p>Broadcom finished the week on 24 Jan 2025 with outstanding results and growth, as one of the companies with the greatest potential to stay in the list of top players in the Artificial Intelligence race. <mark>Published: 2:04 PM GMT · Jan 27, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
