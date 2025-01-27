bc75d6b32fd511c8f56151e96c4cf78095225b05
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-27 17:28:26 -0600

    Update values.

--- opinion/2025/01/27/14/04/index.php
+++ opinion/2025/01/27/14/04/index.php
@@ -1,10 +1,10 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "First bad day of the week for Nvidia. Broadcom growing consistently.";
+  $index->title = "Nvidia highly volatile. Broadcom still growing consistently until 24 Jan 2025.";
   $index->parentPage = "Opinion";
   $index->metaDescription = "Broadcom finished the week on Friday with strong results in the stock market, maintaining its consistency of growth in the era of the boom in artificial intelligence technologies.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20250126-1335.php");
+  require("book-info-20250127-1404.php");
   $index->Display();
 ?>
