cca5f8837cb65a3d7408d2138b6907e989ed5386
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-28 05:56:45 -0600

    Add Opinion entry for 28 Jan 2025.

---
+++ opinion/2025/01/28/08/11/20250128-0811-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/01/28/08/11/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250128-0811-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Brodcom lost its great trajectory overnight. Apple’s stock not hurt by the DeepSeek news. Apple the richest company in the world. Microsoft is the second richest company. Hathaway keeps growing and is wealthier than Broadcom. Ellison experiencing massive losses as China surprises the world with DeepSeek, moving from 3rd to 5th richest person. Arnault not vulnerable to the U.S. market shock becoming the 4th richest person. Zuckerberg survived the DeepSeek news and is now the 3rd richest person. Ortega back to 10th richest person. Huang from 10th to 17th richest person.</h2>
+					<p>As a result of the terrible results for Nvidia in the stock market originated by the news related to DeepSeek, Microsoft is now the second most valuable company in the world, only behind Apple. It is historic to see two iconic companies again fighting for the crown of being the richest on earth. <mark>Published: 8:11 AM GMT · Jan 28, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
