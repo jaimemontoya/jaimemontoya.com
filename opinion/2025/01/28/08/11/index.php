ca1b3f5b401c5f05edfb358b31655a7b1dfc0f9a
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-01-28 06:03:29 -0600

    Change text.

--- opinion/2025/01/28/08/11/index.php
+++ opinion/2025/01/28/08/11/index.php
@@ -1,7 +1,7 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Brodcom lost its great trajectory overnight. Apple’s stock not hurt by the DeepSeek news. Apple the richest company in the world. Microsoft is the second richest company. Hathaway keeps growing and is wealthier than Broadcom. Ellison experiencing massive losses as China surprises the world with DeepSeek, moving from 3rd to 5th richest person. Arnault not vulnerable to the U.S. market shock becoming the 4th richest person. Zuckerberg survived the DeepSeek news and is now the 3rd richest person. Ortega back to 10th richest person. Huang from 10th to 17th richest person.";
+  $index->title = "Broadcom lost its great trajectory overnight. Apple’s stock not hurt by the DeepSeek news. Apple the richest company in the world. Microsoft is the second richest company. Hathaway keeps growing and is wealthier than Broadcom. Ellison experiencing massive losses as China surprises the world with DeepSeek, moving from 3rd to 5th richest person. Arnault not vulnerable to the U.S. market shock becoming the 4th richest person. Zuckerberg survived the DeepSeek news and is now the 3rd richest person. Ortega back to 10th richest person. Huang from 10th to 17th richest person.";
   $index->parentPage = "Opinion";
   $index->metaDescription = "As a result of the terrible results for Nvidia in the stock market originated by the news related to DeepSeek, Microsoft is now the second most valuable company in the world, only behind Apple. It is historic to see two iconic companies again fighting for the crown of being the richest on earth.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
