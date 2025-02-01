4dd37ffb0a548795027cbb62a0793608977f24f6
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-01 07:06:39 -0600

    Update money values for 1 Feb 2025.

---
+++ opinion/2025/02/01/04/54/20250201-0454-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/01/04/54/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250201-0454-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Broadcom richer than Hathaway. Broadcom experiencing solid growth. Nvidia struggling to remain as the AI leader. Page having all-time high price for Alphabeth’s stock. Arnault struggling again as he was in 2024.</h2>
+					<p>Broadcom had lost its position of 9th richest company in the world when on 28 Jan 2025, Berkshire Hathaway took that place. Now the situation has changed to the previous state and Broadcom is number 9 in the ranking. <mark>Published: 4:54 AM GMT · 1 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/02/01/04/54/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Broadcom richer than Hathaway. Broadcom experiencing solid growth. Nvidia struggling to remain as the AI leader. Page having all-time high price for Alphabeth’s stock. Arnault struggling again as he was in 2024.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Broadcom had lost its position of 9th richest company in the world when on 28 Jan 2025, Berkshire Hathaway took that place. Now the situation has changed to the previous state and Broadcom is number 9 in the ranking.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250201-0454.php");
+  $index->Display();
+?>
