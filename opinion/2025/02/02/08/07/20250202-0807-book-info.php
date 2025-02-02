22f047d4cf4197a1534983d4f4bb2fe44a8b61e8
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-02 03:20:53 -0600

    Add Opinion entry for 2 Feb 2025.

---
+++ opinion/2025/02/02/08/07/20250202-0807-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/02/08/07/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250202-0807-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia still not recovering from DeepSeek shock. Broadcom bullish after Meta announcement. Ballmer richer than Ortega.</h2>
+					<p>Amancio Ortega lost its position as 9th richest person in the world and was downgraded to 10th, as Steve Ballmer took the position back that he had lost on 31 Jan 2025. Nothing special happened on the Baller’s side, but it was Ortega who lost money with an amount relatively small but sufficient to make him lose the race, for now. Both Ortega and Ballmer are essentially at the same level in net worth. It is not surprising if one day one is richer, the next day the situation is reversed, and the race continues until one of them consolidates his position. For now, the winner is again, Ballmer. <mark>Published: 8:07 AM GMT · 2 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/02/02/08/07/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Nvidia still not recovering from DeepSeek shock. Broadcom bullish after Meta announcement. Ballmer richer than Ortega.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Amancio Ortega lost its position as 9th richest person in the world and was downgraded to 10th, as Steve Ballmer took the position back that he had lost on 31 Jan 2025. Nothing special happened on the Baller’s side, but it was Ortega who lost money with an amount relatively small but sufficient to make him lose the race, for now. Both Ortega and Ballmer are essentially at the same level in net worth. It is not surprising if one day one is richer, the next day the situation is reversed, and the race continues until one of them consolidates his position. For now, the winner is again, Ballmer.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250202-0807.php");
+  $index->Display();
+?>
