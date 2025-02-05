65d40d194643142a079b7160c93f720542904273
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-05 10:39:02 -0600

    Add Opinion entry for 5 Feb 2025.

---
+++ opinion/2025/02/05/11/41/20250205-1141-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/05/11/41/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250205-1141-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Roller coaster day for Google. Aramco too slow to stay in the elite list of richest companies.</h2>
+					<p>Saudi Aramco may need to be more aggressive in its stock price growth strategy to remain in the elite list of the top richest companies in the world. It is true that Aramco is in the energy minerals sector, which is a defensive stock, normally stable regardless of the overall state of the stock market. But Aramco is in an elite league in terms of market capitalization, playing in a field next to the richest of the richest companies in the world. Without more aggressive growth, probably Aramco would soon be replaced by other companies and still be a wealthy company, but no longer part of the top 10 richest companies in the world. <mark>Published: 11:41 AM GMT · 5 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/02/05/11/41/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Roller coaster day for Google. Aramco too slow to stay in the elite list of richest companies.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Saudi Aramco may need to be more aggressive in its stock price growth strategy to remain in the elite list of the top richest companies in the world. It is true that Aramco is in the energy minerals sector, which is a defensive stock, normally stable regardless of the overall state of the stock market. But Aramco is in an elite league in terms of market capitalization, playing in a field next to the richest of the richest companies in the world. Without more aggressive growth, probably Aramco would soon be replaced by other companies and still be a wealthy company, but no longer part of the top 10 richest companies in the world.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250205-1141.php");
+  $index->Display();
+?>
