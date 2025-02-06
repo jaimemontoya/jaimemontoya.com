c1885b7de6517ba435c25b65b38cf56b3d9ef7ab
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-06 04:56:58 -0600

    Add Opinion entry for 6 Feb 2025.

---
+++ opinion/2025/02/06/00/00/20250206-0000-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/06/00/00/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250206-0000-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Google Cloud disappointing investors. Page experiencing significant losses. Finally positive results for Nvidia after DeepSeek shock. TSMC richer than Berkshire Hathaway. Ellison making his net worth grow.</h2>
+					<p>Larry Ellison had a positive Wednesday to help with keeping his net worth in a solid position as the 4th richest person in the world. His company, Oracle, has had ups and downs this year, but it is overall growing in the stock market. <mark>Published: 0:00 AM GMT · 6 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/02/06/00/00/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Google Cloud disappointing investors. Page experiencing significant losses. Finally positive results for Nvidia after DeepSeek shock. TSMC richer than Berkshire Hathaway. Ellison making his net worth grow.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Larry Ellison had a positive Wednesday to help with keeping his net worth in a solid position as the 4th richest person in the world. His company, Oracle, has had ups and downs this year, but it is overall growing in the stock market.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250206-0000.php");
+  $index->Display();
+?>
