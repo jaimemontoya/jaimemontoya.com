221eb4da234f32ad5fe3952290512e11e8ae4399
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-07 23:43:23 -0600

    Post comments.

--- opinion/2024/12/30/21/47/book-info-20241230-2147.php
+++ opinion/2024/12/30/21/47/book-info-20241230-2147.php
@@ -1,4 +1,26 @@
 <?php
   $index->content .= 
-           "<div><img src=\"/opinion/2024/12/30/21/47/img/20241230-2147-thumb.jpg\" alt=\"20241230-2147-thumb\" width=\"84\" height=\"84\" class=\"floatleft\"><p>Another bad day in the stock market. Weak performance to finish the year.</p><p>The competition between GOOG and AMZN has been fierce in the last few days.</p><p>Musk has no competition, consolidating his position as the richest man in the world. One interesting aspect to highlight is how active Musk has been in politics in recent days, not only in the USA, but also in Germany. If his involvement in politics is in part a purposeful attempt to increase wealth, it remains to be seen how effective and sustainable it would be for his businesses in the future.</p></div><div>Published: 9:47 PM GMT · Dec 30, 2024</div>\n";
+           "<div><img src=\"/opinion/2024/12/30/21/47/img/20241230-2147-thumb.jpg\" alt=\"20241230-2147-thumb\" width=\"84\" height=\"84\" class=\"floatleft\"><p>Another bad day in the stock market. Weak performance to finish the year.</p><p>The competition between GOOG and AMZN has been fierce in the last few days.</p><p>Musk has no competition, consolidating his position as the richest man in the world. One interesting aspect to highlight is how active Musk has been in politics in recent days, not only in the USA, but also in Germany. If his involvement in politics is in part a purposeful attempt to increase wealth, it remains to be seen how effective and sustainable it would be for his businesses in the future.</p></div><div>Published: 9:47 PM GMT · Dec 30, 2024</div>
+		   <div class=\"comment\">
+		     <div class=\"comment-heading\">
+               <div class=\"comment-info\">
+			     <a href=\"https://www.linkedin.com/in/adolfo-rodrigo-ca%C3%B1as-alem%C3%A1n-116a6769/\" class=\"comment-author\">Adolfo Rodrigo Ca&ntilde;as Alem&aacute;n</a>
+			   </div>
+			 </div>
+			 <div class=\"comment-body\">
+			   <p>I agree</p>
+			 </div>
+			 <div class=\"replies\">
+			   <div class=\"comment-heading\">
+                 <div class=\"comment-info\">
+			       <a href=\"https://www.linkedin.com/in/jaimemontoyain/\" class=\"comment-author\">Jaime Montoya</a>
+			     </div>
+			   </div>
+			   <div class=\"comment-body\">
+			     <p>It will be interesting to see what unfolds in 2025.</p>
+			   </div>
+		     </div>
+		     </div>
+		   </div>
+		   \n";
 ?>
