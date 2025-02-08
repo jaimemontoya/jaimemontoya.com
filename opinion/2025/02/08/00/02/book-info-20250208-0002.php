23f42912ad503291261cd09d7564fb7d42a48c7a
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-07 22:21:53 -0600

    Add nested comments.

--- opinion/2025/02/08/00/02/book-info-20250208-0002.php
+++ opinion/2025/02/08/00/02/book-info-20250208-0002.php
@@ -10,6 +10,16 @@
 			 <div class=\"comment-body\">
 			   <p>The narrative focuses on wealth fluctuations tied to stock performance, neglecting the broader economic and social impacts of these companies' actions. For instance, Amazon's sales forecast might reflect shifting consumer behavior rather than inherent weakness, a trend mirrored by the recent surge in subscription-based services. How does this emphasis on individual wealth creation align with the growing discourse on equitable distribution of technological advancements?</p>
 			 </div>
+			 <div class=\"replies\">
+			   <div class=\"comment-heading\">
+                 <div class=\"comment-info\">
+			       <a href=\"https://www.linkedin.com/in/jaimemontoyain/\" class=\"comment-author\">Jaime Montoya</a>
+			     </div>
+			   </div>
+			   <div class=\"comment-body\">
+			     <p>True. For example, why was 2022 bad for Amazon in the stock market? Interest rates were raised aggressively to combat inflation, making tech stocks less attractive to investors. Post-pandemic lockdowns were eased, with consumers shifting to in-person retail services. The overexpansion during the pandemic led to excess capacity in 2022, when things went back to pre-pandemic reality. There are countless variables to explain stock performance. This includes consumer behavior, political changes, etc. It is not necessarily inherent weakness or poor management that may lead to poor performance. But one way or another, companies have wealth fluctuations that reflect many circumstances that take them to the elite, or make them disappear from those privileged positions in the rankings. The equitable distribution of technological advancements can happen in parallel with wealth creation. For example, when Bill Gates thought about all homes having a personal computer. His wealth was growing exponentially and people had computers with an operating system that they could use, at home. The same happened with the Internet, when Netscape provided a revolutionary web browser. Netscape increased its wealth and people browsed the web.</p>
+			   </div>
+			 </div>
 		   </div>
 		   \n";
 ?>
