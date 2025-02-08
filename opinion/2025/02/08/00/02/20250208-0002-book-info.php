f59d36a2af7c5ff32010ec611905b95b722d1939
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-07 23:58:36 -0600

    Post number of comments.

--- opinion/2025/02/08/00/02/20250208-0002-book-info.php
+++ opinion/2025/02/08/00/02/20250208-0002-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Nvidia no longer intimidated by DeepSeek. Amazon had a terrible Friday. Zuckerberg richer than Bezos. Ellison solidifying his position as 4th richest person. Buffett richer than Brin.</h2>
-					<p>Oracle has been enjoying growth in its stock price since 28 Jan 2025, with the company almost reaching half trillion dollars in market capitalization. Larry Ellison is benefiting from those results and solidifying his position as the 4th richest person in the world. <mark>Published: 0:02 AM GMT · 8 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>Oracle has been enjoying growth in its stock price since 28 Jan 2025, with the company almost reaching half trillion dollars in market capitalization. Larry Ellison is benefiting from those results and solidifying his position as the 4th richest person in the world. <mark>Published: 0:02 AM GMT · 8 Feb 2025 · 4 comments</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
