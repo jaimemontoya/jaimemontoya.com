41ae43f72e27bead01954a821d36ffb60788dd8a
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-10 09:53:56 -0600

    Remove comments.

--- opinion/2025/02/09/11/24/20250209-1124-book-info.php
+++ opinion/2025/02/09/11/24/20250209-1124-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Fears against Nvidia may be a fallacy. Amazon with weak guidance for growth.</h2>
-					<p>Amazon’s strong Q4 results were not enough to overshadow the company’s guidance for growth deceleration, interrupting on Friday the solid growth that the company had been experiencing in the stock market. <mark>Published: 11:24 AM GMT · 9 Feb 2025 · <span class=\"comments-counter\">4 comments</span></mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>Amazon’s strong Q4 results were not enough to overshadow the company’s guidance for growth deceleration, interrupting on Friday the solid growth that the company had been experiencing in the stock market. <mark>Published: 11:24 AM GMT · 9 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
