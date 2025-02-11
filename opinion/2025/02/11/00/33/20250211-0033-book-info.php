339a136000688761d2a42e41bf6cdc7dda0e9825
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-10 19:30:27 -0600

    Add Opinion entry for 11 Feb 2025.

---
+++ opinion/2025/02/11/00/33/20250211-0033-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/11/00/33/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250211-0033-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Tesla and Musk in an alarming 2025. Broadcom with solid results. Broadcom richer than TSMC. Brin richer than Buffett. Ellison having an outstanding 2025.</h2>
+					<p>Broadcom is now the 9th richest company in the world, replacing TSMC, which was downgraded to number 10. Broadcom had an outstanding beginning of the week in the stock market. Other than the dramatic fall around the end of January 2025, when DeepSeek headlines crushed the U.S. AI technology stocks, Broadcom has maintained solid results in 2025. <mark>Published: 0:33 AM GMT · 11 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/02/11/00/33/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Tesla and Musk in an alarming 2025. Broadcom with solid results. Broadcom richer than TSMC. Brin richer than Buffett. Ellison having an outstanding 2025.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Broadcom is now the 9th richest company in the world, replacing TSMC, which was downgraded to number 10. Broadcom had an outstanding beginning of the week in the stock market. Other than the dramatic fall around the end of January 2025, when DeepSeek headlines crushed the U.S. AI technology stocks, Broadcom has maintained solid results in 2025.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250211-0033.php");
+  $index->Display();
+?>
