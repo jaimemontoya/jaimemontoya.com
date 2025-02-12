49e857db7d90300bedbcd7243bf281d0cbf4d138
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-11 21:14:48 -0600

    Add Opinion entry for 12 Feb 2025.

---
+++ opinion/2025/02/12/01/41/20250212-0141-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/12/01/41/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250212-0141-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Apple in conversations with Alibaba for Apple Intelligence in China. Tesla and musk continue in freefall. Broadcom richer than Tesla. TSMC richer than Tesla. Buffett richer than Brin.</h2>
+					<p>In the ranking of richest companies, Broadcom surpassed Tesla, moving from 9th to 8th place. TSMC also surpassed Tesla, moving from 10th to 9th place. Tesla was downgraded from 8th to 10th place. <mark>Published: 1:41 AM GMT · 12 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/02/12/01/41/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Apple in conversations with Alibaba for Apple Intelligence in China. Tesla and musk continue in freefall. Broadcom richer than Tesla. TSMC richer than Tesla. Buffett richer than Brin.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "In the ranking of richest companies, Broadcom surpassed Tesla, moving from 9th to 8th place. TSMC also surpassed Tesla, moving from 10th to 9th place. Tesla was downgraded from 8th to 10th place.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250212-0141.php");
+  $index->Display();
+?>
