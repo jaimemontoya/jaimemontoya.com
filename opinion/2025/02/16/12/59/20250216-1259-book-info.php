84ae7c09086aab605533ca2dd9f4802b8e270042
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-16 23:49:37 -0600

    Add Opinion entry for 16 Feb 2025.

---
+++ opinion/2025/02/16/12/59/20250216-1259-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/16/12/59/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250216-1259-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia having a positive 2025. Flat stock price for Broadcom since 10 Feb 2025.</h2>
+					<p>Since Monday, 10 Feb 2025, Broadcom has kept a flat stock price. The U.S. stock markets will be closed on Monday, 17 Feb 2025, in observance of Washington's Birthday. Broadcom will have 4 days from Tuesday to Friday next year, to go back to the consistent pattern of consistent growth that the company had been experiencing from 28 January to 10 February 2025. <mark>Published: 12:59 PM GMT · 16 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
