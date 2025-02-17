10440698560bac8a02d0ac95908bdcd923cdf6f3
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-17 00:16:05 -0600

    Remove redundant word.

--- opinion/2025/02/16/12/59/20250216-1259-book-info.php
+++ opinion/2025/02/16/12/59/20250216-1259-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Nvidia having a positive 2025. Flat stock price for Broadcom since 10 Feb 2025.</h2>
-					<p>Since Monday, 10 Feb 2025, Broadcom has kept a flat stock price. The U.S. stock markets will be closed on Monday, 17 Feb 2025, in observance of Washington's Birthday. Broadcom will have 4 days from Tuesday to Friday next week, to go back to the consistent pattern of consistent growth that the company had been experiencing from 28 January to 10 February 2025. <mark>Published: 12:59 PM GMT · 16 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>Since Monday, 10 Feb 2025, Broadcom has kept a flat stock price. The U.S. stock markets will be closed on Monday, 17 Feb 2025, in observance of Washington's Birthday. Broadcom will have 4 days from Tuesday to Friday next week, to go back to the consistent pattern of growth that the company had been experiencing from 28 January to 10 February 2025. <mark>Published: 12:59 PM GMT · 16 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
--- opinion/2025/02/16/12/59/index.php
+++ opinion/2025/02/16/12/59/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "Nvidia having a positive 2025. Flat stock price for Broadcom since 10 Feb 2025.";
   $index->parentPage = "Opinion";
-  $index->metaDescription = "Since Monday, 10 Feb 2025, Broadcom has kept a flat stock price. The U.S. stock markets will be closed on Monday, 17 Feb 2025, in observance of Washington's Birthday. Broadcom will have 4 days from Tuesday to Friday next week, to go back to the consistent pattern of consistent growth that the company had been experiencing from 28 January to 10 February 2025.";
+  $index->metaDescription = "Since Monday, 10 Feb 2025, Broadcom has kept a flat stock price. The U.S. stock markets will be closed on Monday, 17 Feb 2025, in observance of Washington's Birthday. Broadcom will have 4 days from Tuesday to Friday next week, to go back to the consistent pattern of growth that the company had been experiencing from 28 January to 10 February 2025.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20250216-1259.php");
   $index->Display();
