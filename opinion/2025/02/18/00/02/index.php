7ed44f9d0244ee596c13afeb2b2ffb18af3d234d
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-17 19:28:59 -0600

    Fix path.

--- opinion/2025/02/18/00/02/index.php
+++ opinion/2025/02/18/00/02/index.php
@@ -5,6 +5,6 @@
   $index->parentPage = "Opinion";
   $index->metaDescription = "The January 2025 monthly revenue of TSMC resulted in a 35.9% year-over-year jump that can indicate that the demand for Nvidia's AI chips is still growing. This is because Nvidia is outsourcing the fabrication of the semiconductor chips it produces, focusing only on creating the design of those chips. The manufacturing is done by TSMC, meaning that TSMC's revenue can be an indicator of demand for Nvidia's chips that are currently important as an engine for the AI revolution.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20250217-2240.php");
+  require("book-info-20250218-0002.php");
   $index->Display();
 ?>
