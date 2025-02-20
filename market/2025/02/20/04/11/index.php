7b00988176b744357aa0dcb7b1f16231a8166ac4
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-20 06:50:34 -0600

    Change text position.

--- market/2025/02/20/04/11/index.php
+++ market/2025/02/20/04/11/index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
   $market->parentPage = "Market";
   $market->metaDescription = "The chart illustrates the hypothetical performance of a $10,000 investment in Apple and S&P 500 (SPX).";
   $market->content .= "\t\t\t<h1>".$market->title."</h1>";
-  require("book-info-20250220-0411.php");
   require("chart.php");
+  require("book-info-20250220-0411.php");
   $market->Display();
 ?>
