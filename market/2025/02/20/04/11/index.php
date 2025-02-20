285b7bc7a1e9e61730addf46d70c47fa47df7146
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-20 06:07:13 -0600

    Display PHP errors.

--- market/2025/02/20/04/11/index.php
+++ market/2025/02/20/04/11/index.php
@@ -1,4 +1,6 @@
-<?php
+<?php ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
   require("../../../../../../market.php");
   $market = new CapitalPage();
   $market->title = "Performance comparison: Apple Vs. S&P 500 (SPX)";
