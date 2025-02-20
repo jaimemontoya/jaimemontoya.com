089913b6308c069170f42d48eedc56eda44177bd
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-20 06:18:26 -0600

    Change variable name.

--- market/2025/02/20/04/11/book-info-20250220-0411.php
+++ market/2025/02/20/04/11/book-info-20250220-0411.php
@@ -1,5 +1,5 @@
 <?php
-  $index->content .= 
+  $market->content .= 
            "<p>The chart below illustrates the hypothetical performance of a $10,000 investment in Apple and S&P 500 (SPX).</p>
 			<div>Published: 4:11 AM GMT · Feb 20, 2024</div>\n";
 ?>
