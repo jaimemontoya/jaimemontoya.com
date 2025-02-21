e0ec1ae86d90990ed76d3e1ab7ac5fef90555e1c
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-20 23:45:39 -0600

    Add DJIA values.

---
+++ market/2025/02/21/05/13/book-info-20250221-0513.php
@@ -0,0 +1,5 @@
+<?php
+  $market->content .= 
+           "<p>The chart above illustrates the hypothetical performance of a $1,000 investment in Apple and Global X Dow 30 Covered Call ETF (DJIA).</p>
+			<div>Published: 5:13 AM GMT · Feb 21, 2024</div>\n";
+?>
--- market/index.php
+++ market/index.php
@@ -4,6 +4,7 @@
   $index->title = "Market";
   $index->metaDescription = "Jaime Montoya's monitoring tool to beat the market.";
   $index->content .= "<h1>Market</h1><p>Monitoring tool to beat the market.</p>";
+  require("2025/02/21/05/13/20250221-0513-book-info.php");
   require("2025/02/20/04/11/20250220-0411-book-info.php");
   $index->Display();
 ?>
