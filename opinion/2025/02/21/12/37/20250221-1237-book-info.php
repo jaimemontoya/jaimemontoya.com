4764bc469cf99819b4f367dc2ba8f97a00c583e4
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-21 12:04:24 -0600

    Add Opinion entry for 21 Feb 2025.

---
+++ opinion/2025/02/21/12/37/20250221-1237-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/21/12/37/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250221-1237-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia recovering in Feb 2025. Tesla in an alarming 2025 performance. Roller coaster results for Ellison. Arnault enjoying a great 2025.</h2>
+					<p>Bernard Arnault increased his net worth by $2.8 billion on Thursday. His company LVMH has had generally flat stock results in February 2025, but since January 2025 was outstanding for the company, overall LVMH has a great 2025 balance. <mark>Published: 12:37 PM GMT · 21 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/02/21/12/37/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Nvidia recovering in Feb 2025. Tesla in an alarming 2025 performance. Roller coaster results for Ellison. Arnault enjoying a great 2025.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Bernard Arnault increased his net worth by $2.8 billion on Thursday. His company LVMH has had generally flat stock results in February 2025, but since January 2025 was outstanding for the company, overall LVMH has a great 2025 balance.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250221-1237.php");
+  $index->Display();
+?>
