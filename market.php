b552a84e9af40a4f83801f9537aaf8130908cb04
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-22 09:11:59 -0600

    Change value from Apple to Nvidia.

--- market.php
+++ market.php
@@ -159,7 +159,7 @@ class MarketPage extends Page
 	echo "\t\t\t\t// Set chart options\n";
 	echo "\t\t\t\tvar optionsNvidiaVsSP500 = {chart:{'title':'Performance comparison: Apple Vs. S&P 500 (SPX)',subtitle:'in dollars (USD)'}};\n";	
 	echo "\t\t\t\tvar chartNvidiaVsSP500 = new google.visualization.LineChart(document.getElementById('nvidia-vs-sp500'));\n";
-    echo "\t\t\t\tchartAppleVsSP500.draw(dataNvidiaVsSP500, optionsNvidiaVsSP500);\n";
+    echo "\t\t\t\tchartNvidiaVsSP500.draw(dataNvidiaVsSP500, optionsNvidiaVsSP500);\n";
 	echo "\t\t\t\t// Add our over/out handlers.\n";
 	echo "\t\t\t\tgoogle.visualization.events.addListener(barsVisualization, 'onmouseover', barMouseOver);\n";
 	echo "\t\t\t\tgoogle.visualization.events.addListener(barsVisualization, 'onmouseout', barMouseOut);\n";
