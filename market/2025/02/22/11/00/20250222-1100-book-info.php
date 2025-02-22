f78b42cea5997389a94f44f9e0308e31cfd857d3
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-21 23:42:40 -0600

    Add Nvidia Vs. NASDAQ comparison.

---
+++ market/2025/02/22/11/00/20250222-1100-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/22/11/00/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"no-photo-available-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Performance comparison: Nvidia Vs. NASDAQ Composite Index (COMP)</h2>
+					<p>The chart illustrates the hypothetical performance of a $1,000 investment in Nvidia and NASDAQ Composite Index (COMP). <mark>Published: 11:00 AM GMT · Feb 22, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ market/2025/02/22/11/00/book-info-20250222-1100.php
@@ -0,0 +1,5 @@
+<?php
+  $market->content .= 
+           "<p>The chart above illustrates the hypothetical performance of a $1,000 investment in Nvidia and NASDAQ Composite Index (COMP).</p>
+			<div>Published: 11:00 AM GMT · Feb 22, 2024</div>\n";
+?>
