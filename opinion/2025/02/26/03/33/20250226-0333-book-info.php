238cfe3a7f838edf4f59b266da39d4b4a660bbe5
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-02-26 18:44:10 -0600

    Add Opinion entry for 26 Feb 2025.

---
+++ opinion/2025/02/26/03/33/20250226-0333-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/02/26/03/33/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250226-0333-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nightmare for Tesla. TSMC richer than Tesla. Aramco performing better than all Magnificent 7 stocks. Musk lost $15.6 billion. Buffett having a great week for his net worth. Bezos richer than Zuckerberg.</h2>
+					<p>Saudi Aramco had a positive stock price growth of 0.74% on Tuesday, better than all Magnificent 7 stocks (Apple, Microsoft, Amazon, Alphabet, Meta, Nvidia, and Tesla). <mark>Published: 2:33 AM GMT · 26 Feb 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
