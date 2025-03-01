c5bd04b84de65ca91f8b218a49a88eb01bf04095
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-01 03:46:04 -0600

    Add Opinion entry for 1 March 2025.

---
+++ opinion/2025/03/01/07/27/20250301-0727-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/03/01/07/27/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250301-0727-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia relied on dip-buyers to end February. Saudi Aramco having a negative 2025. Finally a positive day for Tesla. Tesla richer than TSMC and Broadcom. Buffett continues enjoying an outstanding 2025. Arnault experienced modest growth but not as good as his peers.</h2>
+					<p>Dip-buyers allowed Nvidia to close the week on Friday with a positive result of almost 4% for its stock price. <mark>Published: 7:27 AM GMT · 1 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/03/01/07/27/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Nvidia relied on dip-buyers to end February. Saudi Aramco having a negative 2025. Finally a positive day for Tesla. Tesla richer than TSMC and Broadcom. Buffett continues enjoying an outstanding 2025. Arnault experienced modest growth but not as good as his peers.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Dip-buyers allowed Nvidia to close the week on Friday with a positive result of almost 4% for its stock price.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250301-0727.php");
+  $index->Display();
+?>
