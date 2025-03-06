dfd37b16e52d04de2044ec9bfe93c17062d65194
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-06 10:37:52 -0600

    Add Opinion entry for 5 Mar 2025.

---
+++ opinion/2025/03/05/08/48/20250305-0848-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/03/05/08/48/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250305-0848-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Berkshire Hathaway and Buffett experiencing a price fall on Tuesday. TSMC to invest $100 billion in the U.S. TSMC and Broadcom richer than Tesla. Finally a positive day for Larry Page in a difficult 2025.</h2>
+					<p>Larry Page had a good Tuesday to make his net worth grow by $3.1 billion (2.20% increase). That was a little relief considering that his company, Alphabet, has been experiencing consistent decrease in its stock price since 5 Feb 2025 to 3 Mar 2025. <mark>Published: 8:48 AM GMT · 5 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
