cb2becc59339858440f2e28d7cffe46808da4781
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-07 04:07:30 -0600

    Add Opinion entry for 7 Mar 2025.

---
+++ opinion/2025/03/07/04/33/20250307-0433-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/03/07/04/33/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250307-0433-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia at its lowest price in 2025. Berkshire Hathaway navigating well the 2025 uncertainty and Buffett increasing his massive wealth. Tesla richer than Broadcom. Ellison lost $12.1 billion.</h2>
+					<p>Larry Ellison lost $12.1 billion (6.06% of his fortune) on Thursday in the stock market fluctuations as Oracle suffered another bad day in 2025. The AI and technology sector, in general, are having a negative 2025. <mark>Published: 4:33 AM GMT · 7 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/03/07/04/33/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Nvidia at its lowest price in 2025. Berkshire Hathaway navigating well the 2025 uncertainty and Buffett increasing his massive wealth. Tesla richer than Broadcom. Ellison lost $12.1 billion.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Larry Ellison lost $12.1 billion (6.06% of his fortune) on Thursday in the stock market fluctuations as Oracle suffered another bad day in 2025. The AI and technology sector, in general, are having a negative 2025.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250307-0433.php");
+  $index->Display();
+?>
