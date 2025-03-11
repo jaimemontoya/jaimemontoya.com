2a37f75fcd8c64b6ab60e43de0f0a8384c17a462
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-11 05:42:45 -0600

    Add Opinion entry for 11 Mar 2025.

---
+++ opinion/2025/03/11/01/10/20250301-0110-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/03/11/01/10/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250311-0110-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Berkshire Hathaway strong and growing during the 2025 storms. Broadcom experienced heavy share price decline. Broadcom richer than TSMC. Musk losing money at alarming rate. Buffett consistently increasing his wealth even during difficult times. Bezos richer than Zuckerberg.</h2>
+					<p>Elon Musk continues experiencing alarming declines for his net worth. Every single week since President Trump's inauguration in January, Tesla has experienced a price share fall. <mark>Published: 1:10 AM GMT · 11 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
