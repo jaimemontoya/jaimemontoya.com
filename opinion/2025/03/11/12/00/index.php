bd5269a169a486a7d940e199a2cf2b1b416c2398
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-11 06:40:07 -0600

    Update Opinion values.

--- opinion/2025/03/11/12/00/index.php
+++ opinion/2025/03/11/12/00/index.php
@@ -1,10 +1,10 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "Berkshire Hathaway strong and growing during the 2025 storms. Broadcom experienced heavy share price decline. Broadcom richer than TSMC. Musk losing money at alarming rate. Buffett consistently increasing his wealth even during difficult times. Bezos richer than Zuckerberg.";
+  $index->title = "Berkshire Hathaway strong and growing during the 2025 storms. Broadcom experienced heavy share price decline. Amazon richer than Alphabet. Musk losing money at alarming rate. Buffett consistently increasing his wealth even during difficult times. Bezos richer than Zuckerberg.";
   $index->parentPage = "Opinion";
   $index->metaDescription = "Elon Musk continues experiencing alarming declines for his net worth. Every single week since President Trump's inauguration in January, Tesla has experienced a price share fall.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20250311-0110.php");
+  require("book-info-20250311-1200.php");
   $index->Display();
 ?>
