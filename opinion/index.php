c628626d1bdd1f1d2e1f5e29411efe36aa94afc1
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-11 05:48:24 -0600

    Comment out code.

--- opinion/index.php
+++ opinion/index.php
@@ -6,7 +6,7 @@
   $index->content .= "<h1>Opinion</h1><p>Stock Market, Business & Finance Opinion since 2024-Dec-30 9:47 PM GMT.</p>";
   $index->content .= 
            "\t\t\t<iframe src=\"https://docs.google.com/document/d/e/2PACX-1vSI9um9QUifKrVuSKbRJexc1qWy1aaFrSOc3Rjzx8WrGU9_INb5x8vR3EmTPkcdom2mgN5kwW9J6RTq/pub?embedded=true\" id=\"opinion-spreadsheet\"></iframe>\n";
-  require("2025/03/11/01/10/20250311-0110-book-info.php");
+  //require("2025/03/11/01/10/20250311-0110-book-info.php");
   require("2025/03/08/11/23/20250308-1123-book-info.php");
   require("2025/03/07/04/33/20250307-0433-book-info.php");
   require("2025/03/06/14/27/20250306-1427-book-info.php");
