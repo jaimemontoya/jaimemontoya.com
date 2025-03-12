d12a393fb4b10265aee7a4ec0480ab16cd4f5e42
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-11 18:58:46 -0600

    Add Opinion entry for 12 Mar 2025.

---
+++ opinion/2025/03/12/00/09/20250312-0009-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/03/12/00/09/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250312-0009-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Delaying rollout of AI-driven features hurts Apple. Investors finding value in Broadcom stock. Good Tuesday for Musk, finally. Ellison losing money as Oracle did not meet expectations after reporting quarterly results.</h2>
+					<p>Tuesday was finally a day of growth for Elon Musk’s net worth. Nonetheless, this could simply mean that investors are seeing the opportunity to buy low, seeing potential in Tesla for the artificial intelligence and robotics future that the company could revolutionize or at least, participate in the revolution. 2025 is still a nightmare for Elon Musk and Tesla. <mark>Published: 12:09 AM GMT · 12 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/03/12/00/09/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Delaying rollout of AI-driven features hurts Apple. Investors finding value in Broadcom stock. Good Tuesday for Musk, finally. Ellison losing money as Oracle did not meet expectations after reporting quarterly results.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Tuesday was finally a day of growth for Elon Musk’s net worth. Nonetheless, this could simply mean that investors are seeing the opportunity to buy low, seeing potential in Tesla for the artificial intelligence and robotics future that the company could revolutionize or at least, participate in the revolution. 2025 is still a nightmare for Elon Musk and Tesla.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250312-0009.php");
+  $index->Display();
+?>
