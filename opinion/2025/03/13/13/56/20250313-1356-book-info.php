dc57852634a2688d8c974557536fe3d9831e2fe2
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-13 12:21:27 -0600

    Change time.

--- opinion/2025/03/13/13/56/20250313-1356-book-info.php
+++ opinion/2025/03/13/13/56/20250313-1356-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Difficult 2025 for Apple. Nvidia growing only after falling at a faster speed than its growth. TSMC richer than Broadcom. Ellison increased his net worth by $7.9 billion. Ortega’s net worth in decline. Ballmer richer than Ortega.</h2>
-					<p>TSMC is now richer than Broadcom, with TSMC being upgraded from 10th to 9th place, and Broadcom the opposite, in the ranking of richest companies in the world. The price volatility continues for both companies. <mark>Published: 1:56 AM GMT · 13 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>TSMC is now richer than Broadcom, with TSMC being upgraded from 10th to 9th place, and Broadcom the opposite, in the ranking of richest companies in the world. The price volatility continues for both companies. <mark>Published: 1:56 PM GMT · 13 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
