0b86c3ea272a89cb49a131cacc2bd249a276c5a7
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-13 07:19:41 -0600

    Update money values for 13 Mar 2025.

--- opinion/2025/03/12/00/09/20250312-0009-book-info.php
+++ opinion/2025/03/12/00/09/20250312-0009-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Delaying rollout of AI-driven features hurts Apple. Investors finding value in Broadcom stock. Good Tuesday for Musk, finally. Ellison losing money as Oracle did not meet expectations after reporting quarterly results.</h2>
-					<p>Tuesday was finally a day of growth for Elon Musk’s net worth. Nonetheless, this could simply mean that investors are seeing the opportunity to buy low, seeing potential in Tesla for the artificial intelligence and robotics future that the company could revolutionize or at least, participate in the revolution. 2025 is still a nightmare for Elon Musk and Tesla. <mark>Published: 12:09 AM GMT · 12 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>Tuesday was finally a day of growth for Elon Musk’s net worth. Nonetheless, this could simply mean that investors are seeing the opportunity to buy low, predicting potential in Tesla for the artificial intelligence and robotics future that the company could revolutionize or at least, participate in the revolution. 2025 is still a nightmare for Elon Musk and Tesla. <mark>Published: 12:09 AM GMT · 12 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
