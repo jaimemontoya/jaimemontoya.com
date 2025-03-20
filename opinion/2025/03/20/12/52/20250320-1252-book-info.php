4d2e63d24ef2436ff19124a6603d104aa1c2718d
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-20 11:12:34 -0600

    Update money values for 20 Mar 2025.

---
+++ opinion/2025/03/20/12/52/20250320-1252-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/03/19/04/05/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250320-1252-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Broadcom a potential challenger for Nvidia. Meta in negative territory. Broadcom richer than TSMC. Ortega richer than Ballmer. Arnault in a pattern of decline.</h2>
+					<p>Meta had a negative Wednesday for its stock price, which was the last straw to bring the company into negative territory for 2025. The result was that Meta joined all Magnificent 7 Stock companies in the trajectory of negative 2025. Meta was the only one still in positive territory, from that list of 7 giants leading the markets a few months ago (Meta Platforms, Apple, Microsoft, Nvidia, Alphabet, Amazon, and Tesla). <mark>Published: 12:52 PM GMT · 20 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/03/20/12/52/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Broadcom a potential challenger for Nvidia. Meta in negative territory. Broadcom richer than TSMC. Ortega richer than Ballmer. Arnault in a pattern of decline.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Meta had a negative Wednesday for its stock price, which was the last straw to bring the company into negative territory for 2025. The result was that Meta joined all Magnificent 7 Stock companies in the trajectory of negative 2025. Meta was the only one still in positive territory, from that list of 7 giants leading the markets a few months ago (Meta Platforms, Apple, Microsoft, Nvidia, Alphabet, Amazon, and Tesla).";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250320-1252.php");
+  $index->Display();
+?>
