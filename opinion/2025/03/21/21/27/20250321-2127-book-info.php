90ca17933cebcb98556fbf0a2323a38485d04994
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-21 16:30:14 -0600

    Add text.

--- opinion/2025/03/21/21/27/20250321-2127-book-info.php
+++ opinion/2025/03/21/21/27/20250321-2127-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Apple had a successful week. Nvidia in decline. Microsoft richer than Nvidia.</h2>
-					<p>Apple enjoyed a successful week even in the middle of stock market uncertainty and political changes in the United States. With a share price growth of 2.33%, Apple is still the richest company in the world. <mark>Published: 9:27 PM GMT · 21 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>Apple enjoyed a successful week even in the middle of stock market uncertainty and political changes in the United States. With a share price growth of 2.33% this week, Apple is still the richest company in the world. <mark>Published: 9:27 PM GMT · 21 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
--- opinion/2025/03/21/21/27/index.php
+++ opinion/2025/03/21/21/27/index.php
@@ -3,7 +3,7 @@
   $index = new Page();
   $index->title = "Apple had a successful week. Nvidia in decline. Microsoft richer than Nvidia.";
   $index->parentPage = "Opinion";
-  $index->metaDescription = "Apple enjoyed a successful week even in the middle of stock market uncertainty and political changes in the United States. With a share price growth of 2.33%, Apple is still the richest company in the world.";
+  $index->metaDescription = "Apple enjoyed a successful week even in the middle of stock market uncertainty and political changes in the United States. With a share price growth of 2.33% this week, Apple is still the richest company in the world.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
   require("book-info-20250321-2127.php");
   $index->Display();
