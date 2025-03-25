9c33c5ef9ba6a4b3554eb6e7cfbf3bf26c11030c
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-25 04:10:36 -0600

    Add book Robert's Rules of Order In Brief, 3rd edition.

---
+++ reading/roberts-rules-order-in-brief-3rd-ed/roberts-rules-order-in-brief-3rd-ed-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"roberts-rules-order-in-brief-3rd-ed-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Robert's Rules of Order Newly Revised In Brief, 3rd edition</span>
+					<p>By Henry M. Robert, Daniel H. Honemann, Thomas J. Balch, Daniel E. Seabold & Shmuel Gerber. <mark>Book [in progress...] read by Jaime Montoya</mark> from 7:34 PM March 22, 2025 to [in progress...] GMT.</p>
+                </div>
+            </div>\n";
+?>
