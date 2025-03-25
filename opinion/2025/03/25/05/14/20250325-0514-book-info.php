aa2df2003b8e5fb0fdee11e032695883a46a9c0f
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-25 00:21:39 -0600

    Add Opinion entry for 25 Mar 2025.

---
+++ opinion/2025/03/25/05/14/20250325-0514-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/03/25/05/14/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250325-0514-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Meta is back to a trajectory of growth. Broadcom started the week with negative results. Nvidia richer than Microsoft. Musk had a wonderful Monday. Arnault still lagging in 2025.</h2>
+					<p>Nvidia is richer than Microsoft again. Nvidia was upgraded from 3rd to 2nd, while Microsoft was downgraded from 2nd to 3rd, in the ranking of richest companies in the world. Nvidia is well-known for its high volatility. The challenge for the company is to remain in that great position for more than just a couple of days. <mark>Published: 5:14 AM GMT · 25 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/03/25/05/14/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Meta is back to a trajectory of growth. Broadcom started the week with negative results. Nvidia richer than Microsoft. Musk had a wonderful Monday. Arnault still lagging in 2025.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Nvidia is richer than Microsoft again. Nvidia was upgraded from 3rd to 2nd, while Microsoft was downgraded from 2nd to 3rd, in the ranking of richest companies in the world. Nvidia is well-known for its high volatility. The challenge for the company is to remain in that great position for more than just a couple of days.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250325-0514.php");
+  $index->Display();
+?>
