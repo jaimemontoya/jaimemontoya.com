a4afb120b40809c82b51cd543e0df4b88379dbf0
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-25 20:34:10 -0600

    Add Opinion entry for 26 Mar 2025.

---
+++ opinion/2025/03/26/01/40/20250326-0140-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/03/26/01/40/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"opinion-20250326-0140-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Nvidia suffering a negative 2025. Tesla enjoying a rebound. Tesla richer than Broadcom. Page increasing his fortune. Ortega having negative 2025.</h2>
+					<p>Larry Page had a great Tuesday increasing his fortune by $2.2 billion. Alphabet’s stock price has been growing since 19 Mar 2025. That has not been enough to change the reality of a negative 2025 for Alphabet, with a -9.36% year to date performance. <mark>Published: 1:40 AM GMT · 26 Mar 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ opinion/2025/03/26/01/40/index.php
@@ -0,0 +1,10 @@
+<?php
+  require("../../../../../../page.php");
+  $index = new Page();
+  $index->title = "Nvidia suffering a negative 2025. Tesla enjoying a rebound. Tesla richer than Broadcom. Page increasing his fortune. Ortega having negative 2025.";
+  $index->parentPage = "Opinion";
+  $index->metaDescription = "Larry Page had a great Tuesday increasing his fortune by $2.2 billion. Alphabet’s stock price has been growing since 19 Mar 2025. That has not been enough to change the reality of a negative 2025 for Alphabet, with a -9.36% year to date performance.";
+  $index->content .= "\t\t\t<h1>".$index->title."</h1>";
+  require("book-info-20250326-0140.php");
+  $index->Display();
+?>
