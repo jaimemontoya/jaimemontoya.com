0d730ff9c30cd5a33b36f4cb57bf2c3f4d46b2e9
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-31 17:46:54 -0600

    Add image.

--- blog/2025/03/31/23/06/book-info-20250331-2306.php
+++ blog/2025/03/31/23/06/book-info-20250331-2306.php
@@ -1,4 +1,7 @@
 <?php
   $index->content .= 
-           "<p>A<p><div>Published: 11:06 PM GMT · Mar 31, 2025</div>\n";
+           "<p>The goal was to redirect all https://christianmontessorifellowship.com visitors, to the following website: https://internationalcmi.com/.<p>
+		   <p>The domain registrar of <span class=\"cod\">christianmontessorifellowship.com</span> was Namecheap. After logging in to <span class=\"cod\">https://namecheap.com</span>, I clicked <span class=\"cod\">MANAGE</span>:</p>
+		   <div><img src=\"/blog/2025/03/31/23/06/img/manageNamecheapDomain.jpg\" alt=\"Manage Namecheap domain\" width=\"1828\" height=\"1005\"></div>
+		   <div>Published: 11:06 PM GMT · Mar 31, 2025</div>\n";
 ?>
