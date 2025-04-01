e1321cf6ec1270254a69c000fd6327f0e6030533
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-03-31 18:29:45 -0600

    Add image.

--- blog/2025/03/31/23/06/book-info-20250331-2306.php
+++ blog/2025/03/31/23/06/book-info-20250331-2306.php
@@ -7,5 +7,7 @@
 		   <div><img src=\"/blog/2025/03/31/23/06/img/viewNamecheapNameservers.jpg\" alt=\"View Namecheap nameservers\" width=\"1820\" height=\"1002\"></div>
 		   <p>The domain registrar of <span class=\"cod\">internationalcmi.com</span> was Cloudflare. After logging in to <span class=\"cod\">cloudflare.com</span>, under <span class=\"cod\">Domain Registration > Manage Domains</span>, I clicked the <span class=\"cod\">Manage</span> link:</p>
 		   <div><img src=\"/blog/2025/03/31/23/06/img/manageDomainOnCloudflare.jpg\" alt=\"Manage domain on Cloudflare\" width=\"1485\" height=\"801\"></div>
+		   <p>I clicked <span class=\"cod\">Update DNS configuration</span>:</p>
+		   <div><img src=\"/blog/2025/03/31/23/06/img/updateDNSConfigurationFromCloudflare.jpg\" alt=\"Update DNS configuration from Cloudflare\" width=\"1485\" height=\"804\"></div>
 		   <div>Published: 11:06 PM GMT · Mar 31, 2025</div>\n";
 ?>
