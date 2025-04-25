cf28a45688f641751ad1c2c16f1474b38b604166
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-04-25 09:29:19 -0600

    Add entry to News.

--- news/2025/04/25/14/56/index.php
+++ news/2025/04/25/14/56/index.php
@@ -1,10 +1,10 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "NVIDIA Corporation (NVDA), my first stock purchase";
+  $index->title = "Selling my stocks and bringing the money back from Interactive Brokers LLC, to my bank account in El Salvador";
   $index->parentPage = "News";
-  $index->metaDescription = "After reading a few books related to investing and the stock market, using a couple of simulators, and completing my research, I made my first official, real stock purchase from NVIDIA Corporation (NVDA).";
+  $index->metaDescription = "Description of step-by-step process to bing money back from Interactive Brokers LLC, my bank account in El Salvador, after selling my stocks.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20241113-2239.php");
+  require("book-info-20250425-1456.php");
   $index->Display();
 ?>
--- news/index.php
+++ news/index.php
@@ -4,6 +4,7 @@
   $index->title = "News";
   $index->metaDescription = "Personal news since November 7, 2024, 00:00 AM GMT.";
   $index->content .= "<h1>News</h1><p>Personal news since November 7, 2024, 00:00 AM GMT.</p>";
+  require("2025/04/25/14/56/20250425-1456-book-info.php");
   require("2024/11/13/22/39/20241113-2239-book-info.php");
   require("2024/11/11/22/13/20241111-2213-book-info.php");
   require("2024/11/07/00/00/20241107-0000-book-info.php");
