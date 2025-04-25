d92d59e247c9f71cc35d0efe22c2e2b54a519659
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-04-25 15:18:24 -0600

    Change thumbnail.

--- news/2025/04/25/14/56/20250425-1456-book-info.php
+++ news/2025/04/25/14/56/20250425-1456-book-info.php
@@ -2,7 +2,7 @@
   $index->content .= 
            "\t\t\t<a href=\"2025/04/25/14/56/\"><div class=\"book\">
 				<div class=\"book-thumbnail\">
-					<div class=\"book-cover\" id=\"news-20241113-2239-img\"></div>
+					<div class=\"book-cover\" id=\"news-20250425-1456-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Selling my stocks and bringing the money back from Interactive Brokers LLC, to my bank account in El Salvador</h2>
