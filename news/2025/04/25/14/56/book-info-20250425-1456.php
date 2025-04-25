598ab56cf7caefb77fa08ff12f95ac93170018bb
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-04-25 10:27:09 -0600

    Check to confirm information.

--- news/2025/04/25/14/56/book-info-20250425-1456.php
+++ news/2025/04/25/14/56/book-info-20250425-1456.php
@@ -13,6 +13,8 @@
   <div><img src=\"/news/2025/04/25/14/56/img/provideCorrespondentSelection.jpg\" alt=\"Provide Correspondent Selection\" width=\"1867\" height=\"1018\"></div>
   <p>I provided the <span class=\"cod\">SWIFT/BIC Code</span> and clicked <span class=\"cod\">Submit</span>:</p>
   <div><img src=\"/news/2025/04/25/14/56/img/privideSwiftBicCodAndClickSubmit.jpg\" alt=\"Provide SWIFT/BIC Code and click Submit\" width=\"1855\" height=\"1012\"></div>
+  <p>I observed that the information looked correct:</p>
+  <div><img src=\"/news/2025/04/25/14/56/img/informationLookingCorrect.jpg\" alt=\"Information looking correct\" width=\"1858\" height=\"1017\"></div>
   <div><div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   ";
   /*$index->content .= 
