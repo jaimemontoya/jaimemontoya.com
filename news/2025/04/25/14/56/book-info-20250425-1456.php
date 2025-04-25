16ed3485017431d58b8a1fec81eda4fd6bce6514
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-04-25 11:00:01 -0600

    Click Select to withdraw funds.

--- news/2025/04/25/14/56/book-info-20250425-1456.php
+++ news/2025/04/25/14/56/book-info-20250425-1456.php
@@ -26,6 +26,8 @@
   <p>I clicked <span class=\"cod\">Finish</span>:</p>
   <div><img src=\"/news/2025/04/25/14/56/img/processCompletedSuccessfullyPart1Of2.jpg\" alt=\"Process completed successfully. Part 1 of 2.\" width=\"1867\" height=\"1013\"></div>
   <div><img src=\"/news/2025/04/25/14/56/img/processCompletedSuccessfullyPart2Of2.jpg\" alt=\"Process completed successfully. Part 2 of 2.\" width=\"1862\" height=\"1015\"></div>
+  <p>I clicked <span class=\"cod\">Select</span> to withdraw funds:</p>
+  <div><img src=\"/news/2025/04/25/14/56/img/processCompletedSuccessfullyPart2Of2.jpg\" alt=\"Process completed successfully. Part 2 of 2.\" width=\"1862\" height=\"1015\"></div>
   <div><div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   ";
   /*$index->content .= 
