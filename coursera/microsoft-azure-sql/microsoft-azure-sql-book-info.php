3d50c85ec625d7321e1980fdc0cf91dfcb4d0137
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-06-27 23:33:37 -0600

    Add Coursera achievement.

---
+++ coursera/microsoft-azure-sql/microsoft-azure-sql-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"/coursera/microsoft-azure-sql/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"microsoft-azure-sql-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <span>Microsoft Azure SQL</span>
+					<p>By Microsoft. <mark>Course 4 successfully completed by Jaime Montoya</mark> from 2:51 PM August 2, 2023 to 3:00 AM June 28, 2025 GMT. <span class=\"linkstyle\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
---
+++ coursera/microsoft-azure-sql/microsoft-azure-sql.php
@@ -0,0 +1,8 @@
+<?php
+  $index->content .= 
+           "\t\t\t<h1>Microsoft Azure SQL</h1>
+		    <div>
+			    <p>Verify at <a href=\"https://coursera.org/verify/GPB17YP4VI82\">https://coursera.org/verify/GPB17YP4VI82</a>.</p>
+                <img src=\"/coursera/microsoft-azure-sql/img/microsoft-azure-sql.jpg\" alt=\"Microsoft Azure SQL - Coursera - Jaime Montoya\" width=\"1097\" height=\"848\">
+            </div>\n";
+?>
