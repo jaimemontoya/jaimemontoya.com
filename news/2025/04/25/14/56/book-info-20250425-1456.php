afc41d097afc200c5121fb3d6b70199ef14f6fcc
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-02 00:47:59 -0600

    Click Sell for Pfizer.

--- news/2025/04/25/14/56/book-info-20250425-1456.php
+++ news/2025/04/25/14/56/book-info-20250425-1456.php
@@ -242,7 +242,9 @@ Gracias por haber utilizado nuestro servicio de WhatsApp. Esperamos tenga un ex
   </pre>
   <p>From the home page, I clicked <span class=\"cod\">Portfolio</span>:</p>
   <div><img src=\"/news/2025/04/25/14/56/img/clickPortfolio.jpg\" alt=\"Click Portfolio.\" width=\"1098\" height=\"1007\"></div>
-  <div><div>Published: 2:56 PM GMT · Apr 25, 2024</div>
+  <p>I clicked <span class=\"cod\">Sell</span> for <span class=\"cod\">Pfizer</span>:</p>
+  <div><img src=\"/news/2025/04/25/14/56/img/clickSellForPfizer.jpg\" alt=\"Click Sell for Pfizer.\" width=\"1098\" height=\"1011\"></div>
+  <div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   ";
   /*$index->content .= 
            "<p>After reading a few books related to investing and the stock market, using a couple of simulators, and completing my research, I made my first official, real stock purchase from NVIDIA Corporation (NVDA).</p><div><img src=\"/news/2024/11/13/22/39/img/firstStockPurchase.jpg\" alt=\"First stock purchase\" width=\"960\" height=\"538\"></div><p>My wire transfer was completed successfully and with that money, I was able to purchase stock using Interactive Brokers:</p><div><img src=\"/news/2024/11/13/22/39/img/wireTransferPart1Of2.jpg\" alt=\"Wire transfer. Part 1 of 2.\" width=\"639\" height=\"834\"></div><div><img src=\"/news/2024/11/13/22/39/img/wireTransferPart2Of2.jpg\" alt=\"Wire transfer. Part 2 of 2.\" width=\"561\" height=\"914\"></div><p>I had one question related to fees and commissions, which was properly answered by the Interactive Brokers support team:</p><div><img src=\"/news/2024/11/13/22/39/img/feesAndCommissionsToInteractiveBrokers.jpg\" alt=\"Fees and commissions question\" width=\"1066\" height=\"793\"></div><div><div>Published: 10:39 PM GMT · Nov 13, 2024</div>\n";*/
