e26d7e9ddb4ee0c034d5554f89311c055fd7ba9b
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-02 01:08:58 -0600

    Click Accept to submit Sell Order for Pfizer.

--- news/2025/04/25/14/56/book-info-20250425-1456.php
+++ news/2025/04/25/14/56/book-info-20250425-1456.php
@@ -248,6 +248,8 @@ Gracias por haber utilizado nuestro servicio de WhatsApp. Esperamos tenga un ex
   <div><img src=\"/news/2025/04/25/14/56/img/clickPreviewSellOrderForPfizer.jpg\" alt=\"Click Preview Sell for Pfizer.\" width=\"1726\" height=\"1013\"></div>
   <p>I clicked <span class=\"cod\">Submit Sell Order</span>:</p>
   <div><img src=\"/news/2025/04/25/14/56/img/clickSubmitSellOrderForPfizer.jpg\" alt=\"Click Submit Sell Order for Pfizer.\" width=\"1895\" height=\"1005\"></div>
+  <p>I clicked <span class=\"cod\">Accept</span> to submit Sell Order for <span class=\"cod\">Pfizer</span>:</p>
+  <div><img src=\"/news/2025/04/25/14/56/img/clickAcceptToSubmitSellOrderForPfizer.jpg\" alt=\"Click Accept to submit Sell Order for Pfizer.\" width=\"1885\" height=\"1007\"></div>
   <div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   ";
   /*$index->content .= 
