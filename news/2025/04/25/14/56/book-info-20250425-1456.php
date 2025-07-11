e072a62778283352626f3dd72b442ae9cc4a703a
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-10 23:20:31 -0600

    Add paragraph explaining that the bank made a mistake.

--- news/2025/04/25/14/56/book-info-20250425-1456.php
+++ news/2025/04/25/14/56/book-info-20250425-1456.php
@@ -292,6 +292,7 @@ Gracias por haber utilizado nuestro servicio de WhatsApp. Esperamos tenga un ex
   <div><img src=\"/news/2025/04/25/14/56/img/withdrawalSubmittedPart3Of3.jpg\" alt=\"Withdrawal submitted. Part 3 of 3.\" width=\"1886\" height=\"1007\"></div>
   <p>I received an email confirmation for the processed withdrawal:</p>
   <div><img src=\"/news/2025/04/25/14/56/img/emailConfirmationOfProcessedWithdrawal.jpg\" alt=\"Email confirmation of processed withdrawal.\" width=\"1072\" height=\"701\"></div>
+  <p>The money had not arrived yet on 10 July 2025. My investigations with local banks (BAC Credomatic, Banco CUSCATLAN, and Citibank®, N.A. Sucursal El Salvador) revealed that BAC Credomatic made a mistake in the way they provided instructions to me, and BAC Credomatic acknowledged its mistake and apologized. I obtained the following pages from BAC Credomatic and Banco CUSCATLAN as part of my investigations about the information they normally provide for wire transfers, to understand what went wrong and how they provided wrong instructions to me about what to use as the SWIFT code for my transfer to receive money to my bank account:</p>
   <div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   ";
   /*$index->content .= 
