ebb5786dcd872eeddceb48c8f95ff409d4b7a407
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-17 15:36:12 -0600

    Change position of published date.

--- news/2025/04/25/14/56/book-info-20250425-1456.php
+++ news/2025/04/25/14/56/book-info-20250425-1456.php
@@ -319,11 +319,11 @@ Gracias por haber utilizado nuestro servicio de WhatsApp. Esperamos tenga un ex
   <div><img src=\"/news/2025/04/25/14/56/img/chooseWellsFargoBankAsTheCorrespondentBank.jpg\" alt=\"Choose Wells Fargo Bank as the Correspondent Bank\" width=\"1892\" height=\"1012\"></div>
   <p>I clicked <span class=\"cod\">Save Bank Information</span>:</p>
   <div><img src=\"/news/2025/04/25/14/56/img/saveCompleteBankInformation.jpg\" alt=\"Save complete bank information\" width=\"1880\" height=\"1007\"></div>
-  <div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   <p>I confirmed my identity by providing my username and password and clicked <span class=\"cod\">Continue</span>:</p>
   <div><img src=\"/news/2025/04/25/14/56/img/confirmIdentityForWireTransferPart1Of3.jpg\" alt=\"Confirm identity for wire transfer. Part 1 of 3.\" width=\"1892\" height=\"1006\"></div>
   <div><img src=\"/news/2025/04/25/14/56/img/confirmIdentityForWireTransferPart2Of3.jpg\" alt=\"Confirm identity for wire transfer. Part 2 of 3.\" width=\"1882\" height=\"1006\"></div>
   <div><img src=\"/news/2025/04/25/14/56/img/confirmIdentityForWireTransferPart3Of3.jpg\" alt=\"Confirm identity for wire transfer. Part 3 of 3.\" width=\"1902\" height=\"1008\"></div>
+  <div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   ";
   /*$index->content .= 
            "<p>After reading a few books related to investing and the stock market, using a couple of simulators, and completing my research, I made my first official, real stock purchase from NVIDIA Corporation (NVDA).</p><div><img src=\"/news/2024/11/13/22/39/img/firstStockPurchase.jpg\" alt=\"First stock purchase\" width=\"960\" height=\"538\"></div><p>My wire transfer was completed successfully and with that money, I was able to purchase stock using Interactive Brokers:</p><div><img src=\"/news/2024/11/13/22/39/img/wireTransferPart1Of2.jpg\" alt=\"Wire transfer. Part 1 of 2.\" width=\"639\" height=\"834\"></div><div><img src=\"/news/2024/11/13/22/39/img/wireTransferPart2Of2.jpg\" alt=\"Wire transfer. Part 2 of 2.\" width=\"561\" height=\"914\"></div><p>I had one question related to fees and commissions, which was properly answered by the Interactive Brokers support team:</p><div><img src=\"/news/2024/11/13/22/39/img/feesAndCommissionsToInteractiveBrokers.jpg\" alt=\"Fees and commissions question\" width=\"1066\" height=\"793\"></div><div><div>Published: 10:39 PM GMT · Nov 13, 2024</div>\n";*/
