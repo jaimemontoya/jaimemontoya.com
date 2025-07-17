8259b564c90aa8e8e751cecf9276c934bedae191
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-17 17:25:24 -0600

    Show withdrawal history.

--- news/2025/04/25/14/56/book-info-20250425-1456.php
+++ news/2025/04/25/14/56/book-info-20250425-1456.php
@@ -351,6 +351,8 @@ Gracias por haber utilizado nuestro servicio de WhatsApp. Esperamos tenga un ex
   <div><img src=\"/news/2025/04/25/14/56/img/wireWithdrawalSubmittedSuccessfullyPart1Of3.jpg\" alt=\"Wire Withdrawal submitted successfully. Part 2 of 3.\" width=\"1890\" height=\"1007\"></div>
   <div><img src=\"/news/2025/04/25/14/56/img/wireWithdrawalSubmittedSuccessfullyPart2Of3.jpg\" alt=\"Wire Withdrawal submitted successfully. Part 3 of 3.\" width=\"1893\" height=\"1021\"></div>
   <div><img src=\"/news/2025/04/25/14/56/img/wireWithdrawalSubmittedSuccessfullyPart3Of3.jpg\" alt=\"Wire Withdrawal submitted successfully. Part 1 of 3.\" width=\"1893\" height=\"1002\"></div>
+  <p>I checked the status of my withdrawals:</p>
+  <div><img src=\"/news/2025/04/25/14/56/img/showWithdrawalHistory.jpg\" alt=\"Show withdrawal history\" width=\"1892\" height=\"1012\"></div>
   <div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   ";
   /*$index->content .= 
