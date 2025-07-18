9f22df66f010f2338d3a48ae54afbe18948d00d9
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-18 08:39:01 -0600

    Show Wire transfer in my incoming money online.

--- news/2025/04/25/14/56/book-info-20250425-1456.php
+++ news/2025/04/25/14/56/book-info-20250425-1456.php
@@ -362,6 +362,8 @@ Gracias por haber utilizado nuestro servicio de WhatsApp. Esperamos tenga un ex
   <div><img src=\"/news/2025/04/25/14/56/img/fyiSuccessfulWithdrawalNotification.jpg\" alt=\"Show FYI successful withdrawal notification\" width=\"1885\" height=\"1011\"></div>
   <p>I noticed that my withdrawal was for USD 184.72, but the deposit that I received was for USD 88.00. That is, 184.72 - 88.00 = 96.72. The total in fees and payments related to the Wire transfer was USD 96.72:</p>
   <div><img src=\"/news/2025/04/25/14/56/img/showCostOfWireTransferFromUSAToElSalvador.jpg\" alt=\"Show cost of Wire transfer from the USA to El Salvador\" width=\"1896\" height=\"1008\"></div>
+  <p>From online banking, I saw the incoming money sent to my account:</p>
+  <div><img src=\"/news/2025/04/25/14/56/img/wireTransferInMyIncomingMoneyOnline.jpg\" alt=\"Wire transfer in my incoming money online\" width=\"1902\" height=\"1015\"></div>
   <div>Published: 2:56 PM GMT · Apr 25, 2024</div>
   ";
   /*$index->content .= 
