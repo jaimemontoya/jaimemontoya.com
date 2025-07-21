3b5d3438645817a15ae01cac96b196b894c2f73a
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-21 12:42:07 -0600

    Update path of image.

--- news/2025/07/21/17/00/book-info-20250721-1700.php
+++ news/2025/07/21/17/00/book-info-20250721-1700.php
@@ -1,8 +1,8 @@
 <?php
   $index->content .=
            "<p>This is the information that I received last year for the wire transfer:</p>
-		   <div><img src=\"/news/2024/11/11/22/13/img/wireTransfer2024InformationPart1Of2.jpg\" alt=\"Wire transfer 2024 information. Part 1 of 2.\" width=\"1168\" height=\"865\"></div>
-		   <div><img src=\"/news/2024/11/11/22/13/img/wireTransfer2024InformationPart2Of2.jpg\" alt=\"Wire transfer 2024 information. Part 2 of 2.\" width=\"1177\" height=\"867\"></div>
+		   <div><img src=\"/news/2025/07/21/17/00/img/wireTransfer2024InformationPart1Of2.jpg\" alt=\"Wire transfer 2024 information. Part 1 of 2.\" width=\"1168\" height=\"865\"></div>
+		   <div><img src=\"/news/2025/07/21/17/00/img/wireTransfer2024InformationPart2Of2.jpg\" alt=\"Wire transfer 2024 information. Part 2 of 2.\" width=\"1177\" height=\"867\"></div>
 		   <div>Published: 5:00 PM GMT · Jul 21, 2025</div>\n";
   //$index->content .= 
            "<p>Another milestone completed in the process to enter stock market investments. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. The beneficiary bank is:</p><p>Citibank N.A.<br />388 Greenwich Street<br />New York 10013<br />United States</p><p>The beneficiary of the funds is:</p><p>Interactive Brokers LLC<br />One Pickwick Plaza<br />Greenwich, Connecticut 06830<br />United States</p><div><img src=\"/news/2024/11/11/22/13/img/internationalTransferToInteractiveBrokersLLCSent.jpg\" alt=\"International transfer to Interactive Brokers\" width=\"568\" height=\"745\"></div><div>Published: 10:13 PM GMT · Nov 11, 2024</div>\n";
