b08bc0bf36d0db1e745e5c97d32e969b73415975
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-21 11:43:14 -0600

    Add new news entry.

---
+++ news/2025/07/21/17/00/20250721-1700-book-info.php
@@ -0,0 +1,12 @@
+<?php
+  $index->content .= 
+           "\t\t\t<a href=\"2025/07/21/17/00/\"><div class=\"book\">
+				<div class=\"book-thumbnail\">
+					<div class=\"book-cover\" id=\"news-20250721-1700-img\"></div>
+				</div>
+                <div class=\"book-title-authors-read-by\">
+		            <h2>Wire transfer from El Salvador to Interactive Brokers LLC in the USA</h2>
+					<p>Another important milestone completed for stock market investments. Wire transfer sent from my local bank to Interactive Brokers LLC in the USA. <mark>Published: 5:00 PM GMT · Jul 21, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+                </div>
+            </div></a>\n";
+?>
