41535bbc04bdc0127e3c6ef7aa90817c72c541d0
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-21 11:55:11 -0600

    Fix code.

--- news/2025/07/21/17/00/index.php
+++ news/2025/07/21/17/00/index.php
@@ -1,10 +1,10 @@
 <?php
   require("../../../../../../page.php");
   $index = new Page();
-  $index->title = "International transfer to Interactive Brokers LLC, preregistered";
+  $index->title = "Wire transfer from El Salvador to Interactive Brokers LLC in the USA";
   $index->parentPage = "News";
-  $index->metaDescription = "Another milestone completed in the process to enter investments stock market. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC.";
+  $index->metaDescription = "Another important milestone completed for stock market investments. Wire transfer sent from my local bank to Interactive Brokers LLC in the USA.";
   $index->content .= "\t\t\t<h1>".$index->title."</h1>";
-  require("book-info-20241111-2213.php");
+  require("book-info-20250721-1700.php");
   $index->Display();
 ?>
