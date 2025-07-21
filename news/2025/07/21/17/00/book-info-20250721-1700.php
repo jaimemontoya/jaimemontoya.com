18daeb39300a18a82d529c5392e35a2facfc8ada
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-21 16:27:17 -0600

    Click pending tab.

--- news/2025/07/21/17/00/book-info-20250721-1700.php
+++ news/2025/07/21/17/00/book-info-20250721-1700.php
@@ -37,6 +37,8 @@
 		   <p>I clicked <span class=\"cod\">Save template</span>:</p>
 		   <div><img src=\"/news/2025/07/21/17/00/img/saveTemplatePart1Of2.jpg\" alt=\"Save template. Part 1 of 2.\" width=\"1897\" height=\"1007\"></div>
 		   <div><img src=\"/news/2025/07/21/17/00/img/saveTemplatePart2Of2.jpg\" alt=\"Save template. Part 2 of 2.\" width=\"1902\" height=\"1007\"></div>
+		   <p>I navigated to the templates and clicked the <span class=\"cod\">Pending</span> tab:</p>
+		   
 		   <div>Published: 5:00 PM GMT · Jul 21, 2025</div>\n";
   //$index->content .= 
            "<p>Another milestone completed in the process to enter stock market investments. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. The beneficiary bank is:</p><p>Citibank N.A.<br />388 Greenwich Street<br />New York 10013<br />United States</p><p>The beneficiary of the funds is:</p><p>Interactive Brokers LLC<br />One Pickwick Plaza<br />Greenwich, Connecticut 06830<br />United States</p><div><img src=\"/news/2024/11/11/22/13/img/internationalTransferToInteractiveBrokersLLCSent.jpg\" alt=\"International transfer to Interactive Brokers\" width=\"568\" height=\"745\"></div><div>Published: 10:13 PM GMT · Nov 11, 2024</div>\n";
