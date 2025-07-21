5467d83408cfd8aafc3e00ce27c708dc3abf8230
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-21 13:27:11 -0600

    Provide Deposit Amount.

--- news/2025/07/21/17/00/book-info-20250721-1700.php
+++ news/2025/07/21/17/00/book-info-20250721-1700.php
@@ -7,6 +7,9 @@
 		   <div><img src=\"/news/2025/07/21/17/00/img/depositFundsToInteractiveBrokers.jpg\" alt=\"Deposit funds to Interactive Brokers\" width=\"1892\" height=\"1006\"></div>
 		   <p>I chose the method that I had used before:</p>
 		   <div><img src=\"/news/2025/07/21/17/00/img/choosingMethodThatIHadUseBefore.jpg\" alt=\"Choosing method that I have used before\" width=\"1897\" height=\"1013\"></div>
+		   <p>I provided the deposit amount I clicked <span class=\"cod\">Next</span>:</p>
+		   <div><img src=\"/news/2025/07/21/17/00/img/provideDepositAmountPart1Of2.jpg\" alt=\"Provide Deposit Amount. Part 1 of 2.\" width=\"1902\" height=\"1016\"></div>
+		   <div><img src=\"/news/2025/07/21/17/00/img/provideDepositAmountPart2Of2.jpg\" alt=\"Provide Deposit Amount. Part 1 of 2.\" width=\"1897\" height=\"1007\"></div>
 		   <div>Published: 5:00 PM GMT · Jul 21, 2025</div>\n";
   //$index->content .= 
            "<p>Another milestone completed in the process to enter stock market investments. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. The beneficiary bank is:</p><p>Citibank N.A.<br />388 Greenwich Street<br />New York 10013<br />United States</p><p>The beneficiary of the funds is:</p><p>Interactive Brokers LLC<br />One Pickwick Plaza<br />Greenwich, Connecticut 06830<br />United States</p><div><img src=\"/news/2024/11/11/22/13/img/internationalTransferToInteractiveBrokersLLCSent.jpg\" alt=\"International transfer to Interactive Brokers\" width=\"568\" height=\"745\"></div><div>Published: 10:13 PM GMT · Nov 11, 2024</div>\n";
