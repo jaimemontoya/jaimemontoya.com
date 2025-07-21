a9de6cb296e5742a6bcdf6af2904dade04abacaa
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-21 16:02:46 -0600

    Update picture.

--- news/2025/07/21/17/00/book-info-20250721-1700.php
+++ news/2025/07/21/17/00/book-info-20250721-1700.php
@@ -30,6 +30,8 @@
 		   <div><img src=\"/news/2025/07/21/17/00/img/selectDestinationBankFoundBySwiftCode.jpg\" alt=\"Select destination bank found bt SWIFT Code.\" width=\"1716\" height=\"1012\"></div>
 		   <p>From my Interactive Brokers account, I found important information about the Virtual Account Number (VAN):</p>
 		   <div><img src=\"/news/2025/07/21/17/00/img/accountNumberForDestination.jpg\" alt=\"Account number for destination.\" width=\"1898\" height=\"1007\"></div>
+		   <p>I provided the bank information for the template creation and clicked <span class=\"cod\">Continue</span>:</p>
+		   <div><img src=\"/news/2025/07/21/17/00/img/bankInformationForTemplatePart1Of3.jpg\" alt=\".\" width=\"1898\" height=\"1007\"></div>
 		   <div>Published: 5:00 PM GMT · Jul 21, 2025</div>\n";
   //$index->content .= 
            "<p>Another milestone completed in the process to enter stock market investments. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. The beneficiary bank is:</p><p>Citibank N.A.<br />388 Greenwich Street<br />New York 10013<br />United States</p><p>The beneficiary of the funds is:</p><p>Interactive Brokers LLC<br />One Pickwick Plaza<br />Greenwich, Connecticut 06830<br />United States</p><div><img src=\"/news/2024/11/11/22/13/img/internationalTransferToInteractiveBrokersLLCSent.jpg\" alt=\"International transfer to Interactive Brokers\" width=\"568\" height=\"745\"></div><div>Published: 10:13 PM GMT · Nov 11, 2024</div>\n";
