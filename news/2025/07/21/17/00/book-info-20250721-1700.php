7286345b0de9ef04cec7df90d4906c49ddc5d324
Jaime Montoya <jmontoya@jaimemontoya.com>
2025-07-21 15:21:05 -0600

    Create new template.

--- news/2025/07/21/17/00/book-info-20250721-1700.php
+++ news/2025/07/21/17/00/book-info-20250721-1700.php
@@ -19,6 +19,9 @@
 		   <p>I selected my source account and clicked <span class=\"cod\">Create Template</span>:</p>
 		   <div><img src=\"/news/2025/07/21/17/00/img/createTemplatePart1Of2.jpg\" alt=\"Create template. Part 1 of 2.\" width=\"1898\" height=\"1018\"></div>
 		   <div><img src=\"/news/2025/07/21/17/00/img/createTemplatePart2Of2.jpg\" alt=\"Create template. Part 2 of 2.\" width=\"1905\" height=\"1022\"></div>
+		   <p>I provided a new template name and clicked <span class=\"cod\">Continue</span>:
+		   <div><img src=\"/news/2025/07/21/17/00/img/createNewTemplatePart1Of2.jpg\" alt=\"Create new template. Part 1 of 2.\" width=\"1912\" height=\"1016\"></div>
+		   <div><img src=\"/news/2025/07/21/17/00/img/createNewTemplatePart2Of2.jpg\" alt=\"Create new template. Part 2 of 2.\" width=\"1915\" height=\"1010\"></div>
 		   <div>Published: 5:00 PM GMT · Jul 21, 2025</div>\n";
   //$index->content .= 
            "<p>Another milestone completed in the process to enter stock market investments. My local bank approved my request to preregister an international transfer to Interactive Brokers LLC. The beneficiary bank is:</p><p>Citibank N.A.<br />388 Greenwich Street<br />New York 10013<br />United States</p><p>The beneficiary of the funds is:</p><p>Interactive Brokers LLC<br />One Pickwick Plaza<br />Greenwich, Connecticut 06830<br />United States</p><div><img src=\"/news/2024/11/11/22/13/img/internationalTransferToInteractiveBrokersLLCSent.jpg\" alt=\"International transfer to Interactive Brokers\" width=\"568\" height=\"745\"></div><div>Published: 10:13 PM GMT · Nov 11, 2024</div>\n";
