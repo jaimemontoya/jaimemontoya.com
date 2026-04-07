95247b5211431e1aac696cb15219e54317ee47d2
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-04-07 09:21:33 -0600

    Replace 'capital' with 'net worth'.

--- home.php
+++ home.php
@@ -864,7 +864,7 @@ class HomePage extends Page
 	echo "\t\t\t\t\t[new Date(2026, 3 , 6), 30957.42],\n";
     echo "\t\t\t\t]);\n";
     echo "\t\t\t\tvar options = {\n";
-    echo "\t\t\t\t\t'title':'Jaime Montoya\'s capital from 5 May 2024 to 6 April 2026',\n";
+    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 6 April 2026',\n";
     echo "\t\t\t\t\tbackgroundColor: {\n";
     echo "\t\t\t\t\t\tfill: '#000',\n";
     echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#0f0'\n";
