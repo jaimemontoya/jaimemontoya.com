7a3e95107ba566e4dfaab8290c3e5b124730561c
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-04-10 10:11:48 -0600

    Use USD to specify currency.

--- market/2025/02/20/04/11/book-info-20250220-0411.php
+++ market/2025/02/20/04/11/book-info-20250220-0411.php
@@ -1,5 +1,5 @@
 <?php
   $market->content .= 
-           "<p>The chart above illustrates the hypothetical performance of a $1,000 investment in Apple, S&P 500 (SPX), Global X Dow 30 Covered Call ETF (DJIA), and NASDAQ Composite Index (COMP).</p>
+           "<p>The chart above illustrates the hypothetical performance of a 1,000.00 USD investment in Apple, S&P 500 (SPX), Global X Dow 30 Covered Call ETF (DJIA), and NASDAQ Composite Index (COMP).</p>
 			<div>Published: 4:11 AM GMT · Feb 20, 2024</div>\n";
 ?>
