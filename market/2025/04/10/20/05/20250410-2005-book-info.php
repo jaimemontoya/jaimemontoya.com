1c6054f8e61ee0677368bc139682fae7efd8eb87
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-04-10 12:10:37 -0600

    Update beat the market values for Microsoft to 9 April 2026.

--- market/2025/04/10/20/05/20250410-2005-book-info.php
+++ market/2025/04/10/20/05/20250410-2005-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <h2>Performance comparison: Microsoft Vs. S&P 500 (SPX) Vs. Global X Dow 30 Covered Call ETF (DJIA) Vs. NASDAQ Composite Index (COMP)</h2>
-					<p>The chart illustrates the hypothetical performance of a $1,000 investment in Microsoft, S&P 500 (SPX), Global X Dow 30 Covered Call ETF (DJIA), and NASDAQ Composite Index (COMP). <mark>Published: 8:05 PM GMT · Apr 10, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+					<p>The chart illustrates the hypothetical performance of a 1,000.00 USD investment in Microsoft, S&P 500 (SPX), Global X Dow 30 Covered Call ETF (DJIA), and NASDAQ Composite Index (COMP). <mark>Published: 8:05 PM GMT · Apr 10, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
--- market/2025/04/10/20/05/book-info-20250410-2005.php
+++ market/2025/04/10/20/05/book-info-20250410-2005.php
@@ -1,5 +1,5 @@
 <?php
   $market->content .= 
-           "<p>The chart above illustrates the hypothetical performance of a $1,000 investment in Microsoft, S&P 500 (SPX), Global X Dow 30 Covered Call ETF (DJIA), and NASDAQ Composite Index (COMP).</p>
+           "<p>The chart above illustrates the hypothetical performance of a 1,000.00 USD investment in Microsoft, S&P 500 (SPX), Global X Dow 30 Covered Call ETF (DJIA), and NASDAQ Composite Index (COMP).</p>
 			<div>Published: 8:05 AM GMT · Apr 10, 2024</div>\n";
 ?>
