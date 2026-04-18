ea55ab69ec4ed4d74fdd995931b2da12ccd19837
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-04-18 05:47:43 -0600

    Add slogan and update net worth.

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
   require("home.php");
   $index = new HomePage();
-  $index->content .= "<h1 class=\"center\"><a href=\"https://columbia.edu\">columbia.edu</a> · <a href=\"http://bluechipfinances.com\">bluechipfinances.com</a> · <a href=\"http://bluechipfinances.org\">bluechipfinances.org</a></h1>
+  $index->content .= "<h1 class=\"center\"><a href=\"http://bluechipfinances.com\">bluechipfinances.com</a> · <a href=\"http://bluechipfinances.org\">bluechipfinances.org</a> · Freedom from financial debt, and financial greatness through disciplined long term blue chip stock investing</h1>
+  <p>Jaime Montoya's real-time net worth: <span class=\"cod\">34,921.77 USD</span></p>
   <hr>
   <h2>Featured financial charts</h2>
-  <p>Jaime Montoya's real-time net worth: <span class=\"cod\">33,485.47 USD</span></p>
   <div id=\"jaime-montoya-capital\"></div>
   <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vSnedM83IJlnIUy0Ttj4DrDrGWCrqhjC_HXUwxa8cv3nmhraprEToUSuy776FLfXewO49AdS_9SQdUH/pubhtml?widget=true&amp;headers=false\" id=\"investment-spreadsheet\"></iframe>
   <div id=\"richest-company\"></div>
