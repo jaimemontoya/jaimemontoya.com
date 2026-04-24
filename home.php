71a5ace9b66600ed64e4527d73d19b313ac3bad4
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-04-24 09:17:49 -0600

    Update date for richest people.

--- home.php
+++ home.php
@@ -116,7 +116,7 @@ class HomePage extends Page
     echo "\t\t\t\t]);\n";
 	echo "\t\t\t\t// Set chart options\n";
 	echo "\t\t\t\tvar options = {\n";
-	echo "\t\t\t\t\t'title':'Richest people, last updated 17 April 2026',\n";
+	echo "\t\t\t\t\t'title':'Richest people, last updated 23 April 2026',\n";
 	echo "\t\t\t\t\tlegend: 'none',\n";
     echo "\t\t\t\t\thAxis: {\n";
 	echo "\t\t\t\t\t\tgridlines: {\n";
