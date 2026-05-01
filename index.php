fa7c1699a0cba1434ed8ef28cdc9a4af8e6d8a9a
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-01 16:06:23 -0600

    Add date for when I would invest it all in Nvidia.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <hr>
   <p>My investment philosophy is inspired by the Matthew effect, sometimes called the Matthew principle or cumulative advantage. For that reason, whenever I buy stocks I only choose the world's most valuable company by market capitalization. I follow a disciplined long term blue chip stock investing approach. I passionately advocate for 1) Financial greatness 2) Freedom from financial debt. This is the Matthew 25:29 Bible verse in the King James Verse:</p>
   <div class=\"center\"><img src=\"img/kingJamesBibleMatthewCh25V29.jpg\" alt=\"Matthew 25:29 in the King James Version\" width=\"318\" height=\"70\"></div>
-  <p>Following my investment philosophy, if I were to buy stocks today 29 April 2026, Nvidia Corporation (NVDA) is the company where I would invest it all:</p>
+  <p>Following my investment philosophy, if I were to buy stocks today 1 May 2026, Nvidia Corporation (NVDA) is the company where I would invest it all:</p>
   <div class=\"center\"><img src=\"img/NVIDIA_logo.jpg\" alt=\"Nvidia Corporation logo\" width=\"206\" height=\"140\"></div>
   <h2>Featured financial charts</h2>
   <div id=\"jaime-montoya-capital\"></div>
