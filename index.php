f1ceaf6b47de6adf16dae93c48ec73b087c6d49f
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-01 16:03:22 -0600

    Update my net worth value.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require("home.php");
   $index = new HomePage();
-  $index->content .= "<h1 class=\"center\">Jaime Montoya's net worth as of 29 Apr 2026: <span class=\"cod\">35,919.15 USD</span></h1>
+  $index->content .= "<h1 class=\"center\">Jaime Montoya's net worth as of 29 Apr 2026: <span class=\"cod\">34,670.86 USD</span></h1>
   <hr>
   <p>My investment philosophy is inspired by the Matthew effect, sometimes called the Matthew principle or cumulative advantage. For that reason, whenever I buy stocks I only choose the world's most valuable company by market capitalization. I follow a disciplined long term blue chip stock investing approach. I passionately advocate for 1) Financial greatness 2) Freedom from financial debt. This is the Matthew 25:29 Bible verse in the King James Verse:</p>
   <div class=\"center\"><img src=\"img/kingJamesBibleMatthewCh25V29.jpg\" alt=\"Matthew 25:29 in the King James Version\" width=\"318\" height=\"70\"></div>
