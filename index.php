1f923b00f273518fbf5f6fbb8d4bd548955ae7f8
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-01 16:11:23 -0600

    Remove the Mission, Vision, Values, Goals and Objectives section.

--- index.php
+++ index.php
@@ -15,40 +15,6 @@
   <hr>
   <h2>Table of contents</h2><ol><li><a href=\"about/\">About</a>: My one-page resume.<li><a href=\"blog/\">Blog</a>: Software technical documentation blog for myself and the world.<li><a href=\"book/\">Book reviews</a>: My reviews for some of the books I have read from cover to cover.<li><a href=\"capital/\">Capital</a>: Capital history in my records since May 5, 2024.<li><a href=\"coursera/\">Coursera</a>: Coursera courses I have successfully completed.<li><a href=\"finances/\">Finances</a>: Personal finance software I have created.<li><a href=\"investment\">Investment</a>: Mutual fund and stock investments I have made since April 12, 2024.<li><a href=\"market/\">Market</a>: Monitoring tool to beat the market.<li><a href=\"news/\">News</a>: Personal news since November 7, 2024, 00:00 AM GMT.<li><a href=\"opinion/\">Opinion</a>: Stock Market, Business & Finance Opinion since 2024-Dec-30 9:47 PM GMT.<li><a href=\"portfolio/\">Portfolio</a>: Websites I have created, designed, developed, upgraded, maintained and/or hosted.<li><a href=\"reading/\">Reading</a>: Listing all of the books I have read from cover to cover since August 31, 2018.<li><a href=\"run/\">Running</a>: Running history I have endured since August 2, 2020.<li><a href=\"teaching/\">Teaching</a>: In-person classes I have taught.<li><a href=\"weight/\">Weight</a>: Weight history from my body since October 19, 2019.</ol></p>
   <hr>
-  <h2>Values</h2>
-  <ul>
-    <li>Discipline
-	<li>Laser focus
-	<li>Patience
-  </ul>
-  <h2>Goals & objectives</h2>
-  <table>
-    <tr>
-      <th>Goals & objectives</th>
-      <th>Created</th>
-      <th>Accomplished</th>
-    </tr>
-	<tr>
-	  <td>1.2. I will record in my .txt file of personal expenses, the pending transactions only recorded in my Notes on iCloud.com. My Notes document will be empty and all information moved to my Dropbox storage. That will contribute to my software to have a fully populated database without missing values. The .txt file will be up-to-date with the values from my Notes on iCloud.com by 1 August 2025.</td>
-	  <td>Sat Jun 28 09:12:35 2025 -0600</td>
-	  <td>In progress</td>
-    </tr>
-    <tr>
-	  <td>1.1. I will record in my .txt file of personal expenses, the pending cash transactions that only exist in my memory. That will contribute to my software to have a fully populated database without missing values. The .txt file will be up-to-date with the values from my memory by 1 July 2025.</td>
-	  <td>Tue Jun 24 07:00:07 2025 -0600</td>
-	  <td>Sat Jun 28 08:04:00 2025 -0600</td>
-    </tr>
-    <tr>
-      <td>1. I will update my personal finance software database with all the missing values that I need to insert since its first entry to present. That will be foundational input data to build my software. The database will be fully populated and up-to-date by 1 January 2026.</td>
-      <td>Thu Jun 12 13:35:16 2025 -0600</td>
-      <td>In progress</td>
-    </tr>
-  </table>
-  <h2>Mission</h2>
-  <p>To create Blue Chip Finances, an open-source personal financial management (PFM) software that helps people to be free from financial debt and achieve greatness in their personal finances.</p>
-  <h2>Vision</h2>
-  <p>To pursue a future where individuals are empowered to be free from financial debt and achieve financial greatness through disciplined long term blue chip stock investing.</p>
-  <hr>
   <p>The code to build this website has been written by myself, from scratch, and it is entirely available on GitHub at <a href=\"https://github.com/jaimemontoya/jaimemontoya.com\">https://github.com/jaimemontoya/jaimemontoya.com</a>.</p>
   <hr>
   ";
