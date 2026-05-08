2199c04992654bb21c681e4d15616bba2b244229
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-08 16:01:43 -0600

    Specify when the reading of Robert's Rules For Dummies. 4th Edition, was finished.

--- reading/roberts-rules-for-dummies-4rd-ed/roberts-rules-for-dummies-4rd-ed-book-info.php
+++ reading/roberts-rules-for-dummies-4rd-ed/roberts-rules-for-dummies-4rd-ed-book-info.php
@@ -6,7 +6,7 @@
 				</div>
                 <div class=\"book-title-authors-read-by\">
 		            <span>Robert's Rules For Dummies. 4th Edition.</span>
-					<p>By C. Alan Jennings. <mark>Book [in progress...] read by Jaime Montoya</mark> from 11:52 AM May 22, 2025 to [in progress...] GMT.</p>
+					<p>By C. Alan Jennings. <mark>Book 81 read by Jaime Montoya</mark> from 11:52 AM May 22, 2025 to 12:18 PM March 26, 2026 GMT.</p>
                 </div>
             </div>\n";
 ?>
