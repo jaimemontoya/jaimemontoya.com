f9d7efb82a0a785232cfc88f4b43fa19facc8486
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-08 14:08:00 -0600

    Invoke 2026 file.

--- run/2026/chart.php
+++ run/2026/chart.php
@@ -1,4 +1,4 @@
 <?php
   $running->content .= 
-           "\t\t\t<h1>2025 Jaime Montoya's running history by month</h1><div><div id=\"running-2025\"></div></div>\n";
+           "\t\t\t<h1>2025 Jaime Montoya's running history by month</h1><div><div id=\"running-2026\"></div></div>\n";
 ?>
