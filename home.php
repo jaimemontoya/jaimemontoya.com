86b99b4b861b9702fd894ca1d749718408e552a8
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-08 10:24:16 -0600

    Update date.

--- home.php
+++ home.php
@@ -895,7 +895,7 @@ class HomePage extends Page
 	echo "\t\t\t\t\t[new Date(2026, 4 , 7), 38180.38]\n";
     echo "\t\t\t\t]);\n";
     echo "\t\t\t\tvar options = {\n";
-    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 6 May 2026',\n";
+    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 7 May 2026',\n";
     echo "\t\t\t\t\tbackgroundColor: {\n";
     echo "\t\t\t\t\t\tfill: '#000',\n";
     echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#0f0'\n";
