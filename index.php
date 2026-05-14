ee2aa934a6f134ac9915b55715fd2be4d0290082
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-14 04:21:27 -0600

    Add R&D link with course https://www.coursera.org/learn/microsoft-azure-cosmos-db.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   require("home.php");
   $index = new HomePage();
   $index->content .= "<h1 class=\"center\">Jaime Montoya's net worth as of 13 May 2026: <span class=\"cod\">39,530.13 USD</span> <a href=\"about/\">About Jaime Montoya</a></h1>
+  <h2>R&D: <a href=\"https://www.coursera.org/learn/microsoft-azure-cosmos-db\">Microsoft Azure Cosmos DB</a></h2>
   <hr>
   <p>My investment philosophy is inspired by the Matthew effect, sometimes called the Matthew principle or cumulative advantage. For that reason, whenever I buy stocks I only choose the world's most valuable company by market capitalization. I follow a disciplined long term blue chip stock investing approach. I passionately advocate for 1) Financial greatness 2) Freedom from financial debt. This is the Matthew 25:29 Bible verse in the King James Verse:</p>
   <div class=\"center\"><img src=\"img/kingJamesBibleMatthewCh25V29.jpg\" alt=\"Matthew 25:29 in the King James Version\" width=\"318\" height=\"70\"></div>
