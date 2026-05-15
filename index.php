56c3346225527b6d6fcb0dcbfd29efff38162530
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-15 00:24:10 -0600

    Update net worth value to 14 May 2026.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
   require("home.php");
   $index = new HomePage();
-  $index->content .= "<h1 class=\"center\">Jaime Montoya's net worth as of 13 May 2026: <span class=\"cod\">39,530.13 USD</span> <a href=\"about/\">About Jaime Montoya</a></h1>
+  $index->content .= "<h1 class=\"center\">Jaime Montoya's net worth as of 14 May 2026: <span class=\"cod\">40,628.42 USD</span> <a href=\"about/\">About Jaime Montoya</a></h1>
   <h2 class=\"center\">R&D: <a href=\"https://www.coursera.org/learn/microsoft-azure-cosmos-db\">Microsoft Azure Cosmos DB</a></h2>
   <hr>
   <p>My investment philosophy is inspired by the Matthew effect, sometimes called the Matthew principle or cumulative advantage. For that reason, whenever I buy stocks I only choose the world's most valuable company by market capitalization. I follow a disciplined long term blue chip stock investing approach. I passionately advocate for 1) Financial greatness 2) Freedom from financial debt. This is the Matthew 25:29 Bible verse in the King James Verse:</p>
   <div class=\"center\"><img src=\"img/kingJamesBibleMatthewCh25V29.jpg\" alt=\"Matthew 25:29 in the King James Version\" width=\"318\" height=\"70\"></div>
-  <p>Following my investment philosophy, if I were to buy stocks today 13 May 2026, Nvidia Corporation (NVDA) is the company where I would invest it all:</p>
+  <p>Following my investment philosophy, if I were to buy stocks today 14 May 2026, Nvidia Corporation (NVDA) is the company where I would invest it all:</p>
   <div class=\"center\"><img src=\"img/NVIDIA_logo.jpg\" alt=\"Nvidia Corporation logo\" width=\"206\" height=\"140\"></div>
   <h2>Featured financial charts</h2>
   <div id=\"jaime-montoya-capital\"></div>
