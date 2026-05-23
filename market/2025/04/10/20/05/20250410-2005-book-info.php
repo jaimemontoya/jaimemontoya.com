4af1e255dd453b34a4ee57d293e14bd55dc2efa3
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-05-23 06:48:58 -0600

    Update names of market indexes.

--- market/2025/04/10/20/05/20250410-2005-book-info.php
+++ market/2025/04/10/20/05/20250410-2005-book-info.php
@@ -5,8 +5,8 @@
 					<div class=\"book-cover\" id=\"market-20250410-2005-img\"></div>
 				</div>
                 <div class=\"book-title-authors-read-by\">
-		            <h2>Performance comparison: Microsoft Vs. S&P 500 (SPX) Vs. Global X Dow 30 Covered Call ETF (DJIA) Vs. NASDAQ Composite Index (COMP)</h2>
-					<p>The chart illustrates the hypothetical performance of a 1,000.00 USD investment in Microsoft, S&P 500 (SPX), Global X Dow 30 Covered Call ETF (DJIA), and NASDAQ Composite Index (COMP). <mark>Published: 8:05 PM GMT · Apr 10, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
+		            <h2>Performance comparison: Microsoft Vs. S&P 500 Index (SPX) Vs. Dow Jones Industrial Average (DJIA) Vs. NASDAQ Composite Index (COMP)</h2>
+					<p>The chart illustrates the hypothetical performance of a 1,000.00 USD investment in Microsoft, S&P 500 Index (SPX), Dow Jones Industrial Average (DJIA), and NASDAQ Composite Index (COMP). <mark>Published: 8:05 PM GMT · Apr 10, 2025</mark> <span class=\"underline white\">Read more</span> <i class=\"fa fa-link white\" aria-hidden=\"true\"></i></p>
                 </div>
             </div></a>\n";
 ?>
