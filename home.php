5ef42b3f80978a8c1555e9178208a5bcfb8584f2
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-06-24 20:41:49 -0600

    Update market capitalization.

--- home.php
+++ home.php
@@ -939,10 +939,11 @@ class HomePage extends Page
 	echo "\t\t\t\t\t[new Date(2026, 5 , 20), 38046.51],\n";
 	echo "\t\t\t\t\t[new Date(2026, 5 , 21), 38136.74],\n";
 	echo "\t\t\t\t\t[new Date(2026, 5 , 22), 37287.66],\n";
-	echo "\t\t\t\t\t[new Date(2026, 5 , 23), 37073.3]\n";
+	echo "\t\t\t\t\t[new Date(2026, 5 , 23), 37007.09],\n";
+	echo "\t\t\t\t\t[new Date(2026, 5 , 24), 20119.38]\n";
     echo "\t\t\t\t]);\n";
     echo "\t\t\t\tvar options = {\n";
-    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 23 June 2026',\n";
+    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 24 June 2026',\n";
     echo "\t\t\t\t\tbackgroundColor: {\n";
     echo "\t\t\t\t\t\tfill: '#000',\n";
     echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#0f0'\n";
