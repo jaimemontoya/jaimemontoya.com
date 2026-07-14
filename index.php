5a434371a75fb3d3aa3afd6bfa8a9d19b3720c1a
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-07-14 16:47:18 -0600

    Fix grammar.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   require("home.php");
   $index = new HomePage();
   $index->content .= "<h1 class=\"center\">Jaime Montoya's current net worth: <span class=\"cod\">21,205.18 USD</span> as of 14 July 2026.<br>
-  Jaime Montoya's all-time high net worth peak date and value: <span class=\"cod\">40,628.42 USD</span> on 14 May 2026.
+  Jaime Montoya's all-time high net worth: <span class=\"cod\">40,628.42 USD</span> on 14 May 2026.
   </h1>
   <hr>
   <p>My investment philosophy is inspired by the Matthew effect, sometimes called the Matthew principle or cumulative advantage. For that reason, whenever I buy stocks I only choose the world's most valuable company by market capitalization. I follow a disciplined long term blue chip stock investing approach. I passionately advocate for 1) Financial greatness 2) Freedom from financial debt. This is the Matthew 25:29 Bible verse in the King James Verse:</p>
