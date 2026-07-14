04cb361b57e37e6beff761e6865583f10c7addde
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-07-14 16:51:09 -0600

    Add link to resume in header.

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
   require("home.php");
   $index = new HomePage();
-  $index->content .= "<h1 class=\"center\">Jaime Montoya's net worth: 1) Current: <span class=\"cod\">21,205.18 USD</span> as of 14 July 2026. 2) All-time high: <span class=\"cod\">40,628.42 USD</span> on 14 May 2026.
+  $index->content .= "<h1 class=\"center\">Jaime Montoya's net worth: 1) Current: <span class=\"cod\">21,205.18 USD</span> as of 14 July 2026. 2) All-time high: <span class=\"cod\">40,628.42 USD</span> on 14 May 2026.<br>
+  Jaime Montoya's one-page resume: <a href=\"/about\">About</a>
   </h1>
   <hr>
   <p>My investment philosophy is inspired by the Matthew effect, sometimes called the Matthew principle or cumulative advantage. For that reason, whenever I buy stocks I only choose the world's most valuable company by market capitalization. I follow a disciplined long term blue chip stock investing approach. I passionately advocate for 1) Financial greatness 2) Freedom from financial debt. This is the Matthew 25:29 Bible verse in the King James Verse:</p>
