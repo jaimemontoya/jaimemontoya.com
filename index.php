023f9bd85aaaa652eb0fac60c93a76765ae92c59
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-07-14 16:44:24 -0600

    JAIME OSWALDO MONTOYA GUZMAN

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
   require("home.php");
   $index = new HomePage();
-  $index->content .= "<h1 class=\"center\">Jaime Montoya's net worth as of 13 July 2026: <span class=\"cod\">20,421.88 USD</span></h1>
+  $index->content .= "<h1 class=\"center\">Jaime Montoya's current net worth: <span class=\"cod\">21,205.18 USD</span> as of 14 July 2026.<br>
+  Jaime Montoya's all-time high net worth peak date and value: <span class=\"cod\">40,628.42 USD</span> on 14 May 2026.
+  </h1>
   <hr>
   <p>My investment philosophy is inspired by the Matthew effect, sometimes called the Matthew principle or cumulative advantage. For that reason, whenever I buy stocks I only choose the world's most valuable company by market capitalization. I follow a disciplined long term blue chip stock investing approach. I passionately advocate for 1) Financial greatness 2) Freedom from financial debt. This is the Matthew 25:29 Bible verse in the King James Verse:</p>
   <div class=\"center\"><img src=\"img/kingJamesBibleMatthewCh25V29.jpg\" alt=\"Matthew 25:29 in the King James Version\" width=\"318\" height=\"70\"></div>
@@ -12,11 +14,6 @@
   <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vSnedM83IJlnIUy0Ttj4DrDrGWCrqhjC_HXUwxa8cv3nmhraprEToUSuy776FLfXewO49AdS_9SQdUH/pubhtml?widget=true&amp;headers=false\" id=\"investment-spreadsheet\"></iframe>
   <div id=\"richest-company\"></div>
   <div id=\"richest-people\"></div>
-  <!--<hr>
-  <h2>Table of contents</h2><ol><li><a href=\"about/\">About</a>: My one-page resume.<li><a href=\"blog/\">Blog</a>: Software technical documentation blog for myself and the world.<li><a href=\"book/\">Book reviews</a>: My reviews for some of the books I have read from cover to cover.<li><a href=\"capital/\">Capital</a>: Capital history in my records since May 5, 2024.<li><a href=\"coursera/\">Coursera</a>: Coursera courses I have successfully completed.<li><a href=\"finances/\">Finances</a>: Personal finance software I have created.<li><a href=\"investment\">Investment</a>: Mutual fund and stock investments I have made since April 12, 2024.<li><a href=\"market/\">Market</a>: Monitoring tool to beat the market.<li><a href=\"news/\">News</a>: Personal news since November 7, 2024, 00:00 AM GMT.<li><a href=\"opinion/\">Opinion</a>: Stock Market, Business & Finance Opinion since 2024-Dec-30 9:47 PM GMT.<li><a href=\"portfolio/\">Portfolio</a>: Websites I have created, designed, developed, upgraded, maintained and/or hosted.<li><a href=\"reading/\">Reading</a>: Listing all of the books I have read from cover to cover since August 31, 2018.<li><a href=\"run/\">Running</a>: Running history I have endured since August 2, 2020.<li><a href=\"teaching/\">Teaching</a>: In-person classes I have taught.<li><a href=\"weight/\">Weight</a>: Weight history from my body since October 19, 2019.</ol></p>
-  <hr>
-  <p>The code to build this website has been written by myself, from scratch, and it is entirely available on GitHub at <a href=\"https://github.com/jaimemontoya/jaimemontoya.com\">https://github.com/jaimemontoya/jaimemontoya.com</a>.</p>
-  <hr>-->
   ";
   $index->Display();
 ?>
