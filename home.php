7696d319868ddc4cc7d58773bd82037e0515171a
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-17 16:06:56 -0600

    Change the date of net worth to 17 September 2026.

--- home.php
+++ home.php
@@ -1015,7 +1015,7 @@ class HomePage extends Page
 	echo "\t\t\t\t\t[new Date(2026, 8 , 04), 57772.4]\n";
     echo "\t\t\t\t]);\n";
     echo "\t\t\t\tvar options = {\n";
-    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 4 September 2026',\n";
+    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 17 September 2026',\n";
     echo "\t\t\t\t\tbackgroundColor: {\n";
     echo "\t\t\t\t\t\tfill: '#000',\n";
     echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#0f0'\n";
