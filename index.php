ba22ce846a0d398bd1f732cb2f154948dd841a8c
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-22 18:30:54 -0600

    Add period.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   $index = new HomePage();
   $index->content .= "<p>Author: Jaime Montoya.<br>
   Date: Wednesday, 23 September 2026 at 00:00 GMT.<br>
-  Title: The Winner-Take-All Matthew Effect Portfolio</p>
+  Title: The Winner-Take-All Matthew Effect Portfolio.</p>
   <p>My investment approach is anchored in supreme market dominance, directly inspired by the <span=\"bold\">Matthew effect</span>: the economic reality that the rich get richer.</p>
   <ul>
   <li><span=\"bold\">The Strategy:</span> Every quarter, I allocate fresh capital exclusively to the single most valuable company on the planet by market cap. I back the reigning global heavyweight.
