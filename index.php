7376ec96873901d68a292b80afcb2a02f4cb5f98
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-25 01:36:17 -0600

    Reorder elements with the Google sheet below the net worth line graph.

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
   require("home.php");
   $index = new HomePage();
   $index->content .= "<div id=\"jaime-montoya-capital\"></div>
+  <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vSnedM83IJlnIUy0Ttj4DrDrGWCrqhjC_HXUwxa8cv3nmhraprEToUSuy776FLfXewO49AdS_9SQdUH/pubhtml?widget=true&amp;headers=false\" style=\"width: 100%; height: 750px; border: none;\"></iframe>
   <div id=\"richest-company\"></div>
   <div id=\"richest-people\"></div>
-  <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vSnedM83IJlnIUy0Ttj4DrDrGWCrqhjC_HXUwxa8cv3nmhraprEToUSuy776FLfXewO49AdS_9SQdUH/pubhtml?widget=true&amp;headers=false\" style=\"width: 100%; height: 750px; border: none;\"></iframe>
   ";
   $index->Display();
 ?>
