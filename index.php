6bdf335a656277f578400dcdbf1e7aa795b5f9f2
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-25 01:31:45 -0600

    Improve size of Google sheet.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   $index->content .= "<div id=\"jaime-montoya-capital\"></div>
   <div id=\"richest-company\"></div>
   <div id=\"richest-people\"></div>
-  <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vSnedM83IJlnIUy0Ttj4DrDrGWCrqhjC_HXUwxa8cv3nmhraprEToUSuy776FLfXewO49AdS_9SQdUH/pubhtml?widget=true&amp;headers=false\"></iframe>
+  <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vSnedM83IJlnIUy0Ttj4DrDrGWCrqhjC_HXUwxa8cv3nmhraprEToUSuy776FLfXewO49AdS_9SQdUH/pubhtml?widget=true&amp;headers=false\" style=\"width: 100%; height: 750px; border: none;\"></iframe>
   ";
   $index->Display();
 ?>
