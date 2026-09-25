af0db431f11aecf023529681fe20724480c25739
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-25 00:30:02 -0600

    Remove investment strategy text.

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
   require("home.php");
   $index = new HomePage();
-  $index->content .= "<p>Author: Jaime Montoya.<br>
-  Date: Wednesday, 23 September 2026 at 00:00 GMT.<br>
-  Title: The Winner-Take-All Matthew Effect Portfolio.</p>
-  <p>My investment approach is anchored in supreme market dominance, directly inspired by the <span=\"bold\">Matthew effect</span>: the economic reality that the rich get richer.</p>
-  <ul>
-  <li><span=\"bold\">The Strategy:</span> Every quarter, I allocate fresh capital exclusively to the single most valuable company on the planet by market cap. I back the reigning global heavyweight.
-  <li><span=\"bold\">The Discipline:</span> Radical patience. I ignore short-term volatility and never panic-sell.
-  <li><span=\"bold\">The Purpose:</span> I only ever exit a position to fund profound, life-altering milestones such as buying an apartment, earning a degree, or launching a new business.
-  </ul>
-  <p>By matching the compound interest of corporate giants with institutional-grade patience, I invest at the absolute peak of global excellence.</p>
-  <img src=\"img/theWinnerTakeAllMatthewEffectPortfolio.jpg\" alt=\"The Winner-Take-All Matthew Effect Portfolio\" width=\"601\" height=\"491\" style=\"max-width: 100%; height: auto; display: block;\">
-  <br>
-  <div id=\"jaime-montoya-capital\"></div>
+  $index->content .= "<div id=\"jaime-montoya-capital\"></div>
   <div id=\"richest-company\"></div>
   <div id=\"richest-people\"></div>
   ";
