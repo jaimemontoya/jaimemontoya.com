a8538429cbd309be5f830b50c9091b38032009bf
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-26 08:59:21 -0600

    Have only one break line.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   $index->content .= "<div id=\"richest-company\"></div>
   <div id=\"richest-people\"></div>
   <div id=\"jaime-montoya-capital\"></div>
-  <br><br>
+  <br>
   <h2 style=\"cursor: default; user-select: none; -webkit-font-smoothing: antialiased; font-family: Arial; font-size: 16px; font-weight: bold;\">Jaime Montoya's investments from 12 April 2024 to 25 September 2026.</h2>
   <ul>
     <li><a href=\"beat-the-market/Capital.html\" target=\"_blank\" rel=\"noopener noreferrer\">Capital including chronological net worth resulting from investments and amounts in (a) Mutual funds (b) Bank accounts (c) Stock market. Data available from 5 May 2024.</a>
