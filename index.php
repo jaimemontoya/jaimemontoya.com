afee964bd80fe5d71e9d5b17de3a3f6ca45cad15
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-26 06:52:30 -0600

    Add link to Capital.

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <div id=\"richest-company\"></div>
   <div id=\"richest-people\"></div>
   <h2 style=\"cursor: default; user-select: none; -webkit-font-smoothing: antialiased; font-family: Arial; font-size: 16px; font-weight: bold;\">Jaime Montoya's investments from 5 May 2024 to 25 September 2026</h2>
+  <ul>
+    <li><a href=\"beat-the-market/Capital.html\" target=\"_blank\" rel=\"noopener noreferrer\">Capital including chronological net worth resulting from investments and amounts in (a) Mutual funds (b) Bank accounts (c) Stock market.
+  </ul>
   ";
   $index->Display();
 ?>
