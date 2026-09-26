8d4621aa00d941fc52dc0d47e786f1404a46c2e0
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-26 07:13:55 -0600

    Add Pfizer stock link.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <li><a href=\"beat-the-market/MutualFundsPlusStocks.html\" target=\"_blank\" rel=\"noopener noreferrer\">Mutual funds plus stocks investments total profits from 12 April 2024 to 25 September 2026.
     <li><a href=\"beat-the-market/NVDA.html\" target=\"_blank\" rel=\"noopener noreferrer\">Nvidia Corporation (NVDA) investments from 12 Nov 2024 to 25 September 2026.
     <li><a href=\"beat-the-market/AMZN.html\" target=\"_blank\" rel=\"noopener noreferrer\">Amazon.com, Inc. (AMZN) investments from 18 November 2024 to 19 November 2024.
+    <li><a href=\"beat-the-market/PFE.html\" target=\"_blank\" rel=\"noopener noreferrer\">Pfizer Inc. (PFE) investments from 7 January 2025 to 1 July 2025.
   </ul>
   ";
   $index->Display();
