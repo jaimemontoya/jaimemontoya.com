53cb19ec57afde490ce8a1378dc1795ef7837e6f
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-26 09:00:54 -0600

    Add periods at the end of sentences.

--- home.php
+++ home.php
@@ -60,7 +60,7 @@ class HomePage extends Page
     echo "\t\t\t\t]);\n";
 	echo "\t\t\t\t// Set chart options\n";
 	echo "\t\t\t\tvar options = {\n";
-    echo "\t\t\t\t\t'title':'Richest companies in the world by market capitalization, last updated 25 September 2026',\n";
+    echo "\t\t\t\t\t'title':'Richest companies in the world by market capitalization, last updated 25 September 2026.',\n";
 	echo "\t\t\t\t\tlegend: 'none',\n";
     echo "\t\t\t\t\tvAxis: {\n";
     echo "\t\t\t\t\t\ttitle: 'Trillions (USD)',\n";
@@ -116,7 +116,7 @@ class HomePage extends Page
     echo "\t\t\t\t]);\n";
 	echo "\t\t\t\t// Set chart options\n";
 	echo "\t\t\t\tvar options = {\n";
-	echo "\t\t\t\t\t'title':'Richest people, last updated 25 September 2026',\n";
+	echo "\t\t\t\t\t'title':'Richest people, last updated 25 September 2026.',\n";
 	echo "\t\t\t\t\tlegend: 'none',\n";
     echo "\t\t\t\t\thAxis: {\n";
 	echo "\t\t\t\t\t\tgridlines: {\n";
@@ -1036,7 +1036,7 @@ class HomePage extends Page
 	echo "\t\t\t\t\t[new Date(2026, 8 , 25), 53338.08]\n";
     echo "\t\t\t\t]);\n";
     echo "\t\t\t\tvar options = {\n";
-    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 25 September 2026',\n";
+    echo "\t\t\t\t\t'title':'Jaime Montoya\'s net worth from 5 May 2024 to 25 September 2026.',\n";
     echo "\t\t\t\t\tbackgroundColor: {\n";
     echo "\t\t\t\t\t\tfill: '#000',\n";
     echo "\t\t\t\t\t\tfillOpacity: 1,stroke:'#0f0'\n";
