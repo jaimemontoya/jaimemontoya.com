bb4febe92921c310af54bdbf1dcfb766ede8aca6
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-26 06:44:42 -0600

    Add dates for investments.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   $index->content .= "<div id=\"jaime-montoya-capital\"></div>
   <div id=\"richest-company\"></div>
   <div id=\"richest-people\"></div>
-  <h2 style=\"cursor: default; user-select: none; -webkit-font-smoothing: antialiased; font-family: Arial; font-size: 16px; font-weight: bold;\">Jaime Montoya's investments</h2>
+  <h2 style=\"cursor: default; user-select: none; -webkit-font-smoothing: antialiased; font-family: Arial; font-size: 16px; font-weight: bold;\">Jaime Montoya's investments from 5 May 2024 to 25 September 2026</h2>
   ";
   $index->Display();
 ?>
