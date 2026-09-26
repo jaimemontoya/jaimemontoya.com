5393a1fc44e097c827a49061e9f2bb3b8aaa23b6
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-26 06:42:44 -0600

    Add h2 heading for Jaime's investments.

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
   $index->content .= "<div id=\"jaime-montoya-capital\"></div>
   <div id=\"richest-company\"></div>
   <div id=\"richest-people\"></div>
-  <iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vSnedM83IJlnIUy0Ttj4DrDrGWCrqhjC_HXUwxa8cv3nmhraprEToUSuy776FLfXewO49AdS_9SQdUH/pubhtml?widget=true&amp;headers=false\" style=\"width: 100%; height: 750px; border: none;\"></iframe>
-  <iframe src=\"https://docs.google.com/document/d/e/2PACX-1vRePTp-k5OgglOOiBPhxK6z_lrS-Z4OhlZnBY4T6SE0GTT-99VILoPot9IzWI8Z4g/pub?embedded=true\" style=\"width: 100%; height: 750px; border: none;\"></iframe>
+  <h2 style=\"cursor: default; user-select: none; -webkit-font-smoothing: antialiased; font-family: Arial; font-size: 16px; font-weight: bold;\">Jaime Montoya's investments</h2>
   ";
   $index->Display();
 ?>
