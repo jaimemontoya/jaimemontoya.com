a381c8598e85d91abc922d2bfdef1074e6b2b28f
Jaime Montoya <jmontoya@jaimemontoya.com>
2026-09-26 08:46:20 -0600

    Remove the word real.

--- index.php
+++ index.php
@@ -154,7 +154,7 @@
     })();
 </script>
 <!-- Isolated Terminal Style Table Container End -->
-  <h2 style=\"cursor: default; user-select: none; -webkit-font-smoothing: antialiased; font-family: Arial; font-size: 16px; font-weight: bold;\">Jaime Montoya's real stock investments from 12 November 2024 to 25 September 2026 versus hypothetical investments of the same amounts on NASDAQ-100 (QQQ), S&P 500 (SPY), and Dow Jones (DIA)</h2>
+  <h2 style=\"cursor: default; user-select: none; -webkit-font-smoothing: antialiased; font-family: Arial; font-size: 16px; font-weight: bold;\">Jaime Montoya's stock investments from 12 November 2024 to 25 September 2026 versus hypothetical investments of the same amounts on NASDAQ-100 (QQQ), S&P 500 (SPY), and Dow Jones (DIA)</h2>
   <img src=\"beat-the-market/jaimeMontoyasPortfolioBeatingTheMarket.jpg\" 
      alt=\"Jaime Montoya's Portfolio beating the market\" 
      width=\"560\" 
